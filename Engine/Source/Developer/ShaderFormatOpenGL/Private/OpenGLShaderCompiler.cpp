f67c6fa52474d636623e22b13c3f56606f9ce9b8
Rolando Caloca <Rolando.Caloca@epicgames.com>
2014-04-23 17:53:22 -0400

    #UE4 #hlslcc Updates from urban
    [CL 2041602 by Rolando Caloca in Main branch]

--- Engine/Source/Developer/ShaderFormatOpenGL/Private/OpenGLShaderCompiler.cpp
+++ Engine/Source/Developer/ShaderFormatOpenGL/Private/OpenGLShaderCompiler.cpp
@@ -1486,12 +1486,14 @@ void CompileShader_Windows_OGL(const FShaderCompilerInput& Input,FShaderCompiler
 		}
 
 		FGlslCodeBackend GlslBackEnd(CCFlags);
+        FGlslLanguageSpec GLslLanguageSpec(IsES2Platform(Version));
 		int32 Result = HlslCrossCompile(
 			TCHAR_TO_ANSI(*Input.SourceFilename),
 			TCHAR_TO_ANSI(*PreprocessedShader),
 			TCHAR_TO_ANSI(*Input.EntryPointName),
 			Frequency,
 			&GlslBackEnd,
+            &GLslLanguageSpec,
 			CCFlags,
 			HlslCompilerTarget,
 			&GlslShaderSource,
