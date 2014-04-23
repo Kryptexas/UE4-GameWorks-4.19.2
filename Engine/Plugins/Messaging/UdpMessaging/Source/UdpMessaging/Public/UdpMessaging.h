8de6a69f23644ab8549d28657a3b67ea8a348e84
Max Preussner <Max.Preussner@epicgames.com>
2014-04-23 19:17:34 -0400

    forgot to check in
    [CL 2048446 by Max Preussner in Main branch]

---
+++ Engine/Plugins/Messaging/UdpMessaging/Source/UdpMessaging/Public/UdpMessaging.h
@@ -0,0 +1,20 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+/*=============================================================================
+	UdpMessaging.h: UdpMessaging module public header file.
+=============================================================================*/
+
+#pragma once
+
+
+/* Dependencies
+ *****************************************************************************/
+
+#include "Networking.h"
+
+
+/* Interfaces
+ *****************************************************************************/
+
+#include "IUdpMessageTunnelConnection.h"
+#include "IUdpMessageTunnel.h"
