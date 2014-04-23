e10e4e94019a44bdcc368b7ee6d840a9e47ca365
Joe Conley <Joe.Conley@epicgamesjapan.com>
2014-04-23 18:47:42 -0400

    Forgot some files with the last checkin
    [CL 2045873 by Joe Conley in Main branch]

---
+++ Engine/Source/Editor/TranslationEditor/Private/TranslationUnit.cpp
@@ -0,0 +1,18 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+#include "TranslationEditorPrivatePCH.h"
+#include "TranslationEditor.generated.inl"
+
+UTranslationUnit::UTranslationUnit( const class FPostConstructInitializeProperties& PCIP )
+	: Super(PCIP)
+{
+
+}
+
+void UTranslationUnit::PostEditChangeProperty(struct FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	const FName Name = (PropertyChangedEvent.Property != nullptr) ? PropertyChangedEvent.Property->GetFName() : NAME_None;
+
+	TranslationUnitPropertyChangedEvent.Broadcast(Name);
+}
