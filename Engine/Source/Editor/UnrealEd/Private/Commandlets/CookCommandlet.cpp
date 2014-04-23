ad28d18ebce8f5a730dcca0a486ec39d29946ea2
Lina Halper <lina.halper@epicgames.com>
2014-04-23 19:05:56 -0400

    Added comment
    [CL 2047277 by Lina Halper in Main branch]

--- Engine/Source/Editor/UnrealEd/Private/Commandlets/CookCommandlet.cpp
+++ Engine/Source/Editor/UnrealEd/Private/Commandlets/CookCommandlet.cpp
@@ -422,6 +422,7 @@ bool UCookCommandlet::SaveCookedPackage( UPackage* Package, uint32 SaveFlags, bo
 				if (World)
 				{
 					World->PersistentLevel->OwningWorld = World;
+					// we need to initialize the world - at least need physics scene since BP construction script runs during cooking, otherwise trace won't work
 					World->InitWorld(UWorld::InitializationValues().RequiresHitProxies(false).ShouldSimulatePhysics(false).EnableTraceCollision(false).CreateNavigation(false).AllowAudioPlayback(false).CreatePhysicsScene(true).CreateWorldComposition(false));
 				}
 
