71ff6ab2de2d82fae89218a226980709cf0d9205
Ori Cohen <Ori.Cohen@epicgames.com>
2014-04-23 17:25:03 -0400

    Change soft limits to include mass
    [CL 2039209 by Ori Cohen in Main branch]

--- Engine/Source/Runtime/Engine/Private/PrimitiveComponentPhysics.cpp
+++ Engine/Source/Runtime/Engine/Private/PrimitiveComponentPhysics.cpp
@@ -340,7 +340,7 @@ float UPrimitiveComponent::GetMass() const
 	return 0.0f;
 }
 
-float UPrimitiveComponent::CalculateMass()
+float UPrimitiveComponent::CalculateMass(FName)
 {
 	if (BodyInstance.BodySetup.IsValid())
 	{
