bfa71ce7dc101eec5a4784fef9dd22cf527e283e
Bob Tellez <Bob.Tellez@epicgames.com>
2014-04-23 19:16:21 -0400

    UE4: Double clicking a world asset in the content browser now loads it in the level editor.
    [CL 2048399 by Bob Tellez in Main branch]

--- Engine/Source/Developer/AssetTools/Private/AssetTypeActions/AssetTypeActions_World.cpp
+++ Engine/Source/Developer/AssetTools/Private/AssetTypeActions/AssetTypeActions_World.cpp
@@ -4,5 +4,23 @@
 
 #define LOCTEXT_NAMESPACE "AssetTypeActions"
 
+void FAssetTypeActions_World::OpenAssetEditor( const TArray<UObject*>& InObjects, TSharedPtr<class IToolkitHost> EditWithinLevelEditor )
+{
+	for (auto ObjIt = InObjects.CreateConstIterator(); ObjIt; ++ObjIt)
+	{
+		auto World = Cast<UWorld>(*ObjIt);
+		if (World != NULL)
+		{
+			const FString FileToOpen = FPackageName::LongPackageNameToFilename(World->GetOutermost()->GetName(), FPackageName::GetMapPackageExtension());
+			const bool bLoadAsTemplate = false;
+			const bool bShowProgress = false;
+			const bool bWorldComposition = false;
+			FEditorFileUtils::LoadMap( FileToOpen, bLoadAsTemplate, bShowProgress, bWorldComposition );
+
+			// We can only edit one world at a time... so just break after the first valid world to load
+			break;
+		}
+	}
+}
 
 #undef LOCTEXT_NAMESPACE
