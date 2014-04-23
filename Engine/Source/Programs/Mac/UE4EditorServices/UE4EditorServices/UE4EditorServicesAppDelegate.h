e020761b24c0ebee21b1e4165681e648be1bf3eb
Mark Satterthwaite <msatterthwaite@pitbullstudio.co.uk>
2014-04-23 18:03:20 -0400

    #ttp 330910 Mac: UE4EditorServices.app does not run from Matching UE4Editor.app location #branch UE4 #project UE4EditorServices #summary Give the UE4EditorServices the option to choose which editor executable to use to launch with or generate Xcode projects for. #change Add a simple window, with pop-up menu for selecting which version of the editor to use for launching/building. #change When handling services, if there are multiple editors installed then ask the user which to use, the dialog can be suppressed and can be forcibly shown (hold Alt when activating the service). reviewedby michael.trepka
    [CL 2042404 by Mark Satterthwaite in Main branch]

--- Engine/Source/Programs/Mac/UE4EditorServices/UE4EditorServices/UE4EditorServicesAppDelegate.h
+++ Engine/Source/Programs/Mac/UE4EditorServices/UE4EditorServices/UE4EditorServicesAppDelegate.h
@@ -4,8 +4,17 @@
 
 @interface FUE4EditorServicesAppDelegate : NSObject <NSApplicationDelegate>
 
+@property (assign) IBOutlet NSWindow* Window;
+@property (assign) IBOutlet NSPopUpButton* EditorMenu;
+@property (assign) IBOutlet NSButton* CancelButton;
+@property (assign) IBOutlet NSButton* OKButton;
+@property (assign) IBOutlet NSButton* DontShow;
+@property (strong) NSMutableDictionary* DefaultApps;
+
+- (IBAction)onCancelButtonPressed:(id)sender;
+- (IBAction)onOKButtonPressed:(id)sender;
+
 - (void)launchGameService:(NSPasteboard *)pboard userData:(NSString *)userData error:(NSString **)error;
 - (void)generateXcodeFilesService:(NSPasteboard *)pboard userData:(NSString *)userData error:(NSString **)error;
-//- (void)generateAllXcodeFilesService:(NSPasteboard *)pboard userData:(NSString *)userData error:(NSString **)error;
 
 @end
