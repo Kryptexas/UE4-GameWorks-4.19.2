f4fcd3fe1babfef0b862973411706f61d662fdac
Joe Conley <Joe.Conley@epicgamesjapan.com>
2014-04-24 03:38:05 -0400

    fix non-unity build
    [CL 2055226 by Joe Conley in Main branch]

--- Engine/Source/Editor/TranslationEditor/Private/TranslationUnit.cpp
+++ Engine/Source/Editor/TranslationEditor/Private/TranslationUnit.cpp
@@ -1,5 +1,6 @@
 // Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
 #include "TranslationEditorPrivatePCH.h"
+#include "TranslationUnit.h"
 
 UTranslationUnit::UTranslationUnit( const class FPostConstructInitializeProperties& PCIP )
 	: Super(PCIP)
