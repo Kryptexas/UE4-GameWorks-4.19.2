aa11df00de50dc6fb57787f3dfa893c7a0c51a19
Michael Trepka <Michael.Trepka@epicgames.com>
2014-04-23 22:37:00 -0400

    [INTEGRATE] Change 2051645 by Michael.Trepka@Michael.Trepka_a3608 on 2014/04/20 6:01:12
    Fix for TTP #333180 CRITICAL: Unable to open packaged project
    
    [CL 2051648 by Michael Trepka in 4.1 branch]

--- Engine/Source/Programs/AutomationTool/Mac/MacPlatform.Automation.cs
+++ Engine/Source/Programs/AutomationTool/Mac/MacPlatform.Automation.cs
@@ -48,6 +48,8 @@ public class MacPlatform : Platform
 
 		// Copy the splash screen, Mac specific
 		SC.StageFiles(StagedFileType.NonUFS, CombinePaths(SC.ProjectRoot, "Content/Splash"), "Splash.bmp", false, null, null, true);
+
+		SC.StageFiles(StagedFileType.NonUFS, CombinePaths(SC.LocalRoot, "Engine/Content/Localization"), "*.dat", true, null, null, false, !Params.Pak);
 	}
 
 	public override void Package(ProjectParams Params, DeploymentContext SC, int WorkingCL)
