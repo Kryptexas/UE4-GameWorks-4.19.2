84828be43204f2a6414cddc44d759ea46f4ce405
Ben Marsh <Ben.Marsh@epicgames.com>
2014-04-23 22:14:13 -0400

    [INTEGRATE] #ttp 331319: CRITICAL: LIVE: FRAMEWORK: GitHub editor attempts to connect to shared network DDC
    #change Moved internal DDC shared paths to a no-redist module.
    
    [CL 2041845 by Ben Marsh in 4.1 branch]

--- Engine/Source/Developer/DerivedDataCache/Private/DerivedDataBackends.cpp
+++ Engine/Source/Developer/DerivedDataCache/Private/DerivedDataBackends.cpp
@@ -11,6 +11,8 @@
 #include "DerivedDataLimitKeyLengthWrapper.h"
 #include "DerivedDataBackendCorruptionWrapper.h"
 #include "DerivedDataBackendVerifyWrapper.h"
+#include "DerivedDataUtilsInterface.h"
+#include "Misc/EngineBuildSettings.h"
 
 DEFINE_LOG_CATEGORY(LogDerivedDataCache);
 
@@ -474,6 +476,23 @@ public:
 				Path = FilesystemCachePathEnv;
 				UE_LOG( LogDerivedDataCache, Log, TEXT("Found environment variable %s=%s"), *EnvPathOverride, *Path );
 			}
+		}
+		// Check if the Path is a real path or a special keyword
+		if (FEngineBuildSettings::IsInternalBuild())
+		{
+			auto DDCUtils = FModuleManager::LoadModulePtr< IDDCUtilsModuleInterface >("DDCUtils");
+			if (DDCUtils)
+			{
+				FString PathFromName = DDCUtils->GetSharedCachePath(Path);
+				if (!PathFromName.IsEmpty())
+				{
+					Path = PathFromName;
+				}
+			}
+		}
+		else if (Path.StartsWith(TEXT("?")))
+		{
+			Path = TEXT("");
 		}
 
 		if( !Path.Len() )
