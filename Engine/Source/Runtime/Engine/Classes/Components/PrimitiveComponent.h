c7d651dfa46923edcbedf86b99fbd8e4f28c7e23
James Golding <james.golding@epicgames.com>
2014-04-25 06:23:46 -0400

    Expose PrimitiveComponent SetPhysMaterialOverride to BP
    [CL 2056308 by James Golding in Main branch]

--- Engine/Source/Runtime/Engine/Classes/Components/PrimitiveComponent.h
+++ Engine/Source/Runtime/Engine/Classes/Components/PrimitiveComponent.h
@@ -1359,8 +1359,9 @@ public:
 	/** 
 	 *	Changes the current PhysMaterialOverride for this component. 
 	 *	Note that if physics is already running on this component, this will _not_ alter its mass/inertia etc,  
-	 *	it will only change its surface properties like friction and the damping.
+	 *	it will only change its surface properties like friction.
 	 */
+	UFUNCTION(BlueprintCallable, Category="Physics", meta=(FriendlyName="Set PhysicalMaterial Override"))
 	virtual void SetPhysMaterialOverride(class UPhysicalMaterial* NewPhysMaterial);
 
 	/** 
