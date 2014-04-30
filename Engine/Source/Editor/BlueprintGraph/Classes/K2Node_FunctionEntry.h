adea32de6dcd352080042e2d2b5f091a8cf2f1d6
Maciej Mroz <maciej.mroz@epicgames.com>
2014-04-30 13:43:50 -0400

    A comment added for GetWorldContextPin
    [CL 2060152 by Maciej Mroz in Main branch]

--- Engine/Source/Editor/BlueprintGraph/Classes/K2Node_FunctionEntry.h
+++ Engine/Source/Editor/BlueprintGraph/Classes/K2Node_FunctionEntry.h
@@ -46,6 +46,7 @@ class UK2Node_FunctionEntry : public UK2Node_FunctionTerminator
 	// Removes an output pin from the node
 	BLUEPRINTGRAPH_API void RemoveOutputPin(UEdGraphPin* PinToRemove);
 
+	// Returns pin for the automatically added WorldContext parameter (used only by BlueprintFunctionLibrary).
 	BLUEPRINTGRAPH_API UEdGraphPin* GetWorldContextPin() const;
 };
 
