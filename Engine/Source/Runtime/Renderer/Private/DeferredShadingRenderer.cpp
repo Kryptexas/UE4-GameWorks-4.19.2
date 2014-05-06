9cd0c5ed919b8559d309f18ecf8166ef9c761826
Martin Mittring <martin.mittring@epicgames.com>
2014-05-06 17:19:55 -0400

    * fixed ensure when capturing HDR scene color
    [CL 2064916 by Martin Mittring in Main branch]

--- Engine/Source/Runtime/Renderer/Private/DeferredShadingRenderer.cpp
+++ Engine/Source/Runtime/Renderer/Private/DeferredShadingRenderer.cpp
@@ -427,9 +427,9 @@ void FDeferredShadingSceneRenderer::RenderFinish()
 		Scene->MotionBlurInfoData.UpdateMotionBlurCache();
 	}
 
-	// The SceneColor RT should be released as early as possible to allow shading of that memory for other purposes.
+	// Some RT should be released as early as possible to allow shading of that memory for other purposes.
+	// SceneColor should be released in tone mapping.
 	// This becomes even more important with some limited VRam (XBoxOne).
-	GSceneRenderTargets.SetSceneColor(0);
 	GSceneRenderTargets.SetLightAttenuation(0);
 }
 
