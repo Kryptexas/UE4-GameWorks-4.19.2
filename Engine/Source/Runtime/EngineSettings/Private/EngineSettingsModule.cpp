a823aecff7f06efe6c5ee86586d12f00afc8f2de
Max Preussner <Max.Preussner@epicgames.com>
2014-05-08 21:34:16 -0400

    Settings: code cleanup
    [CL 2067722 by Max Preussner in Main branch]

--- Engine/Source/Runtime/EngineSettings/Private/EngineSettingsModule.cpp
+++ Engine/Source/Runtime/EngineSettings/Private/EngineSettingsModule.cpp
@@ -15,6 +15,8 @@ class FEngineSettingsModule
 {
 public:
 
+	// Begin IModuleInterface interface
+
 	virtual void StartupModule( ) OVERRIDE { }
 
 	virtual void ShutdownModule( ) OVERRIDE { }
@@ -23,10 +25,21 @@ public:
 	{
 		return true;
 	}
+
+	// End IModuleInterface interface
 };
+
 
 /* Class constructors
  *****************************************************************************/
+
+UGameMapsSettings::UGameMapsSettings( const class FPostConstructInitializeProperties& PCIP )
+	: Super(PCIP)
+	, bUseSplitscreen(true)
+	, TwoPlayerSplitscreenLayout(ETwoPlayerSplitScreenType::Horizontal)
+	, ThreePlayerSplitscreenLayout(EThreePlayerSplitScreenType::FavorTop)
+{ }
+
 
 UGameNetworkManagerSettings::UGameNetworkManagerSettings( const class FPostConstructInitializeProperties& PCIP )
 	: Super(PCIP)
@@ -52,16 +65,9 @@ UHudSettings::UHudSettings( const class FPostConstructInitializeProperties& PCIP
 	: Super(PCIP)
 { }
 
+
 /* Static functions
  *****************************************************************************/
-
-UGameMapsSettings::UGameMapsSettings(const class FPostConstructInitializeProperties& PCIP)
-	: Super(PCIP)
-	, bUseSplitscreen(true)
-	, TwoPlayerSplitscreenLayout(ETwoPlayerSplitScreenType::Horizontal)
-	, ThreePlayerSplitscreenLayout(EThreePlayerSplitScreenType::FavorTop)
-{ 
-}
 
 const FString& UGameMapsSettings::GetGameDefaultMap( )
 {
