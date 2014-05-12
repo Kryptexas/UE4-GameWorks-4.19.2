c8bb5548a38da14146e7a6752d9155a7ada68dfa
Jaroslaw Palczynski <jpalczynski@pitbullstudio.co.uk>
2014-05-12 08:42:13 -0400

    CIS Fix for Android build. Extra OVERRIDE keyword caused problems during Android compilation.
    [CL 2070230 by Jaroslaw Palczynski in Main branch]

--- Engine/Source/Runtime/Core/Public/Android/AndroidAffinity.h
+++ Engine/Source/Runtime/Core/Public/Android/AndroidAffinity.h
@@ -11,32 +11,32 @@ AndroidAffinity.h: Android affinity profile masks definitions.
 class FAndroidAffinity : public FGenericPlatformAffinity
 {
 public:
-	static const CORE_API uint64 GetMainGameMask() OVERRIDE
+	static const CORE_API uint64 GetMainGameMask()
 	{
 		return MAKEAFFINITYMASK1(0);
 	}
 
-	static const CORE_API uint64 GetRenderingThreadMask() OVERRIDE
+	static const CORE_API uint64 GetRenderingThreadMask()
 	{
 		return MAKEAFFINITYMASK1(1);
 	}
 
-	static const CORE_API uint64 GetRTHeartBeatMask() OVERRIDE
+	static const CORE_API uint64 GetRTHeartBeatMask()
 	{
 		return MAKEAFFINITYMASK1(4);
 	}
 
-	static const CORE_API uint64 GetPoolThreadMask() OVERRIDE
+	static const CORE_API uint64 GetPoolThreadMask()
 	{
 		return MAKEAFFINITYMASK1(4);
 	}
 
-	static const CORE_API uint64 GetTaskGraphThreadMask() OVERRIDE
+	static const CORE_API uint64 GetTaskGraphThreadMask()
 	{
 		return MAKEAFFINITYMASK3(0, 2, 3);
 	}
 
-	static const CORE_API uint64 GetStatsThreadMask() OVERRIDE
+	static const CORE_API uint64 GetStatsThreadMask()
 	{
 		return MAKEAFFINITYMASK1(5);
 	}
