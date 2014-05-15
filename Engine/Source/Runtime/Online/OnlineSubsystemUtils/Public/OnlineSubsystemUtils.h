d4569fad8d0b4f756b17e7501f119184ad1349df
Sam Zamani <sam.zamani@epicgames.com>
2014-05-15 11:17:32 -0400

    - fix for mcp automation tests crashing due to missing GWorld
    [CL 2074244 by Sam Zamani in Main branch]

--- Engine/Source/Runtime/Online/OnlineSubsystemUtils/Public/OnlineSubsystemUtils.h
+++ Engine/Source/Runtime/Online/OnlineSubsystemUtils/Public/OnlineSubsystemUtils.h
@@ -27,9 +27,9 @@ namespace Online
 	static IOnlineSubsystem* GetSubsystem(UWorld* World, const FName& SubsystemName = NAME_None)
 	{
 #if UE_EDITOR // at present, multiple worlds are only possible in the editor
-		check(World);
 		FName Identifier = SubsystemName; 
-		if (GIsPlayInEditorWorld)
+		if (GIsPlayInEditorWorld &&
+			World != NULL)
 		{
 			FWorldContext& CurrentContext = GEngine->GetWorldContextFromWorldChecked(World);
 			if (CurrentContext.WorldType == EWorldType::PIE)
