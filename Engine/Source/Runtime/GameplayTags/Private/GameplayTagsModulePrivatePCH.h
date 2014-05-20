dd4b90249533e48a326f702fcf7e2296bc6df7d9
Billy Bramer <Billy.Bramer@epicgames.com>
2014-05-20 11:23:59 -0400

    Merge over a small gameplay tag CIS fix CL I missed in yesterday's integration.
    [AUTOMERGE]
    
    Fix linux compile
    
    --------
    Integrated using branch Ue4-To-UE4-Fortnite-Simple (reversed) of change#2067749 by Ben.Zeigler on 2014/05/08 21:51:47.
    
    [CL 2078991 by Billy Bramer in Main branch]

--- Engine/Source/Runtime/GameplayTags/Private/GameplayTagsModulePrivatePCH.h
+++ Engine/Source/Runtime/GameplayTags/Private/GameplayTagsModulePrivatePCH.h
@@ -8,15 +8,13 @@
 #include "UnrealEd.h"
 #endif
 
-
 // You should place include statements to your module's private header files here.  You only need to
 // add includes for headers that are used in most of your module's source files though.
 
 #include "Core.h"
 #include "Engine.h"
 
-DECLARE_LOG_CATEGORY_EXTERN(LogGameplayTags, Log, All);
-
 #include "GameplayTagsClasses.h"
 #include "GameplayTagsModule.h"
 
+DECLARE_LOG_CATEGORY_EXTERN(LogGameplayTags, Log, All);
