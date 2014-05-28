4715bea4956dd06ba1bc7bcaa0d507c4a3d82272
Ben Marsh <Ben.Marsh@epicgames.com>
2014-05-28 10:16:02 -0400

    Add a dummy EngineKismetLibraryClasses.h file to ease the upgrade from 4.2. Was included by default in the first person template.
    #codereview Michael.Trepka
    
    [CL 2086994 by Ben Marsh in 4.2 branch]

---
+++ Engine/Source/Runtime/Engine/Public/EngineKismetLibraryClasses.h
@@ -0,0 +1,13 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+/*===========================================================================
+	Deprecated header file. Included to allow upgrade of older template 
+	projects.
+===========================================================================*/
+
+#ifdef _MSC_VER
+	#pragma message(__FILE__"(9): warning : This header file has been deprecated. Please remove the associated #include from your code")
+#else
+	#pragma message("This header file has been deprecated. Please remove the associated #include from your code")
+#endif
+
