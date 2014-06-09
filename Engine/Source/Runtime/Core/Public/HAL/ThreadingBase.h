133450c6948f3ab3f9529e45e1072d47e0f3715c
Jaroslaw Surowiec <Jaroslaw.Surowiec@epicgames.com>
2014-06-09 11:13:50 -0400

    Core - Added LockFreeList using 128-bit CAS operation (disabled by default, may be enabled via setting USE_LOCKFREELIST_128 to 1)
    #codereview Robert.Manuszewski
    
    [CL 2099035 by Jaroslaw Surowiec in Main branch]

--- Engine/Source/Runtime/Core/Public/HAL/ThreadingBase.h
+++ Engine/Source/Runtime/Core/Public/HAL/ThreadingBase.h
@@ -761,6 +761,48 @@ private:
 	volatile int32 Counter;
 };
 
+/** Fake Thread safe counter, used to avoid cluttering code with ifdefs when counters are only used for debugging. */
+class FNoopCounter
+{
+public:
+	FNoopCounter()
+	{
+	}
+	FNoopCounter( const FNoopCounter& Other )
+	{
+	}
+	FNoopCounter( int32 Value )
+	{
+	}
+	int32 Increment()
+	{
+		return 0;
+	}
+	int32 Add( int32 Amount )
+	{
+		return 0;
+	}
+	int32 Decrement()
+	{
+		return 0;
+	}
+	int32 Subtract( int32 Amount )
+	{
+		return 0;
+	}
+	int32 Set( int32 Value )
+	{
+		return 0;
+	}
+	int32 Reset()
+	{
+		return 0;
+	}
+	int32 GetValue() const
+	{
+		return 0;
+	}
+};
 
 /**
  * Implements a scope lock.
