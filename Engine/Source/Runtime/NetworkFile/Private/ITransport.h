cefeb85ea90a304a49cd5e4c61cda03be34ace90
Ankit Khare <akhare@phosphorgames.com>
2014-06-12 17:03:38 -0400

    - Build fix for Mac
    [CL 2103459 by Ankit Khare in Main branch]

--- Engine/Source/Runtime/NetworkFile/Private/ITransport.h
+++ Engine/Source/Runtime/NetworkFile/Private/ITransport.h
@@ -12,7 +12,7 @@ class ITransport
 
 public:
 	ITransport (){};
-	~ITransport(){};
+	virtual ~ITransport(){};
 
 	virtual bool Initialize(const TCHAR*) = 0; 
 	virtual bool SendPayloadAndReceiveResponse(TArray<uint8>& In, TArray<uint8>& Out) = 0; 
