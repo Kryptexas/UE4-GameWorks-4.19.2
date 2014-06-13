b0c94f9890c036990c9ae403270401ad8c92f4ce
Marc Audy <Marc.Audy@epicgames.com>
2014-06-13 13:30:25 -0400

    Add coremod ThirdParty module
    [CL 2104760 by Marc Audy in Main branch]

---
+++ Engine/Source/ThirdParty/coremod/coremod.Build.cs
@@ -0,0 +1,36 @@
+﻿// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class coremod: ModuleRules
+{
+	public coremod(TargetInfo Target)
+	{
+		Type = ModuleType.External;
+
+		string CoreModVersion = "4.2.6";
+		string LibraryPath = UEBuildConfiguration.UEThirdPartyDirectory + "coremod/coremod-" + CoreModVersion + "/";
+
+		PublicIncludePaths.Add(LibraryPath + "include/coremod");
+
+		if (Target.Platform == UnrealTargetPlatform.Win64)
+		{
+			PublicLibraryPaths.Add(LibraryPath + "/lib/Win64/VS2013");
+			PublicAdditionalLibraries.Add("coremod.lib");
+		}
+		else if (Target.Platform == UnrealTargetPlatform.Win32)
+		{
+			PublicLibraryPaths.Add(LibraryPath + "/lib/Win32/VS2013");
+			PublicAdditionalLibraries.Add("coremod.lib");
+		}
+		else if (Target.Platform == UnrealTargetPlatform.Mac)
+		{
+			PublicAdditionalLibraries.Add(LibraryPath + "/lib/Mac/libcoremodMac.a");
+		}
+		else if (Target.Platform == UnrealTargetPlatform.IOS)
+		{
+			PublicLibraryPaths.Add(LibraryPath + "/lib/IOS");
+			PublicAdditionalShadowFiles.Add(LibraryPath + "/lib/IOS/libcoremod.a");
+		}
+	}
+}
