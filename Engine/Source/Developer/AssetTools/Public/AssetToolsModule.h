bf6ab17e30807981310f90476ef66080fa3738ff
Dan Oconnor <Dan.OConnor@epicgames.com>
2014-06-16 14:26:53 -0400

    #UE4 adding an accessor so that I don't have to copy/paste the loadmodule call, matches the empty module template...
    [CL 2107248 by Dan Oconnor in Main branch]

--- Engine/Source/Developer/AssetTools/Public/AssetToolsModule.h
+++ Engine/Source/Developer/AssetTools/Public/AssetToolsModule.h
@@ -14,6 +14,17 @@ public:
 
 	virtual IAssetTools& Get() const;
 
+	/**
+	 * Singleton-like access to this module's interface.  This is just for convenience!
+	 * Beware of calling this during the shutdown phase, though.  Your module might have been unloaded already.
+	 *
+	 * @return Returns singleton instance, loading the module on demand if needed
+	 */
+	static inline FAssetToolsModule& GetModule()
+	{
+		return FModuleManager::LoadModuleChecked< FAssetToolsModule >("AssetTools");
+	}
+
 private:
 	IAssetTools* AssetTools;
 	class FAssetToolsConsoleCommands* ConsoleCommands;
