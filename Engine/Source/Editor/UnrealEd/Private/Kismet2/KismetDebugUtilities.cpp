a8796a3d687f86a0a236be0ab2eb35f5d6323b90
Barnabas McManners <bmcmanners@pitbullstudio.co.uk>
2014-06-19 09:02:02 -0400

    A message box is now displayed when the user is exited due to infinite loop #TTP 331819 - LIVE: FEATURE REQUEST: BLUEPRINTS: Exiting PIE due to a Runaway Loop might benefit from a pop-up message
    #branch UE4
    
    #proj Editor.UnrealEd
    
    #add Added message box which displays the text Infinite loop found in blueprint: <blueprint name>. Reasons for not using a notification: In practice I found that because the infinite loop essentially locked the editor for some time I found that I would often miss the display of the notification. A message box demands attention and highlights the serious issue.
    
    #reviewedby Chris.Wood
    
    [CL 2110361 by Barnabas McManners in Main branch]

--- Engine/Source/Editor/UnrealEd/Private/Kismet2/KismetDebugUtilities.cpp
+++ Engine/Source/Editor/UnrealEd/Private/Kismet2/KismetDebugUtilities.cpp
@@ -272,6 +272,14 @@ void FKismetDebugUtilities::OnScriptException(const UObject* ActiveObject, const
 				{
 					GEditor->RequestEndPlayMap();
 					FSlateApplication::Get().LeaveDebuggingMode();
+					
+					// Launch a message box notifying the user why they have been booted
+					{
+						FString BlueprintName;
+						BlueprintObj->GetName(BlueprintName);
+
+						FMessageDialog::Open( EAppMsgType::Ok, FText::Format(LOCTEXT("InfiniteLoopWarning", "Infinite Loop detected in blueprint: {0}"), FText::FromString(BlueprintName)));
+					}
 				}
 			}
 			break;
