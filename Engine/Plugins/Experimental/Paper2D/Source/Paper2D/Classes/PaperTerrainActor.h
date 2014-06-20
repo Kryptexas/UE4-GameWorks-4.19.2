d94f6c91f270fdc8ee4e4447c5bd01fc7f8443dc
Michael Noland <michael.noland@epicgames.com>
2014-06-19 20:07:41 -0400

    Paper2D: Marked render component in terrain actor with ExposeFunctionCategories
    [CL 2111143 by Michael Noland in Main branch]

--- Engine/Plugins/Experimental/Paper2D/Source/Paper2D/Classes/PaperTerrainActor.h
+++ Engine/Plugins/Experimental/Paper2D/Source/Paper2D/Classes/PaperTerrainActor.h
@@ -19,7 +19,7 @@ class PAPER2D_API APaperTerrainActor : public AActor
 	UPROPERTY()
 	TSubobjectPtr<class UPaperTerrainSplineComponent> SplineComponent;
 
-	UPROPERTY(Category = Sprite, VisibleAnywhere)
+	UPROPERTY(Category=Sprite, VisibleAnywhere, BlueprintReadOnly, meta = (ExposeFunctionCategories = "Sprite,Rendering,Physics,Components|Sprite"))
 	TSubobjectPtr<class UPaperTerrainComponent> RenderComponent;
 
 	// AActor interface
