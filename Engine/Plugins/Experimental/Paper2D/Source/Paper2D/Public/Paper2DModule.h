3bc8e2e4c22d457e15b4e2883fd5f25a138a9bf9
Michael Noland <michael.noland@epicgames.com>
2014-06-27 19:54:48 -0400

    Paper2D: Add note to PaperAxis values
    [CL 2120256 by Michael Noland in Main branch]

--- Engine/Plugins/Experimental/Paper2D/Source/Paper2D/Public/Paper2DModule.h
+++ Engine/Plugins/Experimental/Paper2D/Source/Paper2D/Public/Paper2DModule.h
@@ -11,6 +11,10 @@ class IPaper2DModuleInterface : public IModuleInterface
 {
 };
 
+// NOTE: Experimental: Do not adjust these values, many aspects of the editor or runtime assume PaperX=Right(+X) PaperY=Up(+Z) PaperZ=PaperX cross PaperY=Out(-Y)
+// The ability to adjust these values may be removed entirely in the future, or made to work properly
+//@TODO: PAPER2D: Synchronize with the Box2D axes in the engine (or remove the ability to customize them)
+
 // The 3D vector that corresponds to the sprite X axis
 extern PAPER2D_API FVector PaperAxisX;
 
