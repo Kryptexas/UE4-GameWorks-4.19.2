c5b5f583bf3c81964bc29a0f94b3a59e0cae1ab8
Mark Satterthwaite <msatterthwaite@pitbullstudio.co.uk>
2014-07-01 15:51:16 -0400

    Implement a proper native feedback window for OS X that provides cancellation, progress status and a disclosable output log. #codereview michael.trepka
    [CL 2123780 by Mark Satterthwaite in Main branch]

--- Engine/Source/Developer/DesktopPlatform/Private/Mac/MacNativeFeedbackContext.h
+++ Engine/Source/Developer/DesktopPlatform/Private/Mac/MacNativeFeedbackContext.h
@@ -6,6 +6,30 @@
 
 #pragma once
 
+@interface FMacNativeFeedbackContextWindowController : NSObject
+{
+@public
+	NSTextView* TextView;
+@private
+	NSScrollView* LogView;
+	NSWindow* Window;
+	NSTextField* StatusLabel;
+	NSButton* CancelButton;
+	NSButton* ShowLogButton;
+	NSProgressIndicator* ProgressIndicator;
+}
+-(id)init;
+-(void)dealloc;
+-(void)setShowProgress:(bool)bShowProgress;
+-(void)setShowCancelButton:(bool)bShowCancelButton;
+-(void)setTitleText:(NSString*)Title;
+-(void)setStatusText:(NSString*)Text;
+-(void)setProgress:(double)Progress total:(double)Total;
+-(void)showWindow;
+-(void)hideWindow;
+-(bool)windowOpen;
+@end
+
 /**
  * Feedback context implementation for Mac.
  */
@@ -32,6 +56,13 @@ public:
 	void SetContext( FContextSupplier* InSupplier );
 
 private:
+	void SetDefaultTextColor();
+	
+private:
+	/** Critical section for Serialize() */
+	FCriticalSection CriticalSection;
+	FMacNativeFeedbackContextWindowController* WindowController;
+	NSDictionary* TextViewTextColor;
 	FContextSupplier* Context;
 	int32 SlowTaskCount;
 	bool bShowingConsoleForSlowTask;
