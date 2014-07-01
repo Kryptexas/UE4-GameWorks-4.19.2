ceae2b4af38a45fcf160eeaba1395a8dcd75ebd1
Ben Marsh <Ben.Marsh@epicgames.com>
2014-07-01 13:08:36 -0400

    [INTEGRATE] Change 2122415 by Dmitry.Rekman@RCL_DRekman_Z3937 on 2014/06/30 17:16:39
    	Fix minor case-sensitivity issue in include.
    
    [CL 2123262 by Ben Marsh in 4.3 branch]

--- Engine/Source/Runtime/GameplayAbilities/Public/Abilities/Tasks/AbilityTask.h
+++ Engine/Source/Runtime/GameplayAbilities/Public/Abilities/Tasks/AbilityTask.h
@@ -1,7 +1,7 @@
 // Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
 #pragma once
 #include "GameplayAbility.h"
-#include "AbilityTask.Generated.h"
+#include "AbilityTask.generated.h"
 
 UCLASS(MinimalAPI)
 class UAbilityTask : public UObject
