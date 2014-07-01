f42954d48572dfc82a0060b498fb5e39432fb274
Ori Cohen <Ori.Cohen@epicgames.com>
2014-07-01 16:16:02 -0400

    ragdoll root body inherits DOF from skeletal mesh
    [CL 2123812 by Ori Cohen in Main branch]

--- Engine/Source/Runtime/Engine/Private/SkeletalMeshComponentPhysics.cpp
+++ Engine/Source/Runtime/Engine/Private/SkeletalMeshComponentPhysics.cpp
@@ -823,6 +823,12 @@ void USkeletalMeshComponent::InitArticulated(FPhysScene* PhysScene)
 			// this is not true for all other BodyInstance, but for physics assets it is true. 
 			BodyInst->bSimulatePhysics = false;
 			BodyInst->InstanceBodyIndex = i; // Set body index 
+
+			if (i == RootBodyIndex)
+			{
+				BodyInst->DOF = BodyInstance.DOF;
+			}
+
 #if WITH_PHYSX
 			// Create physics body instance.
 			FTransform BoneTransform = GetBoneTransform( BoneIndex );
