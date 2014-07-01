ebc3123a0d6d4acf836e116119dd1fa964632066
Mark Satterthwaite <msatterthwaite@pitbullstudio.co.uk>
2014-07-01 16:26:01 -0400

    Make hlslcc compile again on OS X and rebuild libraries. #codereview michael.trepka, rolando.caloca
    [CL 2123818 by Mark Satterthwaite in Main branch]

--- Engine/Source/ThirdParty/hlslcc/hlslcc/src/hlslcc_lib/ShaderCompilerCommon.h
+++ Engine/Source/ThirdParty/hlslcc/hlslcc/src/hlslcc_lib/ShaderCompilerCommon.h
@@ -2,8 +2,20 @@
 
 //#pragma once
 
+// Only needed on Mac
+#if __APPLE__
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <limits.h>
+#include <math.h>
+#endif
+
 #include <assert.h>
 #define check(x)	assert(x)
 
+// On Mac we can't use static_assert in .c files as they really are compiled as C
+#if !__APPLE__ || __cplusplus
 typedef unsigned long long uint64_t;
 static_assert(sizeof(uint64_t) == 8, "Bad!");
+#endif
