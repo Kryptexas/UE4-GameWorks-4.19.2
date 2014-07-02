18fbbccf7a32b39c772094d376281ded28e8712b
Mike Fricker <mike.fricker@epicgames.com>
2014-07-02 13:48:51 -0400

    New TwitchLiveStreaming plugin (work in progress) - Implements live video/audio broadcasting to Twitch.tv - Supports displaying your web cam in the editor and game - Partially implemented support for chat and stream discovery - Uses browser-based authentication to login to Twitch
    Important:
    - Epic cannot distribute the Twitch SDK to you, as you need to agree to Twitch's terms before releasing a title that uses Twitch.
    - We currently require you to get the Twitch SDK yourself and recompile the TwitchLiveStreaming plugin (see InstallingTwitchSDK.txt
    
    Other notes:
    - Documentation will be coming later.  This plugin is not quite finished yet.
    - Windows platform only, for now.  Mac and iOS will be coming next.
    - You can run with -TwitchDebug to see log spew from the Twitch SDK
    - "Direct" authentication with Twitch  is implemented but not recommended
    - New "Twitch" project settings are available in your Project Settings UI (when plugin is compiled with SDK available)
    - Various known issues we are working on (see '@todo twitch' in code)
    
    [CL 2125033 by Mike Fricker in Main branch]

---
+++ Engine/Plugins/Runtime/TwitchLiveStreaming/Source/TwitchLiveStreaming/TwitchLiveStreaming.Build.cs
@@ -0,0 +1,48 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+namespace UnrealBuildTool.Rules
+{
+    public class TwitchLiveStreaming : ModuleRules
+    {
+        public TwitchLiveStreaming(TargetInfo Target)
+        {
+            PrivateDependencyModuleNames.AddRange(
+                new string[]
+				{
+					"Core",
+					"CoreUObject",
+                    "Engine",
+					"RenderCore",
+					"RHI"
+				});
+
+			DynamicallyLoadedModuleNames.AddRange(
+				new string[]
+				{
+					"Settings"
+				});
+
+			PublicIncludePathModuleNames.AddRange(
+				new string[]
+				{
+					"Settings"
+				});
+
+ 			bool bHaveTwitchSDK =
+ 				( System.IO.Directory.Exists( System.IO.Path.Combine( UEBuildConfiguration.UEThirdPartySourceDirectory, "Twitch" ) ) &&
+ 				  System.IO.Directory.Exists( System.IO.Path.Combine( UEBuildConfiguration.UEThirdPartySourceDirectory, "Twitch", "Twitch-6.17" ) ) ) ||
+ 				( System.IO.Directory.Exists( System.IO.Path.Combine( UEBuildConfiguration.UEThirdPartySourceDirectory, "NotForLicensees" ) ) &&
+ 				  System.IO.Directory.Exists( System.IO.Path.Combine( UEBuildConfiguration.UEThirdPartySourceDirectory, "NotForLicensees", "Twitch" ) ) &&
+				  System.IO.Directory.Exists( System.IO.Path.Combine( UEBuildConfiguration.UEThirdPartySourceDirectory, "NotForLicensees", "Twitch", "Twitch-6.17" ) ) );
+ 			if( bHaveTwitchSDK )
+			{
+				AddThirdPartyPrivateStaticDependencies( Target, "Twitch" );
+				Definitions.Add( "WITH_TWITCH=1" );
+			}
+			else
+			{
+				Definitions.Add( "WITH_TWITCH=0" );
+			}
+        }
+    }
+}
