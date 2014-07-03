c03d1acdd4534e696c59028506cfe97e0299e9a8
bruce nesbit <bnesbit@pitbullstudio.co.uk>
2014-07-03 06:52:57 -0400

    Added GameMenuBuilder module.
    [CL 2130251 by bruce nesbit in Main branch]

---
+++ Engine/Source/Runtime/GameMenuBuilder/Private/GameMenuBuilderPrivatePCH.h
@@ -0,0 +1,10 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+
+#include "Slate.h"
+#include "SlateGameResources.h"
+#include "Engine.h"
+
+#include "GameMenuBuilder.h"
+#include "GameMenuWidgetStyle.h"
+
