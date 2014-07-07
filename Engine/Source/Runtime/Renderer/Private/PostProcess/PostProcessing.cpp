b83b3be99dac993190950c64df6507aa7abe3d5f
Nick Whiting <Nick.Whiting@epicgames.com>
2014-07-07 18:20:05 -0400

    TTP 340656:  Fix for screen percentage breaking when not in stereoscopic mode
    [CL 2171180 by Nick Whiting in Main branch]

--- Engine/Source/Runtime/Renderer/Private/PostProcess/PostProcessing.cpp
+++ Engine/Source/Runtime/Renderer/Private/PostProcess/PostProcessing.cpp
@@ -1140,7 +1140,7 @@ void FPostProcessing::Process(FRHICommandListImmediate& RHICmdList, const FViewI
 		AddHighResScreenshotMask(Context, SeparateTranslucency);
 
 		// Do not use upscale if SeparateRenderTarget is in use!
-		if(View.UnscaledViewRect != View.ViewRect && !View.Family->EngineShowFlags.HMDDistortion && View.Family->EngineShowFlags.StereoRendering && !View.Family->bUseSeparateRenderTarget) //!!AB
+		if(View.UnscaledViewRect != View.ViewRect && !View.Family->bUseSeparateRenderTarget)
 		{
 			static const auto CVar = IConsoleManager::Get().FindTConsoleVariableDataInt(TEXT("r.UpsampleQuality")); 
 			int32 UpsampleMethod = CVar->GetValueOnRenderThread();
