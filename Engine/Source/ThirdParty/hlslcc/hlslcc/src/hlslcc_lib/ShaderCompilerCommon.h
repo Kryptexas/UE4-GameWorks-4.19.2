30646def8b8adc8d76a301def88c1f5c29118795
Dmitry Rekman <Dmitry.Rekman@epicgames.com>
2014-07-09 20:11:06 -0400

    Added hlslcc for Linux.
    Imperfect for now because:
    - Without architecture support.
    - With -fPIC even for static library.
    - Not cross-compiled, so won't be rebuilt automatically.
    
    [CL 2183225 by Dmitry Rekman in Main branch]

--- Engine/Source/ThirdParty/hlslcc/hlslcc/src/hlslcc_lib/ShaderCompilerCommon.h
+++ Engine/Source/ThirdParty/hlslcc/hlslcc/src/hlslcc_lib/ShaderCompilerCommon.h
@@ -2,21 +2,26 @@
 
 #pragma once
 
-// Only needed on Mac
-#if __APPLE__
+// Needed on platforms other than Windows
+#ifndef WIN32
+#include <stdio.h>
 #include <stdarg.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <limits.h>
 #include <math.h>
+#include <string.h>
+#include <stdlib.h>
 #endif
 
 #include <assert.h>
 #define check(x)	assert(x)
 
-// On Mac we can't use static_assert in .c files as they really are compiled as C
-#if !__APPLE__ || __cplusplus
+// We can't use static_assert in .c files as this is a C++(11) feature
+#if __cplusplus && !__clang__
+#ifdef _MSC_VER
 typedef unsigned long long uint64_t;
+#endif // _MSC_VER
 static_assert(sizeof(uint64_t) == 8, "Bad!");
 #endif
 
