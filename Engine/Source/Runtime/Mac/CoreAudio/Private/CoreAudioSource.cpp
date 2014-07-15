150343eca300242200ea9e361e1c652f546ffc1d
Matthew Griffin <mgriffin@pitbullstudio.co.uk>
2014-07-15 07:04:22 -0400

    Fixing Mac build error
    Missed brackets from end of function call
    
    [CL 2218156 by Matthew Griffin in Main branch]

--- Engine/Source/Runtime/Mac/CoreAudio/Private/CoreAudioSource.cpp
+++ Engine/Source/Runtime/Mac/CoreAudio/Private/CoreAudioSource.cpp
@@ -626,7 +626,7 @@ bool FCoreAudioSoundSource::AttachToAUGraph()
 		ErrorStatus = AudioConverterNew( &Buffer->PCMFormat, &AudioDevice->MatrixMixerInputFormat, &CoreAudioConverter );
 
 		bool bIs6ChannelOGG = Buffer->NumChannels == 6
-							&& ((Buffer->DecompressionState && Buffer->DecompressionState->UsesVorbisChannelOrdering)
+							&& ((Buffer->DecompressionState && Buffer->DecompressionState->UsesVorbisChannelOrdering())
 							|| WaveInstance->WaveData->bDecompressedFromOgg);
 
 		AudioDevice->SetupMatrixMixerInput( DestInputNumber, bIs6ChannelOGG );
