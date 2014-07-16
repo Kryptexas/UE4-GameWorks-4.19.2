e780175a735c706e8175324838b9851d26a2cc29
David Ratti <David.Ratti@epicgames.com>
2014-07-15 20:37:28 -0400

    AbilityTask_WaitConfirm
    [CL 2220102 by David Ratti in Main branch]

---
+++ Engine/Source/Runtime/GameplayAbilities/Public/Abilities/Tasks/AbilityTask_WaitConfirm.h
@@ -0,0 +1,26 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "AbilityTask.h"
+#include "Abilities/GameplayAbilityTypes.h"
+#include "AbilityTask_WaitConfirm.generated.h"
+
+UCLASS(MinimalAPI)
+class UAbilityTask_WaitConfirm : public UAbilityTask
+{
+	GENERATED_UCLASS_BODY()
+
+	UPROPERTY(BlueprintAssignable)
+	FGenericAbilityTaskDelegate	OnConfirm;
+
+	UFUNCTION()
+	void OnConfirmCallback(UGameplayAbility* Ability);
+
+	virtual void Activate() override;
+
+	/** Wait until the server confirms the use of this ability. This is used to gate predictive portions of the ability */
+	UFUNCTION(BlueprintCallable, Category=Abilities, meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", BlueprintInternalUseOnly = "TRUE"))
+	static UAbilityTask_WaitConfirm* WaitConfirm(UObject* WorldContextObject);
+
+public:
+	
+};
