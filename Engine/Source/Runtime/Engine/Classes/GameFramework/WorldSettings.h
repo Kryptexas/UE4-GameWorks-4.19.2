522b9b24c54a15c662249802020effa615793988
Mikolaj Sieluzycki <msieluzycki@pitbullstudio.co.uk>
2014-07-22 07:09:24 -0400

    Header cleanup: WorldSettings.h
    [CL 2226824 by Mikolaj Sieluzycki in Main branch]

--- Engine/Source/Runtime/Engine/Classes/GameFramework/WorldSettings.h
+++ Engine/Source/Runtime/Engine/Classes/GameFramework/WorldSettings.h
@@ -1,10 +1,7 @@
 // Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
 
 #pragma once
-
 #include "Sound/ReverbVolume.h"
-#include "Engine/PostProcessVolume.h"
-
 #include "WorldSettings.generated.h"
 
 UENUM()
