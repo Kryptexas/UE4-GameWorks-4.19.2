5b41e105df4e0a9b8e7150823f5fb4904b7b2d28
Mikolaj Sieluzycki <msieluzycki@pitbullstudio.co.uk>
2014-07-23 08:26:47 -0400

    Header cleanup: PendingNetGame.h, SkeletalRenderCPUSkin.h.
    [CL 2228362 by Mikolaj Sieluzycki in Main branch]

--- Engine/Source/Runtime/Engine/Private/SkeletalRenderCPUSkin.h
+++ Engine/Source/Runtime/Engine/Private/SkeletalRenderCPUSkin.h
@@ -8,7 +8,6 @@
 
 #include "SkeletalRender.h"
 #include "SkeletalRenderPublic.h"
-#include "GPUSkinVertexFactory.h"
 #include "LocalVertexFactory.h"
 
 /** data for a single skinned skeletal mesh vertex */
