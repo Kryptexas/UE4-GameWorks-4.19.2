1196d8ac7a88b3911aa437cbe8846cb00922bd29
Dan Oconnor <Dan.OConnor@epicgames.com>
2014-07-24 12:55:36 -0400

    #ue4 Added check to handle case when superstruct has been 'prelinked' and therefore has all struct size information, but has not been completely created - Fix for 341705
    [CL 2230198 by Dan Oconnor in Main branch]

--- Engine/Source/Runtime/CoreUObject/Private/UObject/LinkerLoad.cpp
+++ Engine/Source/Runtime/CoreUObject/Private/UObject/LinkerLoad.cpp
@@ -2956,6 +2956,20 @@ UObject* ULinkerLoad::CreateExport( int32 Index )
 					return NULL;
 				}
 			}
+			else
+			{
+				// SuperStruct needs to be fully linked so that UStruct::Link will have access to UObject::SuperStruct->PropertySize. 
+				// There are other attempts to force our super struct to load, and I have not verified that they can all be removed
+				// in favor of this one:
+				if( !SuperStruct->HasAnyFlags(RF_LoadCompleted) 
+					&& !SuperStruct->GetClass()->ClassDefaultObject
+					&& SuperStruct->GetLinker() 
+					&& Export.SuperIndex.IsImport() )
+				{
+					SuperStruct->SetFlags(RF_NeedLoad);
+					Preload(SuperStruct);
+				}
+			}
 		}
 
 		// Only UClass objects and UProperty objects of intrinsic classes can have RF_Native set. Those property objects are never
