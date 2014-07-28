9db8bfe97f3f6d9a38d401851fefc9044ae2dfe5
Maciej Mroz <maciej.mroz@epicgames.com>
2014-07-28 11:24:42 -0400

    Compilation Fix (no editor)
    [CL 2234538 by Maciej Mroz in Main branch]

--- Engine/Source/Runtime/Engine/Private/Blueprint.cpp
+++ Engine/Source/Runtime/Engine/Private/Blueprint.cpp
@@ -164,13 +164,14 @@ void UBlueprintCore::GenerateDeterministicGuid()
 
 UBlueprint::UBlueprint(const class FPostConstructInitializeProperties& PCIP)
 	: Super(PCIP)
+#if WITH_EDITORONLY_DATA
 	, bCachedDependenciesUpTpDate(false)
-{
-
-#if WITH_EDITOR
-	bRunConstructionScriptOnDrag = true;
-	bGenerateConstClass = false;
 #endif
+#if WITH_EDITOR
+	, bRunConstructionScriptOnDrag(true)
+	, bGenerateConstClass(false)
+#endif
+{
 }
 
 void UBlueprint::Serialize(FArchive& Ar)
