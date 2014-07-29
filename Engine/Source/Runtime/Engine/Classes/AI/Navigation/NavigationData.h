84ffafc2b0ac386fe386d06f79e4514db9cd8a6c
Ben Marsh <Ben.Marsh@epicgames.com>
2014-07-29 14:03:42 -0400

    [INTEGRATE] Change 2234516 by Jamie.Dale@Pitbull_JDale2 on 2014/07/28 10:57:43
    	Added an include for Actor.h to NavigationData.h so that it works with "include what you use" (without Engine.h)
    
    	#codereview James.Golding
    
    [CL 2236121 by Ben Marsh in 4.4 branch]

--- Engine/Source/Runtime/Engine/Classes/AI/Navigation/NavigationData.h
+++ Engine/Source/Runtime/Engine/Classes/AI/Navigation/NavigationData.h
@@ -1,6 +1,7 @@
 // Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
 
 #pragma once
+#include "GameFramework/Actor.h"
 #include "AI/Navigation/NavigationTypes.h"
 #include "AI/NavDataGenerator.h"
 #include "AI/Navigation/NavFilters/NavigationQueryFilter.h"
