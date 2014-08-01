028ec3683ff2f3463bf517cef8698afc2ff44787
Michael Noland <michael.noland@epicgames.com>
2014-07-31 20:55:09 -0400

    Collision: Save project collision settings (custom profiles/channels/object types) in the DefaultEngine ini, fixing issues in cooked games #TTP 342936 #codereview lina.halper, max.preussner
    [CL 2239287 by Michael Noland in 4.4 branch]

--- Engine/Source/Editor/DetailCustomizations/Private/CollisionProfileDetails.cpp
+++ Engine/Source/Editor/DetailCustomizations/Private/CollisionProfileDetails.cpp
@@ -1815,7 +1815,7 @@ void FCollisionProfileDetails::UpdateChannel(bool bTraceType)
 	}
 
 	CollisionProfile->LoadProfileConfig(true);
-	CollisionProfile->SaveConfig();
+	CollisionProfile->UpdateDefaultConfigFile();
 
 	// update profile list if the name changed, this has to be refreshed
 	RefreshProfileList();
@@ -1827,7 +1827,7 @@ void FCollisionProfileDetails::UpdateProfile()
 	RefreshProfileList();
 	ProfileListView->RequestListRefresh();
 	CollisionProfile->LoadProfileConfig(true);
-	CollisionProfile->SaveConfig();
+	CollisionProfile->UpdateDefaultConfigFile();
 }
 
 void FCollisionProfileDetails::RefreshChannelList(bool bTraceType)
