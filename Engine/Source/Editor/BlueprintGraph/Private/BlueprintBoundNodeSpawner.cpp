d4f141f0e7cd3962817ef0db93b9e0e628a364bd
Mike Beach <Mike.Beach@epicgames.com>
2014-08-04 18:51:11 -0400

    Fixing non-unity build error (was missing an include).
    [CL 2243539 by Mike Beach in Main branch]

--- Engine/Source/Editor/BlueprintGraph/Private/BlueprintBoundNodeSpawner.cpp
+++ Engine/Source/Editor/BlueprintGraph/Private/BlueprintBoundNodeSpawner.cpp
@@ -1,7 +1,7 @@
 // Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
 
 #include "BlueprintGraphPrivatePCH.h"
-#include "BlueprintComponentNodeSpawner.h"
+#include "BlueprintBoundNodeSpawner.h"
 #include "K2Node_ComponentBoundEvent.h"
 #include "BlueprintPropertyNodeSpawner.h"
 #include "K2Node_AddComponent.h"
