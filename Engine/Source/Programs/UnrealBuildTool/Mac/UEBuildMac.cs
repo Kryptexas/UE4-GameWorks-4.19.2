dee247b733a62b3e7abd7b4cb3f37a9d4a28eac9
Peter Sauerbrei <Peter.Sauerbrei@epicgames.com>
2014-08-04 09:48:56 -0400

    remove HTML5 target platform dll from being built as part of the mac and linux editor builds for rocket TTP342631 #ue4 #html5
    [CL 2242372 by Peter Sauerbrei in Main branch]

--- Engine/Source/Programs/UnrealBuildTool/Mac/UEBuildMac.cs
+++ Engine/Source/Programs/UnrealBuildTool/Mac/UEBuildMac.cs
@@ -109,7 +109,6 @@ namespace UnrealBuildTool
 				// allow standalone tools to use target platform modules, without needing Engine
 				if (UEBuildConfiguration.bForceBuildTargetPlatforms)
 				{
-					InModule.AddPlatformSpecificDynamicallyLoadedModule("HTML5TargetPlatform");
 					InModule.AddDynamicallyLoadedModule("MacTargetPlatform");
 					InModule.AddDynamicallyLoadedModule("MacNoEditorTargetPlatform");
 					InModule.AddDynamicallyLoadedModule("MacClientTargetPlatform");
