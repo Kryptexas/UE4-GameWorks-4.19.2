84fc52ce288b869ae0b3125f2a79d12a423cf5b9
Hyojong Shin <Hyojong.Shin@epicgameskorea.com>
2014-08-07 03:15:13 -0400

    TTP #342124 : SkelControl Trail Node integration
    [CL 2246646 by Hyojong Shin in Main branch]

---
+++ Engine/Source/Editor/AnimGraph/Classes/AnimGraphNode_Trail.h
@@ -0,0 +1,27 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+#include "AnimGraphNode_SkeletalControlBase.h"
+#include "Animation/BoneControllers/AnimNode_Trail.h"
+#include "AnimGraphNode_Trail.generated.h"
+
+UCLASS(MinimalAPI)
+class UAnimGraphNode_Trail : public UAnimGraphNode_SkeletalControlBase
+{
+	GENERATED_UCLASS_BODY()
+
+		UPROPERTY(EditAnywhere, Category = Settings)
+		FAnimNode_Trail Node;
+
+public:
+	// UEdGraphNode interface
+	virtual FText GetNodeTitle(ENodeTitleType::Type TitleType) const override;
+//	virtual FString GetNodeNativeTitle(ENodeTitleType::Type TitleType) const override;
+	virtual FString GetTooltip() const override;
+	// End of UEdGraphNode interface
+
+protected:
+	// UAnimGraphNode_SkeletalControlBase interface
+	virtual FText GetControllerDescription() const override;
+	// End of UAnimGraphNode_SkeletalControlBase interface
+};
---
+++ Engine/Source/Editor/AnimGraph/Private/AnimGraphNode_Trail.cpp
@@ -0,0 +1,42 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+#include "AnimGraphPrivatePCH.h"
+#include "AnimGraphNode_Trail.h"
+
+/////////////////////////////////////////////////////
+// UAnimGraphNode_TrailBone
+
+#define LOCTEXT_NAMESPACE "A3Nodes"
+
+UAnimGraphNode_Trail::UAnimGraphNode_Trail(const FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+}
+
+FText UAnimGraphNode_Trail::GetControllerDescription() const
+{
+	return LOCTEXT("TrailController", "Trail controller");
+}
+
+FString UAnimGraphNode_Trail::GetTooltip() const
+{
+	return LOCTEXT("AnimGraphNode_Trail_Tooltip", "The Trail Controller.").ToString();
+}
+
+FText UAnimGraphNode_Trail::GetNodeTitle(ENodeTitleType::Type TitleType) const
+{
+	FFormatNamedArguments Args;
+	Args.Add(TEXT("ControllerDescription"), GetControllerDescription());
+	Args.Add(TEXT("BoneName"), FText::FromName(Node.TrailBone.BoneName));
+
+	if (TitleType == ENodeTitleType::ListView)
+	{
+		return FText::Format(LOCTEXT("AnimGraphNode_Trail_Title", "{ControllerDescription} - Bone: {BoneName}"), Args);
+	}
+	else
+	{
+		return FText::Format(LOCTEXT("AnimGraphNode_Trail_Title", "{ControllerDescription}\nBone: {BoneName}"), Args);
+	}
+}
+
+#undef LOCTEXT_NAMESPACE
