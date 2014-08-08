21a6cdca5d6d437360b653cd4a74d770d8635838
Richard Hinckley <Richard.Hinckley@epicgames.com>
2014-08-08 10:08:01 -0400

    Attempt to fix a GUBP compile error.
    [CL 2248465 by Richard Hinckley in Main branch]

--- Engine/Source/Runtime/GameplayAbilities/Public/Abilities/GameplayAbilityWorldReticle.h
+++ Engine/Source/Runtime/GameplayAbilities/Public/Abilities/GameplayAbilityWorldReticle.h
@@ -1,8 +1,8 @@
 // Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
 #pragma once
 
+#include "GameplayAbilityTargetActor.h"
 #include "GameplayAbilityWorldReticle.generated.h"
-
 
 UCLASS(Blueprintable)
 class GAMEPLAYABILITIES_API AGameplayAbilityWorldReticle : public AActor
