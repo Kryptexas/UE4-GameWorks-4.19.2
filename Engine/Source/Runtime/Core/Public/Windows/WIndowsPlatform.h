df167b2be75d110009d341bdf224a6d9900cfbfe
Mike Fricker <mike.fricker@epicgames.com>
2014-08-19 16:02:32 -0400

    Don't allow 128-bit atomics on Windows platform unless compiling with Windows 8 as min spec (part 2) - Fixed bad Windows SDK version comparison
    [CL 2263048 by Mike Fricker in Main branch]

--- Engine/Source/Runtime/Core/Public/Windows/WIndowsPlatform.h
+++ Engine/Source/Runtime/Core/Public/Windows/WIndowsPlatform.h
@@ -71,7 +71,7 @@ typedef FWindowsPlatformTypes FPlatformTypes;
 
 // Intrinsics for 128-bit atomics on Windows platform requires Windows 8 or higher (WINVER>0x0602)
 // http://msdn.microsoft.com/en-us/library/windows/desktop/hh972640.aspx
-#define PLATFORM_HAS_128BIT_ATOMICS							(!HACK_HEADER_GENERATOR && PLATFORM_64BITS && (WINVER > 0x602))
+#define PLATFORM_HAS_128BIT_ATOMICS							(!HACK_HEADER_GENERATOR && PLATFORM_64BITS && (WINVER >= 0x602))
 #define PLATFORM_USES_ANSI_STRING_FOR_EXTERNAL_PROFILING	0
 
 #if _MSC_VER < 1700
