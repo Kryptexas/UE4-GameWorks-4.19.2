c1eeb4a51fc9fc62351c04555c979f3ba0a0cc53
Martin Mittring <martin.mittring@epicgames.com>
2014-08-21 14:24:07 -0400

    * nicer subsurface profile asset color
    [CL 2266294 by Martin Mittring in Main branch]

--- Engine/Source/Developer/AssetTools/Private/AssetTypeActions/AssetTypeActions_SubsurfaceProfile.h
+++ Engine/Source/Developer/AssetTools/Private/AssetTypeActions/AssetTypeActions_SubsurfaceProfile.h
@@ -7,7 +7,7 @@ class FAssetTypeActions_SubsurfaceProfile : public FAssetTypeActions_Base
 public:
 	// IAssetTypeActions Implementation
 	virtual FText GetName() const override { return NSLOCTEXT("AssetTypeActions", "AssetTypeActions_SubsurfaceProfile", "Subsurface Profile"); }
-	virtual FColor GetTypeColor() const override { return FColor(192, 121, 64); }
+	virtual FColor GetTypeColor() const override { return FColor(255, 196, 128); }
 	virtual UClass* GetSupportedClass() const override;
 
 	virtual uint32 GetCategories() override { return EAssetTypeCategories::MaterialsAndTextures; }
