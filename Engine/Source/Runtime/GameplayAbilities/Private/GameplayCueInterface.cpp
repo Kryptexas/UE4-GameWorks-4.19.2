d63037d36d8cc9fcc34047a56dc1273dfc6443f8
David Ratti <David.Ratti@epicgames.com>
2014-08-26 15:45:38 -0400

    non unity fix
    [CL 2272743 by David Ratti in Main branch]

--- Engine/Source/Runtime/GameplayAbilities/Private/GameplayCueInterface.cpp
+++ Engine/Source/Runtime/GameplayAbilities/Private/GameplayCueInterface.cpp
@@ -1,6 +1,7 @@
 // Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
 
 #include "AbilitySystemPrivatePCH.h"
+#include "AbilitySystemComponent.h"
 #include "GameplayCueInterface.h"
 
 UGameplayCueInterface::UGameplayCueInterface(const FPostConstructInitializeProperties& PCIP)
