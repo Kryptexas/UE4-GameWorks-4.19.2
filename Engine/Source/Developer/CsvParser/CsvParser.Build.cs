7c63160804fef7debad39052b8537da1f75ba742
Andrew Rodham <arodham@pitbullstudio.co.uk>
2014-09-02 09:30:24 -0400

    Added csv parser
    The csv parser takes a string buffer which it splits into a series of null terminated, unescaped cell strings. In this way it can completely avoid heap allocation (esecially if constructed with a moved string). The parser copes with quoted cell values, and attempts to behave in the same way as Microsoft Excel for interoperability.
    
    [CL 2281417 by Andrew Rodham in Main branch]

---
+++ Engine/Source/Developer/CsvParser/CsvParser.Build.cs
@@ -0,0 +1,16 @@
+﻿// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class CsvParser : ModuleRules
+{
+    public CsvParser(TargetInfo Target)
+	{
+		PrivateDependencyModuleNames.AddRange(
+			new string[] 
+			{ 
+				"Core"
+			}
+            );
+	}
+}
