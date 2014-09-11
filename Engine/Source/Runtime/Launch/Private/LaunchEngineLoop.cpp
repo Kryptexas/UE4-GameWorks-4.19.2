1b251d38a1bc60147b553ced943b8aa7581902ed
Ben Marsh <Ben.Marsh@epicgames.com>
2014-09-11 11:19:00 -0400

    Add the project name to the compile-on-startup dialog.
    [CL 2293809 by Ben Marsh in Main branch]

--- Engine/Source/Runtime/Launch/Private/LaunchEngineLoop.cpp
+++ Engine/Source/Runtime/Launch/Private/LaunchEngineLoop.cpp
@@ -2369,7 +2369,7 @@ bool FEngineLoop::AppInit( )
 			}
 
 			// Ask whether to compile before continuing
-			if(FPlatformMisc::MessageBoxExt(EAppMsgType::YesNo, *ModulesList, TEXT("Question")) == EAppReturnType::No)
+			if(FPlatformMisc::MessageBoxExt(EAppMsgType::YesNo, *ModulesList, *FString::Printf(TEXT("Error loading %s"), GGameName)) == EAppReturnType::No)
 			{
 				return false;
 			}
