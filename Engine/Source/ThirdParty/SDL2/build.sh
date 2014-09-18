9363b47e5ccd4969a1034129e9fae8dd356886fe
Dmitry Rekman <Dmitry.Rekman@epicgames.com>
2014-09-17 23:03:45 -0400

    Build SDL_fPIC as a static lib with -fPIC, don't build .so
    [CL 2302015 by Dmitry Rekman in Main branch]

--- Engine/Source/ThirdParty/SDL2/build.sh
+++ Engine/Source/ThirdParty/SDL2/build.sh
@@ -11,5 +11,6 @@ cd $BUILD_DIR
 CFLAGS=-fPIC cmake ../$SDL_DIR
 make
 cp --remove-destination libSDL2.a ../$SDL_DIR/lib/Linux/x86_64-unknown-linux-gnu/libSDL2.a
+cp --remove-destination libSDL2_fPIC.a ../$SDL_DIR/lib/Linux/x86_64-unknown-linux-gnu/libSDL2_fPIC.a
 set +e
 
