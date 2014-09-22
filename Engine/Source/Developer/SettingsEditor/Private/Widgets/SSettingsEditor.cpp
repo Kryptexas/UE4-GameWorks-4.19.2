258a723282d40d08c66562fbc6272a723849df65
Ben Marsh <Ben.Marsh@epicgames.com>
2014-09-22 15:15:29 -0400

    [INTEGRATE] Change 2304574 by Michael.Noland@mnoland-T2784-HDepot on 2014/09/19 18:13:31
    	Editor: Further reduce excess padding in settings editor (40->16)
    
    [CL 2305877 by Ben Marsh in 4.5 branch]

--- Engine/Source/Developer/SettingsEditor/Private/Widgets/SSettingsEditor.cpp
+++ Engine/Source/Developer/SettingsEditor/Private/Widgets/SSettingsEditor.cpp
@@ -203,7 +203,7 @@ void SSettingsEditor::Construct( const FArguments& InArgs, const ISettingsEditor
 
 						+ SVerticalBox::Slot()
 							.FillHeight(1.0f)
-							.Padding(0.0f, 40.0f, 0.0f, 0.0f)
+							.Padding(0.0f, 16.0f, 0.0f, 0.0f)
 							[
 								// settings area
 								SNew(SOverlay)
