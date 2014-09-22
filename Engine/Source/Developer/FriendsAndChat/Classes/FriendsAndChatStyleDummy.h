e34532cdc1812f5bf24cd8b78af8dae4072a92db
Max Preussner <Max.Preussner@epicgames.com>
2014-09-22 11:28:58 -0400

    temporarily backing out until fix for module UClass dummies is in
    [CL 2305650 by Max Preussner in Main branch]

---
+++ Engine/Source/Developer/FriendsAndChat/Classes/FriendsAndChatStyleDummy.h
@@ -0,0 +1,15 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+/*=============================================================================
+	FriendsAndChatStyle.h: Declares the FFriendsAndChatStyle interface.
+=============================================================================*/
+#pragma once
+
+#include "FriendsAndChatStyleDummy.generated.h"
+
+// Dummy class to make module ordering work
+UCLASS()
+class FRIENDSANDCHAT_API UFriendsAndChatStyleDummy : public UObject
+{
+	GENERATED_UCLASS_BODY()
+};
