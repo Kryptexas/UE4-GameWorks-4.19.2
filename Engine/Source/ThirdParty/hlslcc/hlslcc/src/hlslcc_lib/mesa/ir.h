227498cb64e475642c2fe6013bdca26a7a06695d
Rolando Caloca <Rolando.Caloca@epicgames.com>
2014-09-25 10:56:28 -0400

    UE4 - clang compile fix
    [CL 2309803 by Rolando Caloca in Main branch]

--- Engine/Source/ThirdParty/hlslcc/hlslcc/src/hlslcc_lib/mesa/ir.h
+++ Engine/Source/ThirdParty/hlslcc/hlslcc/src/hlslcc_lib/mesa/ir.h
@@ -1543,9 +1543,9 @@ class ir_texture : public ir_rvalue
 {
 public:
 	ir_texture(enum ir_texture_opcode op, const SSourceLocation& InSourceLocation)
-		: sampler(NULL), coordinate(NULL), op(op),
-		projector(NULL), shadow_comparitor(NULL), offset(NULL),
-		channel(ir_channel_none), SamplerState(NULL)
+		: op(op), sampler(nullptr), coordinate(nullptr), 
+		projector(nullptr), shadow_comparitor(nullptr), offset(nullptr),
+		channel(ir_channel_none), SamplerState(nullptr)
 	{
 		SourceLocation = InSourceLocation;
 		this->ir_type = ir_type_texture;
