037f0be2e64ca1c3b72ce59d05372f9ed098e479
Zak Middleton <Zak.Middleton@epicgames.com>
2014-09-26 19:03:24 -0400

    #docs - Fix @see in Pawn.AddMovementInput() hiding pin comments.
    [CL 2311768 by Zak Middleton in Main branch]

--- Engine/Source/Runtime/Engine/Classes/GameFramework/Pawn.h
+++ Engine/Source/Runtime/Engine/Classes/GameFramework/Pawn.h
@@ -328,11 +328,11 @@ public:
 	/**
 	 * Add movement input along the given world direction vector (usually normalized) scaled by 'ScaleValue'. If ScaleValue < 0, movement will be in the opposite direction.
 	 * Base Pawn classes won't automatically apply movement, it's up to the user to do so in a Tick event. Subclasses such as Character and DefaultPawn automatically handle this input and move.
-	 * @see GetPendingMovementInputVector(), GetLastMovementInputVector(), ConsumeMovementInputVector()
 	 *
 	 * @param WorldDirection	Direction in world space to apply input
 	 * @param ScaleValue		Scale to apply to input. This can be used for analog input, ie a value of 0.5 applies half the normal value, while -1.0 would reverse the direction.
 	 * @param bForce			If true always add the input, ignoring the result of IsMoveInputIgnored().
+	 * @see GetPendingMovementInputVector(), GetLastMovementInputVector(), ConsumeMovementInputVector()
 	 */
 	UFUNCTION(BlueprintCallable, Category="Pawn|Input", meta=(Keywords="AddInput"))
 	virtual void AddMovementInput(FVector WorldDirection, float ScaleValue = 1.0f, bool bForce = false);
