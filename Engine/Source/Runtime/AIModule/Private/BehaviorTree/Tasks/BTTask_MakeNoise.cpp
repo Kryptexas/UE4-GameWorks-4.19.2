7fa70623f92247e55fc87bdfe0486ea4dd2ef774
Marc Audy <Marc.Audy@epicgames.com>
2014-09-26 11:32:12 -0400

    Remove unneeded includes
    [CL 2311100 by Marc Audy in Main branch]

--- Engine/Source/Runtime/AIModule/Private/BehaviorTree/Tasks/BTTask_MakeNoise.cpp
+++ Engine/Source/Runtime/AIModule/Private/BehaviorTree/Tasks/BTTask_MakeNoise.cpp
@@ -1,8 +1,6 @@
 // Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
 
 #include "AIModulePrivate.h"
-#include "SoundDefinitions.h"
-#include "Sound/ReverbVolume.h"
 #include "BehaviorTree/Tasks/BTTask_MakeNoise.h"
 
 UBTTask_MakeNoise::UBTTask_MakeNoise(const FPostConstructInitializeProperties& PCIP) 
@@ -27,7 +25,7 @@ EBTNodeResult::Type UBTTask_MakeNoise::ExecuteTask(UBehaviorTreeComponent* Owner
 
 	return EBTNodeResult::Failed;
 }
-
+	
 #if WITH_EDITOR
 
 FName UBTTask_MakeNoise::GetNodeIconName() const
