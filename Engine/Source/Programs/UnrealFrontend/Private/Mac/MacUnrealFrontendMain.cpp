5f8787888815c56b5279a1b957eede71b30932f6
Ben Marsh <Ben.Marsh@epicgames.com>
2014-09-30 11:42:13 -0400

    [INTEGRATE] Change 2314311 by Michael.Trepka@Michael.Trepka_a4202 on 2014/09/30 10:49:27
    	FSlateMacMenu::UpdateCachedState() now checks with the global tab manager if there's any active tab instead of depending on not being called when there's not. This should solve any remaining issues with executing it while the app is quitting.
    
    [CL 2314317 by Ben Marsh in 4.5 branch]

--- Engine/Source/Programs/UnrealFrontend/Private/Mac/MacUnrealFrontendMain.cpp
+++ Engine/Source/Programs/UnrealFrontend/Private/Mac/MacUnrealFrontendMain.cpp
@@ -27,7 +27,6 @@ static FString GSavedCommandLine;
 - (IBAction)requestQuit:(id)Sender
 {
 	GIsRequestingExit = true;
-	FPlatformMisc::UpdateCachedMacMenuState = nullptr;
 }
 
 - (void) runGameThread:(id)Arg
