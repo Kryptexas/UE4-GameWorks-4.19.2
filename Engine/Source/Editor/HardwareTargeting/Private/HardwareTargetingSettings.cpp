d47385c930e5adc2543a111441da34dd4cb6f0a5
Andrew Rodham <arodham@pitbullstudio.co.uk>
2014-10-01 05:58:03 -0400

    Moved hardware targeting applied settings to DefaultEngine.ini rather than a saved Editor.ini
    This ensures that multiple users will not attempt to update the project settings on start-up.
    
    #codereview Michael.Noland
    
    [CL 2315685 by Andrew Rodham in Main branch]

--- Engine/Source/Editor/HardwareTargeting/Private/HardwareTargetingSettings.cpp
+++ Engine/Source/Editor/HardwareTargeting/Private/HardwareTargetingSettings.cpp
@@ -10,71 +10,20 @@
 UHardwareTargetingSettings::UHardwareTargetingSettings(const FPostConstructInitializeProperties& PCIP)
 	: Super(PCIP)
 	, TargetedHardwareClass(EHardwareClass::Unspecified)
+	, AppliedTargetedHardwareClass(EHardwareClass::Unspecified)
 	, DefaultGraphicsPerformance(EGraphicsPreset::Unspecified)
+	, AppliedDefaultGraphicsPerformance(EGraphicsPreset::Unspecified)
 {
 }
 
 bool UHardwareTargetingSettings::HasPendingChanges() const
 {
-	UHardwareTargetingSettings* Settings = GetMutableDefault<UHardwareTargetingSettings>();
-
-	if (Settings->TargetedHardwareClass != EHardwareClass::Unspecified)
+	if (TargetedHardwareClass == EHardwareClass::Unspecified || DefaultGraphicsPerformance == EGraphicsPreset::Unspecified)
 	{
-		FString AppliedHardwareEnumValue;
-		const bool bFoundKey = GConfig->GetString(TEXT("AppliedHardwareTargetingSettings"), TEXT("AppliedTargetedHardwareClass"), AppliedHardwareEnumValue, GEditorIni);
-		if (!bFoundKey)
-		{
-			return true;
-		}
-		else if (AppliedHardwareEnumValue.IsNumeric())
-		{
-			// We used to write out an int instead of a string
-			const EHardwareClass::Type EnumValue = static_cast<EHardwareClass::Type>(FPlatformString::Atoi(*AppliedHardwareEnumValue));
-			if (EnumValue != Settings->TargetedHardwareClass)
-			{
-				return true;
-			}
-		}
-		else
-		{
-			UEnum* Enum = FindObject<UEnum>(ANY_PACKAGE, TEXT("EHardwareClass"), true);
-			check(Enum);
-			if (Enum->GetEnumName(Settings->TargetedHardwareClass) != AppliedHardwareEnumValue)
-			{
-				return true;
-			}
-		}
+		return false;
 	}
 
-	if (Settings->DefaultGraphicsPerformance != EGraphicsPreset::Unspecified)
-	{
-		FString AppliedGraphicsEnumValue;
-		const bool bFoundKey = GConfig->GetString(TEXT("AppliedHardwareTargetingSettings"), TEXT("AppliedDefaultGraphicsPerformance"), AppliedGraphicsEnumValue, GEditorIni);
-		if (!bFoundKey)
-		{
-			return true;
-		}
-		else if (AppliedGraphicsEnumValue.IsNumeric())
-		{
-			// We used to write out an int instead of a string
-			const EGraphicsPreset::Type EnumValue = EGraphicsPreset::Type(FPlatformString::Atoi(*AppliedGraphicsEnumValue));
-			if (EnumValue != Settings->DefaultGraphicsPerformance)
-			{
-				return true;
-			}
-		}
-		else
-		{
-			UEnum* Enum = FindObject<UEnum>(ANY_PACKAGE, TEXT("EGraphicsPreset"), true);
-			check(Enum);
-			if (Enum->GetEnumName(Settings->DefaultGraphicsPerformance) != AppliedGraphicsEnumValue)
-			{
-				return true;
-			}
-		}
-	}
-
-	return false;
+	return AppliedTargetedHardwareClass != TargetedHardwareClass || AppliedDefaultGraphicsPerformance != DefaultGraphicsPerformance;
 }
 
 void UHardwareTargetingSettings::PostEditChangeProperty( struct FPropertyChangedEvent& PropertyChangedEvent )
