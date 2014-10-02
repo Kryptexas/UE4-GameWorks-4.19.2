8473a8811c6a85368680e375f12e9302cfc3e29b
David Ratti <David.Ratti@epicgames.com>
2014-10-02 19:39:33 -0400

    comment
    [CL 2318124 by David Ratti in Main branch]

--- Engine/Source/Runtime/GameplayAbilities/Public/Abilities/Tasks/AbilityTask_MoveToLocation.h
+++ Engine/Source/Runtime/GameplayAbilities/Public/Abilities/Tasks/AbilityTask_MoveToLocation.h
@@ -17,6 +17,8 @@ class UPrimitiveComponent;
 
 /**
  *	Move to a location, ignoring clipping, over a given length of time. Ends when the TargetLocation is reached.
+ *	This will RESET your character's current movement mode! If you wish to maintain PHYS_Flying or PHYS_Custom, you must
+ *	reset it on completion.!
  */
 UCLASS(MinimalAPI)
 class UAbilityTask_MoveToLocation : public UAbilityTask
