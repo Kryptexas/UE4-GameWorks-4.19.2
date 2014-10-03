e27bd994cc18ebfea7b7dc51fa24fff1727df11e
Daniel Wright <Daniel.Wright@epicgames.com>
2014-10-03 18:02:05 -0400

    Fixed rare GPU hang from DFAO - DrawIndirect was done with uninitialized memory in very specific circumstances
    [CL 2319077 by Daniel Wright in Main branch]

--- Engine/Source/Runtime/Renderer/Private/DistanceFieldSurfaceCacheLighting.cpp
+++ Engine/Source/Runtime/Renderer/Private/DistanceFieldSurfaceCacheLighting.cpp
@@ -3353,6 +3353,8 @@ bool FDeferredShadingSceneRenderer::RenderDistanceFieldAOSurfaceCache(FRHIComman
 					RHICmdList.ClearUAV(SurfaceCacheResources.Level[DepthLevel]->ScatterDrawParameters.UAV, ClearValues);
 				}
 
+				RHICmdList.ClearUAV(SurfaceCacheResources.TempResources->ScatterDrawParameters.UAV, ClearValues);
+
 				SurfaceCacheResources.bClearedResources = true;
 			}
 
