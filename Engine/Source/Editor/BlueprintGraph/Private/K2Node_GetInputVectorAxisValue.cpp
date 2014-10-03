198201d5cf66d4f6e00478f31f3af00e6c260ed0
Mike Beach <Mike.Beach@epicgames.com>
2014-10-03 11:35:29 -0400

    Making vector axis getter nodes distinguisable (and searchable with the phrase "get"). TTP #343490
    [CL 2318639 by Mike Beach in Main branch]

--- Engine/Source/Editor/BlueprintGraph/Private/K2Node_GetInputVectorAxisValue.cpp
+++ Engine/Source/Editor/BlueprintGraph/Private/K2Node_GetInputVectorAxisValue.cpp
@@ -97,6 +97,7 @@ void UK2Node_GetInputVectorAxisValue::GetMenuActions(FBlueprintActionDatabaseReg
 
 		UBlueprintNodeSpawner* NodeSpawner = UBlueprintNodeSpawner::Create(GetClass());
 		check(NodeSpawner != nullptr);
+		NodeSpawner->DefaultMenuSignature.MenuName = FText::Format(NSLOCTEXT("K2Node_GetInputVectorAxisValue", "MenuName", "Get {0}"), Key.GetDisplayName());
 
 		NodeSpawner->CustomizeNodeDelegate = UBlueprintNodeSpawner::FCustomizeNodeDelegate::CreateStatic(CustomizeInputNodeLambda, Key);
 		ActionRegistrar.AddBlueprintAction(ActionKey, NodeSpawner);
