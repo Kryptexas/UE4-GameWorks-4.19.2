41c9f10f4cf2be8d5091adc30c2c2586a646a298
Mikolaj Sieluzycki <Mikolaj.Sieluzycki@epicgames.com>
2014-10-07 07:41:29 -0400

    Mac CIS fix.
    [CL 2321097 by Mikolaj Sieluzycki in Main branch]

--- Engine/Source/Editor/UMGEditor/Private/UMGEditorPrivatePCH.h
+++ Engine/Source/Editor/UMGEditor/Private/UMGEditorPrivatePCH.h
@@ -1,7 +1,7 @@
 // Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
 
 #pragma once
-
+#include "Platform.h"
 #include "EditorStyleSet.h"
 #include "Internationalization/Internationalization.h"
 #include "Internationalization/Text.h"
