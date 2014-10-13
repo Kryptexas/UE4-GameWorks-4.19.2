2dc6fd78170b08e784c10aa1e53fcfa78f02f917
Dmitry Rekman <Dmitry.Rekman@epicgames.com>
2014-10-13 13:18:08 -0400

    Fix broken text rendering in ShooterGame (FIXIF TTP #349209).
    #codereview Mark.Satterthwaite, Niklas.Smedberg, Michael.Trepka
    
    [CL 2327366 by Dmitry Rekman in 4.5 branch]

--- Engine/Source/Runtime/OpenGLDrv/Private/OpenGLDevice.cpp
+++ Engine/Source/Runtime/OpenGLDrv/Private/OpenGLDevice.cpp
@@ -711,7 +711,7 @@ static void InitRHICapabilitiesForGL()
 	{
 		// Not supported for rendering:
 		SetupTextureFormat( PF_G16,				FOpenGLTextureFormat( GL_R16,					GL_R16,					GL_RED,			GL_UNSIGNED_SHORT,					false,	false));
-#if PLATFORM_MAC // @todo On OS X specifying GL_R8 as the internal SRGB format results in incorrect rendering, so specifying SRGB8 as before for now
+#if PLATFORM_MAC || PLATFORM_LINUX // @todo On OS X and Linux specifying GL_R8 as the internal SRGB format results in incorrect rendering, so specifying SRGB8 as before for now
 		SetupTextureFormat( PF_G8,				FOpenGLTextureFormat( GL_R8,					GL_SRGB8,				GL_RED,			GL_UNSIGNED_BYTE,					false,	false));
 #else
 		SetupTextureFormat( PF_G8,				FOpenGLTextureFormat( GL_R8,					GL_R8,					GL_RED,			GL_UNSIGNED_BYTE,					false,	false));
