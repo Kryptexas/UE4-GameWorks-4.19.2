6a6cecf2d03768b9ceb2ce30432837a1dbccae1d
Maciej Mroz <maciej.mroz@epicgames.com>
2014-10-20 08:33:02 -0400

    class ArchiveObjectCrc32 - Calculates a checksum on an object's serialized data stream. Written originally by Phillip Kavan.
    [CL 2334477 by Maciej Mroz in Main branch]

--- Engine/Source/Runtime/CoreUObject/Public/Serialization/ArchiveUObject.h
+++ Engine/Source/Runtime/CoreUObject/Public/Serialization/ArchiveUObject.h
@@ -1509,3 +1509,43 @@ private:
 	/** Compression flags determining compression of CompressedChunks.				*/
 	ECompressionFlags				CompressionFlags;
 };
+
+/*----------------------------------------------------------------------------
+FArchiveObjectCrc32
+----------------------------------------------------------------------------*/
+
+/**
+* Calculates a checksum on an object's serialized data stream.
+*/
+class COREUOBJECT_API FArchiveObjectCrc32 : public FArchiveUObject
+{
+public:
+	/**
+	* Default constructor.
+	*/
+	FArchiveObjectCrc32();
+
+	// Begin FArchive Interface
+	virtual void Serialize(void* Data, int64 Length);
+	virtual FArchive& operator<<(class FName& Name);
+	virtual FArchive& operator<<(class UObject*& Object);
+	virtual FString GetArchiveName() const { return TEXT("FArchiveObjectCrc32"); }
+	// End FArchive Interface
+
+	/**
+	* Serialize the given object, calculate and return its checksum.
+	*/
+	uint32 Crc32(UObject* Object, uint32 CRC = 0);
+
+private:
+	/** Internal archive used for serialization */
+	FMemoryWriter MemoryWriter;
+	/** Internal byte array used for serialization */
+	TArray<uint8> SerializedObjectData;
+	/** Internal queue of object references awaiting serialization */
+	TQueue<UObject*> ObjectsToSerialize;
+	/** Internal currently serialized object */
+	const UObject* ObjectBeingSerialized;
+	/** Internal root object */
+	const UObject* RootObject;
+};
