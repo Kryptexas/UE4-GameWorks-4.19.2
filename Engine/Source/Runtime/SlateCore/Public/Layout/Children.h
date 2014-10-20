a1154cc3dd0c23f33bdb06e5f33b792422dfb468
Cody Haskell <Cody.Haskell@epicgames.com>
2014-10-20 18:00:27 -0400

    Back out changelist 2334875
    [CL 2335004 by Cody Haskell in Main branch]

--- Engine/Source/Runtime/SlateCore/Public/Layout/Children.h
+++ Engine/Source/Runtime/SlateCore/Public/Layout/Children.h
@@ -237,7 +237,7 @@ public:
 	template <class PREDICATE_CLASS>
 	void Sort( const PREDICATE_CLASS& Predicate )
 	{
-		::Sort(GetData(), Num(), Predicate);
+		TIndirectArray< SlotType >::Sort( Predicate );
 	}
 
 	void Swap( int32 IndexA, int32 IndexB )
