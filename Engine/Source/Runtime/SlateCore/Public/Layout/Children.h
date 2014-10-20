1c8c2aa756bd0d3deebc821295c53a1f0bc9c588
Cody Haskell <Cody.Haskell@epicgames.com>
2014-10-20 18:30:16 -0400

    #UE4
    - Making Sort work on TPanelChildren
    
    [CL 2335037 by Cody Haskell in Main branch]

--- Engine/Source/Runtime/SlateCore/Public/Layout/Children.h
+++ Engine/Source/Runtime/SlateCore/Public/Layout/Children.h
@@ -237,7 +237,7 @@ public:
 	template <class PREDICATE_CLASS>
 	void Sort( const PREDICATE_CLASS& Predicate )
 	{
-		TIndirectArray< SlotType >::Sort( Predicate );
+		::Sort(TIndirectArray< SlotType >::GetData(), TIndirectArray<SlotType>::Num(), Predicate);
 	}
 
 	void Swap( int32 IndexA, int32 IndexB )
