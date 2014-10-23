d81462f76c260d75ffba645f58a4ddfd6de69e2e
Saul Abreu <sabreu@phosphorgames.com>
2014-10-23 14:11:16 -0400

    Modified ICU data generation settings to generate EFIGS + CJK + Leet.
    [CL 2338818 by Saul Abreu in Main branch]

--- Engine/Source/ThirdParty/ICU/icu4c-53_1/source/data/brkitr/brklocal.mk
+++ Engine/Source/ThirdParty/ICU/icu4c-53_1/source/data/brkitr/brklocal.mk
@@ -3,11 +3,17 @@ BRK_DICT_SOURCE = cjdict.txt
 
 
 # List of break iterator files (brk).
-BRK_SOURCE = char.txt line.txt line_ja.txt\
- sent.txt title.txt word.txt word_POSIX.txt
+BRK_SOURCE = char.txt line.txt\
+ sent.txt title.txt word.txt word_POSIX.txt\
+ line_ja.txt
 
 
 # Ordinary resources
-BRK_RES_SOURCE = de.txt en.txt en_US.txt\
- en_US_POSIX.txt es.txt fr.txt it.txt\
- ja.txt pt.txt ru.txt leet.txt
+BRK_RES_SOURCE = en.txt en_US.txt\
+ en_US_POSIX.txt\
+ fr.txt\
+ it.txt\
+ de.txt\
+ es.txt\
+ ja.txt\
+ leet.txt
--- Engine/Source/ThirdParty/ICU/icu4c-53_1/source/data/locales/reslocal.mk
+++ Engine/Source/ThirdParty/ICU/icu4c-53_1/source/data/locales/reslocal.mk
@@ -1,14 +1,12 @@
 # Aliases without a corresponding xx.xml file (see icu-config.xml & build.xml)
 GENRB_SYNTHETIC_ALIAS = en_NH.txt en_RH.txt\
- ja_JP_TRADITIONAL.txt\
  zh_CN.txt\
- zh_HK.txt zh_MO.txt zh_SG.txt zh_TW.txt
+ zh_HK.txt zh_MO.txt zh_SG.txt zh_TW.txt\
+ ja_JP_TRADITIONAL.txt
 
 
 # Ordinary resources
-GENRB_SOURCE = de.txt de_AT.txt\
- de_BE.txt de_CH.txt de_DE.txt de_LI.txt de_LU.txt\
- en.txt en_001.txt en_150.txt en_AG.txt\
+GENRB_SOURCE = en.txt en_001.txt en_150.txt en_AG.txt\
  en_AI.txt en_AS.txt en_AU.txt en_BB.txt en_BE.txt\
  en_BM.txt en_BS.txt en_BW.txt en_BZ.txt en_CA.txt\
  en_CC.txt en_CK.txt en_CM.txt en_CX.txt en_DG.txt\
@@ -27,12 +25,6 @@ GENRB_SOURCE = de.txt de_AT.txt\
  en_TT.txt en_TV.txt en_TZ.txt en_UG.txt en_UM.txt\
  en_US.txt en_US_POSIX.txt en_VC.txt en_VG.txt en_VI.txt\
  en_VU.txt en_WS.txt en_ZA.txt en_ZM.txt en_ZW.txt\
- es.txt es_419.txt es_AR.txt es_BO.txt\
- es_CL.txt es_CO.txt es_CR.txt es_CU.txt es_DO.txt\
- es_EA.txt es_EC.txt es_ES.txt es_GQ.txt es_GT.txt\
- es_HN.txt es_IC.txt es_MX.txt es_NI.txt es_PA.txt\
- es_PE.txt es_PH.txt es_PR.txt es_PY.txt es_SV.txt\
- es_US.txt es_UY.txt es_VE.txt\
  fr.txt fr_BE.txt fr_BF.txt fr_BI.txt fr_BJ.txt\
  fr_BL.txt fr_CA.txt fr_CD.txt fr_CF.txt fr_CG.txt\
  fr_CH.txt fr_CI.txt fr_CM.txt fr_DJ.txt fr_DZ.txt\
@@ -43,20 +35,19 @@ GENRB_SOURCE = de.txt de_AT.txt\
  fr_PM.txt fr_RE.txt fr_RW.txt fr_SC.txt fr_SN.txt\
  fr_SY.txt fr_TD.txt fr_TG.txt fr_TN.txt fr_VU.txt\
  fr_WF.txt fr_YT.txt\
- hi.txt\
- hi_IN.txt\
  it.txt it_CH.txt it_IT.txt it_SM.txt\
+ de.txt de_AT.txt\
+ de_BE.txt de_CH.txt de_DE.txt de_LI.txt de_LU.txt\
+ es.txt es_419.txt es_AR.txt es_BO.txt\
+ es_CL.txt es_CO.txt es_CR.txt es_CU.txt es_DO.txt\
+ es_EA.txt es_EC.txt es_ES.txt es_GQ.txt es_GT.txt\
+ es_HN.txt es_IC.txt es_MX.txt es_NI.txt es_PA.txt\
+ es_PE.txt es_PH.txt es_PR.txt es_PY.txt es_SV.txt\
+ es_US.txt es_UY.txt es_VE.txt\
+ zh.txt\
+ zh_Hans.txt zh_Hans_CN.txt zh_Hans_HK.txt zh_Hans_MO.txt zh_Hans_SG.txt\
+ zh_Hant.txt zh_Hant_HK.txt zh_Hant_MO.txt zh_Hant_TW.txt\ 
  ja.txt ja_JP.txt\
  ko.txt ko_KP.txt\
  ko_KR.txt\
- pl.txt\
- pl_PL.txt pt.txt pt_AO.txt\
- pt_BR.txt pt_CV.txt pt_GW.txt pt_MO.txt pt_MZ.txt\
- pt_PT.txt pt_ST.txt pt_TL.txt\
- ru.txt ru_BY.txt ru_KG.txt\
- ru_KZ.txt ru_MD.txt ru_RU.txt ru_UA.txt\
- sv.txt sv_AX.txt sv_FI.txt sv_SE.txt\
- zh.txt\
- zh_Hans.txt zh_Hans_CN.txt zh_Hans_HK.txt zh_Hans_MO.txt zh_Hans_SG.txt\
- zh_Hant.txt zh_Hant_HK.txt zh_Hant_MO.txt zh_Hant_TW.txt\
  leet.txt
