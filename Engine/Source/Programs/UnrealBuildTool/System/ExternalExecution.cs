0da3bfc2990017db1f8cbac774d0e23713c47864
Robert Manuszewski <Robert.Manuszewski@epicgames.com>
2014-10-24 08:09:56 -0400

    Changed ECompilationResult enum to match exit code 3 as assert or crash as used by the engine.
    [CL 2339712 by Robert Manuszewski in Main branch]

--- Engine/Source/Programs/UnrealBuildTool/System/ExternalExecution.cs
+++ Engine/Source/Programs/UnrealBuildTool/System/ExternalExecution.cs
@@ -27,7 +27,9 @@ namespace UnrealBuildTool
 		/** Compilation failed because generated code changed which was not supported */
 		FailedDueToHeaderChange = 1,
 		/** Compilation failed due to compilation errors */
-		OtherCompilationError,
+		OtherCompilationError = 2,
+		/** The process has most likely crashed. This is what UE returns in case of an assert */
+		CrashOrAssert = 3,
 		/** Compilation is not supported in the current build */
 		Unsupported,
 		/** Unknown error */
