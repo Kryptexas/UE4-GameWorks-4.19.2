c3f743c1e24cb8836bc93ce7def35fcf75343c8d
Max Preussner <Max.Preussner@epicgames.com>
2014-10-27 14:57:50 -0400

    ShmMessaging: Initial revision of the plug-in
    [CL 2341413 by Max Preussner in Main branch]

---
+++ Engine/Plugins/Messaging/ShmMessaging/Source/ShmMessaging/Private/ShmMessagingClasses.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+#include "ShmMessagingPrivatePCH.h"
+
+
+UShmMessagingSettings::UShmMessagingSettings( const FObjectInitializer& ObjectInitializer )
+	: Super(ObjectInitializer)
+	, EnableTransport(true)
+{ }
