8a749b7f35a433ba56b49b37045d866771b2dd29
UnrealBot <unrealbot@users.noreply.github.com>
2014-11-04 16:10:57 -0500

    Snapshot for changelist 2348932.
    [CL 2348932 in Main branch]

---
+++ GitSetup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+set -e
+
+cd "`dirname "$0"`"
+
+if [ ! -f Engine/Binaries/DotNET/GitDependencies.exe ]; then
+	echo "GitSetup ERROR: This script does not appear to be located \
+       in the root UE4 directory and must be run from there."
+	exit 1
+fi 
+
+if [ "$(uname)" = "Darwin" ]; then
+	cd Engine/Build/BatchFiles/Mac
+	source SetupMono.sh "`pwd`"
+	cd ../../../..
+fi
+
+mono Engine/Binaries/DotNET/GitDependencies.exe "$@"
