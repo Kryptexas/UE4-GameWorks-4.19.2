e252b559562e9ca14cc80ac24ad27a6015c5f8d7
Nick Darnell <Nick.Darnell@epicgames.com>
2014-11-11 16:58:44 -0500

    UMG - The play PIE button is now visibile in the designer.
    [CL 2356398 by Nick Darnell in Main branch]

--- Engine/Source/Editor/UMGEditor/Private/BlueprintModes/WidgetDesignerApplicationMode.cpp
+++ Engine/Source/Editor/UMGEditor/Private/BlueprintModes/WidgetDesignerApplicationMode.cpp
@@ -124,9 +124,11 @@ FWidgetDesignerApplicationMode::FWidgetDesignerApplicationMode(TSharedPtr<FWidge
 	ToolbarExtender = MakeShareable(new FExtender);
 	InWidgetEditor->GetWidgetToolbarBuilder()->AddWidgetBlueprintEditorModesToolbar(ToolbarExtender);
 	InWidgetEditor->GetToolbarBuilder()->AddCompileToolbar(ToolbarExtender);
-	//InPersona->GetToolbarBuilder()->AddScriptingToolbar(ToolbarExtender);
-	//InPersona->GetToolbarBuilder()->AddBlueprintGlobalOptionsToolbar(ToolbarExtender);
-	//InPersona->GetToolbarBuilder()->AddDebuggingToolbar(ToolbarExtender);
+	//InWidgetEditor->GetToolbarBuilder()->AddComponentsToolbar(ToolbarExtender);
+	InWidgetEditor->GetToolbarBuilder()->AddDebuggingToolbar(ToolbarExtender);
+
+	//InWidgetEditor->GetToolbarBuilder()->AddScriptingToolbar(ToolbarExtender);
+	//InWidgetEditor->GetToolbarBuilder()->AddBlueprintGlobalOptionsToolbar(ToolbarExtender);
 }
 
 void FWidgetDesignerApplicationMode::RegisterTabFactories(TSharedPtr<FTabManager> InTabManager)
