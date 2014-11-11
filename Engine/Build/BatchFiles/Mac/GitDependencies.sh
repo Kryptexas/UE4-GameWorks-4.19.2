523e288bb6e9fad77223104963c8b8d198d265d5
Ben Marsh <Ben.Marsh@epicgames.com>
2014-11-11 13:00:43 -0500

    Change git hook to a symlink to Engine/Build/BatchFiles/Mac/GitDependenciesHook.sh (which calls GitDependencies.sh), and allow GitDependencies.sh to be run as a link from elsewhere.
    #codereview Dmitry.Rekman
    
    [CL 2356053 by Ben Marsh in Main branch]

--- Engine/Build/BatchFiles/Mac/GitDependencies.sh
+++ Engine/Build/BatchFiles/Mac/GitDependencies.sh
@@ -3,14 +3,17 @@
 
 set -e
 
-cd "`dirname "$0"`"
+SCRIPT_PATH=$0
+if [ -h $SCRIPT_PATH ]; then
+	SCRIPT_PATH=$(dirname $SCRIPT_PATH)/$(readlink $SCRIPT_PATH)
+fi
+
+cd $(dirname $SCRIPT_PATH)
 
 if [ ! -f ../../../Binaries/DotNET/GitDependencies.exe ]; then
 	echo "Cannot find GitDependencies.exe. This script should be placed in Engine/Build/BatchFiles/Mac."
 	exit 1
 fi 
-
-echo from `pwd`
 
 source SetupMono.sh "`pwd`"
 
