8f42ee804bdd44a26e76c909c4f3a6fc5b90bbdc
Ben Marsh <Ben.Marsh@epicgames.com>
2014-11-12 17:56:02 -0500

    [INTEGRATE] Change 2357044 by Jaroslaw.Surowiec@jaroslaw.surowiec on 2014/11/12 09:43:00
    	UnrealEd - UE-4499 - UDN: UE4.5 crash after running test (UE4Editor-Core.dll!TStaticIndirectArrayThreadSafeRead)
    
    	#codereview Robert.Manuszewski
    
    [CL 2357637 by Ben Marsh in 4.6 branch]

--- Engine/Source/Editor/UnrealEd/Private/FileHelpers.cpp
+++ Engine/Source/Editor/UnrealEd/Private/FileHelpers.cpp
@@ -2518,6 +2518,11 @@ bool FEditorFileUtils::SaveDirtyPackages(const bool bPromptUserToSave, const boo
 		*bOutPackagesNeededSaving = false;
 	}
 
+	if( bSaveContentPackages )
+	{
+		CollectGarbage(GARBAGE_COLLECTION_KEEPFLAGS);
+	}
+
 	// A list of all packages that need to be saved
 	TArray<UPackage*> PackagesToSave;
 
@@ -2526,11 +2531,9 @@ bool FEditorFileUtils::SaveDirtyPackages(const bool bPromptUserToSave, const boo
 		GetDirtyWorldPackages(PackagesToSave);
 	}
 
-	// Don't iterate through content packages if we dont plan on saving them
+	// Don't iterate through content packages if we don't plan on saving them
 	if( bSaveContentPackages )
 	{
-		CollectGarbage(GARBAGE_COLLECTION_KEEPFLAGS);
-
 		GetDirtyContentPackages(PackagesToSave);
 	}
 
