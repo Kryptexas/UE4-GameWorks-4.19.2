96938b301555a15343b8c9b73520123217335b55
Benn Gallagher <Benn.Gallagher@epicgames.com>
2014-11-13 06:21:53 -0500

    Fix for time being incorrect when pasting a linkable montage element in Persona.
    [CL 2358313 by Benn Gallagher in Main branch]

--- Engine/Source/Runtime/Engine/Classes/Animation/AnimLinkableElement.h
+++ Engine/Source/Runtime/Engine/Classes/Animation/AnimLinkableElement.h
@@ -105,6 +105,9 @@ struct FAnimLinkableElement
 	 */
 	ENGINE_API void SetSegmentIndex(int32 NewSegmentIndex) {SegmentIndex = NewSegmentIndex;}
 
+	/** Relinks this element if it is outside of its segments */
+	ENGINE_API void ConditionalRelink();
+
 protected:
 
 	/** Gets the segment in the current montage in the current slot that is at the time of this element */
@@ -150,9 +153,6 @@ protected:
 	UAnimSequenceBase* LinkedSequence;
 
 private:
-
-	/** Relinks this element if it is outside of its segments */
-	void ConditionalRelink();
 
 	/** Gets a time from an absolute LinkValue
 	 * @param NewLinkMethod The type of time to return
