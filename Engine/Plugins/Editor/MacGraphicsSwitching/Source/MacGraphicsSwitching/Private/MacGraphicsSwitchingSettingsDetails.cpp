0e24fd667a22c414205b456d0a373c1c4f2d238e
James Golding <james.golding@epicgames.com>
2014-11-14 07:05:33 -0500

    Add include to fix non-unity Mac
    [CL 2359890 by James Golding in Main branch]

--- Engine/Plugins/Editor/MacGraphicsSwitching/Source/MacGraphicsSwitching/Private/MacGraphicsSwitchingSettingsDetails.cpp
+++ Engine/Plugins/Editor/MacGraphicsSwitching/Source/MacGraphicsSwitching/Private/MacGraphicsSwitchingSettingsDetails.cpp
@@ -1,6 +1,7 @@
 // Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
 
 #include "MacGraphicsSwitchingModule.h"
+#include "MacGraphicsSwitchingSettings.h"
 #include "MacGraphicsSwitchingSettingsDetails.h"
 #include "MacGraphicsSwitchingWidget.h"
 
