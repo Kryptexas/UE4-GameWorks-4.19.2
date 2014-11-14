8e49ba141b6a9f0e26e30d521a3ad3637613805f
Matthew Griffin <Matthew.Griffin@epicgames.com>
2014-11-14 04:20:34 -0500

    Adding ability to set amount of loops on looping node
    [CL 2359801 by Matthew Griffin in Main branch]

--- Engine/Source/Runtime/Engine/Classes/Sound/SoundNodeLooping.h
+++ Engine/Source/Runtime/Engine/Classes/Sound/SoundNodeLooping.h
@@ -13,6 +13,14 @@ class USoundNodeLooping : public USoundNode
 {
 	GENERATED_UCLASS_BODY()
 
+	/* The amount of times to loop */
+	UPROPERTY(EditAnywhere, Category = Looping, meta = (ClampMin = 1))
+	int32 LoopCount;
+
+	/* If enabled, the node will continue to loop indefinitely regardless of the Loop Count value. */
+	UPROPERTY(EditAnywhere, Category = Looping)
+	uint32 bLoopIndefinitely : 1;
+
 public:	
 	// Begin USoundNode interface. 
 	virtual bool NotifyWaveInstanceFinished( struct FWaveInstance* WaveInstance ) override;
