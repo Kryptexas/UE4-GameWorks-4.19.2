d47f4f8afb3087ccb65c66d17030dc0bdb8f23ee
David Ratti <David.Ratti@epicgames.com>
2014-11-18 13:49:49 -0500

    guard MoveToLocation task against the avatar being destroyed
    [CL 2363668 by David Ratti in Main branch]

--- Engine/Source/Runtime/GameplayAbilities/Private/Abilities/Tasks/AbilityTask_MoveToLocation.cpp
+++ Engine/Source/Runtime/GameplayAbilities/Private/Abilities/Tasks/AbilityTask_MoveToLocation.cpp
@@ -17,7 +17,11 @@ UAbilityTask_MoveToLocation* UAbilityTask_MoveToLocation::MoveToLocation(class U
 {
 	auto MyObj = NewTask<UAbilityTask_MoveToLocation>(WorldContextObject, TaskInstanceName);
 
-	MyObj->StartLocation = MyObj->GetAvatarActor()->GetActorLocation();
+	if (MyObj->GetAvatarActor() != nullptr)
+	{
+		MyObj->StartLocation = MyObj->GetAvatarActor()->GetActorLocation();
+	}
+
 	MyObj->TargetLocation = Location;
 	MyObj->DurationOfMovement = FMath::Max(Duration, 0.001f);		// Avoid negative or divide-by-zero cases
 	MyObj->TimeMoveStarted = MyObj->GetWorld()->GetTimeSeconds();
@@ -88,6 +92,11 @@ void UAbilityTask_MoveToLocation::TickTask(float DeltaTime)
 
 			MyActor->SetActorLocation(FMath::Lerp<FVector, float>(StartLocation, TargetLocation, MoveFraction));
 		}
+	}
+	else
+	{
+		bIsFinished = true;
+		EndTask();
 	}
 }
 
