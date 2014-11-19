2d288e76ef0ded16b0c263a8aad841b25404c0ca
Ben Marsh <Ben.Marsh@epicgames.com>
2014-11-19 10:10:45 -0500

    Fix description in blueprint documentation changelists.
    [CL 2364908 by Ben Marsh in Main branch]

--- Engine/Source/Programs/AutomationTool/Scripts/BuildDocumentation.Automation.cs
+++ Engine/Source/Programs/AutomationTool/Scripts/BuildDocumentation.Automation.cs
@@ -114,7 +114,7 @@ class BlueprintDocumentationNode : DocumentationNode
 	public override void DoBuild(GUBP bp)
 	{
 		ExecuteApiDocTool("-rebuildblueprint", "APIDocTool-Blueprint");
-		SubmitOutputs("Code documentation", "Engine/Documentation/Builds/BlueprintAPI-*", "Engine/Documentation/CHM/BlueprintAPI.chm");
+		SubmitOutputs("Blueprint documentation", "Engine/Documentation/Builds/BlueprintAPI-*", "Engine/Documentation/CHM/BlueprintAPI.chm");
 		base.DoBuild(bp);
 	}
 
