c1637141f65678dd6823bb20752a898422e3faa4
Nick Penwarden <Nick.Penwarden@epicgames.com>
2014-11-20 16:26:14 -0500

    MikkTSpace third party library, in preparation for using it to compute tangents for meshes
    [CL 2366833 by Nick Penwarden in Main branch]

---
+++ Engine/Source/ThirdParty/MikkTSpace/MikkTSpace.build.cs
@@ -0,0 +1,25 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+using UnrealBuildTool;
+
+public class MikkTSpace : ModuleRules
+{
+	public MikkTSpace(TargetInfo Target)
+	{
+		Type = ModuleType.External;
+
+		string MikkTSpacePath = UEBuildConfiguration.UEThirdPartySourceDirectory + "MikkTSpace/";
+
+		PublicIncludePaths.Add(MikkTSpacePath + "inc/");
+
+		if (Target.Platform == UnrealTargetPlatform.Win64)
+		{
+			PublicLibraryPaths.Add(MikkTSpacePath + "lib/x64/");
+			PublicAdditionalLibraries.Add("MikkTSpace.lib");
+		}
+		else if (Target.Platform == UnrealTargetPlatform.Win32)
+		{
+			PublicLibraryPaths.Add(MikkTSpacePath + "lib/x86/");
+			PublicAdditionalLibraries.Add("MikkTSpace.lib");
+		}
+	}
+}
