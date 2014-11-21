6fc5e6bdaea19bc810d162edd7f0794d1fa3cd94
David Ratti <David.Ratti@epicgames.com>
2014-11-21 17:07:04 -0500

    Make  GetAbilitySystemComponentFromActor NOT look through component list by default for an ASC
    [CL 2368013 by David Ratti in Main branch]

--- Engine/Source/Runtime/GameplayAbilities/Public/AbilitySystemGlobals.h
+++ Engine/Source/Runtime/GameplayAbilities/Public/AbilitySystemGlobals.h
@@ -73,7 +73,7 @@ class GAMEPLAYABILITIES_API UAbilitySystemGlobals : public UObject
 
 	static UAbilitySystemGlobals& Get();
 
-	static UAbilitySystemComponent* GetAbilitySystemComponentFromActor(AActor* Actor, bool LookForComponent=true);
+	static UAbilitySystemComponent* GetAbilitySystemComponentFromActor(AActor* Actor, bool LookForComponent=false);
 
 	/** Should allocate a project specific AbilityActorInfo struct. Caller is responsible for deallocation */
 	virtual FGameplayAbilityActorInfo * AllocAbilityActorInfo() const;
