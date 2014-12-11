d9e37213a0b02f312a6acb9be0ffeb7a827f4dd9
Jeff Farris <jeff.farris@epicgames.com>
2014-12-11 14:19:53 -0500

    [UE-3017] Updated header comments for DamageType
    [CL 2385873 by Jeff Farris in Main branch]

--- Engine/Source/Runtime/Engine/Classes/GameFramework/DamageType.h
+++ Engine/Source/Runtime/Engine/Classes/GameFramework/DamageType.h
@@ -1,27 +1,33 @@
 // Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
 
-/**
- * DamageType, the base class of all damage types.
- * These are never instanced, just used as static code and data holders.
- */
-
 #pragma once
 #include "DamageType.generated.h"
 
+/**
+ * A DamageType is intended to define and describe a particular form of damage and to provide an avenue 
+ * for customizing responses to damage from various sources.
+ *
+ * For example, a game could make a DamageType_Fire set it up to ignite the damaged actor.
+ *
+ * DamageTypes are never instanced and should be treated as immutable data holders with static code
+ * functionality.  They should never be stateful.
+ */
 UCLASS(MinimalAPI, const, Blueprintable, BlueprintType)
 class UDamageType : public UObject
 {
 	GENERATED_UCLASS_BODY()
 
+	/** True if this damagetype is caused by the world (falling off level, into lava, etc). */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=DamageType)
-	uint32 bCausedByWorld:1;    //this damage was caused by the world (falling off level, into lava, etc)
+	uint32 bCausedByWorld:1;
 
-	/** Whether to scale imparted momentum by the receiving pawn's mass for pawns using character movement */
+	/** True to scale imparted momentum by the receiving pawn's mass for pawns using character movement */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=DamageType)
 	uint32 bScaleMomentumByMass:1;
 
+	/** The magnitude of impulse to apply to the Actors damaged by this type. */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=RigidBody)
-	float DamageImpulse;    // magnitude of impulse applied to  AActor  due to this damage type.
+	float DamageImpulse;
 
 	/** When applying radial impulses, whether to treat as impulse or velocity change. */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=RigidBody)
@@ -38,7 +44,6 @@ class UDamageType : public UObject
 	/** Damage fall-off for radius damage (exponent).  Default 1.0=linear, 2.0=square of distance, etc. */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=DamageType)
 	float DamageFalloff;
-
 };
 
 
