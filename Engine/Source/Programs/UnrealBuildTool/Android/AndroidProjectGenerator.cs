40649b813d19cfc6a030fa8a875c50f5eb5ac731
Josh Adams <josh.adams@epicgames.com>
2014-12-11 15:29:40 -0500

    - Missed this file in the ES31 checkin
    [CL 2386006 by Josh Adams in Main branch]

--- Engine/Source/Programs/UnrealBuildTool/Android/AndroidProjectGenerator.cs
+++ Engine/Source/Programs/UnrealBuildTool/Android/AndroidProjectGenerator.cs
@@ -189,7 +189,7 @@ namespace UnrealBuildTool
 			IntermediateDirectoryPath = Path.GetDirectoryName(NMakeOutputPath) + "/../../Intermediate/Android/APK";
 
 			// string for <OverrideAPKPath>
-			string APKPath = Path.ChangeExtension(NMakeOutputPath, ".apk");
+			string APKPath = Path.ChangeExtension(NMakeOutputPath, "-armv7-es2.apk");
 
 			// string for <BuildXmlPath> and <AndroidManifestPath>
 			string BuildXmlPath = IntermediateDirectoryPath;
