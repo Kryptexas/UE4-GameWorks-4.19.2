f668e941791cb40c206e8aa57ace30fd8b6dac90
Joe Conley <Joe.Conley@epicgamesjapan.com>
2014-12-17 02:55:20 -0500

    Translation Editor: Keep track of open translation editors and prevent the accidental simultaneous edit of the same archive in multiple editors.
    [CL 2390882 by Joe Conley in Main branch]

--- Engine/Source/Editor/TranslationEditor/Public/ITranslationEditor.h
+++ Engine/Source/Editor/TranslationEditor/Public/ITranslationEditor.h
@@ -13,12 +13,32 @@ class ITranslationEditor : public FAssetEditorToolkit
 
 public:
 
-	static void OpenTranslationEditor(const FString& ManifestFile, const FString& ArchiveFile)
-	{
-		FTranslationEditorModule& TranslationEditorModule = FModuleManager::LoadModuleChecked<FTranslationEditorModule>( "TranslationEditor" );
-		TSharedRef< FTranslationEditor > NewTranslationEditor = TranslationEditorModule.CreateTranslationEditor(ManifestFile, ArchiveFile);
-	}
+	TRANSLATIONEDITOR_API static void OpenTranslationEditor(const FString& InManifestFile, const FString& InArchiveFile);
 
+	ITranslationEditor(const FString& InManifestFile, const FString& InArchiveFile)
+		: ManifestFilePath(InManifestFile)
+		, ArchiveFilePath(InArchiveFile)
+	{}
+
+	virtual bool OnRequestClose() override;
+
+protected:
+
+	/** Name of the project we are translating for */
+	FString ManifestFilePath;
+	/** Name of the language we are translating to */
+	FString ArchiveFilePath;
+
+private:
+
+	/** To keep track of what translation editors are open editing which manifest files */
+	static TMap<FString, ITranslationEditor*> OpenTranslationEditors;
+	
+	/** Called on open to add this to our list of open translation editors */
+	void RegisterTranslationEditor();
+	
+	/** Called on close to remove this from our list of open translation editors */
+	void UnregisterTranslationEditor();
 };
 
 
