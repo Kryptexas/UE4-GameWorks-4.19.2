deb1e570f14ce341cee6a61bfb331084ddbb0733
Ben Marsh <Ben.Marsh@epicgames.com>
2015-01-06 14:51:58 -0500

    [INTEGRATE] Change 2398869 by Mike.Beach@Mike.Beach_UE4 on 2015/01/06 13:28:05
    	[UE-7169] Fixing it so actor-bound event nodes reference the full generated class (instead of the skel class).
    
    [CL 2398990 by Ben Marsh in 4.7 branch]

--- Engine/Source/Editor/BlueprintGraph/Private/K2Node_ActorBoundEvent.cpp
+++ Engine/Source/Editor/BlueprintGraph/Private/K2Node_ActorBoundEvent.cpp
@@ -67,8 +67,10 @@ void UK2Node_ActorBoundEvent::DestroyNode()
 				Delegate.BindUFunction(LSA, CustomFunctionName);
 				
 				// Attempt to remove it from the target's MC delegate
-				FMulticastScriptDelegate* TargetDelegate = GetTargetDelegate();
-				TargetDelegate->Remove(Delegate);
+				if (FMulticastScriptDelegate* TargetDelegate = GetTargetDelegate())
+				{
+					TargetDelegate->Remove(Delegate);
+				}				
 			}
 		}
 		
@@ -138,7 +140,7 @@ void UK2Node_ActorBoundEvent::InitializeActorBoundEventParams(AActor* InEventOwn
 		EventOwner = InEventOwner;
 		DelegatePropertyName = InDelegateProperty->GetFName();
 		EventSignatureName = InDelegateProperty->SignatureFunction->GetFName();
-		EventSignatureClass = CastChecked<UClass>(InDelegateProperty->SignatureFunction->GetOuter());
+		EventSignatureClass = CastChecked<UClass>(InDelegateProperty->SignatureFunction->GetOuter())->GetAuthoritativeClass();
 		CustomFunctionName = FName( *FString::Printf(TEXT("BndEvt__%s_%s_%s"), *InEventOwner->GetName(), *GetName(), *EventSignatureName.ToString()) );
 		bOverrideFunction = false;
 		bInternalEvent = true;
