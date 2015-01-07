a26e6f97530ccf42f13a6a24514db19ce70fb09d
Ben Marsh <Ben.Marsh@epicgames.com>
2015-01-07 10:06:27 -0500

    [INTEGRATE] Change 2398590 by Simon.Tovey@Pitbull-STovey on 2015/01/06 05:46:09
    	Fix for UE-7108
    
    [CL 2399811 by Ben Marsh in 4.7 branch]

--- Engine/Source/Runtime/Engine/Private/Slate/SceneViewport.cpp
+++ Engine/Source/Runtime/Engine/Private/Slate/SceneViewport.cpp
@@ -1136,10 +1136,14 @@ void FSceneViewport::EnqueueBeginRenderFrame()
 		// Get the viewport for this window from the renderer so we can render directly to the backbuffer
 		TSharedPtr<FSlateRenderer> Renderer = FSlateApplication::Get().GetRenderer();
 		FWidgetPath WidgetPath;
-		void* ViewportResource = Renderer->GetViewportResource( *FSlateApplication::Get().FindWidgetWindow( ViewportWidget.Pin().ToSharedRef(), WidgetPath ) );
-		if( ViewportResource )
+		auto WidgetWindow = FSlateApplication::Get().FindWidgetWindow(ViewportWidget.Pin().ToSharedRef(), WidgetPath);
+		if (WidgetWindow.IsValid())
 		{
-			ViewportRHI = *((FViewportRHIRef*)ViewportResource);
+			void* ViewportResource = Renderer->GetViewportResource(*WidgetWindow);
+			if (ViewportResource)
+			{
+				ViewportRHI = *((FViewportRHIRef*)ViewportResource);
+			}
 		}
 	}
 
