fad9767bcbaa468d394369f9eca72f7ab2647ef8
Brad Angelcyk <Brad.Angelcyk@epicgames.com>
2015-01-07 10:19:26 -0500

    sets a target platform if HostPlatform is null
    [CL 2399841 by Brad Angelcyk in Main branch]

--- Engine/Source/Programs/AutomationTool/ProjectParams.cs
+++ Engine/Source/Programs/AutomationTool/ProjectParams.cs
@@ -419,7 +419,26 @@ namespace AutomationTool
             {
                 this.ClientDependentPlatformMap = ClientDependentPlatformMap;
             }
-			this.ClientTargetPlatforms = SetupTargetPlatforms(ref this.ClientDependentPlatformMap, Command, ClientTargetPlatforms, new ParamList<UnrealTargetPlatform>() {HostPlatform.Current.HostEditorPlatform}, true, "TargetPlatform", "Platform");
+
+			UnrealTargetPlatform TargetPlatform;
+			try
+			{
+				TargetPlatform = HostPlatform.Current.HostEditorPlatform;
+			}
+			catch
+			{
+				if (this.ClientDependentPlatformMap != null)
+				{
+					// if a platform map is provided, assume key of first element is the target platform
+					TargetPlatform = this.ClientDependentPlatformMap.Keys.First();
+				}
+				else
+				{
+					// should never get here
+					throw new Exception("A host platform is not defined.");
+				}
+			}
+			this.ClientTargetPlatforms = SetupTargetPlatforms(ref this.ClientDependentPlatformMap, Command, ClientTargetPlatforms, new ParamList<UnrealTargetPlatform>() { TargetPlatform }, true, "TargetPlatform", "Platform");
 
             // Parse command line params for server platforms "-ServerTargetPlatform=Win64+Mac", "-ServerPlatform=Win64+Mac". "-Win64" etc is not allowed here
             if (ServerDependentPlatformMap != null)
