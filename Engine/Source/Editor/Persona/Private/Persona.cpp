7a7d678c442f1da6b1fe50d277ba86d8fee51c76
Martin Wilson <Martin.Wilson@epicgames.com>
2015-01-09 13:23:30 -0500

    Temporary fix for issue with attached assets not showing in Persona
    [CL 2402108 by Martin Wilson in Main branch]

--- Engine/Source/Editor/Persona/Private/Persona.cpp
+++ Engine/Source/Editor/Persona/Private/Persona.cpp
@@ -190,6 +190,10 @@ FPersona::FPersona()
 	OnPropertyChangedHandleDelegateHandle = FCoreUObjectDelegates::OnObjectPropertyChanged.Add(OnPropertyChangedHandle);
 
 	GEditor->OnBlueprintPreCompile().AddRaw(this, &FPersona::OnBlueprintPreCompile);
+
+	//Temporary fix for missing attached assets - MDW
+	PreviewScene.GetWorld()->GetWorldSettings()->SetIsTemporarilyHiddenInEditor(false);
+
 }
 
 FPersona::~FPersona()
