9ed0bcbefce2d97098d5fe515560639f5fbe9aa9
Richard TalbotWatkin <epic@richtech.es>
2015-01-13 10:32:02 -0500

    Fix for 'stat' commands so that they don't incorrectly report as unrecognized. #jira UE-7377 - console "stat" commands function properly but report command as not recognized
    [CL 2404694 by Richard TalbotWatkin in Main branch]

--- Engine/Source/Runtime/Core/Private/Stats/StatsCommand.cpp
+++ Engine/Source/Runtime/Core/Private/Stats/StatsCommand.cpp
@@ -1426,12 +1426,12 @@ bool DirectStatsCommand(const TCHAR* Cmd, bool bBlockForCompletion /*= false*/, 
 	{
 		FString AddArgs;
 		const TCHAR* TempCmd = Cmd;
-		bResult = true;
 
 		FString ArgNoWhitespaces = FDefaultValueHelper::RemoveWhitespaces(TempCmd);
 		const bool bIsEmpty = ArgNoWhitespaces.IsEmpty();
 #if STATS
-		if( bIsEmpty && Ar )
+		bResult = true;
+		if (bIsEmpty && Ar)
 		{
 			PrintStatsHelpToOutputDevice( *Ar );
 		}
@@ -1500,10 +1500,22 @@ bool DirectStatsCommand(const TCHAR* Cmd, bool bBlockForCompletion /*= false*/, 
 		{
 		}
 		else
-#endif
 		{
-			bResult = false;
+			FString Param;
+			if (FParse::Token(TempCmd, Param, false))
+			{
+				const FName MaybeGroupFName = FName(*(FString(TEXT("STATGROUP_")) + Param));
+				FStatsThreadState& Stats = FStatsThreadState::GetLocalState();
+				bResult = Stats.Groups.Contains(MaybeGroupFName);
+			}
+			else
+			{
+				bResult = false;
+			}
 		}
+#else
+		bResult = false;
+#endif
 
 		check(IsInGameThread());
 		if( !bIsEmpty )
