f10b1a659c4f1327acd969e3125ce46a16bf09e1
Ben Marsh <Ben.Marsh@epicgames.com>
2015-01-13 17:30:51 -0500

    [INTEGRATE] Change 2403225 by Thomas.Sarkanen@Pitbull-TSarkanen on 2015/01/12 04:36:22
    	Fixed stalls in tutorials
    
    	API break: added a new pure virtual to IAssetEditorInstance - InvokeTab().
    
    	Tutorials were attempting to load a bogus asset for editing for each stage, causing stalls each time the user clicked Next.
    	Re-arranged code that attempted to open asset editor tabs so that it wasnt attempting ot open a bogus asset (used the correct member).
    	Order of tab manager preference is now Per-stage Blueprint asset editor->Per-'tutorial asset' editor->level editor.
    	Added new function to allow us to open tabs in asset editors. This avoids a dangerous up-cast to FAssetEditorToolkit to access its tab manager.
    
    	#codereview Bruce.Nesbit
    
    [CL 2405258 by Ben Marsh in 4.7 branch]

--- Engine/Source/Editor/UnrealEd/Private/Toolkits/AssetEditorToolkit.cpp
+++ Engine/Source/Editor/UnrealEd/Private/Toolkits/AssetEditorToolkit.cpp
@@ -318,6 +318,10 @@ bool FAssetEditorToolkit::CloseWindow()
 	return true;
 }
 
+void FAssetEditorToolkit::InvokeTab(const FTabId& TabId)
+{
+	GetTabManager()->InvokeTab(TabId);
+}
 
 TSharedPtr< IToolkitHost > FAssetEditorToolkit::GetPreviousWorldCentricToolkitHost()
 {
