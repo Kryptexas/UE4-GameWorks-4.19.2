211d419a4f798a1bcccdd8a3e84a8b51c308b110
Lukasz Furman <Lukasz.Furman@epicgames.com>
2015-01-14 04:09:08 -0500

    merged fixes for behavior tree decorator relevancy issues
    [CL 2405757 by Lukasz Furman in Main branch]

--- Engine/Source/Runtime/AIModule/Private/BehaviorTree/BehaviorTreeComponent.cpp
+++ Engine/Source/Runtime/AIModule/Private/BehaviorTree/BehaviorTreeComponent.cpp
@@ -958,10 +958,11 @@ void UBehaviorTreeComponent::ProcessExecutionRequest()
 		// additional operations for restarting:
 		if (!ExecutionRequest.bTryNextChild)
 		{
-			// mark all decorators less important than current search parent for removal
-			const int32 ParentIndexWithServices = ExecutionRequest.ExecuteNode->GetChildExecutionIndex(0, EBTChildIndex::FirstNode) - 1;
-			FBTNodeIndex SearchParentIdx(ExecutionRequest.ExecuteInstanceIdx, ParentIndexWithServices);
-			UnregisterAuxNodesUpTo(SearchParentIdx);
+			// mark all decorators less important than current search start node for removal
+			// (all from first node on branch with search start)
+			const int32 StartNodeIdx = ExecutionRequest.ExecuteNode->GetBranchExecutionIndex(ExecutionRequest.SearchStart.ExecutionIndex) - 1;
+			const FBTNodeIndex DeactivateIdx(ExecutionRequest.ExecuteInstanceIdx, FMath::Max(StartNodeIdx, 0));
+			UnregisterAuxNodesUpTo(DeactivateIdx);
 
 			// reactivate top search node, so it could use search range correctly
 			BT_SEARCHLOG(SearchData, Verbose, TEXT("Reactivate node: %s [restart]"), *UBehaviorTreeTypes::DescribeNodeHelper(TestNode));
