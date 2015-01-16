516d4b7783f8fa120ece9571aba2a8565822824a
Keith Judge <Keith.Judge@epicgames.com>
2015-01-16 11:19:22 -0500

    Fix crash in Xbox One Suspend()/Resume() handling. - Suspend/resume is thread safe (so long as we're not issuing rendering commands), so we don't need the RHIFlush which will only run on the render thread. Changed the macro we use to expose the suspend/resume functions. - Moved Xbox One specific suspend/resume handling code out of the generic EngineLoop.
    [CL 2408917 by Keith Judge in Main branch]

--- Engine/Source/Runtime/Launch/Private/LaunchEngineLoop.cpp
+++ Engine/Source/Runtime/Launch/Private/LaunchEngineLoop.cpp
@@ -2383,42 +2383,6 @@ void FEngineLoop::ClearPendingCleanupObjects()
 	PendingCleanupObjects = NULL;
 }
 
-#if PLATFORM_XBOXONE
-
-void FEngineLoop::OnResuming(_In_ Platform::Object^ Sender, _In_ Platform::Object^ Args)
-{
-	// Make the call down to the RHI to Resume the GPU state
-	RHIResumeRendering();
-
-	// Notify application of resume
-	FCoreDelegates::ApplicationHasEnteredForegroundDelegate.Broadcast();
-}
-
-void FEngineLoop::OnSuspending(_In_ Platform::Object^ Sender, _In_ Windows::ApplicationModel::SuspendingEventArgs^ Args)
-{
-	// Get the Suspending Event
-	Windows::ApplicationModel::SuspendingDeferral^ SuspendingEvent = Args->SuspendingOperation->GetDeferral();
-	
-	// Notify application of suspend. Application should kick off an async save at this point.
-	FCoreDelegates::ApplicationWillEnterBackgroundDelegate.Broadcast();
-
-	// Flush the RenderingThread
-	FlushRenderingCommands();
-
-	// Make the call down to the RHI to Suspend the GPU state
-	RHISuspendRendering();
-
-	// @TODO Wait for async save to complete
-	// Flush the log so it's all written to disk
-	GLog->FlushThreadedLogs();
-	GLog->Flush();
-
-	// Tell the callback that we are done
-	SuspendingEvent->Complete();
-}
-
-#endif // PLATFORM_XBOXONE
-
 #endif // WITH_ENGINE
 
 
--- Engine/Source/Runtime/RHI/Public/RHIMethods.h
+++ Engine/Source/Runtime/RHI/Public/RHIMethods.h
@@ -2066,11 +2066,11 @@ DEFINE_RHIMETHOD_0(
 	);
 
 // Operations to suspend title rendering and yield control to the system
-DEFINE_RHIMETHOD_GLOBAL_0(
+DEFINE_RHIMETHOD_GLOBALTHREADSAFE_0(
 	void,SuspendRendering,
 	,
 	);
-DEFINE_RHIMETHOD_GLOBAL_0(
+DEFINE_RHIMETHOD_GLOBALTHREADSAFE_0(
 	void,ResumeRendering,
 	,
 	);
