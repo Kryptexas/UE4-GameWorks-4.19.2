8f843074df0d91f9b3b4a99adcccebac8da0d531
Mike Fricker <mike.fricker@epicgames.com>
2015-01-16 16:50:50 -0500

    [INTEGRATE] Change 2404695 by bruce.nesbit@Pitbull-BNesbit on 2015/01/13 10:35:31
    	Code to support 'has feature pack' flag in templatedefs file
    	Also added code to display an icon on templates that have an equivalent feature pack.
    	Revised new project text to indicated what the icon is for.
    
    [CL 2409572 by Mike Fricker in 4.7 branch]

--- Engine/Source/Editor/GameProjectGeneration/Classes/TemplateProjectDefs.h
+++ Engine/Source/Editor/GameProjectGeneration/Classes/TemplateProjectDefs.h
@@ -96,6 +96,8 @@ class UTemplateProjectDefs : public UObject
 	UPROPERTY(config)
 	FString AssetTypes;
 
+	UPROPERTY(config)
+	bool bHasFeaturePack;
 
 	/** Fixes up all strings in this definitions object to replace \%TEMPLATENAME\% with the supplied template name and \%PROJECTNAME\% with the supplied project name */
 	void FixupStrings(const FString& TemplateName, const FString& ProjectName);
