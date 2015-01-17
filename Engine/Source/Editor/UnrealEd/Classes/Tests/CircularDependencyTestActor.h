1234528b2b2760a51d0e46d3cfb48d571572b183
Mike Fricker <mike.fricker@epicgames.com>
2015-01-17 15:23:08 -0500

    [INTEGRATE] Change 2406520 by Mike.Beach@Mike.Beach_UE4 on 2015/01/14 17:53:51
    	[UE-7296] Circular dependency test assets
    
    [CL 2410135 by Mike Fricker in 4.7 branch]

---
+++ Engine/Source/Editor/UnrealEd/Classes/Tests/CircularDependencyTestActor.h
@@ -0,0 +1,55 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Gameframework/Actor.h"
+#include "CircularDependencyTestActor.generated.h"
+
+UENUM(BlueprintType)
+namespace ETestResult
+{
+	enum Type
+	{
+		Unknown,
+		Failed,
+		Succeeded
+	};
+}
+
+UCLASS(Abstract, Blueprintable, BlueprintType, meta=(ChildCanTick))
+class ACircularDependencyTestActor : public AActor
+{
+	GENERATED_UCLASS_BODY()
+public:
+
+	UFUNCTION(BlueprintCallable, Category="Development|Tests")
+	bool SetTestState(ETestResult::Type NewState);
+
+	UFUNCTION(BlueprintImplementableEvent, Category="Development|Tests")
+	void VisualizeNewTestState(ETestResult::Type OldState, ETestResult::Type NewState);
+
+	UFUNCTION(BlueprintCallable, Category="Development|Tests")
+	bool TestVerifyClass(bool bCheckPropertyType = true);
+
+	UFUNCTION(BlueprintCallable, Category="Development|Tests")
+	bool TestVerifyStructMember(UScriptStruct* Struct, bool bCheckPropertyType = true);
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="Development|Tests")
+	bool RunVerificationTests();
+
+	UFUNCTION(BlueprintCallable, Category="Development|Tests")
+	bool TestVerifyIsBlueprintTypeVar(FName VarName, bool bCheckPropertyType);
+
+	UFUNCTION(BlueprintCallable, Category="Development|Tests")
+	void ForceResetFailure();
+
+	UFUNCTION(BlueprintCallable, Category="Development|Tests")
+	bool CheckObjectValIsBlueprint(UObject* BPObject);
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category="Development|Tests")
+	TEnumAsByte<ETestResult::Type> TestState;
+
+private: 
+	bool TestVerifyProperty(UProperty* Property, bool bCheckPropertyType);
+};
