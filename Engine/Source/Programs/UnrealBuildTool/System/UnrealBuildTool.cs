a34f579c384713e4b2e22aa6279fc92c6085f1f4
Matthew Griffin <Matthew.Griffin@epicgames.com>
2015-01-20 09:00:31 -0500

    [INTEGRATE] Change 2411813 by Ankit.Khare@Ankit-PhosphorWHome on 2015/01/20 00:14:31
    	UE-7304: Force UBT to return success back to calling process for both compilation succeeded (existing behvaiour) and target up-to-date (newly added)
    
    	#codereview Robert.Manuszewski
    
    [CL 2412111 by Matthew Griffin in 4.7 branch]

--- Engine/Source/Programs/UnrealBuildTool/System/UnrealBuildTool.cs
+++ Engine/Source/Programs/UnrealBuildTool/System/UnrealBuildTool.cs
@@ -1233,7 +1233,9 @@ namespace UnrealBuildTool
             // Print some performance info
             Log.TraceVerbose("Execution time: {0}", (DateTime.UtcNow - StartTime - MutexWaitTime).TotalSeconds);
 
-            return (int) Result;
+            int RetVal = (Result == ECompilationResult.Succeeded || Result == ECompilationResult.UpToDate) ? 0 : (int)Result;
+
+            return RetVal; 
         }
 
 
