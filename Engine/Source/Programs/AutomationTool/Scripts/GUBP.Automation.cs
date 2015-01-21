f82c4f2884531b64e034843d311b6fb05bf57a8b
Kellan Carr <Kellan.Carr@epicgames.com>
2015-01-21 10:19:23 -0500

    build script, comment out depended on by to save some ec resources until real fix can be figured out
    [CL 2413782 by Kellan Carr in Main branch]

--- Engine/Source/Programs/AutomationTool/Scripts/GUBP.Automation.cs
+++ Engine/Source/Programs/AutomationTool/Scripts/GUBP.Automation.cs
@@ -6517,10 +6517,10 @@ public class GUBP : BuildCommand
             {
                 ECProps.Add(string.Format("SortKey/{0}={1}", NodePair.Key, NodePair.Value));
             }
-			foreach (var NodePair in FullNodeDependedOnBy)
-			{
-				ECProps.Add(string.Format("DependedOnBy/{0}={1}", NodePair.Key, NodePair.Value));
-			}
+			//foreach (var NodePair in FullNodeDependedOnBy)
+			//{
+			//	ECProps.Add(string.Format("DependedOnBy/{0}={1}", NodePair.Key, NodePair.Value));
+			//}
 			foreach (var NodePair in FullNodeDependentPromotions)
 			{
 				ECProps.Add(string.Format("DependentPromotions/{0}={1}", NodePair.Key, NodePair.Value));
