4ba1f6425d683cc35de869544f8e86b66e751288
Mike Fricker <mike.fricker@epicgames.com>
2015-01-24 11:24:03 -0500

    Fixed SCS tree displaying Actor FName instead of Actor Label at root of tree - I also made this change a few days ago but somehow it got clobbered (CL 2412172) #codereview matt.kuhlenschmidt, chris.gagnon
    [CL 2417809 by Mike Fricker in 4.7 branch]

--- Engine/Source/Editor/Kismet/Private/SSCSEditor.cpp
+++ Engine/Source/Editor/Kismet/Private/SSCSEditor.cpp
@@ -2550,7 +2550,7 @@ FText SSCS_RowWidget_ActorRoot::GetActorDisplayText() const
 			}
 			else
 			{
-				DefaultActor->GetName(Name);
+				Name = DefaultActor->GetActorLabel();
 				return FText::Format(LOCTEXT("DefaultActor_Name", "{0}"), FText::FromString(Name));
 			}
 		}
