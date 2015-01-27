724efb7754ac15d16c9e3996c9d17b6905e44c7e
Ben Marsh <Ben.Marsh@epicgames.com>
2015-01-26 19:12:40 -0500

    [INTEGRATE] Change 2416423 by Matt.Kuhlenschmidt@matt_kuhlenschmidt_remote_releases on 2015/01/22 23:43:30
    	Changed category for Actor tags.  Made it an advanced setting
    
    [CL 2419584 by Ben Marsh in Main branch]

--- Engine/Source/Runtime/Engine/Classes/GameFramework/Actor.h
+++ Engine/Source/Runtime/Engine/Classes/GameFramework/Actor.h
@@ -466,7 +466,7 @@ public:
 	uint32 bEnableAutoLODGeneration:1;
 
 	/** Array of tags that can be used for grouping and categorizing. */
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Tags)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, AdvancedDisplay, Category=Actor)
 	TArray<FName> Tags;
 
 	/** Bitflag to represent which views this actor is hidden in, via per-view layer visibility. */
