cb20d4a0d3651f58a1b69a4fc767547060956727
Matt Kuhlenschmidt <matt.kuhlenschmidt@epicgames.com>
2015-01-27 09:56:06 -0500

    Move SActorDetails to another file
    [CL 2420317 by Matt Kuhlenschmidt in 4.7 branch]

---
+++ Engine/Source/Editor/LevelEditor/Private/SActorDetails.h
@@ -0,0 +1,42 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+/**
+ * Wraps a details panel customized for viewing actors
+ */
+class SActorDetails : public SCompoundWidget, public FEditorUndoClient
+{
+public:
+	SLATE_BEGIN_ARGS(SActorDetails){}
+	SLATE_END_ARGS()
+
+	void Construct(const FArguments& InArgs, const FName TabIdentifier);
+	~SActorDetails();
+
+	/**
+	 * Sets the objects to be viewed by the details panel
+	 *
+	 * @param InObjects	The objects to set
+	 */
+	void SetObjects(const TArray<UObject*>& InObjects);
+
+	/** FEditorUndoClient Interface */
+	virtual void PostUndo(bool bSuccess) override;
+	virtual void PostRedo(bool bSuccess) override;
+
+private:
+	void OnEditorSelectionChanged(UObject* Object);
+	AActor* GetSelectedActor() const;
+	void OnSCSEditorRootSelected(AActor* Actor);
+	void OnSCSEditorTreeViewSelectionChanged(const TArray<TSharedPtr<class FSCSEditorTreeNode> >& SelectedNodes);
+	void UpdateComponentTreeFromEditorSelection();
+
+private:
+	TSharedPtr<SSplitter> DetailsSplitter;
+	TSharedPtr<class IDetailsView> DetailsView;
+	TSharedPtr<SBox> ComponentsBox;
+	TSharedPtr<class SSCSEditor> SCSEditor;
+
+	bool bSelectionGuard;
+};
