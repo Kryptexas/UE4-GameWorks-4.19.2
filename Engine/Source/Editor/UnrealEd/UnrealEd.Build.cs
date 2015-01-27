ea75a365f50473d174d26b279888d1cb3c04ea1a
Ben Marsh <Ben.Marsh@epicgames.com>
2015-01-27 08:55:35 -0500

    Remove the dependency on Simplygon from UnrealEd. Not sure why it's here (I suspect legacy reasons), but it doesn't seem to be necessary. SimplygonMeshReduction is the only module which actually needs it.
    [CL 2420228 by Ben Marsh in Main branch]

--- Engine/Source/Editor/UnrealEd/UnrealEd.Build.cs
+++ Engine/Source/Editor/UnrealEd/UnrealEd.Build.cs
@@ -249,13 +249,6 @@ public class UnrealEd : ModuleRules
 
 		SetupModulePhysXAPEXSupport(Target);
 
-		if ((UEBuildConfiguration.bCompileSimplygon == true) &&
-			(Directory.Exists(UEBuildConfiguration.UEThirdPartySourceDirectory + "NotForLicensees") == true) &&
-			(Directory.Exists(UEBuildConfiguration.UEThirdPartySourceDirectory + "NotForLicensees/Simplygon") == true))
-		{
-			AddThirdPartyPrivateStaticDependencies(Target, "Simplygon");
-		}
-
 		if (UEBuildConfiguration.bCompileRecast)
 		{
             PrivateDependencyModuleNames.Add("Navmesh");
