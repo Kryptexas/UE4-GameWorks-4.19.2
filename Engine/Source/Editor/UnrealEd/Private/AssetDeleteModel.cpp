9b3b5106b07878fbe79280d187278015404a556e
Phillip Kavan <pkavan@phosphorgames.com>
2015-01-28 14:23:11 -0500

    Delete Blueprint Class instances when users force delete the source asset.
    #jira UE-7247 - Editor locks on PIE after deleting BP with billboard and another component
    
    change summary:
    - alert and clear transaction buffer on delete when instances are present
    - modified ForceDeleteObjects() to find and destroy BPGC instances (routing through standard editor code paths for Actor-based instances to ensure that references are cleared, panels are updated, etc.)
    
    [CL 2422593 by Phillip Kavan in 4.7 branch]

--- Engine/Source/Editor/UnrealEd/Private/AssetDeleteModel.cpp
+++ Engine/Source/Editor/UnrealEd/Private/AssetDeleteModel.cpp
@@ -537,6 +537,36 @@ void FPendingDelete::CheckForReferences()
 	const int32 NonUndoReferenceCount = MemoryReferences.ExternalReferences.Num() + MemoryReferences.InternalReferences.Num();
 
 	bIsReferencedInMemoryByUndo = TotalReferenceCount > NonUndoReferenceCount;
+
+	// If the object itself isn't in the transaction buffer, check to see if it's a Blueprint asset. We might have instances of the
+	// Blueprint in the transaction buffer, in which case we also want to both alert the user and clear it prior to deleting the asset.
+	if ( !bIsReferencedInMemoryByUndo )
+	{
+		UBlueprint* Blueprint = Cast<UBlueprint>( Object );
+		if ( Blueprint && Blueprint->GeneratedClass )
+		{
+			TArray<FReferencerInformation> ExternalMemoryReferences = MemoryReferences.ExternalReferences;
+			for ( auto RefIt = ExternalMemoryReferences.CreateIterator(); RefIt && !bIsReferencedInMemoryByUndo; ++RefIt )
+			{
+				FReferencerInformation& RefInfo = *RefIt;
+				if ( RefInfo.Referencer->IsA( Blueprint->GeneratedClass ) )
+				{
+					if ( IsReferenced( RefInfo.Referencer, GARBAGE_COLLECTION_KEEPFLAGS, true, &ReferencesIncludingUndo ) )
+					{
+						GEditor->Trans->DisableObjectSerialization();
+
+						FReferencerInformationList ReferencesExcludingUndo;
+						if ( IsReferenced( RefInfo.Referencer, GARBAGE_COLLECTION_KEEPFLAGS, true, &ReferencesExcludingUndo ) )
+						{
+							bIsReferencedInMemoryByUndo = ( ReferencesIncludingUndo.InternalReferences.Num() + ReferencesIncludingUndo.ExternalReferences.Num() ) > ( ReferencesExcludingUndo.InternalReferences.Num() + ReferencesExcludingUndo.ExternalReferences.Num() );
+						}
+
+						GEditor->Trans->EnableObjectSerialization();
+					}
+				}
+			}
+		}
+	}
 }
 
 bool FPendingDelete::operator == ( const FPendingDelete& Other ) const
