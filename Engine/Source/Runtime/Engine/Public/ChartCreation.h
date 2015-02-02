c16c439ad429145974fa732f4303ea6ce76be579
Jaroslaw Palczynski <Jaroslaw.Palczynski@epicgames.com>
2015-02-02 07:24:35 -0500

    UECORE-64: Add ENGINE_API to FPS Chart global variables
    [CL 2428002 by Jaroslaw Palczynski in Main branch]

--- Engine/Source/Runtime/Engine/Public/ChartCreation.h
+++ Engine/Source/Runtime/Engine/Public/ChartCreation.h
@@ -75,11 +75,13 @@ enum FPSChartStats
 	STAT_FPSChart_TotalUnitGame,
 	STAT_FPSChart_TotalUnitGPU,
 };
-/** Start time of current FPS chart.										*/
-extern double			GFPSChartStartTime;
-/** FPS chart information. Time spent for each bucket and count.			*/
-extern FFPSChartEntry	GFPSChart[STAT_FPSChartLastBucketStat - STAT_FPSChartFirstStat];
 
+/** Start time of current FPS chart.										*/
+extern ENGINE_API double GFPSChartStartTime;
+/** FPS chart information. Time spent for each bucket and count.			*/
+extern ENGINE_API FFPSChartEntry GFPSChart[STAT_FPSChartLastBucketStat - STAT_FPSChartFirstStat];
+/** The total GPU time taken to render all frames. In seconds.				*/
+extern ENGINE_API double GTotalGPUTime;
 
 /** Helper structure for hitch entries. */
 struct FHitchChartEntry
