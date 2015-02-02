06eddc7b35aaad44e3e82ac28e27d0fc3c211371
Steve Robb <Steve.Robb@epicgames.com>
2015-02-02 07:27:47 -0500

    GitHub 777 - More informative error message.
    Integrated.
    
    #codereview robert.manuszewski
    
    [CL 2428006 by Steve Robb in Main branch]

--- Engine/Source/Runtime/CoreUObject/Private/UObject/LinkerLoad.cpp
+++ Engine/Source/Runtime/CoreUObject/Private/UObject/LinkerLoad.cpp
@@ -2138,7 +2138,7 @@ bool ULinkerLoad::VerifyImportInner(const int32 ImportIndex, FString& WarningSuf
 	// Find or load the linker load that contains the FObjectExport for this import
 	if (Import.OuterIndex.IsNull() && Import.ClassName!=NAME_Package )
 	{
-		UE_LOG(LogLinker, Warning, TEXT("%s has an inappropriate outermost, it was probably saved with a deprecated outer."), *Import.ObjectName.ToString());
+		UE_LOG(LogLinker, Warning, TEXT("%s has an inappropriate outermost, it was probably saved with a deprecated outer (file: %s)"), *Import.ObjectName.ToString(), *Filename);
 		Import.SourceLinker = NULL;
 		return false;
 	}
