f3681152d64bf0a6e7e25ecbac6fac8b003a5b79
Matthew Griffin <Matthew.Griffin@epicgames.com>
2015-02-03 09:13:55 -0500

    [INTEGRATE] Change 2428754 by Peter.Sauerbrei@peter.sauerbrei_T2782 on 2015/02/02 17:06:34
    	fix for change in iTunes Mobile Device location with latest iTunes
    	UE-8861
    	#ios
    
    [CL 2429848 by Matthew Griffin in 4.7 branch]

--- Engine/Source/Developer/iOS/IOSTargetPlatform/Private/IOSTargetPlatform.cpp
+++ Engine/Source/Developer/iOS/IOSTargetPlatform/Private/IOSTargetPlatform.cpp
@@ -90,8 +90,11 @@ bool FIOSTargetPlatform::IsSdkInstalled(bool bProjectHasCode, FString& OutTutori
 		unsigned long pathSize = 256;
 		if (lRes != ERROR_SUCCESS || RegQueryValueEx(hKey, TEXT("iTunesMobileDeviceDLL"), 0, NULL, (BYTE*)dllPath, &pathSize) != ERROR_SUCCESS || IFileManager::Get().FileSize(*FString(dllPath)) == INDEX_NONE)
 		{
-			OutTutorialPath = FString("/Engine/Tutorial/Mobile/InstallingiTunesTutorial.InstallingiTunesTutorial");
-			biOSSDKInstalled = false;
+			if (RegQueryValueEx(hKey, TEXT("MobileDeviceDLL"), 0, NULL, (BYTE*)dllPath, &pathSize) != ERROR_SUCCESS || IFileManager::Get().FileSize(*FString(dllPath)) == INDEX_NONE)
+			{
+				OutTutorialPath = FString("/Engine/Tutorial/Mobile/InstallingiTunesTutorial.InstallingiTunesTutorial");
+				biOSSDKInstalled = false;
+			}
 		}
 	}
 
