153709495bce428f239c8c2909da3f8c4c62b87f
Nick Darnell <Nick.Darnell@epicgames.com>
2015-02-04 12:03:13 -0500

    Editor - Improving the SDropTarget Editor Widget to be more customizable.
    [CL 2431888 by Nick Darnell in 4.7 branch]

--- Engine/Source/Editor/EditorWidgets/Public/SDropTarget.h
+++ Engine/Source/Editor/EditorWidgets/Public/SDropTarget.h
@@ -14,9 +14,30 @@ public:
 	DECLARE_DELEGATE_RetVal_OneParam(bool, FVerifyDrag, TSharedPtr<FDragDropOperation>);
 
 	SLATE_BEGIN_ARGS(SDropTarget)
+		: _ValidColor(FLinearColor(0, 1, 0, 1))
+		, _InvalidColor(FLinearColor(1, 0, 0, 1))
+		, _BackgroundColor(FLinearColor(1, 1, 1, 0.50f))
+		, _BackgroundColorHover(FLinearColor(1, 1, 1, 0.25f))
+		, _VerticalImage(FEditorStyle::GetBrush("WideDash.Vertical"))
+		, _HorizontalImage(FEditorStyle::GetBrush("WideDash.Horizontal"))
+		, _BackgroundImage(FEditorStyle::GetBrush("WhiteBrush"))
 	{ }
 		/* Content to display for the in the drop target */
 		SLATE_DEFAULT_SLOT( FArguments, Content )
+		/** The color of the vertical/horizontal images when the drop data is valid */
+		SLATE_ARGUMENT(FLinearColor, ValidColor)
+		/** The color of the vertical/horizontal images when the drop data is not valid */
+		SLATE_ARGUMENT(FLinearColor, InvalidColor)
+		/** The color multiplier that is applied to the background image. */
+		SLATE_ARGUMENT(FLinearColor, BackgroundColor)
+		/** The color multiplier that is applied to the background image on hover. */
+		SLATE_ARGUMENT(FLinearColor, BackgroundColorHover)
+		/** Vertical border image that is used. */
+		SLATE_ARGUMENT(const FSlateBrush*, VerticalImage)
+		/** Horizontal border image that is used. */
+		SLATE_ARGUMENT(const FSlateBrush*, HorizontalImage)
+		/** The background image that is applied after the surface. */
+		SLATE_ARGUMENT(const FSlateBrush*, BackgroundImage)
 		/** Called when a valid asset is dropped */
 		SLATE_EVENT(FOnDrop, OnDrop)
 		/** Called to check if an asset is acceptable for dropping */
@@ -35,8 +56,6 @@ protected:
 	virtual bool OnIsRecognized(TSharedPtr<FDragDropOperation> DragDropOperation) const;
 
 protected:
-	FSlateColor GetDropBorderColor() const;
-
 	// SWidget interface
 	virtual FReply OnDragOver( const FGeometry& MyGeometry, const FDragDropEvent& DragDropEvent ) override;
 	virtual FReply OnDrop( const FGeometry& MyGeometry, const FDragDropEvent& DragDropEvent ) override;
@@ -45,7 +64,7 @@ protected:
 	virtual int32 OnPaint(const FPaintArgs& Args, const FGeometry& AllottedGeometry, const FSlateRect& MyClippingRect, FSlateWindowElementList& OutDrawElements, int32 LayerId, const FWidgetStyle& InWidgetStyle, bool bParentEnabled) const override;
 	// End of SWidget interface
 
-	/** @return Visibility of the overlay text when dragging is occuring. */
+	/** @return Visibility of the overlay text when dragging is occurring. */
 	EVisibility GetDragOverlayVisibility() const;
 
 	/** Get the brightness on the background. */
@@ -58,6 +77,21 @@ private:
 	FVerifyDrag AllowDropEvent;
 	/** Delegate to call to check validity of the asset */
 	FVerifyDrag IsRecognizedEvent;
+
+	/** The color of the vertical/horizontal images when the drop data is valid */
+	FLinearColor ValidColor;
+	/** The color of the vertical/horizontal images when the drop data is not valid */
+	FLinearColor InvalidColor;
+	/** The color multiplier that is applied to the background image. */
+	FLinearColor BackgroundColor;
+	/** The color multiplier that is applied to the background image on hover. */
+	FLinearColor BackgroundColorHover;
+	/** The background image that is applied after the surface. */
+	const FSlateBrush* BackgroundImage;
+	/** Vertical border image that is used. */
+	const FSlateBrush* VerticalImage;
+	/** Horizontal border image that is used. */
+	const FSlateBrush* HorizontalImage;
 
 	/** Whether or not we are being dragged over by a recognized event*/
 	mutable bool bIsDragEventRecognized;
