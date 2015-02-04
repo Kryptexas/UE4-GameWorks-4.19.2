b90c50c1b0d27e8c379231894917c0a637ddbeb6
Mike Beach <Mike.Beach@epicgames.com>
2015-02-04 14:09:06 -0500

    [UE-9004] Making sure blueprint component classes are fully regenerated before we instantiate templates (so we don't have to re-instance on load, in cyclic component scenarios).
    #codereview Robert.Manuszewski, Nick.Whiting
    
    [CL 2432091 by Mike Beach in 4.7 branch]

--- Engine/Source/Runtime/CoreUObject/Public/UObject/UnrealType.h
+++ Engine/Source/Runtime/CoreUObject/Public/UObject/UnrealType.h
@@ -1802,10 +1802,7 @@ class COREUOBJECT_API UObjectProperty : public TUObjectPropertyBase<UObject*>
 	{
 		return GetPropertyValue(PropertyValueAddress);
 	}
-	virtual void SetObjectPropertyValue(void* PropertyValueAddress, UObject* Value) const override
-	{
-		SetPropertyValue(PropertyValueAddress, Value);
-	}
+	virtual void SetObjectPropertyValue(void* PropertyValueAddress, UObject* Value) const override;
 	// End of UObjectPropertyBase interface
 };
 
