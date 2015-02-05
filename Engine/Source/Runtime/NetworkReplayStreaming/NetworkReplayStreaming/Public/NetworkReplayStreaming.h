ec9faeab6c6c7ceaa77783088ed664d3cd59a7d8
John Pollard <John.Pollard@epicgames.com>
2015-02-05 14:20:28 -0500

    Initial network replay streaming interface
    [CL 2433975 by John Pollard in Main branch]

---
+++ Engine/Source/Runtime/NetworkReplayStreaming/NetworkReplayStreaming/Public/NetworkReplayStreaming.h
@@ -0,0 +1,40 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+// Dependencies.
+#include "ModuleManager.h"
+#include "Core.h"
+
+DECLARE_MULTICAST_DELEGATE_TwoParams( FOnStreamReady, bool, bool );
+typedef FOnStreamReady::FDelegate FOnStreamReadyDelegate;
+
+/** Generic interface for network replay streaming */
+class INetworkReplayStreamer 
+{
+public:
+	virtual ~INetworkReplayStreamer() {}
+
+	virtual void StartStreaming( FString& StreamName, bool bRecord, const FOnStreamReadyDelegate& Delegate ) = 0;
+	virtual void StopStreaming() = 0;
+	virtual FArchive* GetStreamingArchive() = 0;
+};
+
+/** Replay streamer factory */
+class INetworkReplayStreamingFactory : public IModuleInterface
+{
+public:
+	virtual TSharedPtr< INetworkReplayStreamer > CreateReplayStreamer() = 0;
+};
+
+/** Replay streaming factory manager */
+class FNetworkReplayStreaming : public IModuleInterface
+{
+public:
+	static inline FNetworkReplayStreaming& Get()
+	{
+		return FModuleManager::LoadModuleChecked< FNetworkReplayStreaming >( "NetworkReplayStreaming" );
+	}
+
+	virtual INetworkReplayStreamingFactory& GetFactory();
+};
