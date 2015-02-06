5205ff7c69c7ec5d1b472c8e6a1b5ef3f240f0ce
Ori Cohen <Ori.Cohen@epicgames.com>
2015-02-06 15:04:31 -0500

    Remove physx 2012 from automated build
    [CL 2435863 by Ori Cohen in Main branch]

--- Engine/Source/ThirdParty/PhysX/UE4_BuildThirdPartyLib.bat
+++ Engine/Source/ThirdParty/PhysX/UE4_BuildThirdPartyLib.bat
@@ -7,7 +7,7 @@ set PhysXRoot=..\NotForLicensees\PhysX
 
 REM Go to the branch
 pushd %PhysXRoot%
-call UE4_BuildThirdPartyLib.bat %THIRD_PARTY_CHANGELIST% -submit -config debug -config profile -platformPhysx vc11win32;Win32 -platformPhysx vc11win64;x64 -platformPhysx vc12win32;Win32 -platformPhysx vc12win64;x64 -platformAPEX vc11win32-PhysX_3.3;Win32 -platformAPEX vc11win64-PhysX_3.3;x64 -platformAPEX vc12win32-PhysX_3.3;Win32 -platformAPEX vc12win64-PhysX_3.3;x64 -platformAPEX
+call UE4_BuildThirdPartyLib.bat %THIRD_PARTY_CHANGELIST% -submit -config debug -config profile -platformPhysx vc12win32;Win32 -platformPhysx vc12win64;x64 -platformAPEX vc12win32-PhysX_3.3;Win32 -platformAPEX vc12win64-PhysX_3.3;x64 -platformAPEX
 popd
 
 endlocal
