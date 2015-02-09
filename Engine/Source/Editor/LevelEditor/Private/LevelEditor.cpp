0c1b0829361b5b1892c494d7fdff90a64b6eefbb
Dan Hertzka <Dan.Hertzka@epicgames.com>
2015-02-09 12:20:44 -0500

    [UE-8712] - Right-clicking a component in the world brings up a context menu with basic options - Fixed issue where duplicated components retained incorrect AttachChildren
    [CL 2438318 by Dan Hertzka in 4.7 branch]

--- Engine/Source/Editor/LevelEditor/Private/LevelEditor.cpp
+++ Engine/Source/Editor/LevelEditor/Private/LevelEditor.cpp
@@ -635,7 +635,7 @@ void FLevelEditorModule::BindGlobalLevelEditorCommands()
 		);
 
 	ActionList.MapAction( 
-		Commands.SnapCameraToActor, 
+		Commands.SnapCameraToObject,
 		FExecuteAction::CreateStatic( &FLevelEditorActionCallbacks::ExecuteExecCommand, FString( TEXT("CAMERA SNAP") ) )
 		);
 
