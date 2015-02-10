1e05f65908807fe5024bcf46500a65f105d062d1
Marc Audy <Marc.Audy@epicgames.com>
2015-02-10 15:11:31 -0500

    Properties of BP created non-Scene components save across reconstruction Primitive components will mark render state dirty if registered after instance data applied Instanced static mesh will save properties and attachment across reconstruction even if there is no static lighting or selected instances
    [CL 2440263 by Marc Audy in 4.7 branch]

--- Engine/Source/Runtime/Engine/Private/SceneComponent.cpp
+++ Engine/Source/Runtime/Engine/Private/SceneComponent.cpp
@@ -1212,6 +1212,11 @@ FComponentInstanceDataBase* USceneComponent::GetComponentInstanceData() const
 		}
 	}
 
+	if (InstanceData == nullptr)
+	{
+		InstanceData = Super::GetComponentInstanceData();
+	}
+
 	return InstanceData;
 }
 
