0efffe04b77d361833ab79569a0ea1041a7b6e49
Michael Noland <michael.noland@epicgames.com>
2015-02-10 13:29:52 -0500

    Blueprints: Allow any Actor or Component subclass to tick by default - Added a new project setting bCanBlueprintsTickByDefault, which defaults to true - Added a new class metadata flag ChildCannotTick, which can be used to prevent Blueprint subclasses from ticking - Improved the warning messages when ticking isn't possible
    - Blueprint subclasses are now allowed to tick as long as any of the following are true of the most derived C++ parent class:
      - C++ parent has bCanEverTick = true on their primary tick function
      - C++ parent is AActor or UActorComponent
      - C++ parent does not have ChildCannotTick metadata and bCanBlueprintsTickByDefault is true
      - C++ parent does not have ChildCannotTick metadata but does have ChildCanTick metadata
    
    Upgrade note: If you are carefully managing which Blueprints are allowed to tick (e.g., for dedicated server performance), you will probably want to set bCanBlueprintsTickByDefault to false for your project and explicitly opt in for ticking via ChildCanTick on specific cosmetic classes.
    [UE-9179]
    
    [CL 2440043 by Michael Noland in 4.7 branch]

--- Engine/Source/Runtime/Engine/Private/GameEngine.cpp
+++ Engine/Source/Runtime/Engine/Private/GameEngine.cpp
@@ -387,6 +387,8 @@ UEngine::UEngine(const FObjectInitializer& ObjectInitializer)
 
 	BeginStreamingPauseDelegate = NULL;
 	EndStreamingPauseDelegate = NULL;
+
+	bCanBlueprintsTickByDefault = true;
 }
 
 void UGameEngine::Init(IEngineLoop* InEngineLoop)
