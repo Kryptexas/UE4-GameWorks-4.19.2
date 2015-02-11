b25675ed96f6359bc2deabb53f88af58b4695135
Jaroslaw Surowiec <Jaroslaw.Surowiec@epicgames.com>
2015-02-11 08:30:15 -0500

    CrashReport - Helper queries tweaks
    [CL 2441463 by Jaroslaw Surowiec in Main branch]

--- Engine/Source/Programs/CrashReporter/CrashReportWebSite/Models/Query-GetTodayCrashes.sql
+++ Engine/Source/Programs/CrashReporter/CrashReportWebSite/Models/Query-GetTodayCrashes.sql
@@ -1,22 +1,9 @@
-﻿SELECT [Id]
-      --,[Summary]
-      ,[Status]
-      --,[ChangeListVersion]
-      --,[EngineMode]
-      ,[RawCallStack]
-      --,[CommandLine]
-      --,[Selected]
-      --,[LanguageExt]
-      --,[BuildVersion]
-      --,[Version]
-      ,[TTPID]
-      --,[Processed]
-      --,[HasMiniDumpFile]
-      --,[HasDiagnosticsFile]
-      --,[Branch]
-      --,[UserNameId]
-      --,[SourceContext]
-      --,[EngineVersion]
+﻿use crashreport
+SELECT *
+from crashes
 where [TimeOfCrash] > CAST(GETDATE() AS DATE) 
 --where [EpicAccountId] is not null and buildversion != '4.5.0.0' and buildversion != '4.5.1.0
 --where buildversion like '4.5.1%' and [ChangeListVersion] != '0' and [PlatformName] not like 'Mac%' and [Branch] like 'UE4-Re%'
+
+
+
