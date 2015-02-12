e9ae7e306721d6a283912695ccff20a3173f88d8
Marc Audy <Marc.Audy@epicgames.com>
2015-02-12 17:20:36 -0500

    [INTEGRATE] Change 2442128 by Marc.Audy@Marc.Audy_Z2487 on 2015/02/11 14:52
    	Ensure that all ActorComponents that belong to a BlueprintGeneratedClass are correctly flagged as RF_Public in the ExportMap
    	Remove now unnecessary code previously trying to get flag set correctly
    
    #lockdown Zachary.EdgertonJones
    
    [CL 2443726 by Marc Audy in Main branch]

--- Engine/Source/Runtime/Engine/Classes/Components/ActorComponent.h
+++ Engine/Source/Runtime/Engine/Classes/Components/ActorComponent.h
@@ -565,7 +565,6 @@ public:
 	 */
 	virtual void ApplyWorldOffset(const FVector& InOffset, bool bWorldShift) {};
 
-	virtual void Serialize(FArchive& Ar) override;
 #if !(UE_BUILD_SHIPPING || UE_BUILD_TEST)
 
 private:
