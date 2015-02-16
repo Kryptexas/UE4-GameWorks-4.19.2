591d9cb6580fc55fba37f5cb46c8be148f58ca52
Zak Middleton <Zak.Middleton@epicgames.com>
2015-02-16 14:40:51 -0500

    #ue4 - Fix Projectile orientation when bRotationFollowsVelocity is set but there is no velocity.
    [CL 2447579 by Zak Middleton in Main branch]

--- Engine/Source/Runtime/Engine/Private/ProjectileMovementComponent.cpp
+++ Engine/Source/Runtime/Engine/Private/ProjectileMovementComponent.cpp
@@ -113,7 +113,7 @@ void UProjectileMovementComponent::TickComponent(float DeltaTime, enum ELevelTic
 	int32 NumBounces = 0;
 	int32 Iterations = 0;
 	FHitResult Hit(1.f);
-
+	
 	while( RemainingTime >= MIN_TICK_TIME && (Iterations < MaxSimulationIterations) && !ActorOwner->IsPendingKill() && UpdatedComponent )
 	{
 		Iterations++;
@@ -126,7 +126,7 @@ void UProjectileMovementComponent::TickComponent(float DeltaTime, enum ELevelTic
 		const FVector OldVelocity = Velocity;
 		FVector MoveDelta = ComputeMoveDelta(OldVelocity, TimeTick);
 
-		const FRotator NewRotation = bRotationFollowsVelocity ? OldVelocity.Rotation() : ActorOwner->GetActorRotation();
+		const FRotator NewRotation = (bRotationFollowsVelocity && !OldVelocity.IsNearlyZero()) ? OldVelocity.Rotation() : ActorOwner->GetActorRotation();
 
 		// Move the component
 		if (bShouldBounce)
