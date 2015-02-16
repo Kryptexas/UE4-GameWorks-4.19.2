4cb633fb8821ff62762b5d6692388673f8e283dc
Matthew Griffin <Matthew.Griffin@epicgames.com>
2015-02-16 10:23:28 -0500

    [INTEGRATE] Change 2438892 by Matt.Kuhlenschmidt@matt_kuhlenschmidt_main on 2015/02/09 17:50:38
    	Fix annoying issue of not being able to move the gizmo afte dragging it, releasing, and not moving the mouse.
    
    [CL 2447125 by Matthew Griffin in Main branch]

--- Engine/Source/Editor/UnrealEd/Private/EditorViewportClient.cpp
+++ Engine/Source/Editor/UnrealEd/Private/EditorViewportClient.cpp
@@ -2041,15 +2041,17 @@ void FEditorViewportClient::StopTracking()
 		Widget->SetCurrentAxis( EAxisList::None );
 		Invalidate( true, true );
 
-	/*	if ( !MouseDeltaTracker->UsingDragTool() )
-		{
-			Widget->SetCurrentAxis( AXIS_None );
-			Invalidate( true, true );
-		}*/
-
 		SetRequiredCursorOverride( false );
 
 		bWidgetAxisControlledByDrag = false;
+
+		// Update the hovered hit proxy here.  If the user didnt move the mouse
+		// they still need to be able to pick up the gizmo without moving the mouse again
+		Viewport->InvalidateHitProxy();
+		HHitProxy* HitProxy = Viewport->GetHitProxy(CachedMouseX,CachedMouseY);
+
+		CheckHoveredHitProxy(HitProxy);
+
 		bIsTracking = false;	
 	}
 
