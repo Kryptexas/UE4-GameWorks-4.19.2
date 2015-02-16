184b3bfab21d24c4424506dba2c4b31e66f1f4de
Matthew Griffin <Matthew.Griffin@epicgames.com>
2015-02-16 09:09:40 -0500

    [INTEGRATE] Change 2438311 by Mike.Fricker@MFRICKER_G3686_Alpha on 2015/02/09 12:12:12
    	Editor startup splash improvements (part 2)
    	- Fixed extra spaces in the task bar app name string
    
    [CL 2446999 by Matthew Griffin in Main branch]

--- Engine/Source/Runtime/Core/Private/Windows/WindowsPlatformSplash.cpp
+++ Engine/Source/Runtime/Core/Private/Windows/WindowsPlatformSplash.cpp
@@ -659,7 +659,7 @@ void FWindowsPlatformSplash::Show()
 					else
 					{
 						VersionInfo = FText::Format( NSLOCTEXT( "UnrealEd", "UnrealEdTitleWithVersion_F", "Unreal Editor {0}  -  {1}" ), Version, GameName );
-						AppName = FText::Format( NSLOCTEXT( "UnrealEd", "UnrealEdTitle_F", "Unreal Editor  -  {0}" ), GameName );
+						AppName = FText::Format( NSLOCTEXT( "UnrealEd", "UnrealEdTitle_F", "Unreal Editor - {0}" ), GameName );
 					}
 
 					StartSetSplashText( SplashTextType::VersionInfo1, *VersionInfo.ToString() );
