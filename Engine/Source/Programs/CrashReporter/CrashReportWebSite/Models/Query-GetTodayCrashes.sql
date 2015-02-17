a244eec81da2ee45a0eb465b853a6d4ab8cb40e0
Jaroslaw Surowiec <Jaroslaw.Surowiec@epicgames.com>
2015-02-17 04:41:57 -0500

    SQL
    [CL 2448427 by Jaroslaw Surowiec in Main branch]

--- Engine/Source/Programs/CrashReporter/CrashReportWebSite/Models/Query-GetTodayCrashes.sql
+++ Engine/Source/Programs/CrashReporter/CrashReportWebSite/Models/Query-GetTodayCrashes.sql
@@ -2,6 +2,7 @@
 SELECT *
 from crashes
 where [TimeOfCrash] > CAST(GETDATE() AS DATE) 
+order by [TimeOfCrash]
 --where [EpicAccountId] is not null and buildversion != '4.5.0.0' and buildversion != '4.5.1.0
 --where buildversion like '4.5.1%' and [ChangeListVersion] != '0' and [PlatformName] not like 'Mac%' and [Branch] like 'UE4-Re%'
 
