33bd8b487910c6f747c25dbc2d9322aff01b3189
Andrew Rodham <Andrew.Rodham@epicgames.com>
2015-02-19 10:18:01 -0500

    Moved construction of FTaskbarList to FWindowsApplication constructor
    Previously this was being created for every unhandled windows message that was routed through WindowsApplication, which occaisonally caused significant stalls waiting for the construction of a new COM interface.
    
    [CL 2451613 by Andrew Rodham in Main branch]

--- Engine/Source/Runtime/Core/Private/Windows/WindowsApplication.cpp
+++ Engine/Source/Runtime/Core/Private/Windows/WindowsApplication.cpp
@@ -78,6 +78,8 @@ FWindowsApplication::FWindowsApplication( const HINSTANCE HInstance, const HICON
 		TextInputMethodSystem.Reset();
 	}
 
+	TaskbarList = FTaskbarList::Create();
+
 	// Get initial display metrics. (display information for existing desktop, before we start changing resolutions)
 	FDisplayMetrics::GetDisplayMetrics(InitialDisplayMetrics);
 
@@ -630,10 +632,6 @@ LRESULT CALLBACK FWindowsApplication::AppWndProc(HWND hwnd, uint32 msg, WPARAM w
 
 int32 FWindowsApplication::ProcessMessage( HWND hwnd, uint32 msg, WPARAM wParam, LPARAM lParam )
 {
-	//The message id for when the taskbar button becoems created.
-	//This is set to s proper id once a WM_CREATE message is received.
-	static DWORD wmTaskbarButtonCreate = (DWORD)-1; 
-
 	TSharedPtr< FWindowsWindow > CurrentNativeEventWindowPtr = FindWindowByHWND( Windows, hwnd );
 
 	if( Windows.Num() && CurrentNativeEventWindowPtr.IsValid() )
@@ -1098,22 +1096,13 @@ int32 FWindowsApplication::ProcessMessage( HWND hwnd, uint32 msg, WPARAM wParam,
 			break;
 		
 		case WM_CREATE:
-			{
-				wmTaskbarButtonCreate = RegisterWindowMessage(TEXT("TaskbarButtonCreated"));
-				return 0;
-			}
-			break;
+			return 0;
 
 		case WM_DEVICECHANGE:
 			{
 				XInput->SetNeedsControllerStateUpdate(); 
 				QueryConnectedMice();
 			}
-		}
-
-		if (wmTaskbarButtonCreate)
-		{
-			TaskbarList = FTaskbarList::Create();
 		}
 	}
 
