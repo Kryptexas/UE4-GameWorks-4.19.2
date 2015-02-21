a104f49dc5cbe65cd0200fb8d46e5afe8dd75c10
Niklas Smedberg <niklas.smedberg@epicgames.com>
2015-02-21 00:56:15 -0500

    Fixed unity compile error
    [CL 2454475 by Niklas Smedberg in Main branch]

--- Engine/Plugins/Runtime/WebBrowserAsPlugin/Source/WebBrowserAsPlugin/Private/WebBrowserAsPluginPrivatePCH.h
+++ Engine/Plugins/Runtime/WebBrowserAsPlugin/Source/WebBrowserAsPlugin/Private/WebBrowserAsPluginPrivatePCH.h
@@ -1,5 +1,7 @@
 // Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
 
+#pragma once
+
 #include "WebBrowserAsPluginModule.h"
 
 DECLARE_LOG_CATEGORY_EXTERN(LogWebBrowserAsPlugin, Verbose, All);
