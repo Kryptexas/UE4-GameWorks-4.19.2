9c3fe2a8a0a3cdddedea8e534183fcb27bfa170f
Chris Babcock <Chris.Babcock@epicgames.com>
2015-02-27 18:11:18 -0500

    Read Android commandline from proper file path #ue4 #android #codereview Daniel.Lamb,Rob.Jones
    [CL 2464765 by Chris Babcock in Main branch]

--- Engine/Source/Runtime/Launch/Private/Android/LaunchAndroid.cpp
+++ Engine/Source/Runtime/Launch/Private/Android/LaunchAndroid.cpp
@@ -175,7 +175,7 @@ static void InitCommandLine()
 	FCommandLine::Set(TEXT(""));
 
 	// read in the command line text file from the sdcard if it exists
-	FString CommandLineFilePath = GFilePathBase + FString("/") + (!FApp::IsGameNameEmpty() ? FApp::GetGameName() : FPlatformProcess::ExecutableName()) + FString("/UE4CommandLine.txt");
+	FString CommandLineFilePath = GFilePathBase + FString("/UE4Game/") + (!FApp::IsGameNameEmpty() ? FApp::GetGameName() : FPlatformProcess::ExecutableName()) + FString("/UE4CommandLine.txt");
 	FILE* CommandLineFile = fopen(TCHAR_TO_UTF8(*CommandLineFilePath), "r");
 	if(CommandLineFile == NULL)
 	{
