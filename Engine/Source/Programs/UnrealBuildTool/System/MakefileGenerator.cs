9b1c48bbc0b0cf98ac51fa0eff0951af1546d149
UnrealBot <unrealbot@users.noreply.github.com>
2015-03-09 15:40:00 -0400

    Snapshot for changelist 2472867.
    [CL 2472867 in Main branch]

--- Engine/Source/Programs/UnrealBuildTool/System/MakefileGenerator.cs
+++ Engine/Source/Programs/UnrealBuildTool/System/MakefileGenerator.cs
@@ -34,6 +34,15 @@ namespace UnrealBuildTool
 	/// </summary>
 	public class MakefileGenerator : ProjectFileGenerator
 	{
+		/// True if intellisense data should be generated (takes a while longer)
+		bool bGenerateIntelliSenseData = false;
+
+		/// True if we should include IntelliSense data in the generated project files when possible
+		override public bool ShouldGenerateIntelliSenseData()
+		{
+			return bGenerateIntelliSenseData;
+		}
+
 		/// File extension for project files we'll be generating (e.g. ".vcxproj")
 		override public string ProjectFileExtension
 		{
