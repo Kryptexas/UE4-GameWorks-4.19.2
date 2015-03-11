5c766fe28fb73b2d20c9f4deb09a735bdbf61472
Maciej Mroz <maciej.mroz@epicgames.com>
2015-03-11 17:43:11 -0400

    UE-11460 A level will not save if a Blueprint that uses a Blueprint Component with an Event Dispatcher is placed in it
    [CL 2475795 by Maciej Mroz in Main branch]

--- Engine/Source/Editor/UnrealEd/Private/Kismet2/KismetReinstanceUtilities.cpp
+++ Engine/Source/Editor/UnrealEd/Private/Kismet2/KismetReinstanceUtilities.cpp
@@ -871,10 +871,15 @@ void FBlueprintCompileReinstancer::ReplaceInstancesOfClass(UClass* OldClass, UCl
 			}
 			else
 			{
+				auto OldFlags = OldObject->GetFlags();
+
 				FName OldName(OldObject->GetFName());
 				OldObject->Rename(NULL, OldObject->GetOuter(), REN_DoNotDirty | REN_DontCreateRedirectors);
 				NewUObject = NewObject<UObject>(OldObject->GetOuter(), NewClass, OldName);
 				check(NewUObject != nullptr);
+
+				auto FlagMask = RF_Public | RF_ArchetypeObject | RF_Transactional | RF_Transient | RF_TextExportTransient | RF_InheritableComponentTemplate; //TODO: what about RF_RootSet and RF_Standalone ?
+				NewUObject->SetFlags(OldFlags & FlagMask);
 
 				UEditorEngine::CopyPropertiesForUnrelatedObjects(OldObject, NewUObject);
 
