68875a982903b9a82decb19cb5b7bb67037eb469
Jeff Farris <jeff.farris@epicgames.com>
2015-03-17 14:24:57 -0400

    When editing a CameraAnim, display the anim object's properties in the details panel by default.
    #codereview matt.kuhlenschmidt
    
    [CL 2481910 by Jeff Farris in Main branch]

--- Engine/Source/Editor/Matinee/Private/MatineePropertyWindow.cpp
+++ Engine/Source/Editor/Matinee/Private/MatineePropertyWindow.cpp
@@ -12,6 +12,18 @@ void FMatinee::BuildPropertyWindow()
 
 	FPropertyEditorModule& PropertyModule = FModuleManager::LoadModuleChecked<FPropertyEditorModule>("PropertyEditor");
 	PropertyWindow = PropertyModule.CreateDetailView( Args );
+
+	if (IsCameraAnim())
+	{
+		// if editing a CameraAnim, show the CameraAnim object properties by default (when no track or group selected)
+		AMatineeActorCameraAnim* const CamAnimMatineeActor = Cast<AMatineeActorCameraAnim>(MatineeActor);
+		if ((CamAnimMatineeActor != nullptr) && (CamAnimMatineeActor->CameraAnim != nullptr))
+		{
+			TArray<UObject*> Objects;
+			Objects.Add(CamAnimMatineeActor->CameraAnim);
+			PropertyWindow->SetObjects(Objects);
+		}
+	}
 }
 
 
@@ -48,6 +60,15 @@ void FMatinee::UpdatePropertyWindow()
 		for (int32 Index=0; Index < SelectedGroups.Num(); Index++)
 		{
 			Objects.Add(SelectedGroups[Index]);
+		}
+	}
+	else if (IsCameraAnim())
+	{
+		// if editing a CameraAnim, show the CameraAnim properties when nothing else selected
+		AMatineeActorCameraAnim* const CamAnimMatineeActor = Cast<AMatineeActorCameraAnim>(MatineeActor);
+		if ((CamAnimMatineeActor != nullptr) && (CamAnimMatineeActor->CameraAnim != nullptr))
+		{
+			Objects.Add(CamAnimMatineeActor->CameraAnim);
 		}
 	}
 	// else send nothing
