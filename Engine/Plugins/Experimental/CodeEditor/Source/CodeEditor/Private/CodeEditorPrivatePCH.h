805319000928a4e10a537a477a97cddc5f3946fa
Thomas Sarkanen <Thomas.Sarkanen@epicgames.com>
2015-03-17 06:36:00 -0400

    Add experimental code editing plugin
    Super early version allowing game code to be viewed, edited, saved and built inside the editor.
    Comes from an Epic Friday project I have been working on.
    
    Lots of stuff TODO:
    
    - Code cleanup.
    - Add ISourceCodeAccessor API so this can be used with the editor's various "Go to source..." options.
    - Add source control integration.
    - Proper error reporting when saving files.
    - Better undo/redo support.
    - Use Clang (or similar) for syntax highlighting.
    - Monitor files/directories properly so we pick up file adds/deletions etc.
    - Line numbers.
    - Variable tab widths.
    - C# syntax highlighting.
    - Ctrl+Tab support.
    - Output log for builds.
    - Expose project settings for editing.
    - <insert your feature here>
    
    [CL 2481374 by Thomas Sarkanen in Main branch]

---
+++ Engine/Plugins/Experimental/CodeEditor/Source/CodeEditor/Private/CodeEditorPrivatePCH.h
@@ -0,0 +1,11 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Core.h"
+#include "CoreUObject.h"
+#include "SlateBasics.h"
+#include "EditorStyle.h"
+#include "PropertyEditing.h"
+#include "UnrealEd.h"
+#include "DirectoryWatcherModule.h"
