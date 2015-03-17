3bca18b7d441f72fb55f3f4d0b7e06f0a70b30c4
Chris Wood <chris.wood@epicgames.com>
2015-03-17 06:34:08 -0400

    #UnrealVS - version number change missed from last checkin
    [CL 2481371 by Chris Wood in Main branch]

--- Engine/Source/Programs/UnrealVS/UnrealVSPackage.cs
+++ Engine/Source/Programs/UnrealVS/UnrealVSPackage.cs
@@ -55,7 +55,7 @@ namespace UnrealVS
 	{
 		/** Constants */
 
-		private const string VersionString = "v1.33";
+		private const string VersionString = "v1.34";
 		private const string UnrealSolutionFileNamePrefix = "UE4";
 		private const string ExtensionName = "UnrealVS";
 		private const string CommandLineOptionKey = ExtensionName + "CommandLineMRU";
