621a5d4ee43506bc2a475a29a2b8ef31c1ab962b
Terence Burns <Terence.Burns@epicgames.com>
2015-03-18 07:32:12 -0400

    Fix for null texture lod settings on certain platforms. Wrong target platform list was processed.
    [CL 2482896 by Terence Burns in Main branch]

--- Engine/Source/Runtime/Engine/Private/DeviceProfiles/DeviceProfileManager.cpp
+++ Engine/Source/Runtime/Engine/Private/DeviceProfiles/DeviceProfileManager.cpp
@@ -254,7 +254,7 @@ void UDeviceProfileManager::LoadProfiles()
 
 		// Register Texture LOD settings with each Target Platform
 		ITargetPlatformManagerModule& TargetPlatformManager = GetTargetPlatformManagerRef();
-		const TArray<ITargetPlatform*>& TargetPlatforms = TargetPlatformManager.GetActiveTargetPlatforms();
+		const TArray<ITargetPlatform*>& TargetPlatforms = TargetPlatformManager.GetTargetPlatforms();
 		for (int32 PlatformIndex = 0; PlatformIndex < TargetPlatforms.Num(); ++PlatformIndex)
 		{
 			ITargetPlatform* Platform = TargetPlatforms[PlatformIndex];
