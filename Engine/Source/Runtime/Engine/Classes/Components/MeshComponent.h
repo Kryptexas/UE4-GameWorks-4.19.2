4631d2a542c03c8885c483d8dbb9e009558d97ec
Matt Kuhlenschmidt <matt.kuhlenschmidt@epicgames.com>
2015-03-20 11:14:23 -0400

    Fix confusion about override materials vs materials list in details panel
    https://jira.ol.epicgames.net/browse/UE-11858
    
    [CL 2486183 by Matt Kuhlenschmidt in Main branch]

--- Engine/Source/Runtime/Engine/Classes/Components/MeshComponent.h
+++ Engine/Source/Runtime/Engine/Classes/Components/MeshComponent.h
@@ -20,7 +20,7 @@ class ENGINE_API UMeshComponent : public UPrimitiveComponent
 	GENERATED_UCLASS_BODY()
 
 	/** Per-Component material overrides.  These must NOT be set directly or a race condition can occur between GC and the rendering thread. */
-	UPROPERTY(EditAnywhere, Category=Rendering, Meta=(ToolTip="Material overrides."))
+	UPROPERTY(EditAnywhere, AdvancedDisplay, Category=Rendering, Meta=(ToolTip="Material overrides."))
 	TArray<class UMaterialInterface*> OverrideMaterials;
 	
 	UFUNCTION(BlueprintCallable, Category="Components|Mesh")
