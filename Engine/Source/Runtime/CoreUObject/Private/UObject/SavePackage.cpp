78988ec8423c7a31af38a2eb82a84d8e11cc9bab
Ben Marsh <Ben.Marsh@epicgames.com>
2015-03-27 12:03:29 -0400

    [INTEGRATE] Change 2492798 by Maciej.Mroz@maciej.mroz_Z3302_4727 on 2015/03/26 18:42:20
    	UE-12106 [CrashReport] Saving after blueprint change causes crash.
    
    	A better handling for null CDO in FExportReferenceSorter::ProcessStruct().
    
    	#codereview Robert.Manuszewski
    
    [CL 2493947 by Ben Marsh in 4.7 branch]

--- Engine/Source/Runtime/CoreUObject/Private/UObject/SavePackage.cpp
+++ Engine/Source/Runtime/CoreUObject/Private/UObject/SavePackage.cpp
@@ -1890,7 +1890,8 @@ public:
 				if ( ClassObject != NULL )
 				{
 					UObject* CDO = ClassObject->GetDefaultObject();
-					if ( ProcessedObjects.Find(CDO) == INDEX_NONE )
+					ensureMsgf(nullptr != CDO, TEXT("Error: Invalid CDO in class %s"), *GetPathNameSafe(ClassObject));
+					if ((ProcessedObjects.Find(CDO) == INDEX_NONE) && (nullptr != CDO))
 					{
 						ProcessedObjects.Add(CDO);
 
