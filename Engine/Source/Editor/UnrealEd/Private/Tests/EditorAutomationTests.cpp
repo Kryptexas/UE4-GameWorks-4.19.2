71c4cac56b5a2806689e3d285c554f3548e0f0c5
Jaroslaw Surowiec <Jaroslaw.Surowiec@epicgames.com>
2015-03-30 06:28:41 -0400

    Editor - Added FRunPIEAndExit automation test
    [CL 2495480 by Jaroslaw Surowiec in Main branch]

--- Engine/Source/Editor/UnrealEd/Private/Tests/EditorAutomationTests.cpp
+++ Engine/Source/Editor/UnrealEd/Private/Tests/EditorAutomationTests.cpp
@@ -2119,3 +2119,27 @@ bool FLaunchOnTest::RunTest(const FString& Parameters)
 
 	return true;
 }
+
+/**
+ * Pie Test
+ * Verification PIE works
+ */
+IMPLEMENT_SIMPLE_AUTOMATION_TEST( FRunPIEAndExitTest, "RunPIEAndExit", EAutomationTestFlags::ATF_Editor )
+
+/** 
+ * Execute the loading of one map to verify PIE works
+ *
+ * @param Parameters - Unused for this test
+ * @return	TRUE if the test was successful, FALSE otherwise
+ */
+ bool FRunPIEAndExitTest::RunTest( const FString& Parameters )
+{
+	ADD_LATENT_AUTOMATION_COMMAND( FStartPIECommand( true ) );
+	ADD_LATENT_AUTOMATION_COMMAND( FWaitLatentCommand( 3.0f ) );
+	ADD_LATENT_AUTOMATION_COMMAND( FEndPlayMapCommand() );
+
+	ADD_LATENT_AUTOMATION_COMMAND( FWaitLatentCommand( 3.0f ) );
+	ADD_LATENT_AUTOMATION_COMMAND( FRequestExitCommand() );
+
+	return true;
+}
