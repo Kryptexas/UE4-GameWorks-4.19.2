866c25c343cedeb0aeee97c965be676b223ec8c9
Chris Wood <chris.wood@epicgames.com>
2015-03-30 07:11:34 -0400

    Changed Close Major tab command's default input chord to Ctrl+F4
    [CL 2495500 by Chris Wood in Main branch]

--- Engine/Source/Runtime/Slate/Private/Framework/Docking/TabCommands.cpp
+++ Engine/Source/Runtime/Slate/Private/Framework/Docking/TabCommands.cpp
@@ -7,7 +7,7 @@
 
 void FTabCommands::RegisterCommands()
 {
-	UI_COMMAND(CloseMajorTab, "Close Major Tab", "Closes the focused major tab", EUserInterfaceActionType::Button, FInputChord(EModifierKey::Control | EModifierKey::Alt, EKeys::F4))
+	UI_COMMAND(CloseMajorTab, "Close Major Tab", "Closes the focused major tab", EUserInterfaceActionType::Button, FInputChord(EModifierKey::Control, EKeys::F4))
 	UI_COMMAND(CloseMinorTab, "Close Minor Tab", "Closes the current window's active minor tab", EUserInterfaceActionType::Button, FInputChord())
 }
 
