6a5bbed419307388f2c325772073ea94abe7f393
Max Preussner <Max.Preussner@epicgames.com>
2015-04-01 17:17:47 -0400

    Messaging: Refactored message tracer to use lambdas instead of delegates
    [CL 2499122 by Max Preussner in Main branch]

--- Engine/Source/Runtime/Messaging/Private/Bus/MessageTracer.cpp
+++ Engine/Source/Runtime/Messaging/Private/Bus/MessageTracer.cpp
@@ -22,6 +22,312 @@ FMessageTracer::~FMessageTracer()
 }
 
 
+/* FMessageTracer interface
+ *****************************************************************************/
+
+void FMessageTracer::TraceAddedInterceptor( const IMessageInterceptorRef& Interceptor, const FName& MessageType )
+{
+	double Timestamp = FPlatformTime::Seconds();
+
+	Traces.Enqueue(
+		[=]() {
+			// @todo gmp: trace added message interceptors
+		}
+	);
+}
+
+
+void FMessageTracer::TraceAddedRecipient( const FMessageAddress& Address, const IReceiveMessagesRef& Recipient )
+{
+	double Timestamp = FPlatformTime::Seconds();
+
+	Traces.Enqueue(
+		[=]() {
+			// create endpoint information
+			FMessageTracerEndpointInfoPtr& EndpointInfo = RecipientsToEndpointInfos.FindOrAdd(Recipient->GetRecipientId());
+
+			if (!EndpointInfo.IsValid())
+			{
+				EndpointInfo = MakeShareable(new FMessageTracerEndpointInfo());
+			}
+
+			// initialize endpoint information
+			FMessageTracerAddressInfoRef AddressInfo = MakeShareable(new FMessageTracerAddressInfo());
+			AddressInfo->Address = Address;
+			AddressInfo->TimeRegistered = Timestamp;
+			AddressInfo->TimeUnregistered = 0;
+
+			EndpointInfo->AddressInfos.Add(Address, AddressInfo);
+			EndpointInfo->Name = Recipient->GetDebugName();
+			EndpointInfo->Remote = Recipient->IsRemote();
+
+			// add to address table
+			AddressesToEndpointInfos.Add(Address, EndpointInfo);
+		}
+	);
+}
+
+
+void FMessageTracer::TraceAddedSubscription( const IMessageSubscriptionRef& Subscription )
+{
+	if (!Running)
+	{
+		return;
+	}
+
+	double Timestamp = FPlatformTime::Seconds();
+
+	Traces.Enqueue(
+		[=]() {
+			// @todo gmp: trace added subscriptions
+		}
+	);
+}
+
+
+void FMessageTracer::TraceDispatchedMessage( const IMessageContextRef& Context, const IReceiveMessagesRef& Recipient, bool Async )
+{
+	if (!Running)
+	{
+		return;
+	}
+
+	double Timestamp = FPlatformTime::Seconds();
+
+	Traces.Enqueue(
+		[=]() {
+			// look up message & endpoint info
+			FMessageTracerMessageInfoPtr MessageInfo = MessageInfos.FindRef(Context);
+
+			if (!MessageInfo.IsValid())
+			{
+				return;
+			}
+
+			FMessageTracerEndpointInfoPtr& EndpointInfo = RecipientsToEndpointInfos.FindOrAdd(Recipient->GetRecipientId());
+
+			if (!EndpointInfo.IsValid())
+			{
+				return;
+			}
+
+			// update message information
+			FMessageTracerDispatchStateRef DispatchState = MakeShareable(new FMessageTracerDispatchState());
+
+			DispatchState->DispatchLatency = Timestamp - MessageInfo->TimeSent;
+			DispatchState->DispatchType = Async ? EMessageTracerDispatchTypes::TaskGraph : EMessageTracerDispatchTypes::Direct;
+			DispatchState->EndpointInfo = EndpointInfo;
+			DispatchState->TimeDispatched = Timestamp;
+			DispatchState->TimeHandled = 0.0;
+
+			MessageInfo->DispatchStates.Add(EndpointInfo, DispatchState);
+
+			// update database
+			EndpointInfo->ReceivedMessages.Add(MessageInfo);
+		}
+	);
+}
+
+
+void FMessageTracer::TraceHandledMessage( const IMessageContextRef& Context, const IReceiveMessagesRef& Recipient )
+{
+	if (!Running)
+	{
+		return;
+	}
+
+	double Timestamp = FPlatformTime::Seconds();
+
+	Traces.Enqueue(
+		[=]() {
+			// look up message & endpoint info
+			FMessageTracerMessageInfoPtr MessageInfo = MessageInfos.FindRef(Context);
+
+			if (!MessageInfo.IsValid())
+			{
+				return;
+			}
+
+			FMessageTracerEndpointInfoPtr EndpointInfo = RecipientsToEndpointInfos.FindRef(Recipient->GetRecipientId());
+
+			if (!EndpointInfo.IsValid())
+			{
+				return;
+			}
+
+			// update message information
+			FMessageTracerDispatchStatePtr DispatchState = MessageInfo->DispatchStates.FindRef(EndpointInfo);
+
+			if (DispatchState.IsValid())
+			{
+				DispatchState->TimeHandled = Timestamp;
+			}
+		}
+	);
+}
+
+
+void FMessageTracer::TraceInterceptedMessage( const IMessageContextRef& Context, const IMessageInterceptorRef& Interceptor )
+{
+	if (!Running)
+	{
+		return;
+	}
+
+	double Timestamp = FPlatformTime::Seconds();
+
+	Traces.Enqueue(
+		[=]() {
+			// @todo gmp: trace intercepted messages
+		}
+	);		
+}
+
+
+void FMessageTracer::TraceRemovedInterceptor( const IMessageInterceptorRef& Interceptor, const FName& MessageType )
+{
+	if (!Running)
+	{
+		return;
+	}
+
+	double Timestamp = FPlatformTime::Seconds();
+
+	Traces.Enqueue(
+		[=]() {
+			// @todo gmp: trace removed message interceptors
+		}
+	);
+}
+
+
+void FMessageTracer::TraceRemovedRecipient( const FMessageAddress& Address )
+{
+	if (!Running)
+	{
+		return;
+	}
+
+	double Timestamp = FPlatformTime::Seconds();
+
+	Traces.Enqueue(
+		[=]() {
+			FMessageTracerEndpointInfoPtr EndpointInfo = AddressesToEndpointInfos.FindRef(Address);
+
+			if (!EndpointInfo.IsValid())
+			{
+				return;
+			}
+
+			// update endpoint information
+			FMessageTracerAddressInfoPtr AddressInfo = EndpointInfo->AddressInfos.FindRef(Address);
+
+			if (AddressInfo.IsValid())
+			{
+				AddressInfo->TimeUnregistered = Timestamp;
+			}
+		}
+	);
+}
+
+
+void FMessageTracer::TraceRemovedSubscription( const IMessageSubscriptionRef& Subscription, const FName& MessageType )
+{
+	if (!Running)
+	{
+		return;
+	}
+
+	double Timestamp = FPlatformTime::Seconds();
+
+	Traces.Enqueue(
+		[=]() {
+			// @todo gmp: trace removed message subscriptions
+		}
+	);
+}
+
+
+void FMessageTracer::TraceRoutedMessage( const IMessageContextRef& Context )
+{
+	if (!Running)
+	{
+		return;
+	}
+
+	double Timestamp = FPlatformTime::Seconds();
+
+	if (ShouldBreak(Context))
+	{
+		Breaking = true;
+		ContinueEvent->Wait();
+	}
+
+	Traces.Enqueue(
+		[=]() {
+			// update message information
+			FMessageTracerMessageInfoPtr MessageInfo = MessageInfos.FindRef(Context);
+
+			if (MessageInfo.IsValid())
+			{
+				MessageInfo->TimeRouted = Timestamp;
+			}
+		}
+	);
+}
+
+
+void FMessageTracer::TraceSentMessage( const IMessageContextRef& Context )
+{
+	if (!Running)
+	{
+		return;
+	}
+
+	double Timestamp = FPlatformTime::Seconds();
+
+	Traces.Enqueue(
+		[=]() {
+			// look up endpoint info
+			FMessageTracerEndpointInfoPtr EndpointInfo = AddressesToEndpointInfos.FindRef(Context->GetSender());
+
+			if (!EndpointInfo.IsValid())
+			{
+				return;
+			}
+
+			// create message info
+			FMessageTracerMessageInfoRef MessageInfo = MakeShareable(new FMessageTracerMessageInfo());
+	
+			MessageInfo->Context = Context;
+			MessageInfo->SenderInfo = EndpointInfo;
+			MessageInfo->TimeRouted = 0.0;
+			MessageInfo->TimeSent = Timestamp;
+			MessageInfos.Add(Context, MessageInfo);
+
+			// add message type
+			FMessageTracerTypeInfoPtr& TypeInfo = MessageTypes.FindOrAdd(Context->GetMessageType());
+
+			if (!TypeInfo.IsValid())
+			{
+				TypeInfo = MakeShareable(new FMessageTracerTypeInfo());
+				TypeInfo->TypeName = Context->GetMessageType();
+
+				TypeAddedDelegate.Broadcast(TypeInfo.ToSharedRef());
+			}
+
+			TypeInfo->Messages.Add(MessageInfo);
+
+			// update database
+			EndpointInfo->SentMessages.Add(MessageInfo);
+			MessageInfo->TypeInfo = TypeInfo;
+
+			MessagesAddedDelegate.Broadcast(MessageInfo);
+		}
+	);
+}
+
+
 /* IMessageTracer interface
  *****************************************************************************/
 
@@ -63,11 +369,11 @@ bool FMessageTracer::Tick( float DeltaTime )
 	}
 
 	// process new traces
-	TraceDelegate Trace;
+	TFunction<void()> Trace;
 
 	while (Traces.Dequeue(Trace))
 	{
-		Trace.Execute();
+		Trace();
 	}
 
 	return true;
@@ -76,183 +382,6 @@ bool FMessageTracer::Tick( float DeltaTime )
 
 /* FMessageTracer implementation
  *****************************************************************************/
-
-void FMessageTracer::ProcessAddedInterceptor( FString Name, FName MessageType, double TimeSeconds )
-{
-	
-}
-
-
-void FMessageTracer::ProcessAddedRecipient( FMessageAddress Address, FRecipientInfo RecipientInfo, double TimeSeconds )
-{
-	// create endpoint information
-	FMessageTracerEndpointInfoPtr& EndpointInfo = RecipientsToEndpointInfos.FindOrAdd(RecipientInfo.Id);
-
-	if (!EndpointInfo.IsValid())
-	{
-		EndpointInfo = MakeShareable(new FMessageTracerEndpointInfo());
-	}
-
-	// initialize endpoint information
-	FMessageTracerAddressInfoRef AddressInfo = MakeShareable(new FMessageTracerAddressInfo());
-	AddressInfo->Address = Address;
-	AddressInfo->TimeRegistered = TimeSeconds;
-	AddressInfo->TimeUnregistered = 0;
-
-	EndpointInfo->AddressInfos.Add(Address, AddressInfo);
-	EndpointInfo->Name = RecipientInfo.Name;
-	EndpointInfo->Remote = RecipientInfo.Remote;
-
-	// add to address table
-	AddressesToEndpointInfos.Add(Address, EndpointInfo);
-}
-
-
-void FMessageTracer::ProcessAddedSubscriptionTrace( double TimeSeconds )
-{
-
-}
-
-
-void FMessageTracer::ProcessDispatchedMessage( IMessageContextRef Context, double TimeSeconds, FGuid RecipientId, bool Async )
-{
-	FMessageTracerMessageInfoPtr MessageInfo = MessageInfos.FindRef(Context);
-
-	if (!MessageInfo.IsValid())
-	{
-		return;
-	}
-
-	FMessageTracerEndpointInfoPtr& EndpointInfo = RecipientsToEndpointInfos.FindOrAdd(RecipientId);
-
-	if (!EndpointInfo.IsValid())
-	{
-		return;
-	}
-
-	// update message information
-	FMessageTracerDispatchStateRef DispatchState = MakeShareable(new FMessageTracerDispatchState());
-
-	DispatchState->DispatchLatency = TimeSeconds - MessageInfo->TimeSent;
-	DispatchState->DispatchType = Async ? EMessageTracerDispatchTypes::TaskGraph : EMessageTracerDispatchTypes::Direct;
-	DispatchState->EndpointInfo = EndpointInfo;
-	DispatchState->TimeDispatched = TimeSeconds;
-	DispatchState->TimeHandled = 0.0;
-
-	MessageInfo->DispatchStates.Add(EndpointInfo, DispatchState);
-
-	// update database
-	EndpointInfo->ReceivedMessages.Add(MessageInfo);
-}
-
-
-void FMessageTracer::ProcessHandledMessage( IMessageContextRef Context, double TimeSeconds, FGuid RecipientId )
-{
-	FMessageTracerMessageInfoPtr MessageInfo = MessageInfos.FindRef(Context);
-
-	if (!MessageInfo.IsValid())
-	{
-		return;
-	}
-
-	FMessageTracerEndpointInfoPtr EndpointInfo = RecipientsToEndpointInfos.FindRef(RecipientId);
-
-	if (!EndpointInfo.IsValid())
-	{
-		return;
-	}
-
-	// update message information
-	FMessageTracerDispatchStatePtr DispatchState = MessageInfo->DispatchStates.FindRef(EndpointInfo);
-
-	if (DispatchState.IsValid())
-	{
-		DispatchState->TimeHandled = TimeSeconds;
-	}
-}
-
-
-void FMessageTracer::ProcessRemovedInterceptor( IMessageInterceptorRef Interceptor, FName MessageType, double TimeSeconds )
-{
-
-}
-
-
-void FMessageTracer::ProcessRemovedRecipient( FMessageAddress Address, double TimeSeconds )
-{
-	FMessageTracerEndpointInfoPtr EndpointInfo = AddressesToEndpointInfos.FindRef(Address);
-
-	if (!EndpointInfo.IsValid())
-	{
-		return;
-	}
-
-	// update endpoint information
-	FMessageTracerAddressInfoPtr AddressInfo = EndpointInfo->AddressInfos.FindRef(Address);
-
-	if (AddressInfo.IsValid())
-	{
-		AddressInfo->TimeUnregistered = TimeSeconds;
-	}
-}
-
-
-void FMessageTracer::ProcessRemovedSubscription( FName MessageType, double TimeSeconds )
-{
-
-}
-
-
-void FMessageTracer::ProcessRoutedMessage( IMessageContextRef Context, double TimeSeconds )
-{
-	// update message information
-	FMessageTracerMessageInfoPtr MessageInfo = MessageInfos.FindRef(Context);
-
-	if (MessageInfo.IsValid())
-	{
-		MessageInfo->TimeRouted = TimeSeconds;
-	}
-}
-
-
-void FMessageTracer::ProcessSentMessage( IMessageContextRef Context, double TimeSeconds )
-{
-	FMessageTracerEndpointInfoPtr EndpointInfo = AddressesToEndpointInfos.FindRef(Context->GetSender());
-
-	if (!EndpointInfo.IsValid())
-	{
-		return;
-	}
-
-	// create message info
-	FMessageTracerMessageInfoRef MessageInfo = MakeShareable(new FMessageTracerMessageInfo());
-	
-	MessageInfo->Context = Context;
-	MessageInfo->SenderInfo = EndpointInfo;
-	MessageInfo->TimeRouted = 0.0;
-	MessageInfo->TimeSent = TimeSeconds;
-	MessageInfos.Add(Context, MessageInfo);
-
-	// add message type
-	FMessageTracerTypeInfoPtr& TypeInfo = MessageTypes.FindOrAdd(Context->GetMessageType());
-
-	if (!TypeInfo.IsValid())
-	{
-		TypeInfo = MakeShareable(new FMessageTracerTypeInfo());
-		TypeInfo->TypeName = Context->GetMessageType();
-
-		TypeAddedDelegate.Broadcast(TypeInfo.ToSharedRef());
-	}
-
-	TypeInfo->Messages.Add(MessageInfo);
-
-	// update database
-	EndpointInfo->SentMessages.Add(MessageInfo);
-	MessageInfo->TypeInfo = TypeInfo;
-
-	MessagesAddedDelegate.Broadcast(MessageInfo);
-}
-
 
 void FMessageTracer::ResetMessages()
 {
