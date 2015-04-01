d0a745cbdc773ea0b2cc50ed610bee84af7852f7
Ben Marsh <Ben.Marsh@epicgames.com>
2015-04-01 08:14:14 -0400

    Add XmlParser as an optional runtime module.
    [CL 2498454 by Ben Marsh in Main branch]

--- Engine/Source/UE4Game.Target.cs
+++ Engine/Source/UE4Game.Target.cs
@@ -58,6 +58,7 @@ public class UE4GameTarget : TargetRules
 		ModuleNames.Add("Launch");
 		ModuleNames.Add("InputDevice");
 		ModuleNames.Add("GameMenuBuilder");
+		ModuleNames.Add("XmlParser");
 		if (Target.Platform == UnrealTargetPlatform.Win32 || Target.Platform == UnrealTargetPlatform.Win64)
 		{
 			ModuleNames.Add("OnlineSubsystemNull");
