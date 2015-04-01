b2fbac454dae1f5c8820a7b6263e7671ff7679cd
Max Preussner <Max.Preussner@epicgames.com>
2015-04-01 18:12:41 -0400

    BP: Fixed non-unity build
    #CodeReview: jeff.farris
    
    [CL 2499229 by Max Preussner in Main branch]

--- Engine/Source/Editor/BlueprintGraph/Classes/K2Node_EnumInequality.h
+++ Engine/Source/Editor/BlueprintGraph/Classes/K2Node_EnumInequality.h
@@ -1,8 +1,11 @@
 // Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
 
 #pragma once
+
 #include "K2Node.h"
+#include "K2Node_EnumEquality.h"
 #include "K2Node_EnumInequality.generated.h"
+
 
 UCLASS(MinimalAPI)
 class UK2Node_EnumInequality : public UK2Node_EnumEquality
@@ -22,4 +25,3 @@ class UK2Node_EnumInequality : public UK2Node_EnumEquality
 	/** Gets the name and class of the EqualEqual_ByteByte function */
 	BLUEPRINTGRAPH_API virtual void GetConditionalFunction(FName& FunctionName, UClass** FunctionClass) const override;
 };
-
