511b53fcece34bdbb7ad329cc0f1882fec500ccb
Marcus Wassmer <Marcus.Wassmer@epicgames.com>
2015-04-08 17:58:20 -0400

    CIS fix.
    [CL 2506003 by Marcus Wassmer in Main branch]

--- Engine/Source/Runtime/Renderer/Private/PostProcess/PostProcessing.cpp
+++ Engine/Source/Runtime/Renderer/Private/PostProcess/PostProcessing.cpp
@@ -1518,14 +1518,14 @@ void FPostProcessing::Process(FRHICommandListImmediate& RHICmdList, FViewInfo& V
 			{
 				Node = Context.Graph.RegisterPass(new FRCPassPostProcessHMD());
 			}
-
+#if HAS_MORPHEUS
 			else if(DeviceType == EHMDDeviceType::DT_Morpheus)
 			{
 				FRCPassPostProcessMorpheus* MorpheusPass = new FRCPassPostProcessMorpheus();
 				MorpheusPass->SetInput(ePId_Input0, FRenderingCompositeOutputRef(Context.FinalOutput));
 				Node = Context.Graph.RegisterPass(MorpheusPass);
 			}
-
+#endif
 			
 			if(Node)
 			{
