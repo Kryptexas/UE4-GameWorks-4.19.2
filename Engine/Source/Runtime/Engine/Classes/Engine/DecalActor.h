ad1cce62c888687c757a57da965b8a1804fe44ea
Martin Mittring <martin.mittring@epicgames.com>
2015-04-08 19:33:42 -0400

    added comments
    [CL 2506128 by Martin Mittring in Main branch]

--- Engine/Source/Runtime/Engine/Classes/Engine/DecalActor.h
+++ Engine/Source/Runtime/Engine/Classes/Engine/DecalActor.h
@@ -7,9 +7,11 @@
 
 class UBoxComponent;
 
-
 /**
-* Decal to add texture/material on existing geometry.
+* DecalActor contains a DecalComponent which can be used to render material modifications on top of existing geometry.
+*
+* @see https://docs.unrealengine.com/latest/INT/Engine/Actors/DecalActor
+* @see UDecalComponent
 */
 UCLASS(hideCategories=(Collision, Attachment, Actor, Input, Replication), showCategories=("Input|MouseInput", "Input|TouchInput"), ComponentWrapperClass,MinimalAPI)
 class ADecalActor
