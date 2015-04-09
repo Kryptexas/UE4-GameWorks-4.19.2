248d404b5055b967bceb7372ab45fb3458ca55f3
Michael Noland <michael.noland@epicgames.com>
2015-04-09 13:15:12 -0400

    Tile Maps: Added a call to ConditionGeometry when importing geometry from Tiled to extract box primitives from polygons that meet the criteria
    [CL 2506984 by Michael Noland in Main branch]

--- Engine/Plugins/2D/Paper2D/Source/PaperTiledImporter/Private/PaperTiledImporterFactory.cpp
+++ Engine/Plugins/2D/Paper2D/Source/PaperTiledImporter/Private/PaperTiledImporterFactory.cpp
@@ -1197,6 +1197,8 @@ void FTiledObject::AddToSpriteGeometryCollection(const FVector2D& Offset, const 
 			InOutShapes.Shapes.Last().Rotation = RotationUnwound;
 		}
 	}
+
+	InOutShapes.ConditionGeometry();
 }
 
 //////////////////////////////////////////////////////////////////////////
