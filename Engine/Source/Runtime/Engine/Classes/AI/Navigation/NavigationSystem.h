c104d403683f02f55b04459b830da9aedcaa9aa8
Mieszko Zielinski <mieszko.zielinski@epicgames.com>
2015-04-13 07:53:39 -0400

    Renamed UNavigationSystem::bAllowClientSideNavigation_Experimental to bAllowClientSideNavigation, since it seems to just work #UE4
    [CL 2510196 by Mieszko Zielinski in Main branch]

--- Engine/Source/Runtime/Engine/Classes/AI/Navigation/NavigationSystem.h
+++ Engine/Source/Runtime/Engine/Classes/AI/Navigation/NavigationSystem.h
@@ -144,7 +144,7 @@ protected:
 	uint32 bAutoCreateNavigationData:1;
 
 	UPROPERTY(config, EditAnywhere, Category=NavigationSystem)
-	uint32 bAllowClientSideNavigation_Experimental:1;
+	uint32 bAllowClientSideNavigation:1;
 
 	/** gets set to true if gathering navigation data (like in navoctree) is required due to the need of navigation generation 
 	 *	Is always true in Editor Mode. In other modes it depends on bRebuildAtRuntime of every required NavigationData class' CDO
@@ -476,7 +476,7 @@ public:
 
 	bool ShouldGenerateNavigationEverywhere() const { return bWholeWorldNavigable; }
 
-	virtual bool ShouldLoadNavigationOnClient(ANavigationData* NavData = nullptr) const { return bAllowClientSideNavigation_Experimental; }
+	virtual bool ShouldLoadNavigationOnClient(ANavigationData* NavData = nullptr) const { return bAllowClientSideNavigation; }
 
 	FBox GetWorldBounds() const;
 	
--- Engine/Source/Runtime/Engine/Private/AI/Navigation/NavigationSystem.cpp
+++ Engine/Source/Runtime/Engine/Private/AI/Navigation/NavigationSystem.cpp
@@ -2024,7 +2024,7 @@ UNavigationSystem* UNavigationSystem::CreateNavigationSystem(UWorld* WorldOwner)
 #if WITH_SERVER_CODE || WITH_EDITOR
 	// create navigation system for editor and server targets, but remove it from game clients
 	if (WorldOwner && (*GEngine->NavigationSystemClass != nullptr) 
-		&& (GEngine->NavigationSystemClass->GetDefaultObject<UNavigationSystem>()->bAllowClientSideNavigation_Experimental || WorldOwner->GetNetMode() != NM_Client))
+		&& (GEngine->NavigationSystemClass->GetDefaultObject<UNavigationSystem>()->bAllowClientSideNavigation || WorldOwner->GetNetMode() != NM_Client))
 	{
 		AWorldSettings* WorldSettings = WorldOwner->GetWorldSettings();
 		if (WorldSettings == NULL || WorldSettings->bEnableNavigationSystem)
