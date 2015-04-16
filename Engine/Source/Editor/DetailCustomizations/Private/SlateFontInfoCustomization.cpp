4fe626b03a7efef74c184953b319dc4a6910e2d5
Jamie Dale <Jamie.Dale@epicgames.com>
2015-04-16 11:46:50 -0400

    Made FSlateFontInfoStructCustomization use GET_MEMBER_NAME_CHECKED
    [CL 2514358 by Jamie Dale in Main branch]

--- Engine/Source/Editor/DetailCustomizations/Private/SlateFontInfoCustomization.cpp
+++ Engine/Source/Editor/DetailCustomizations/Private/SlateFontInfoCustomization.cpp
@@ -14,15 +14,19 @@ TSharedRef<IPropertyTypeCustomization> FSlateFontInfoStructCustomization::MakeIn
 
 void FSlateFontInfoStructCustomization::CustomizeHeader(TSharedRef<IPropertyHandle> InStructPropertyHandle, FDetailWidgetRow& InHeaderRow, IPropertyTypeCustomizationUtils& InStructCustomizationUtils)
 {
+	static const FName FontObjectPropertyName = GET_MEMBER_NAME_CHECKED(FSlateFontInfo, FontObject);
+	static const FName TypefaceFontNamePropertyName = GET_MEMBER_NAME_CHECKED(FSlateFontInfo, TypefaceFontName);
+	static const FName SizePropertyName = GET_MEMBER_NAME_CHECKED(FSlateFontInfo, Size);
+
 	StructPropertyHandle = InStructPropertyHandle;
 
-	FontObjectProperty = InStructPropertyHandle->GetChildHandle(FName("FontObject"));
+	FontObjectProperty = InStructPropertyHandle->GetChildHandle(FontObjectPropertyName);
 	check(FontObjectProperty.IsValid());
 
-	TypefaceFontNameProperty = InStructPropertyHandle->GetChildHandle(FName("TypefaceFontName"));
+	TypefaceFontNameProperty = InStructPropertyHandle->GetChildHandle(TypefaceFontNamePropertyName);
 	check(TypefaceFontNameProperty.IsValid());
 
-	FontSizeProperty = InStructPropertyHandle->GetChildHandle(FName("Size"));
+	FontSizeProperty = InStructPropertyHandle->GetChildHandle(SizePropertyName);
 	check(FontSizeProperty.IsValid());
 
 	InHeaderRow
