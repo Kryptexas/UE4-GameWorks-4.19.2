c1a0165d1827b674e66156ea8ddba8e0c3fa2000
Max Preussner <Max.Preussner@epicgames.com>
2015-04-16 11:51:29 -0400

    Media: Properly handling empty URLs
    [CL 2514382 by Max Preussner in Main branch]

--- Engine/Source/Editor/DetailCustomizations/Private/MediaPlayerCustomization.cpp
+++ Engine/Source/Editor/DetailCustomizations/Private/MediaPlayerCustomization.cpp
@@ -388,7 +388,7 @@ FString FMediaPlayerCustomization::HandleUrlPickerFileTypeFilter() const
 
 void FMediaPlayerCustomization::HandleUrlPickerPathPicked( const FString& PickedPath )
 {
-	if (PickedPath.StartsWith(TEXT("./")) || PickedPath.Contains(TEXT("://")))
+	if (PickedPath.IsEmpty() || PickedPath.StartsWith(TEXT("./")) || PickedPath.Contains(TEXT("://")))
 	{
 		UrlProperty->SetValue(PickedPath);
 	}
