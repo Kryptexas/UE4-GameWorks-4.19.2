632b3abe5b079a7034f19f550020dd7e93b4c74c
Ryan Gerleve <Ryan.Gerleve@epicgames.com>
2015-04-20 12:11:00 -0400

    Fix non-unity compile
    [CL 2517752 by Ryan Gerleve in Main branch]

--- Engine/Source/Runtime/Engine/Private/EngineTypes.cpp
+++ Engine/Source/Runtime/Engine/Private/EngineTypes.cpp
@@ -1,6 +1,7 @@
 // Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
 
 #include "EnginePrivate.h"
+#include "PhysicalMaterials/PhysicalMaterial.h"
 
 UEngineBaseTypes::UEngineBaseTypes(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
