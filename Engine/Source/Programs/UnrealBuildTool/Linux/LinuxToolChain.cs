93e0abe8d483c87af843e943e5622a8791698edd
Dmitry Rekman <Dmitry.Rekman@epicgames.com>
2015-04-21 11:51:45 -0400

    Move LinuxNativeDialogs to a more appropriate location.
    - Build script corrected.
    - Unfortunately, yet another hardcoded rpath.
    
    #codereview Ben.Marsh
    
    [CL 2519366 by Dmitry Rekman in Main branch]

--- Engine/Source/Programs/UnrealBuildTool/Linux/LinuxToolChain.cs
+++ Engine/Source/Programs/UnrealBuildTool/Linux/LinuxToolChain.cs
@@ -469,6 +469,7 @@ namespace UnrealBuildTool
 			Result += " -Wl,-rpath=${ORIGIN}/..";	// for modules that are in sub-folders of the main Engine/Binary/Linux folder
 			// FIXME: really ugly temp solution. Modules need to be able to specify this
             Result += " -Wl,-rpath=${ORIGIN}/../../../Engine/Binaries/ThirdParty/ICU/icu4c-53_1/Linux/x86_64-unknown-linux-gnu";
+            Result += " -Wl,-rpath=${ORIGIN}/../../../Engine/Binaries/ThirdParty/LinuxNativeDialogs/Linux/x86_64-unknown-linux-gnu";
 
             if (CrossCompiling())
             {
