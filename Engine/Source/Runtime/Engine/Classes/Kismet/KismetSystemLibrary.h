9a6aafc6b46caca25f54948740f8d0b3b3649098
Michael Noland <michael.noland@epicgames.com>
2015-04-20 22:15:57 -0400

    Blueprints: Further clarified the comment on the Blueprint-exposed GetDisplayName method
    [CL 2518663 by Michael Noland in Main branch]

--- Engine/Source/Runtime/Engine/Classes/Kismet/KismetSystemLibrary.h
+++ Engine/Source/Runtime/Engine/Classes/Kismet/KismetSystemLibrary.h
@@ -83,7 +83,9 @@ class ENGINE_API UKismetSystemLibrary : public UBlueprintFunctionLibrary
 	UFUNCTION(BlueprintPure, Category = "Utilities")
 	static FString GetObjectName(const UObject* Object);
 
-	// Returns the display name (or actor label), for displaying to end users.  Note:  In editor builds, this is the actor label.  In non-editor builds, this is the actual object name.  This function should not be used to uniquely identify actors!
+	// Returns the display name (or actor label), for displaying as a debugging aid.
+	// Note: In editor builds, this is the actor label.  In non-editor builds, this is the actual object name.  This function should not be used to uniquely identify actors!
+	// It is not localized and should not be used for display to an end user of a game.
 	UFUNCTION(BlueprintPure, Category="Utilities")
 	static FString GetDisplayName(const UObject* Object);
 
