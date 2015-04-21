ed7d3812016dba3b90d6d71fd96a31d03916ba14
Mike Beach <Mike.Beach@epicgames.com>
2015-04-21 15:25:00 -0400

    [UE-14168] Fixing it so CPF_HasGetValueTypeHash flags are not compared when checking to see if two function signatures match.
    #codereview Steve.Robb
    
    [CL 2519754 by Mike Beach in Main branch]

--- Engine/Source/Runtime/CoreUObject/Public/UObject/Class.h
+++ Engine/Source/Runtime/CoreUObject/Public/UObject/Class.h
@@ -1293,7 +1293,7 @@ public:
 	FORCEINLINE static uint64 GetDefaultIgnoredSignatureCompatibilityFlags()
 	{
 		//@TODO: UCREMOVAL: CPF_ConstParm added as a hack to get blueprints compiling with a const DamageType parameter.
-		const uint64 IgnoreFlags = CPF_PersistentInstance | CPF_ExportObject | CPF_InstancedReference | CPF_ContainsInstancedReference | CPF_ComputedFlags | CPF_ConstParm;
+		const uint64 IgnoreFlags = CPF_PersistentInstance | CPF_ExportObject | CPF_InstancedReference | CPF_ContainsInstancedReference | CPF_ComputedFlags | CPF_ConstParm | CPF_HasGetValueTypeHash;
 		return IgnoreFlags;
 	}
 
