a0c9af80d7d6918c2bcfdf64c91c923f1f9ac701
Martin Wilson <Martin.Wilson@epicgames.com>
2015-04-27 09:14:56 -0400

    Destructible mesh thumbnail so DM assets dont go through the skeletal mesh thumbnail renderer (causes output spam)
    [CL 2526835 by Martin Wilson in Main branch]

--- Engine/Source/Editor/UnrealEd/Private/ThumbnailHelpers.cpp
+++ Engine/Source/Editor/UnrealEd/Private/ThumbnailHelpers.cpp
@@ -15,6 +15,8 @@
 #include "Engine/SimpleConstructionScript.h"
 #include "Engine/StaticMesh.h"
 #include "Engine/TextureCube.h"
+#include "Engine/DestructibleMesh.h"
+#include "PhysicsEngine/DestructibleActor.h"
 
 /*
 ***************************************************************
@@ -429,6 +431,75 @@ void FSkeletalMeshThumbnailScene::GetViewMatrixParameters(const float InFOVDegre
 	OutOrbitZoom = TargetDistance + ThumbnailInfo->OrbitZoom;
 }
 
+/*
+***************************************************************
+FDestructibleMeshThumbnailScene
+***************************************************************
+*/
+
+FDestructibleMeshThumbnailScene::FDestructibleMeshThumbnailScene()
+	: FThumbnailPreviewScene()
+{
+	bForceAllUsedMipsResident = false;
+	// Create preview actor
+	// checked
+	FActorSpawnParameters SpawnInfo;
+	SpawnInfo.bNoCollisionFail = true;
+	SpawnInfo.bNoFail = true;
+	SpawnInfo.ObjectFlags = RF_Transient;
+	PreviewActor = GetWorld()->SpawnActor<ADestructibleActor>(SpawnInfo);
+
+	PreviewActor->SetActorEnableCollision(false);
+}
+
+void FDestructibleMeshThumbnailScene::SetDestructibleMesh(class UDestructibleMesh* InMesh)
+{
+	PreviewActor->GetDestructibleComponent()->OverrideMaterials.Empty();
+	PreviewActor->GetDestructibleComponent()->SetDestructibleMesh(InMesh);
+
+	if (InMesh)
+	{
+		FTransform MeshTransform = FTransform::Identity;
+
+		PreviewActor->SetActorLocation(FVector(0, 0, 0), false);
+		PreviewActor->GetDestructibleComponent()->UpdateBounds();
+
+		// Center the mesh at the world origin then offset to put it on top of the plane
+		const float BoundsZOffset = GetBoundsZOffset(PreviewActor->GetDestructibleComponent()->Bounds);
+		PreviewActor->SetActorLocation(-PreviewActor->GetDestructibleComponent()->Bounds.Origin + FVector(0, 0, BoundsZOffset), false);
+		PreviewActor->GetDestructibleComponent()->RecreateRenderState_Concurrent();
+	}
+}
+
+void FDestructibleMeshThumbnailScene::GetViewMatrixParameters(const float InFOVDegrees, FVector& OutOrigin, float& OutOrbitPitch, float& OutOrbitYaw, float& OutOrbitZoom) const
+{
+	check(PreviewActor->GetDestructibleComponent());
+	check(PreviewActor->GetDestructibleComponent()->GetDestructibleMesh());
+
+	const float HalfFOVRadians = FMath::DegreesToRadians<float>(InFOVDegrees) * 0.5f;
+	// No need to add extra size to view slightly outside of the sphere to compensate for perspective since skeletal meshes already buffer bounds.
+	const float HalfMeshSize = PreviewActor->GetDestructibleComponent()->Bounds.SphereRadius;
+	const float BoundsZOffset = GetBoundsZOffset(PreviewActor->GetDestructibleComponent()->Bounds);
+	const float TargetDistance = HalfMeshSize / FMath::Tan(HalfFOVRadians);
+
+	USceneThumbnailInfo* ThumbnailInfo = Cast<USceneThumbnailInfo>(PreviewActor->GetDestructibleComponent()->DestructibleMesh->ThumbnailInfo);
+	if (ThumbnailInfo)
+	{
+		if (TargetDistance + ThumbnailInfo->OrbitZoom < 0)
+		{
+			ThumbnailInfo->OrbitZoom = -TargetDistance;
+		}
+	}
+	else
+	{
+		ThumbnailInfo = USceneThumbnailInfo::StaticClass()->GetDefaultObject<USceneThumbnailInfo>();
+	}
+
+	OutOrigin = FVector(0, 0, -BoundsZOffset);
+	OutOrbitPitch = ThumbnailInfo->OrbitPitch;
+	OutOrbitYaw = ThumbnailInfo->OrbitYaw;
+	OutOrbitZoom = TargetDistance + ThumbnailInfo->OrbitZoom;
+}
 
 /*
 ***************************************************************
