2f3639fedd9448fb4396303d979fddcbb8f83772
Marc Audy <Marc.Audy@epicgames.com>
2015-05-01 16:52:40 -0400

    Fix issues with nested components after rename/undo/redo * Cached owner not being updated * In the incorrect Actor's OwnedComponents array
    [CL 2534306 by Marc Audy in Main branch]

--- Engine/Source/Runtime/Engine/Private/Components/ActorComponent.cpp
+++ Engine/Source/Runtime/Engine/Private/Components/ActorComponent.cpp
@@ -211,6 +211,25 @@ void UActorComponent::PostRename(UObject* OldOuter, const FName OldName)
 			{
 				Owner->AddOwnedComponent(this);
 			}
+
+			TArray<UObject*> Children;
+			GetObjectsWithOuter(this, Children);
+
+			for (UObject* Child : Children)
+			{
+				if (UActorComponent* ChildComponent = Cast<UActorComponent>(Child))
+				{
+					ChildComponent->Owner = Owner;
+					if (OldOwner)
+					{
+						OldOwner->RemoveOwnedComponent(ChildComponent);
+					}
+					if (Owner)
+					{
+						Owner->AddOwnedComponent(ChildComponent);
+					}
+				}
+			}
 		}
 	}
 
@@ -474,6 +493,25 @@ void UActorComponent::PostEditUndo()
 		if (Owner)
 		{
 			Owner->AddOwnedComponent(this);
+		}
+
+		TArray<UObject*> Children;
+		GetObjectsWithOuter(this, Children);
+
+		for (UObject* Child : Children)
+		{
+			if (UActorComponent* ChildComponent = Cast<UActorComponent>(Child))
+			{
+				if (ChildComponent->Owner)
+				{
+					ChildComponent->Owner->RemoveOwnedComponent(ChildComponent);
+				}
+				ChildComponent->Owner = Owner;
+				if (Owner)
+				{
+					Owner->AddOwnedComponent(ChildComponent);
+				}
+			}
 		}
 
 		if (GetWorld())
