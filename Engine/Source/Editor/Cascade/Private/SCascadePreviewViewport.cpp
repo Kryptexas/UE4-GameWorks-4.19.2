558d18f5fb3a21119d768dafb1f4461c9ec70066
Simon.Tovey <simon.tovey@epicgames.com>
2015-05-05 09:58:28 -0400

    [INTEGRATE] Change 2533407 by Simon.Tovey@Simon.Tovey_Primary on 2015/05/01 07:30:06
    	Fix for UE-11697 - Major performance hit with multiple Particle Systems open and tabbed
    
    	This was caused by the cascade viewport widget rendering all the time, even when in a hidden tab.
    
    [CL 2537343 by bruce nesbit in 4.8 branch]

--- Engine/Source/Editor/Cascade/Private/SCascadePreviewViewport.cpp
+++ Engine/Source/Editor/Cascade/Private/SCascadePreviewViewport.cpp
@@ -32,7 +32,7 @@ void SCascadePreviewViewport::RefreshViewport()
 
 bool SCascadePreviewViewport::IsVisible() const
 {
-	return ViewportWidget.IsValid() && (!ParentTab.IsValid() || ParentTab.Pin()->IsForeground());
+	return ViewportWidget.IsValid() && (!ParentTab.IsValid() || ParentTab.Pin()->IsForeground()) && SEditorViewport::IsVisible();
 }
 
 TSharedPtr<FSceneViewport> SCascadePreviewViewport::GetViewport() const
