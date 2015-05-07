56a1390541ffead1fc33a7aefe809abdc9f17e77
Nick Darnell <Nick.Darnell@epicgames.com>
2015-05-07 16:02:57 -0400

    UMG - Adding utility methods to go from a widget's geometry to viewport space.
    [CL 2541519 by Nick Darnell in Main branch]

--- Engine/Source/Runtime/UMG/Private/SlateBlueprintLibrary.cpp
+++ Engine/Source/Runtime/UMG/Private/SlateBlueprintLibrary.cpp
@@ -3,6 +3,7 @@
 #include "UMGPrivatePCH.h"
 
 #include "Slate/SlateBrushAsset.h"
+#include "Runtime/Engine/Classes/Engine/UserInterfaceSettings.h"
 #include "SlateBlueprintLibrary.h"
 
 #define LOCTEXT_NAMESPACE "UMG"
@@ -33,6 +34,40 @@ FVector2D USlateBlueprintLibrary::LocalToAbsolute(const FGeometry& Geometry, FVe
 FVector2D USlateBlueprintLibrary::GetLocalSize(const FGeometry& Geometry)
 {
 	return Geometry.GetLocalSize();
+}
+
+void USlateBlueprintLibrary::LocalToViewport(UObject* WorldContextObject, const FGeometry& Geometry, FVector2D LocalCoordinate, FVector2D& ScreenPosition, FVector2D& ViewportPosition)
+{
+	FVector2D AbsoluteCoordinate = Geometry.LocalToAbsolute(LocalCoordinate);
+	return AbsoluteToViewport(WorldContextObject, Geometry, AbsoluteCoordinate, ScreenPosition, ViewportPosition);
+}
+
+void USlateBlueprintLibrary::AbsoluteToViewport(UObject* WorldContextObject, const FGeometry& Geometry, FVector2D AbsoluteCoordinate, FVector2D& ScreenPosition, FVector2D& ViewportPosition)
+{
+	UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject);
+	if ( World && World->IsGameWorld() )
+	{
+		if ( UGameViewportClient* ViewportClient = World->GetGameViewport() )
+		{
+			if ( FViewport* Viewport = ViewportClient->Viewport )
+			{
+				FVector2D ViewportSize;
+				ViewportClient->GetViewportSize(ViewportSize);
+
+				FVector2D AbsolutePositionPostDPI = Viewport->VirtualDesktopPixelToViewport(FIntPoint((int32)AbsoluteCoordinate.X, (int32)AbsoluteCoordinate.Y)) * ViewportSize;
+
+				float CurrentViewportScale = GetDefault<UUserInterfaceSettings>(UUserInterfaceSettings::StaticClass())->GetDPIScaleBasedOnSize(FIntPoint(ViewportSize.X, ViewportSize.Y));
+
+				ScreenPosition = AbsolutePositionPostDPI;
+				ViewportPosition = AbsolutePositionPostDPI / CurrentViewportScale;
+
+				return;
+			}
+		}
+	}
+
+	ScreenPosition = FVector2D(0, 0);
+	ViewportPosition = FVector2D(0, 0);
 }
 
 #undef LOCTEXT_NAMESPACE
