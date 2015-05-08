7f291d9a911b624947035f5aa0b07df1fa032306
Steve Robb <Steve.Robb@epicgames.com>
2015-05-08 19:25:06 -0400

    Only register and unregister the class during deserialization.
    #codereview robert.manuszewski
    
    [CL 2543984 by Steve Robb in Main branch]

--- Engine/Source/Runtime/CoreUObject/Private/UObject/Class.cpp
+++ Engine/Source/Runtime/CoreUObject/Private/UObject/Class.cpp
@@ -3199,11 +3199,18 @@ void UClass::SetSuperStruct(UStruct* NewSuperStruct)
 void UClass::SerializeSuperStruct(FArchive& Ar)
 {
 #if UCLASS_FAST_ISA_IMPL & 2
-	FFastIndexingClassTree::Unregister(this);
+	bool bIsLoading = Ar.IsLoading();
+	if (bIsLoading)
+	{
+		FFastIndexingClassTree::Unregister(this);
+	}
 #endif
 	Super::SerializeSuperStruct(Ar);
 #if UCLASS_FAST_ISA_IMPL & 2
-	FFastIndexingClassTree::Register(this);
+	if (bIsLoading)
+	{
+		FFastIndexingClassTree::Register(this);
+	}
 #endif
 }
 
