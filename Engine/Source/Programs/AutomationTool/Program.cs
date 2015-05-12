bbe14d3161c1e29449548700b4310b0446c3c403
Peter.Sauerbrei <peter.sauerbrei@epicgames.com>
2015-05-12 09:11:30 -0400

    [INTEGRATE] Change 2546258 by Peter.Sauerbrei@peter.sauerbrei_T2782_3 on 2015/05/11 19:34:55
    	messaging for when instruments launch fails
    	UE-12395, UE-9979
    	#ios
    
    [CL 2547100 by Matthew Griffin in 4.8 branch]

--- Engine/Source/Programs/AutomationTool/Program.cs
+++ Engine/Source/Programs/AutomationTool/Program.cs
@@ -54,10 +54,13 @@ namespace AutomationTool
         Error_ObbNotFound = 53,
         Error_AndroidBuildToolsPathNotFound = 54,
         Error_NoApkSuitableForArchitecture = 55,
-        Error_LauncherFailed = 100,
+		Error_FilesInstallFailed = 56,
+		Error_LauncherFailed = 100,
 		Error_UATLaunchFailure = 101,
 		Error_FailedToDeleteStagingDirectory = 102,
 		Error_MissingExecutable = 103,
+		Error_DeviceNotSetupForDevelopment = 150,
+		Error_DeviceOSNewerThanSDK = 151,
 	};
 
 	public class ErrorReporter
