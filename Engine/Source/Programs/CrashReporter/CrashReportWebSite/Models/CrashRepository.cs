43598768621e1568f18f9b7587a7384e88dcdf5f
Jaroslaw Surowiec <Jaroslaw.Surowiec@epicgames.com>
2015-05-12 05:39:52 -0400

    CrashFrontend - Fix for ensures being treated as crashes
    [CL 2546926 by Jaroslaw Surowiec in Main branch]

--- Engine/Source/Programs/CrashReporter/CrashReportWebSite/Models/CrashRepository.cs
+++ Engine/Source/Programs/CrashReporter/CrashReportWebSite/Models/CrashRepository.cs
@@ -618,7 +618,7 @@ namespace Tools.CrashReporter.CrashReportWebSite.Models
 				{
 					NewCrash.CrashType = 2;
 				}
-				else if (NewCrash.RawCallStack.Contains( "FDebug::EnsureFailed" ))
+				else if (NewCrash.RawCallStack.Contains( "FDebug::Ensure" ))
 				{
 					NewCrash.CrashType = 3;
 				}
