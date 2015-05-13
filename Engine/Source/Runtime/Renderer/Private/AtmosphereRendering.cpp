3f951aaef6167a2398ad2607348993a659c90130
Gil Gribb <gil.gribb@epicgames.com>
2015-05-13 08:28:41 -0400

    fix cis
    [CL 2548773 by Gil Gribb in Main branch]

--- Engine/Source/Runtime/Renderer/Private/AtmosphereRendering.cpp
+++ Engine/Source/Runtime/Renderer/Private/AtmosphereRendering.cpp
@@ -1601,11 +1601,12 @@ FAtmosphericFogSceneInfo::FAtmosphericFogSceneInfo(UAtmosphericFogComponent* InC
 	FMatrix WorldToLight = InComponent->ComponentToWorld.ToMatrixNoScale().InverseFast();
 	DefaultSunDirection = FVector(WorldToLight.M[0][0],WorldToLight.M[1][0],WorldToLight.M[2][0]);
 
+#if WITH_EDITORONLY_DATA
 	if (Component->PrecomputeCounter != UAtmosphericFogComponent::EValid)
 	{
 		bNeedRecompute = true;
 	}
-
+#endif
 	TransmittanceResource = Component->TransmittanceResource;
 	IrradianceResource = Component->IrradianceResource;
 	InscatterResource = Component->InscatterResource;
