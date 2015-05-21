4f765e4bf70ad72b82c6b3b1f5995501dd4c3dfc
Ori Cohen <Ori.Cohen@epicgames.com>
2015-05-21 11:13:58 -0400

    Fix complex collision that is movable not being set as part of physics simulation. Was not correctly setting actors as kinematic.
    UE-15899
    
    [CL 2560854 by Ori Cohen in Main branch]

--- Engine/Source/Runtime/Engine/Private/PhysicsEngine/BodyInstance.cpp
+++ Engine/Source/Runtime/Engine/Private/PhysicsEngine/BodyInstance.cpp
@@ -1055,7 +1055,7 @@ struct FInitBodiesHelper
 
 	physx::PxRigidActor* CreateActor_PhysX_AssumesLocked(FBodyInstance* Instance, const PxTransform& PTransform) const
 	{
-		physx::PxRigidActor* PNewDynamic = nullptr;
+		physx::PxRigidDynamic* PNewDynamic = nullptr;
 
 		if (bCompileStatic || bStatic)
 		{
@@ -1075,6 +1075,11 @@ struct FInitBodiesHelper
 			else
 			{
 				Instance->RigidActorSync = PNewDynamic;
+			}
+
+			if(!Instance->ShouldInstanceSimulatingPhysics())
+			{
+				PNewDynamic->setRigidDynamicFlag(PxRigidDynamicFlag::eKINEMATIC, true);
 			}
 		}
 
