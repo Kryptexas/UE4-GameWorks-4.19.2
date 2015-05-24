73f6314fd7080b46425108105703b5cddfe1e423
Max Preussner <Max.Preussner@epicgames.com>
2015-05-24 10:21:57 -0400

    Async: Refactored async task API
    [CL 2563968 by Max Preussner in Main branch]

--- Engine/Source/Runtime/Portal/Services/Public/IPortalServicesModule.h
+++ Engine/Source/Runtime/Portal/Services/Public/IPortalServicesModule.h
@@ -3,9 +3,9 @@
 #pragma once
 
 #include "ModuleInterface.h"
+#include "TypeContainer.h"
 
 
-class FTypeContainer;
 class IPortalService;
 
 
@@ -23,4 +23,23 @@ public:
 	 * @return The type container.
 	 */
 	virtual FTypeContainer& GetServiceContainer() = 0;
+
+public:
+
+	/**
+	 * Gets a shared pointer to an instance of the specified service.
+	 *
+	 * @param T The type of service to get an instance for.
+	 * @return The service instance, or nullptr if not available.
+	 */
+	template<class T>
+	TSharedPtr<T> GetService()
+	{
+		return GetServiceContainer().GetInstance<T>();
+	}
+
+public:
+
+	/** Virtual destructor. */
+	virtual ~IPortalServicesModule() { }
 };
