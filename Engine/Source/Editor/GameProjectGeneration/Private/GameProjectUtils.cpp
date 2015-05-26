352ccab35b44735d2abb032cb033b2826c2bd4d2
Peter.Sauerbrei <peter.sauerbrei@epicgames.com>
2015-05-26 11:27:27 -0400

    [INTEGRATE] Change 2564999 by Peter.Sauerbrei@peter.sauerbrei_T2782_3 on 2015/05/26 11:22:09
    	enable plugin linking in the binary release
    	UE-16129
    
    [CL 2565008 by Ben Marsh in 4.8 branch]

--- Engine/Source/Editor/GameProjectGeneration/Private/GameProjectUtils.cpp
+++ Engine/Source/Editor/GameProjectGeneration/Private/GameProjectUtils.cpp
@@ -2971,10 +2971,11 @@ bool GameProjectUtils::ProjectRequiresBuild(const FName InPlatformInfoName)
 	{
 		// check to see if the default build settings have changed
 		bRequiresBuild |= !HasDefaultBuildSettings(InPlatformInfoName);
-
-		// check to see if any plugins beyond the defaults have been enabled
-		bRequiresBuild |= IProjectManager::Get().IsNonDefaultPluginEnabled();
 	}
+
+	// check to see if any plugins beyond the defaults have been enabled
+	bRequiresBuild |= IProjectManager::Get().IsNonDefaultPluginEnabled();
+
 	return bRequiresBuild;
 }
 
