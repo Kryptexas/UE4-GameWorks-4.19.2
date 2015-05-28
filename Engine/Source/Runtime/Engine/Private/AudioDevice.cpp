b1485d9b91ad68eb98a0fbe1600e53216c4abf27
Bob Tellez <Bob.Tellez@epicgames.com>
2015-05-27 21:33:36 -0400

    [AUTOMERGE]
    #UE4 Added an ensure to audiodevice to help track down a crash
    
    --------
    Integrated using branch Ue4-To-UE4-Fortnite-Simple (reversed) of change#2551989 by Bob.Tellez on 2015/05/14 23:06:14.
    
    [CL 2567841 by Bob Tellez in Main branch]

--- Engine/Source/Runtime/Engine/Private/AudioDevice.cpp
+++ Engine/Source/Runtime/Engine/Private/AudioDevice.cpp
@@ -1667,6 +1667,7 @@ int32 FAudioDevice::GetSortedActiveWaveInstances(TArray<FWaveInstance*>& WaveIns
 	// Tick all the active audio components.  Use a copy as some operations may remove elements from the list, but we want
 	// to evaluate in the order they were added
 	TArray<FActiveSound*> ActiveSoundsCopy = ActiveSounds;
+	ensureOnceMsgf(ActiveSoundsCopy.Num() < 10000, TEXT("Encountered a large number of sounds in the ActiveSounds array. Num=%d. LastSound=%s"), ActiveSoundsCopy.Num(), ActiveSoundsCopy.Last() ? *GetNameSafe(ActiveSoundsCopy.Last()->Sound) : TEXT("None"));
 	for( int32 i = 0; i < ActiveSoundsCopy.Num(); ++i )
 	{
 		FActiveSound* ActiveSound = ActiveSoundsCopy[i];
