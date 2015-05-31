5eb22b35c07062ef4d2705fee1a9b563140fcaf1
Robert Manuszewski <Robert.Manuszewski@epicgames.com>
2015-05-31 18:44:49 -0400

    Don't add Linker imports to GC (potential fix for GC perf regression)
    #lockdown Nick.Penwarden
    
    [CL 2572066 by Robert Manuszewski in Main branch]

--- Engine/Source/Runtime/CoreUObject/Private/UObject/Linker.cpp
+++ Engine/Source/Runtime/CoreUObject/Private/UObject/Linker.cpp
@@ -160,10 +160,6 @@ void FLinker::AddReferencedObjects(FReferenceCollector& Collector)
 		Collector.AddReferencedObject(*(UObject**)&LinkerRoot);
 	}
 #endif
-	for (auto& Import : ImportMap)
-	{
-		Collector.AddReferencedObject(Import.XObject);
-	}
 }
 
 // FLinker interface.
