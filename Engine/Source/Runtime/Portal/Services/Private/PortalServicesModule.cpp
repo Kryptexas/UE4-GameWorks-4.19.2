fc8490ec01098ed023e736f5d08dd6d84c145150
Max Preussner <Max.Preussner@epicgames.com>
2015-05-31 14:33:27 -0400

    Portal: made PortalServices module include-only
    [CL 2572010 by Max Preussner in Main branch]

--- Engine/Source/Runtime/Portal/Services/Private/PortalServicesModule.cpp
+++ Engine/Source/Runtime/Portal/Services/Private/PortalServicesModule.cpp
@@ -1,7 +1,6 @@
 // Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
 
 #include "PortalServicesPrivatePCH.h"
-#include "TypeContainer.h"
 #include "ModuleManager.h"
 
 
@@ -9,7 +8,7 @@
  * Implements the PortalServices module.
  */
 class FPortalServicesModule
-	: public IPortalServicesModule
+	: public IModuleInterface
 {
 public:
 
@@ -18,34 +17,11 @@ public:
 
 public:
 
-	// IPortalServicesModule
-
-	virtual FTypeContainer& GetServiceContainer() override
-	{
-		return ServiceContainer;
-	}
-
-public:
-
 	// IModuleInterface interface
 
-	virtual void StartupModule() override
-	{
-	}
-
-	virtual void ShutdownModule() override
-	{
-	}
-
-	virtual bool SupportsDynamicReloading() override
-	{
-		return false;
-	}
-
-private:
-
-	/** Holds registered service instances. */
-	FTypeContainer ServiceContainer;
+	virtual void StartupModule() override { }
+	virtual void ShutdownModule() override { }
+	virtual bool SupportsDynamicReloading() override { return true; }
 };
 
 
