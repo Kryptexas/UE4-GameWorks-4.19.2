9f2934acd6e4f72f7a31e69a8f005c572341b3a0
Andrew Rodham <Andrew.Rodham@epicgames.com>
2015-06-01 05:56:14 -0400

    Added support for unit-based numeric math expressions in unit based numeric input boxes This addresses UE-13765 and UE-13443
    [CL 2572301 by Andrew Rodham in Main branch]

--- Engine/Source/Editor/EditorStyle/Private/EditorStyleClasses.cpp
+++ Engine/Source/Editor/EditorStyle/Private/EditorStyleClasses.cpp
@@ -1,7 +1,6 @@
 // Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
 
 #include "EditorStylePrivatePCH.h"
-#include "UnitConversion.h"
 
 /* UEditorStyleSettings interface
  *****************************************************************************/
