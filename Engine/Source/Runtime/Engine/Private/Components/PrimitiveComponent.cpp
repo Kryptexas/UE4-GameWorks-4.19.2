615925c38c77d182af9d6c045434b155e7836dff
Zak Middleton <Zak.Middleton@epicgames.com>
2015-06-03 13:11:47 -0400

    #ue4 - Fix physics volume change notifications not working for the first change in physics volume.
    [CL 2575793 by Zak Middleton in Main branch]

--- Engine/Source/Runtime/Engine/Private/Components/PrimitiveComponent.cpp
+++ Engine/Source/Runtime/Engine/Private/Components/PrimitiveComponent.cpp
@@ -2456,10 +2456,7 @@ void UPrimitiveComponent::UpdatePhysicsVolume( bool bTriggerNotifiers )
 				}
 			}
 
-			if (BestVolume != GetPhysicsVolume())
-			{
-				SetPhysicsVolume(BestVolume, bTriggerNotifiers);
-			}
+			SetPhysicsVolume(BestVolume, bTriggerNotifiers);
 		}
 		else
 		{
