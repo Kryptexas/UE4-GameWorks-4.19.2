2dffb421abea9efc1ed4f3449e22c8e736cd31d3
Kellan.Carr <kellan.carr@epicgames.com>
2015-06-04 09:34:12 -0400

    [INTEGRATE] Change 2572947 by Kellan.Carr@kellan.carr_G5046_all on 2015/06/01 15:48:06
    	build script, retry copy publish on mac
    
    [CL 2576888 by Ben Marsh in 4.8 branch]

--- Engine/Source/Programs/AutomationTool/Scripts/RocketBuild.Automation.cs
+++ Engine/Source/Programs/AutomationTool/Scripts/RocketBuild.Automation.cs
@@ -911,7 +911,26 @@ namespace Rocket
 					if(DebugExtensions.Contains(Extension) || Extension == ".exe" || Extension == ".dll") // Need all windows build products for crash reporter
 					{
 						string OutputFileName = CommandUtils.MakeRerootedFilePath(InputFileName, CommandUtils.CmdEnv.LocalRoot, SymbolsOutputDir);
-						CommandUtils.CopyFile(InputFileName, OutputFileName);
+                        if (SymbolsOutputDir.StartsWith("/Volumes"))
+                        {
+                            int Retry = 0;
+                            int NumRetries = 60;
+                            bool bCopied = false;
+                            while (!bCopied && Retry < NumRetries)
+                            {
+                                if (Retry > 0)
+                                {
+                                    CommandUtils.Log("*** Mac temp storage retry {0}", OutputFileName);
+                                    System.Threading.Thread.Sleep(1000);
+                                }
+                                bCopied = CommandUtils.CopyFile_NoExceptions(InputFileName, OutputFileName, true);
+                                Retry++;
+                            }
+                        }
+                        else
+                        {
+                            CommandUtils.CopyFile(InputFileName, OutputFileName);
+                        }
 					}
 				}
 			}
