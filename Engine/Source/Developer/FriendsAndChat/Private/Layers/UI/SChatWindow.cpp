d4631113203100403603e3960d926d65d84f4601
Antony Carter <antony.carter@epicgames.com>
2015-06-15 14:10:37 -0400

    Merging
    //depot/UE4-LauncherDev/Engine/Source/Developer/FriendsAndChat/Private/Layers/UI/SChatWindow.cpp
    
    to D:\\Perforce\\Epic\\UE4\\Engine\\Source\\Developer\\FriendsAndChat\\Private\\Layers\\UI\\SChatWindow.cpp
    
    [CL 2587628 by Antony Carter in Main branch]

--- Engine/Source/Developer/FriendsAndChat/Private/Layers/UI/SChatWindow.cpp
+++ Engine/Source/Developer/FriendsAndChat/Private/Layers/UI/SChatWindow.cpp
@@ -246,7 +246,7 @@ private:
 		}
 	
 		TArray<TSharedRef<FChatItemViewModel>> Messages = ViewModel->GetMessages();
-		for (LastMessageIndex = 0; LastMessageIndex < Messages.Num(); ++LastMessageIndex)
+		for (;LastMessageIndex < Messages.Num(); ++LastMessageIndex)
 		{
 			TSharedRef<FChatItemViewModel> ChatItem = Messages[LastMessageIndex];
 
