ae8f75457298cfe9cb7b87fc7f09c0c1be74ad24
Nick.Whiting <nick.whiting@epicgames.com>
2015-06-19 10:12:05 -0400

    [INTEGRATE] Change 2590896 by Nick.Whiting@z2421_SSD on 2015/06/17 16:44:09
    	UE-16177 Fix for crash when the oculus service is running, but no device detected.
    
    [CL 2593453 by Matthew Griffin in 4.8 branch]

--- Engine/Plugins/Runtime/OculusRift/Source/OculusRift/Private/OculusRiftHMD.cpp
+++ Engine/Plugins/Runtime/OculusRift/Source/OculusRift/Private/OculusRiftHMD.cpp
@@ -1136,6 +1136,12 @@ bool FOculusRiftHMD::InitDevice()
 
 	FSettings* CurrentSettings = GetSettings();
 
+	ovrResult DetectResult = ovrHmd_Detect();
+	if (!OVR_SUCCESS(DetectResult))
+	{
+		return false;
+	}
+
 	ovrResult result = ovrHmd_Create(0, &Hmd);
 	if (OVR_SUCCESS(result) && Hmd)
 	{
