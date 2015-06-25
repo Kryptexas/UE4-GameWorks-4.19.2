9bb8657734f3b71f22528cd93af94c769d145950
Allan Bentham <Allan.Bentham@epicgames.com>
2015-06-25 09:52:23 -0400

    Fix modulated shadow rendering depth/stencil setup. Fixes rendering on DX11 ES2 preview.
    #codereview Jack.Porter
    
    [CL 2600465 by Allan Bentham in Main branch]

--- Engine/Source/Runtime/Renderer/Private/ShadowRendering.cpp
+++ Engine/Source/Runtime/Renderer/Private/ShadowRendering.cpp
@@ -2904,7 +2904,7 @@ void FSceneRenderer::RenderProjections(
 	
 	if (bForwardShading)
 	{
-		SceneContext.BeginRenderingSceneColor(RHICmdList);
+		SceneContext.BeginRenderingSceneColor(RHICmdList, ESimpleRenderTargetMode::EUninitializedColorExistingDepth, FExclusiveDepthStencil::DepthRead_StencilWrite);
 	}
 	else
 	{
