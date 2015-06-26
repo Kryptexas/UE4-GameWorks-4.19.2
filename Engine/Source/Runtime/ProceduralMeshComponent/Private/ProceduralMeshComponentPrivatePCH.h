1fa8b8482552431e006113eaead82224603f8e47
Jurre DeBaare <jurre.debaare@epicgames.com>
2015-06-26 05:13:40 -0400

    GitHub 1205 : Fix UProceduralMeshComponent Not Updating Bounds #1205 Moved BodySetup include
    [CL 2601930 by Jurre DeBaare in Main branch]

--- Engine/Source/Runtime/ProceduralMeshComponent/Private/ProceduralMeshComponentPrivatePCH.h
+++ Engine/Source/Runtime/ProceduralMeshComponent/Private/ProceduralMeshComponentPrivatePCH.h
@@ -2,7 +2,6 @@
 
 #include "CoreUObject.h"
 #include "Engine.h"
-#include "PhysicsEngine/BodySetup.h"
 
 // You should place include statements to your module's private header files here.  You only need to
 // add includes for headers that are used in most of your module's source files though.
