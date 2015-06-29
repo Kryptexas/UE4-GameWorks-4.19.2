d7ffaa71540b72deac87298b684301ac37232619
Richard Hinckley <Richard.Hinckley@epicgames.com>
2015-06-29 10:10:56 -0400

    Minor python script cleanup.
    [CL 2603915 by Richard Hinckley in Main branch]

--- Engine/Source/Programs/UnrealDocTool/DoxygenInputFilter/CommentFixup.py
+++ Engine/Source/Programs/UnrealDocTool/DoxygenInputFilter/CommentFixup.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
 import sys
 f = file(sys.argv[1], 'r')
 line = f.readline()
 while line:
+    # Pad the string with an extra space at the end to catch end-of-line block comments.
     line = line + " "
     line = line.replace("/* ", "/** ")
     line = line.replace("// ", "/// ")
