b84610124619d4bff5db237cbbb2af18258fe635
John Pollard <John.Pollard@epicgames.com>
2015-07-02 16:56:13 -0400

    PR 1234: Added voice related exports to NetConnection.h
    [CL 2609732 by John Pollard in Main branch]

--- Engine/Source/Runtime/Engine/Classes/Engine/NetConnection.h
+++ Engine/Source/Runtime/Engine/Classes/Engine/NetConnection.h
@@ -352,7 +352,7 @@ public:
 	 *
 	 * @return true if it should be sent on this connection, false otherwise
 	 */
-	bool ShouldReplicateVoicePacketFrom(const FUniqueNetId& Sender);
+	ENGINE_API bool ShouldReplicateVoicePacketFrom(const FUniqueNetId& Sender);
 	
 	/**
 	 * @hack: set to net connection currently inside CleanUp(), for HasClientLoadedCurrentWorld() to be able to find it during PlayerController
@@ -596,7 +596,7 @@ public:
 	/**
 	 * @return Finds the voice channel for this connection or NULL if none
 	 */
-	class UVoiceChannel* GetVoiceChannel();
+	ENGINE_API class UVoiceChannel* GetVoiceChannel();
 
 	void FlushDormancy(class AActor* Actor);
 
