8ab6bde0005a2b9a7a0172d3c0b9677e43b673b9
Jaroslaw Surowiec <Jaroslaw.Surowiec@epicgames.com>
2015-07-03 13:00:41 -0400

    UE-6778/Inconsistent documentation on how to use AsyncTasks
    [CL 2610448 by Jaroslaw Surowiec in Main branch]

--- Engine/Source/Runtime/Core/Public/Async/AsyncWork.h
+++ Engine/Source/Runtime/Core/Public/Async/AsyncWork.h
@@ -35,13 +35,15 @@
 		}
 	};
 
-	start an example job
+	
+	void Example()
+	{
+		// start an example job
+		(new FAutoDeleteAsyncTask<ExampleAutoDeleteAsyncTask>(5)->StartBackgroundTask();
 
-	(new FAutoDeleteAsyncTask<ExampleAutoDeleteAsyncTask>(5)->StartBackgroundTask();
-
-	do an example job now, on this thread
-
-	(new FAutoDeleteAsyncTask<ExampleAutoDeleteAsyncTask>(5)->StartSynchronousTask();
+		// do an example job now, on this thread
+		(new FAutoDeleteAsyncTask<ExampleAutoDeleteAsyncTask>(5)->StartSynchronousTask();
+	}
 
 **/
 template<typename TTask>
@@ -216,31 +218,31 @@ public:
 		}
 	};
 
-	start an example job
-
-	FAutoDeleteAsyncTask<ExampleAutoDeleteAsyncTask> MyTask(5);
-	
-	MyTask.StartBackgroundTask();
-
-	-- or -- 
-
-	MyTask.StartSynchronousTask();
-
-	to just do it now on this thread
-
-	Check if the task is done:
-
-	if (MyTask.IsDone())
+	void Example()
 	{
+
+		//start an example job
+
+		FAsyncTask<ExampleAsyncTask>* MyTask = new FAsyncTask<ExampleAsyncTask>( 5 );
+		MyTask->StartBackgroundTask();
+
+		//--or --
+
+		MyTask->StartSynchronousTask();
+
+		//to just do it now on this thread
+		//Check if the task is done :
+
+		if (MyTask->IsDone())
+		{
+		}
+
+		//Spinning on IsDone is not acceptable( see EnsureCompletion ), but it is ok to check once a frame.
+		//Ensure the task is done, doing the task on the current thread if it has not been started, waiting until completion in all cases.
+
+		MyTask->EnsureCompletion();
+		delete Task;
 	}
-
-	Spinning on IsDone is not acceptable (see EnsureCompletion), but it is ok to check once a frame.
-
-	Ensure the task is done, doing the task on the current thread if it has not been started, waiting until completion in all cases.
-
-	Task.EnsureCompletion();
-
-
 **/
 template<typename TTask>
 class FAsyncTask
@@ -378,7 +380,7 @@ class FAsyncTask
 	}
 
 	/** 
-	* Internal call to intialize internal variables
+	* Internal call to initialize internal variables
 	**/
 	void Init()
 	{
