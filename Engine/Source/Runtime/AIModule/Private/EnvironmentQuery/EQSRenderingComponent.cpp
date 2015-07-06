e5cfeb29e2f44a9d11b3ea77f3ce6b46ccd918ca
sebastian kowalczyk <Sebastian.Kowalczyk@epicgames.com>
2015-07-06 07:17:29 -0400

    Fixed compilation isssue for monolitics and/or non unity builds.
    [CL 2610887 by sebastian kowalczyk in Main branch]

--- Engine/Source/Runtime/AIModule/Private/EnvironmentQuery/EQSRenderingComponent.cpp
+++ Engine/Source/Runtime/AIModule/Private/EnvironmentQuery/EQSRenderingComponent.cpp
@@ -1,6 +1,7 @@
 // Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
 
 #include "AIModulePrivate.h"
+#include "Engine/Canvas.h"
 #include "DebugRenderSceneProxy.h"
 #include "EnvironmentQuery/EQSRenderingComponent.h"
 #include "EnvironmentQuery/EQSQueryResultSourceInterface.h"
