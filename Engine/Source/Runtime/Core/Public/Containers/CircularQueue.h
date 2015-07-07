f1317f1bb0e4c0e535bd704488e249733cfccb0a
Max Preussner <Max.Preussner@epicgames.com>
2015-07-07 11:37:28 -0400

    Fixed code comment
    [CL 2612410 by Max Preussner in Main branch]

--- Engine/Source/Runtime/Core/Public/Containers/CircularQueue.h
+++ Engine/Source/Runtime/Core/Public/Containers/CircularQueue.h
@@ -14,7 +14,7 @@
  * The number of items that can be enqueued is one less than the queue's capacity,
  * because one item will be used for detecting full and empty states.
  *
- * @param ElementType - The type of elements held in the queue.
+ * @param ElementType The type of elements held in the queue.
  */
 template<typename ElementType> class TCircularQueue
 {
