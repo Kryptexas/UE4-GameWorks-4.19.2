a1eceea2178b9d640f8271df3831cd1a27b6852a
Rolando Caloca <Rolando.Caloca@epicgames.com>
2015-07-14 15:37:26 -0400

    UE4 - Delay load d3d12.dll
    [CL 2620702 by Rolando Caloca in Main branch]

--- Engine/Source/ThirdParty/Windows/DX12/DX12.Build.cs
+++ Engine/Source/ThirdParty/Windows/DX12/DX12.Build.cs
@@ -19,11 +19,9 @@ public class DX12 : ModuleRules
 			PublicLibraryPaths.Add(DirectXSDKDir + "/Lib/x86");
 		}
 
-		// If we're targeting Windows XP, then always delay-load D3D12 as it won't exist on that architecture
-		if (WindowsPlatform.IsWindowsXPSupported())
-		{
-			PublicDelayLoadDLLs.AddRange( new string[] {
-				"d3d12.dll"
+		// Always delay-load D3D12
+		PublicDelayLoadDLLs.AddRange( new string[] {
+			"d3d12.dll"
 			} );
 		}
 
