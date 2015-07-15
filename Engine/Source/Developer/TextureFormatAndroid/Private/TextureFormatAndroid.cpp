2d048dc1b9ec56e7747c6c2be5f17233613b2e88
Terence Burns <Terence.Burns@epicgames.com>
2015-07-15 09:20:40 -0400

    VS2015 - Upgrade necessary third party libs - update QualComm TextureConverter usage to dll
    [CL 2621658 by Terence Burns in Main branch]

--- Engine/Source/Developer/TextureFormatAndroid/Private/TextureFormatAndroid.cpp
+++ Engine/Source/Developer/TextureFormatAndroid/Private/TextureFormatAndroid.cpp
@@ -243,6 +243,14 @@ class FTextureFormatAndroid : public ITextureFormat
  */
 static ITextureFormat* Singleton = NULL;
 
+
+
+#if PLATFORM_WINDOWS
+	HMODULE	TextureConverterHandle = NULL;
+	FString QualCommBinariesRoot = FPaths::EngineDir() / TEXT("Binaries/ThirdParty/QualComm/Win64/");
+#endif
+
+
 class FTextureFormatAndroidModule : public ITextureFormatModule
 {
 public:
@@ -250,11 +258,18 @@ public:
 	{
 		delete Singleton;
 		Singleton = NULL;
+
+#if PLATFORM_WINDOWS
+		FreeLibrary(TextureConverterHandle);
+#endif
 	}
 	virtual ITextureFormat* GetTextureFormat()
 	{
 		if (!Singleton)
 		{
+#if PLATFORM_WINDOWS
+			TextureConverterHandle = LoadLibraryW(*(QualCommBinariesRoot + "TextureConverter.dll"));
+#endif
 			Singleton = new FTextureFormatAndroid();
 		}
 		return Singleton;
--- Engine/Source/ThirdParty/Qualcomm/TextureConverter/QualcommTextureConverter.build.cs
+++ Engine/Source/ThirdParty/Qualcomm/TextureConverter/QualcommTextureConverter.build.cs
@@ -17,19 +17,12 @@ public class QualcommTextureConverter : ModuleRules
 			string LibraryPath = UEBuildConfiguration.UEThirdPartySourceDirectory + "Qualcomm/TextureConverter/Lib/";
 			string LibraryName = "TextureConverter";
 			string LibraryExtension = ".lib";
-			if (Target.Configuration == UnrealTargetConfiguration.Debug && BuildConfiguration.bDebugBuildsActuallyUseDebugCRT && Target.Platform != UnrealTargetPlatform.Mac)
-			{
-				LibraryName += "_d";
-			}
 
 			if (Target.Platform == UnrealTargetPlatform.Win64)
 			{
-				LibraryPath += "vs" + WindowsPlatform.GetVisualStudioCompilerVersionName() + "/x64";
-			}
-			else if (Target.Platform == UnrealTargetPlatform.Win32)
-			{
-				LibraryPath += "vs" + WindowsPlatform.GetVisualStudioCompilerVersionName() + "/Win32";
-			}
+                LibraryPath += "vs" + WindowsPlatform.GetVisualStudioCompilerVersionName() + "/x64";
+                RuntimeDependencies.Add(new RuntimeDependency("$(EngineDir)/Binaries/ThirdParty/QualComm/Win64/" + LibraryName + ".dll"));
+            }
 			else if (Target.Platform == UnrealTargetPlatform.Mac)
 			{
 				LibraryPath += "osx64";
