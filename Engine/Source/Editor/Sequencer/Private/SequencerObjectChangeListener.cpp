d512131bd1ab080d799db0ec286505e317b9f622
Matt Kuhlenschmidt <matt.kuhlenschmidt@epicgames.com>
2015-07-20 15:04:15 -0400

    Do not call deprecated functions with Sequencer
    #codereview frank.fella
    
    [CL 2626377 by Matt Kuhlenschmidt in Main branch]

--- Engine/Source/Editor/Sequencer/Private/SequencerObjectChangeListener.cpp
+++ Engine/Source/Editor/Sequencer/Private/SequencerObjectChangeListener.cpp
@@ -105,7 +105,12 @@ bool FSequencerObjectChangeListener::FindPropertySetter( const UClass& ObjectCla
 
 		FName FunctionName = FName(*FunctionString);
 
-		bFound = ObjectClass.FindFunctionByName(FunctionName) != nullptr;
+		static const FName DeprecatedFunctionName(TEXT("DeprecatedFunction"));
+		UFunction* Function = ObjectClass.FindFunctionByName(FunctionName);
+		if( Function && !Function->HasMetaData(DeprecatedFunctionName) )
+		{
+			bFound = true;
+		}
 	}
 
 	return bFound;
