c8d8559c527fae336ae8e5bc8b2d3af6cd32c36b
nbjk667 <nbjk667@users.noreply.github.com>
2015-07-20 10:42:17 -0400

    PR #1390: Setup.bat improvement (Contributed by nbjk667)
    [CL 2625875 by Ben Marsh in Main branch]

--- Setup.bat
+++ Setup.bat
@@ -10,9 +10,9 @@ rem Setup the git hooks...
 if not exist .git\hooks goto no_git_hooks_directory
 echo Registering git hooks...
 echo #!/bin/sh >.git\hooks\post-checkout
-echo Engine/Binaries/DotNET/GitDependencies.exe >>.git\hooks\post-checkout
+echo Engine/Binaries/DotNET/GitDependencies.exe %* >>.git\hooks\post-checkout
 echo #!/bin/sh >.git\hooks\post-merge
-echo Engine/Binaries/DotNET/GitDependencies.exe >>.git\hooks\post-merge
+echo Engine/Binaries/DotNET/GitDependencies.exe %* >>.git\hooks\post-merge
 :no_git_hooks_directory
 
 rem Install prerequisites...
