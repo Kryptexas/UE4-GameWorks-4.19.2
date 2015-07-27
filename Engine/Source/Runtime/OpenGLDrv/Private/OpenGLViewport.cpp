acbc826191e87efc231fec8320bfe1a5799847cc
Chris Babcock <Chris.Babcock@epicgames.com>
2015-07-27 13:10:25 -0400

    Changed FClearValueBinding::None to Transparent #jira UE-19175 #ue4 #codereview Marcus.Wassmer
    [CL 2634083 by Chris Babcock in Main branch]

--- Engine/Source/Runtime/OpenGLDrv/Private/OpenGLViewport.cpp
+++ Engine/Source/Runtime/OpenGLDrv/Private/OpenGLViewport.cpp
@@ -283,7 +283,7 @@ void FOpenGLViewport::Resize(uint32 InSizeX,uint32 InSizeY,bool bInIsFullscreen)
 	BackBuffer = (FOpenGLTexture2D*)PlatformCreateBuiltinBackBuffer(OpenGLRHI, InSizeX, InSizeY);
 	if (!BackBuffer)
 	{
-		BackBuffer = (FOpenGLTexture2D*)OpenGLRHI->CreateOpenGLTexture(InSizeX, InSizeY, false, false, PixelFormat, 1, 1, 1, TexCreate_RenderTargetable, FClearValueBinding::None);
+		BackBuffer = (FOpenGLTexture2D*)OpenGLRHI->CreateOpenGLTexture(InSizeX, InSizeY, false, false, PixelFormat, 1, 1, 1, TexCreate_RenderTargetable, FClearValueBinding::Transparent);
 	}
 
 	PlatformResizeGLContext(OpenGLRHI->PlatformDevice, OpenGLContext, InSizeX, InSizeY, bInIsFullscreen, bIsFullscreen, BackBuffer->Target, BackBuffer->Resource);
