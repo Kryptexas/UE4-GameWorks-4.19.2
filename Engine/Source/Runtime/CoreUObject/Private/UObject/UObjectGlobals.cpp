f95e8c60ee08d92677e34449ef3e6df15a275177
Mike.Beach <mike.beach@epicgames.com>
2015-07-29 08:44:29 -0400

    [INTEGRATE] Change 2627755 by Mike.Beach@Mike.Beach_UE4 on 2015/07/21 14:02:25
    	[UE-17464] Making sure we defer serialization for CDO that have had their initialization deferred (need to initialize CDOs with their parent's values before we load overridden values, not vise versa).
    
    	#codereview Maciej.Mroz
    
    #jira UE-17464
    
    [CL 2636824 by Matthew Griffin in 4.9 branch]

--- Engine/Source/Runtime/CoreUObject/Private/UObject/UObjectGlobals.cpp
+++ Engine/Source/Runtime/CoreUObject/Private/UObject/UObjectGlobals.cpp
@@ -2178,13 +2178,13 @@ FObjectInitializer::~FObjectInitializer()
 	if (bIsCDO && (ObjectArchetype != nullptr) && !FBlueprintSupport::IsDeferredCDOInitializationDisabled())
 	{
 		UClass* ArchetypeClass = ObjectArchetype->GetClass();
+		const bool bSuperCDONeedsLoad = ObjectArchetype->HasAnyFlags(RF_NeedLoad) ||
+			(ArchetypeClass->GetLinker() && ArchetypeClass->GetLinker()->IsBlueprintFinalizationPending()) ||
+			FDeferredObjInitializerTracker::IsCdoDeferred(ArchetypeClass);
+
 		// if this is a blueprint CDO that derives from another blueprint, and 
 		// that parent (archetype) CDO isn't fully serialized
-		if ( !Class->HasAnyFlags(RF_Native) && !ArchetypeClass->HasAnyFlags(RF_Native) && 
-			(	ObjectArchetype->HasAnyFlags(RF_NeedLoad) ||
-				(ArchetypeClass->GetLinker() && ArchetypeClass->GetLinker()->IsBlueprintFinalizationPending()) ||
-				FDeferredObjInitializerTracker::IsCdoDeferred(ArchetypeClass))
-			)
+		if (!Class->HasAnyFlags(RF_Native) && !ArchetypeClass->HasAnyFlags(RF_Native) && bSuperCDONeedsLoad)
 		{
 			FLinkerLoad* ClassLinker = Class->GetLinker();
 			if ((ClassLinker != nullptr) && (ClassLinker->LoadFlags & LOAD_DeferDependencyLoads) != 0x00)
