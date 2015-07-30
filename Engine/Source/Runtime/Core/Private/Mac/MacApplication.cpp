24c2ae5db18a1814583ed2fb0663e398cfaeb067
Michael.Trepka <michael.trepka@epicgames.com>
2015-07-30 05:22:16 -0400

    [INTEGRATE] Change 2635451 by Michael.Trepka@Michael.Trepka_a3608 on 2015/07/28 11:21:50
    	Fixed mouse position setting/reporting in fullscreen mode on Mac
    
    	#codereview Mark.Satterthwaite
    
    #jira UE-16932
    
    [CL 2638366 by Matthew Griffin in 4.9 branch]

--- Engine/Source/Runtime/Core/Private/Mac/MacApplication.cpp
+++ Engine/Source/Runtime/Core/Private/Mac/MacApplication.cpp
@@ -716,7 +716,7 @@ void FMacApplication::ProcessMouseMovedEvent(const FDeferredMacEvent& Event, TSh
 		}
 		else
 		{
-			MacCursor->UpdateCurrentPosition(CurrentPosition);
+			MacCursor->UpdateCurrentPosition(CurrentPosition / MouseScaling);
 		}
 
 		if (EventWindow.IsValid())
