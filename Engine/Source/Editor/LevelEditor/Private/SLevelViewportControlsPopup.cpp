a2e676a275ebbb4b1bd55c7bc09cdb24b149fd76
Chris.Wood <chris.wood@epicgames.com>
2015-08-05 09:35:20 -0400

    [INTEGRATE] Change 2644736 by Chris.Wood@Chris.Wood.Alpha on 2015/08/05 08:54:28
    	Fixed URL for the LevelViewport controls help popup to work on Mac
    	[UE-18355] - Clicking on show viewport controls on mac comes up with missing page error on mac
    
    	#fix Prefixed URLs correctly with file:// in SLevelViewportControlsPopup
    
    #jira UE-18355
    
    [CL 2644772 by Matthew Griffin in 4.9 branch]

--- Engine/Source/Editor/LevelEditor/Private/SLevelViewportControlsPopup.cpp
+++ Engine/Source/Editor/LevelEditor/Private/SLevelViewportControlsPopup.cpp
@@ -9,11 +9,11 @@ void SLevelViewportControlsPopup::Construct(const FArguments& InArgs)
 {
 	PopupSize.Set(252, 558);
 #if PLATFORM_WINDOWS
-	PopupPath = FPaths::ConvertRelativePathToFull(FPaths::Combine(*FPaths::EngineDir(), TEXT("Documentation/Source/Shared/Editor/Overlays/ViewportControlsWindows.html")));
+	PopupPath = FString(TEXT("file:///")) + FPaths::ConvertRelativePathToFull(FPaths::Combine(*FPaths::EngineDir(), TEXT("Documentation/Source/Shared/Editor/Overlays/ViewportControlsWindows.html")));
 #elif PLATFORM_LINUX
-	PopupPath = FPaths::ConvertRelativePathToFull(FPaths::Combine(*FPaths::EngineDir(), TEXT("Documentation/Source/Shared/Editor/Overlays/ViewportControlsWindows.html")));
+	PopupPath = FString(TEXT("file://")) + FPaths::ConvertRelativePathToFull(FPaths::Combine(*FPaths::EngineDir(), TEXT("Documentation/Source/Shared/Editor/Overlays/ViewportControlsWindows.html")));
 #elif PLATFORM_MAC
-	PopupPath = FPaths::ConvertRelativePathToFull(FPaths::Combine(*FPaths::EngineDir(), TEXT("Documentation/Source/Shared/Editor/Overlays/ViewportControlsMac.html")));
+	PopupPath = FString(TEXT("file://")) + FPaths::ConvertRelativePathToFull(FPaths::Combine(*FPaths::EngineDir(), TEXT("Documentation/Source/Shared/Editor/Overlays/ViewportControlsMac.html")));
 #endif
 
 	if (!PopupPath.IsEmpty())
