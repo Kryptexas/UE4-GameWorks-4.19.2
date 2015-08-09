29311f283e443c257222f48e868cdb8eb57881b9
Dmitry Rekman <Dmitry.Rekman@epicgames.com>
2015-08-08 21:53:51 -0400

    Fix compilation on Linux / mono.
    #codereview Jeff.Campeau
    
    [CL 2649011 by Dmitry Rekman in Main branch]

--- Engine/Plugins/Runtime/SoundMod/Source/SoundMod/SoundMod.Build.cs
+++ Engine/Plugins/Runtime/SoundMod/Source/SoundMod/SoundMod.Build.cs
@@ -21,12 +21,15 @@ namespace UnrealBuildTool.Rules
 				}
 			);
 
-			// VS2015 updated some of the CRT definitions but not all of the Windows SDK has been updated to match.
-			// Microsoft provides this shim library to enable building with VS2015 until they fix everything up.
-			//@todo: remove when no longer neeeded (no other code changes should be necessary).
-			if (WindowsPlatform.Compiler == WindowsCompiler.VisualStudio2015)
+			if (Target.Platform == UnrealTargetPlatform.Win64 || Target.Platform == UnrealTargetPlatform.Win32)
 			{
-				PublicAdditionalLibraries.Add("legacy_stdio_definitions.lib");
+				// VS2015 updated some of the CRT definitions but not all of the Windows SDK has been updated to match.
+				// Microsoft provides this shim library to enable building with VS2015 until they fix everything up.
+				//@todo: remove when no longer neeeded (no other code changes should be necessary).
+				if (WindowsPlatform.Compiler == WindowsCompiler.VisualStudio2015)
+				{
+					PublicAdditionalLibraries.Add("legacy_stdio_definitions.lib");
+				}
 			}
 
 			// Link with managed Perforce wrapper assemblies
