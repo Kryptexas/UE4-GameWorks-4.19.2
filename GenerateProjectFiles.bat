df57428a1ecfe6345c07fd4e9cc2343c2175b02e
Ben.Marsh <ben.marsh@epicgames.com>
2015-08-10 15:17:21 -0400

    [INTEGRATE] Change 2649788 by Ben.Marsh@Ben.Marsh_T3245_Main on 2015/08/10 14:39:14
    	Fix errorlevel not getting passed as the exit code from CMD.EXE when running GenerateProjectFiles.bat.
    
    [CL 2649858 by Ben Marsh in 4.9 branch]

--- GenerateProjectFiles.bat
+++ GenerateProjectFiles.bat
@@ -4,11 +4,9 @@ cmd.exe /v /c "%~dp0Engine\Build\BatchFiles\CopyVisualizers.bat"
 
 if not exist "%~dp0Engine\Build\BatchFiles\GenerateProjectFiles.bat" goto Error_BatchFileInWrongLocation
 call "%~dp0Engine\Build\BatchFiles\GenerateProjectFiles.bat" %*
-goto Exit
+exit /B %ERRORLEVEL%
 
 :Error_BatchFileInWrongLocation
 echo GenerateProjectFiles ERROR: The batch file does not appear to be located in the root UE4 directory.  This script must be run from within that directory.
 pause
-goto Exit
-
-:Exit
+exit /B 1
