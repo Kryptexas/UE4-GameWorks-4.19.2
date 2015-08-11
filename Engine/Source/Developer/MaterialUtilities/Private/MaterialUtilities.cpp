add3fb34833358b18d8ccda0d1719e4a1e97d263
Dmitriy.Dyomin <dmitriy.dyomin@epicgames.com>
2015-08-11 03:55:59 -0400

    [INTEGRATE] Change 2648963 by Dmitriy.Dyomin@Dmitriy-PC_Gunja on 2015/08/08 07:37:01
    	Fixed issues with normal map during material flattening
    
    #jira UE-19878
    
    [CL 2650832 by Matthew Griffin in 4.9 branch]

--- Engine/Source/Developer/MaterialUtilities/Private/MaterialUtilities.cpp
+++ Engine/Source/Developer/MaterialUtilities/Private/MaterialUtilities.cpp
@@ -673,7 +673,7 @@ bool FMaterialUtilities::ExportMaterial(UWorld* InWorld, UMaterialInterface* InM
 		RenderTargetNormal->ClearColor = FLinearColor(0.0f, 0.0f, 0.0f, 0.0f);
 		RenderTargetNormal->InitCustomFormat(
 			OutFlattenMaterial.NormalSize.X,
-			OutFlattenMaterial.NormalSize.Y, PF_FloatRGB, true);
+			OutFlattenMaterial.NormalSize.Y, PF_B8G8R8A8, true);
 
 		//
 		OutFlattenMaterial.NormalSamples.Empty(OutFlattenMaterial.NormalSize.X * OutFlattenMaterial.NormalSize.Y);
