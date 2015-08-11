62e2fada9634778a02643023153d7ef0fac10b1e
Ori Cohen <Ori.Cohen@epicgames.com>
2015-08-11 15:19:37 -0400

    Fix bad merge of create_projects stomping the custom logic for generating projects in main.
    [CL 2651598 by Ori Cohen in Main branch]

--- Engine/Source/ThirdParty/PhysX/UE4_GenerateThirdPartyProjects.bat
+++ Engine/Source/ThirdParty/PhysX/UE4_GenerateThirdPartyProjects.bat
@@ -1,6 +1,19 @@
 @echo off
 setlocal
 
+:args
+if "%1" == "-c" set THIRD_PARTY_CHANGELIST=-c %2
+if "%1" == "" goto start
+shift
+goto args
+
+:start
+
+p4 edit %THIRD_PARTY_CHANGELIST% PhysX-3.3/*
+p4 edit %THIRD_PARTY_CHANGELIST% PhysX-3.3/Source/compiler/...
+p4 edit %THIRD_PARTY_CHANGELIST% APEX-1.3/*
+p4 edit %THIRD_PARTY_CHANGELIST% APEX-1.3/compiler/...
+
 xcopy /I /Y /S /D ".\..\NotForLicensees\PhysX\PhysX_3.3\buildtools\*" ".\PhysX-3.3\buildtools"
 xcopy /I /Y /S /D ".\..\NotForLicensees\PhysX\PhysX_3.3\source\compiler\xpj\*" ".\PhysX-3.3\source\compiler\xpj"
 xcopy /I /Y /S /D ".\..\NotForLicensees\PhysX\APEX_1.3_vs_PhysX_3.3\compiler\xpj\*" ".\APEX-1.3\compiler\xpj"
@@ -16,5 +29,7 @@ popd
 pushd APEX-1.3\compiler\xpj
 call create_projects.cmd
 popd
+
+p4 revert -a %THIRD_PARTY_CHANGELIST%
 
 endlocal
