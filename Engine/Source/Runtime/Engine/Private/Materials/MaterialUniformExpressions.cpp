a0cdd73a64cbc54b822dd739630b781efb7631d6
Gareth.Martin <gareth.martin@epicgames.com>
2015-08-11 05:32:38 -0400

    [INTEGRATE] Change 2649547 by Gareth.Martin@Pitbull-GMartin on 2015/08/10 11:51:22
    	UE-17608 - More log info to try and narrow down the cause
    
    #jira UE-17608
    
    [CL 2650901 by Matthew Griffin in 4.9 branch]

--- Engine/Source/Runtime/Engine/Private/Materials/MaterialUniformExpressions.cpp
+++ Engine/Source/Runtime/Engine/Private/Materials/MaterialUniformExpressions.cpp
@@ -349,6 +349,12 @@ FUniformBufferRHIRef FUniformExpressionSet::CreateUniformBuffer(const FMaterialR
 			const UTexture* Value;
 			ESamplerSourceMode SourceMode;
 			Uniform2DTextureExpressions[ExpressionIndex]->GetTextureValue(MaterialRenderContext,MaterialRenderContext.Material,Value,SourceMode);
+
+			// gmartin: Trying to locate UE-17608
+			checkf(!Value || Value->IsValidLowLevel(), TEXT("Texture not valid! UE-17608! Parameter (%s)"),
+				   Uniform2DTextureExpressions[ExpressionIndex]->GetType() == &FMaterialUniformExpressionTextureParameter::StaticType ?
+				   	*((FMaterialUniformExpressionTextureParameter&)*Uniform2DTextureExpressions[ExpressionIndex]).GetParameterName().ToString() : TEXT("non-parameter"));
+
 			if (Value && Value->Resource)
 			{
 				//@todo-rco: Help track down a invalid values
