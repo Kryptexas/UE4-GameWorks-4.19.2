5e1e768ad8be9035a979d60155b9f797a87f2a81
Richard.TalbotWatkin <richard.talbotwatkin@epicgames.com>
2015-08-13 11:59:29 -0400

    [INTEGRATE] Change 2654602 by Richard.TalbotWatkin@Richard.TalbotWatkin_Main on 2015/08/13 10:36:03
    	Fixed issue where a group of material nodes containing a MaterialFunctionCall node lost their connections when copy+pasted.
    	#jira UE-19536 - Material nodes do not keep their connections when copied to another material
    	#codereview Matt.Kuhlenschmidt
    
    #jira UE-19536
    
    [CL 2654813 by Ben Marsh in 4.9 branch]

--- Engine/Source/Runtime/Engine/Private/Materials/MaterialExpressions.cpp
+++ Engine/Source/Runtime/Engine/Private/Materials/MaterialExpressions.cpp
@@ -7180,7 +7180,7 @@ bool UMaterialExpressionMaterialFunctionCall::SetMaterialFunction(
 }
 
 
-void UMaterialExpressionMaterialFunctionCall::UpdateFromFunctionResource()
+void UMaterialExpressionMaterialFunctionCall::UpdateFromFunctionResource(bool bRecreateAndLinkNode)
 {
 	TArray<FFunctionExpressionInput> OriginalInputs = FunctionInputs;
 	TArray<FFunctionExpressionOutput> OriginalOutputs = FunctionOutputs;
@@ -7243,7 +7243,7 @@ void UMaterialExpressionMaterialFunctionCall::UpdateFromFunctionResource()
 	}
 
 #if WITH_EDITOR
-	if (GraphNode)
+	if (GraphNode && bRecreateAndLinkNode)
 	{
 		// Check whether number of input/outputs or transient pointers have changed
 		bool bUpdatedFromFunction = false;
