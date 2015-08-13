48d55868bc5f546a1a65cd07e2912357741c88fb
Daniel Lamb <Daniel.Lamb@epicgames.com>
2015-08-13 15:47:02 -0400

    Fixed chunking issue where secondary chunks wouldn't be generated because file paths didn't, because they were compaired with / without extensions. #codereview James.Moran
    [CL 2655108 by Daniel Lamb in Main branch]

--- Engine/Source/Programs/AutomationTool/Scripts/CopyBuildToStagingDirectory.Automation.cs
+++ Engine/Source/Programs/AutomationTool/Scripts/CopyBuildToStagingDirectory.Automation.cs
@@ -937,7 +937,15 @@ public partial class Project : CommandUtils
 			bool bAddedToChunk = false;
 			for (int ChunkIndex = 0; !bAddedToChunk && ChunkIndex < ChunkResponseFiles.Length; ++ChunkIndex)
 			{
-				if (ChunkResponseFiles[ChunkIndex].Contains(StagingFile.Key) || ChunkResponseFiles[ChunkIndex].Contains(StagingFile.Key.Replace('/','\\')))
+                string OriginalFilename = StagingFile.Key;
+                string NoExtension = CombinePaths(Path.GetDirectoryName(OriginalFilename), Path.GetFileNameWithoutExtension(OriginalFilename));
+                string OriginalReplaceSlashes = OriginalFilename.Replace('/', '\\');
+                string NoExtensionReplaceSlashes = NoExtension.Replace('/', '\\');
+
+				if (ChunkResponseFiles[ChunkIndex].Contains(OriginalFilename) || 
+                    ChunkResponseFiles[ChunkIndex].Contains(OriginalReplaceSlashes) ||
+                    ChunkResponseFiles[ChunkIndex].Contains(NoExtension) ||
+                    ChunkResponseFiles[ChunkIndex].Contains(NoExtensionReplaceSlashes))
 				{
 					PakResponseFiles[ChunkIndex].Add(StagingFile.Key, StagingFile.Value);
 					bAddedToChunk = true;
