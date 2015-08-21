8180ae7d1a311b515043afcbd4b93daa74b549af
Mike Beach <Mike.Beach@epicgames.com>
2015-08-21 16:03:29 -0400

    Fix to CIS: missing include file.
    [CL 2664856 by Mike Beach in Main branch]

--- Engine/Plugins/Experimental/UnitTestFramework/Source/UnitTestFrameworkEditor/Private/UTFEditorAutomationTests.cpp
+++ Engine/Plugins/Experimental/UnitTestFramework/Source/UnitTestFrameworkEditor/Private/UTFEditorAutomationTests.cpp
@@ -1,6 +1,7 @@
 // Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
 
 #include "UnitTestFrameworkEditorPCH.h"
+#include "UTFEditorAutomationTests.h"
 #include "AutomationTest.h"
 #include "AssetRegistryModule.h"	// for FAssetRegistry::GetAssets()
 #include "AssetData.h"
