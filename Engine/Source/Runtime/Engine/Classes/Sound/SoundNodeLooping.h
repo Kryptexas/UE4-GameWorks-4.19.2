5bf5e4b5b695856c19f34b2d7f8cc2d7748f3723
Richard Hinckley <Richard.Hinckley@epicgames.com>
2015-08-24 15:11:43 -0400

    [UEDOC-2241] Adding suggested text to Sound Looping Node tooltip.
    [CL 2666462 by Richard Hinckley in Main branch]

--- Engine/Source/Runtime/Engine/Classes/Sound/SoundNodeLooping.h
+++ Engine/Source/Runtime/Engine/Classes/Sound/SoundNodeLooping.h
@@ -6,7 +6,10 @@
 #include "SoundNodeLooping.generated.h"
 
 /** 
- * Defines how a sound loops; either indefinitely, or for a set number of times
+ * Defines how a sound loops; either indefinitely, or for a set number of times.
+ * Note: The Looping node should only be used for logical or procedural looping such as introducing a delay.
+ * These sounds will not be played seamlessly. If you want a sound to loop seamlessly and indefinitely,
+ * use the Looping flag on the Wave Player node for that sound.
  */
 UCLASS(hidecategories=Object, editinlinenew, MinimalAPI, meta=( DisplayName="Looping" ))
 class USoundNodeLooping : public USoundNode
@@ -22,7 +25,7 @@ class USoundNodeLooping : public USoundNode
 	uint32 bLoopIndefinitely : 1;
 
 public:	
-	// Begin USoundNode interface. 
+	//~ Begin USoundNode interface. 
 	virtual bool NotifyWaveInstanceFinished( struct FWaveInstance* WaveInstance ) override;
 	virtual float MaxAudibleDistance( float CurrentMaxDistance ) override 
 	{ 
@@ -30,7 +33,7 @@ public:
 	}
 	virtual float GetDuration( void ) override;
 	virtual void ParseNodes( FAudioDevice* AudioDevice, const UPTRINT NodeWaveInstanceHash, FActiveSound& ActiveSound, const FSoundParseParameters& ParseParams, TArray<FWaveInstance*>& WaveInstances ) override;
-	// End USoundNode interface. 
+	//~ End USoundNode interface. 
 };
 
 
