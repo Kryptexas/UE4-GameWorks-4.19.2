d916582bdd1f3ff5d913b3a6d3108f4394e3ab48
Dan Oconnor <Dan.OConnor@epicgames.com>
2015-08-27 19:19:58 -0400

    2549150 was a little overzealous, default values of inherited variables (blueprint or native) should still be editable. #codereview Michael.Schoell
    [CL 2671770 by Dan Oconnor in Main branch]

--- Engine/Source/Editor/Kismet/Private/BlueprintDetailsCustomization.cpp
+++ Engine/Source/Editor/Kismet/Private/BlueprintDetailsCustomization.cpp
@@ -715,16 +715,12 @@ void FBlueprintVarActionDetails::CustomizeDetails( IDetailLayoutBuilder& DetailL
 
 				IDetailPropertyRow* Row = DefaultValueCategory.AddExternalProperty(StructData, VariableProperty->GetFName());
 			}
-			else if (GetPropertyOwnerBlueprint())
+			else
 			{
 				// Things are in order, show the property and allow it to be edited
 				TArray<UObject*> ObjectList;
-				ObjectList.Add(GetPropertyOwnerBlueprint()->GeneratedClass->GetDefaultObject());
+				ObjectList.Add(Blueprint->GeneratedClass->GetDefaultObject());
 				IDetailPropertyRow* Row = DefaultValueCategory.AddExternalProperty(ObjectList, VariableProperty->GetFName());
-				if (Row != nullptr)
-				{
-					Row->IsEnabled(IsVariableInBlueprint());
-				}
 			}
 		}
 
