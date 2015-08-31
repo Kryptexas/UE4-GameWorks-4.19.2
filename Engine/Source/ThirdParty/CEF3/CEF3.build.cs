fc8a9a5d238e7b5be61b8d85a76839c4f1c546ff
Keli Hlodversson <Keli.Hlodversson@epicgames.com>
2015-08-31 18:02:48 -0400

    [INTEGREATE] Merging CL#2661261  using UE4_to_UE4-Fortnite: Include CEF ThirdParty binaries in the build receipt. #jira FORT-13868
    [CL 2674788 by Keli Hlodversson in Main branch]

--- Engine/Source/ThirdParty/CEF3/CEF3.build.cs
+++ Engine/Source/ThirdParty/CEF3/CEF3.build.cs
@@ -31,20 +31,20 @@ public class CEF3 : ModuleRules
 		{
 			Definitions.Add("WITH_CEF3=1");
 
-			string PlatformPath = UEBuildConfiguration.UEThirdPartySourceDirectory + "CEF3/cef_binary_" + CEFVersion + "_" + CEFPlatform;
+			string PlatformPath = Path.Combine(UEBuildConfiguration.UEThirdPartySourceDirectory, "CEF3", "cef_binary_" + CEFVersion + "_" + CEFPlatform);
 
 			PublicSystemIncludePaths.Add(PlatformPath);
 
-			string LibraryPath = PlatformPath + "/Release";
+			string LibraryPath = Path.Combine(PlatformPath, "Release");
+            string RuntimePath = Path.Combine(UEBuildConfiguration.UEThirdPartyBinariesDirectory , "CEF3", Target.Platform.ToString());
 
 			if (Target.Platform == UnrealTargetPlatform.Win64 || Target.Platform == UnrealTargetPlatform.Win32)
 			{
-                string VSVersionFolderName = "/VS" + WindowsPlatform.GetVisualStudioCompilerVersionName();
-                LibraryPath += VSVersionFolderName;
+                string VSVersionFolderName = "VS" + WindowsPlatform.GetVisualStudioCompilerVersionName();
+                LibraryPath = Path.Combine(LibraryPath, VSVersionFolderName);
 
                 PublicLibraryPaths.Add(LibraryPath);
 
-				//PublicAdditionalLibraries.Add("cef_sandbox.lib");
 				PublicAdditionalLibraries.Add("libcef.lib");
 				if (Target.Configuration == UnrealTargetConfiguration.Debug && BuildConfiguration.bDebugBuildsActuallyUseDebugCRT)
 				{
@@ -55,20 +55,37 @@ public class CEF3 : ModuleRules
 					PublicAdditionalLibraries.Add("libcef_dll_wrapper.lib");
 				}
 
-				PublicDelayLoadDLLs.Add("d3dcompiler_43.dll");
-				PublicDelayLoadDLLs.Add("d3dcompiler_47.dll");
-				PublicDelayLoadDLLs.Add("ffmpegsumo.dll");
-				PublicDelayLoadDLLs.Add("libcef.dll");
-				PublicDelayLoadDLLs.Add("libEGL.dll");
-                PublicDelayLoadDLLs.Add("libGLESv2.dll");
-                PublicDelayLoadDLLs.Add("pdf.dll");
+                
+                string[] Dlls = {
+                    "d3dcompiler_43.dll",
+                    "d3dcompiler_47.dll",
+                    "ffmpegsumo.dll",
+                    "libcef.dll",
+                    "libEGL.dll",
+                    "libGLESv2.dll",
+                    "pdf.dll",
+                };
 
-				// Add contents of binaries directory as runtime dependencies
-				foreach (string FilePath in Directory.EnumerateFiles(UEBuildConfiguration.UEThirdPartyBinariesDirectory + "CEF3/" + Target.Platform.ToString() + "/", "*", SearchOption.AllDirectories))
-				{
-					RuntimeDependencies.Add(new RuntimeDependency(FilePath));
-				}
-			}
+				PublicDelayLoadDLLs.AddRange(Dlls);
+
+                // Add the runtime dlls to the build receipt
+                foreach (string Dll in Dlls)
+                {
+                    RuntimeDependencies.Add(new RuntimeDependency("$(EngineDir)/Binaries/ThirdParty/CEF3/" + Target.Platform.ToString() + "/" + Dll));
+                }
+                // We also need the icu translations table required by CEF
+                RuntimeDependencies.Add(new RuntimeDependency("$(EngineDir)/Binaries/ThirdParty/CEF3/" + Target.Platform.ToString() + "/icudtl.dat"));
+
+                // And the entire Resources folder. Enunerate the entire directory instead of mentioning each file manually here.
+                foreach (string FileName in Directory.EnumerateFiles(Path.Combine(RuntimePath, "Resources"), "*", SearchOption.AllDirectories))
+                {
+                    string DependencyName = FileName.Substring(UEBuildConfiguration.UEThirdPartyBinariesDirectory.Length).Replace('\\', '/');
+                    RuntimeDependencies.Add(new RuntimeDependency("$(EngineDir)/Binaries/ThirdParty/" + DependencyName));
+                }
+
+                // UnrealCefSubprocess should also be added @Tdodo is this the right place to mention this? Seems like a reversal of dependencies to me
+                RuntimeDependencies.Add(new RuntimeDependency("$(EngineDir)/Binaries/" + Target.Platform.ToString() + "/UnrealCEFSubProcess.exe"));
+            }
 			// TODO: Ensure these are filled out correctly when adding other platforms
 			else if (Target.Platform == UnrealTargetPlatform.Mac)
 			{
