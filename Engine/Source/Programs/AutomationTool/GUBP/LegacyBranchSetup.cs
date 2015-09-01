4d4850714bbf36003be348faaa5136df9e0dbae4
Bob Tellez <Bob.Tellez@epicgames.com>
2015-08-31 23:05:10 -0400

    #UE4 CIS fix. Removed code recently merged from UE4-Fortnite adding GameAggregatePromotableNodes. These no longer exist.
    [CL 2675088 by Bob Tellez in Main branch]

--- Engine/Source/Programs/AutomationTool/GUBP/LegacyBranchSetup.cs
+++ Engine/Source/Programs/AutomationTool/GUBP/LegacyBranchSetup.cs
@@ -1171,9 +1171,6 @@ partial class GUBP
 		foreach (BranchInfo.BranchUProject GameProj in Branch.AllProjects)
 		{
 			List<string> NodeNames = new List<string>();
-			{
-				NodeNames.Add(GameAggregatePromotableNode.StaticGetFullName(GameProj));
-			}
 			foreach(GUBP.GUBPNode Node in BranchConfig.GUBPNodes.Values)
 			{
 				if (Node.GameNameIfAnyForTempStorage() == GameProj.GameName)
