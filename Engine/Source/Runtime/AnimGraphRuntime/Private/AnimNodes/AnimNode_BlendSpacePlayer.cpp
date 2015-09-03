50599f7ed2cf896214f7fbaddd62b6b12ed166ba
Zak Middleton <Zak.Middleton@epicgames.com>
2015-09-03 18:11:12 -0400

    #ue4 - Fix for crash running QAGame PIE 100% of the time in anim node code in debug.
    #codereview Thomas.Sarkanen, Lina.Halper
    
    [CL 2679501 by Zak Middleton in Main branch]

--- Engine/Source/Runtime/AnimGraphRuntime/Private/AnimNodes/AnimNode_BlendSpacePlayer.cpp
+++ Engine/Source/Runtime/AnimGraphRuntime/Private/AnimNodes/AnimNode_BlendSpacePlayer.cpp
@@ -21,6 +21,8 @@ FAnimNode_BlendSpacePlayer::FAnimNode_BlendSpacePlayer()
 
 void FAnimNode_BlendSpacePlayer::Initialize(const FAnimationInitializeContext& Context)
 {
+	FAnimNode_AssetPlayerBase::Initialize(Context);
+
 	BlendSampleDataCache.Empty();
 	
 	EvaluateGraphExposedInputs.Execute(Context);
