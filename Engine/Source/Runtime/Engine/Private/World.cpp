952575fdd37cacd6d2f4344cc8861f7237f98c08
Marc Audy <Marc.Audy@epicgames.com>
2015-09-06 14:45:35 -0400

    Fix RepairWorldSettings crash
    [CL 2682007 by Marc Audy in Main branch]

--- Engine/Source/Runtime/Engine/Private/World.cpp
+++ Engine/Source/Runtime/Engine/Private/World.cpp
@@ -836,6 +836,14 @@ void UWorld::RepairWorldSettings()
 			PersistentLevel->Actors[0]->Rename(NULL, PersistentLevel, REN_ForceNoResetLoaders);
 		}
 		
+		bool bClearOwningWorld = false;
+
+		if (PersistentLevel->OwningWorld == nullptr)
+		{
+			bClearOwningWorld = true;
+			PersistentLevel->OwningWorld = this;
+		}
+
 		FActorSpawnParameters SpawnInfo;
 		SpawnInfo.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
 		SpawnInfo.Name = GEngine->WorldSettingsClass->GetFName();
@@ -861,6 +869,10 @@ void UWorld::RepairWorldSettings()
 		// Re-sort actor list as we just shuffled things around.
 		PersistentLevel->SortActorList();
 
+		if (bClearOwningWorld)
+		{
+			PersistentLevel->OwningWorld = nullptr;
+		}
 	}
 	check(GetWorldSettings());
 }
