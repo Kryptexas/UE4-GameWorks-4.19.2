e67345d3b7c44c9c6e57e5dc7514c27c1aa24401
Jamie Dale <Jamie.Dale@epicgames.com>
2015-09-07 13:28:57 -0400

    Added a re-usable FString <-> icu::UnicodeString converter object
    This can be used to avoid the overhead of creating a new ICU UConverter object for each string conversion.
    
    [CL 2682544 by Jamie Dale in Main branch]

--- Engine/Source/Runtime/Core/Private/Internationalization/ICUUtilities.h
+++ Engine/Source/Runtime/Core/Private/Internationalization/ICUUtilities.h
@@ -7,9 +7,37 @@
 
 namespace ICUUtilities
 {
-	/** Convert FString <-> icu::UnicodeString */
+	/** 
+	 * An object that can convert between FString and icu::UnicodeString
+	 * Note: This object is not thread-safe.
+	 */
+	class FStringConverter
+	{
+	public:
+		FStringConverter();
+		~FStringConverter();
+
+		/** Convert FString -> icu::UnicodeString */
+		void ConvertString(const FString& Source, icu::UnicodeString& Destination, const bool ShouldNullTerminate = true);
+		icu::UnicodeString ConvertString(const FString& Source, const bool ShouldNullTerminate = true);
+
+		/** Convert icu::UnicodeString -> FString */
+		void ConvertString(const icu::UnicodeString& Source, FString& Destination);
+		FString ConvertString(const icu::UnicodeString& Source);
+
+	private:
+		/** Non-copyable */
+		FStringConverter(const FStringConverter&);
+		FStringConverter& operator=(const FStringConverter&);
+
+		UConverter* ICUConverter;
+	};
+
+	/** Convert FString -> icu::UnicodeString */
 	void ConvertString(const FString& Source, icu::UnicodeString& Destination, const bool ShouldNullTerminate = true);
 	icu::UnicodeString ConvertString(const FString& Source, const bool ShouldNullTerminate = true);
+
+	/** Convert icu::UnicodeString -> FString */
 	void ConvertString(const icu::UnicodeString& Source, FString& Destination);
 	FString ConvertString(const icu::UnicodeString& Source);
 }
