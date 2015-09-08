acb3127c6b10ab7f4d3e2df5ba6829f57eafd3a5
Jurre deBaare <jurre.debaare@epicgames.com>
2015-09-08 04:41:06 -0400

    Mac Non-unity build errors
    [CL 2682784 by Jurre deBaare in Main branch]

--- Engine/Source/Editor/GeometryCacheEd/Public/GeometryCacheEdModulePublicPCH.h
+++ Engine/Source/Editor/GeometryCacheEd/Public/GeometryCacheEdModulePublicPCH.h
@@ -1,7 +1,5 @@
 // Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
 
-//#include "ActorFactoryGeometryCache.h"
-//#include "AssetTypeActions_GeometryCache.h"
-//#include "GeometryCacheAssetBroker.h"
-//#include "GeometryCacheThumbnailRenderer.h"
-//#include "GeometryCacheThumbnailScene.h"
+#include "Core.h"
+#include "CoreUObject.h"
+#include "Engine.h"
