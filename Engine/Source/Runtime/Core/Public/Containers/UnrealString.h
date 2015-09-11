954b59c1c544013e532ec920caba809f05ce7a0d
Steve Robb <Steve.Robb@epicgames.com>
2015-09-11 10:57:23 -0400

    Undoing overzealous sizeof.
    #codereview robert.manuszewki
    
    [CL 2687972 by Steve Robb in Main branch]

--- Engine/Source/Runtime/Core/Public/Containers/UnrealString.h
+++ Engine/Source/Runtime/Core/Public/Containers/UnrealString.h
@@ -1773,7 +1773,7 @@ inline const uint8 TCharToNibble( const TCHAR Char )
 inline int32 HexToBytes( const FString& HexString, uint8* OutBytes )
 {
 	int32 NumBytes = 0;
-	const bool bPadNibble = ( HexString.Len() % sizeof(TCHAR) ) == 1;
+	const bool bPadNibble = ( HexString.Len() % 2 ) == 1;
 	const TCHAR* CharPos = *HexString;
 	if( bPadNibble )
 	{
