f70e6dc198214f66d7277f4514b2ce1efc6868b7
Martin Mittring <martin.mittring@epicgames.com>
2015-09-17 19:19:43 -0400

    Merging
    //depot/UE4-Orion/Engine/Source/Runtime/Renderer/Private/ConvertToUniformMesh.cpp
    
    to //depot/UE4/Engine/Source/Runtime/Renderer/Private/ConvertToUniformMesh.cpp
    
    fixed comment
    
    [CL 2696322 by Martin Mittring in Main branch]

--- Engine/Source/Runtime/Renderer/Private/ConvertToUniformMesh.cpp
+++ Engine/Source/Runtime/Renderer/Private/ConvertToUniformMesh.cpp
@@ -1,7 +1,7 @@
 // Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
 
 /*=============================================================================
-	SurfelTree.cpp
+	ConvertToUniformMesh.cpp
 =============================================================================*/
 
 #include "RendererPrivate.h"
