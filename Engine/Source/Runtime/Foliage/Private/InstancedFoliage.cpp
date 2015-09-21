1ef165d80b02799829b19ca6946521f151d0e5f8
Jack Porter <jack.porter@epicgameskorea.com>
2015-09-21 04:28:21 -0400

    Merge of Main CL 2643030 Fixed instance highlight selection remaining after undo #jira UE-19385
    [CL 2698685 by Jack Porter in 4.10 branch]

--- Engine/Source/Runtime/Foliage/Private/InstancedFoliage.cpp
+++ Engine/Source/Runtime/Foliage/Private/InstancedFoliage.cpp
@@ -1061,10 +1061,10 @@ void FFoliageMeshInfo::ReapplyInstancesToComponent()
 			Component->AddInstanceWorldSpace(Instance.GetInstanceWorldTransform());
 		}
 
+		Component->ClearInstanceSelection();
+
 		if (SelectedIndices.Num())
-		{
-			Component->ClearInstanceSelection();
-			
+		{	
 			for (int32 i : SelectedIndices)
 			{
 				Component->SelectInstance(true, i, 1);
