e64d3e9c5060229026b85529f50361c21dc90e25
Ben Marsh <Ben.Marsh@epicgames.com>
2015-09-21 08:08:29 -0400

    Remove "DO NOT MERGE THIS BACK TO MAIN" code.
    [CL 2698791 by Ben Marsh in Main branch]

--- Engine/Source/Programs/UnrealBuildTool/System/Unity.cs
+++ Engine/Source/Programs/UnrealBuildTool/System/Unity.cs
@@ -277,9 +277,7 @@ namespace UnrealBuildTool
 				AllUnityFiles = CPPUnityFileBuilder.GetUnityFiles();
 			}
 
-			//THIS CHANGE WAS MADE TO FIX THE BUILD IN OUR BRANCH
-			//DO NOT MERGE THIS BACK TO MAIN
-			string PCHHeaderNameInCode = CPPFiles.Count > 0 ? CPPFiles[0].PCHHeaderNameInCode : "";
+			string PCHHeaderNameInCode = CPPFiles[0].PCHHeaderNameInCode;
 			if( CompileEnvironment.Config.PrecompiledHeaderIncludeFilename != null )
 			{
 				PCHHeaderNameInCode = ToolChain.ConvertPath( CompileEnvironment.Config.PrecompiledHeaderIncludeFilename.FullName );
