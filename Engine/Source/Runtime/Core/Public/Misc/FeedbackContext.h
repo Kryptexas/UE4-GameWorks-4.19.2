2fc8234639932ad90fa2902c2a9a84f6efdd4274
Andrew Rodham <Andrew.Rodham@epicgames.com>
2015-09-24 05:29:25 -0400

    Changed verify to an ensure This is a non-fatal error
    [CL 2704042 by Andrew Rodham in Main branch]

--- Engine/Source/Runtime/Core/Public/Misc/FeedbackContext.h
+++ Engine/Source/Runtime/Core/Public/Misc/FeedbackContext.h
@@ -269,7 +269,7 @@ public:
 		CompletedWork += CurrentFrameScope;
 
 		const float WorkRemaining = TotalAmountOfWork - CompletedWork;
-		verifySlow(ExpectedWorkThisFrame <= WorkRemaining);
+		ensureMsgf(ExpectedWorkThisFrame <= WorkRemaining, TEXT("Work overflow in slow task. Please revise call-site to account for entire progress range."));
 		CurrentFrameScope = FMath::Min(WorkRemaining, ExpectedWorkThisFrame);
 
 		if (!bCreatedDialog && OpenDialogThreshold.IsSet() && static_cast<float>(FPlatformTime::Seconds() - StartTime) > OpenDialogThreshold.GetValue())
