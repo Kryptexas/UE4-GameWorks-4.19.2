8ca86b2e3ca7d40185fa908c14045dabedfb3d01
Daniel Lamb <Daniel.Lamb@epicgames.com>
2015-09-30 16:33:48 -0400

    Fixed compilation error.
    [CL 2711818 by Daniel Lamb in Main branch]

--- Engine/Source/Runtime/CoreUObject/Private/Serialization/BulkData.cpp
+++ Engine/Source/Runtime/CoreUObject/Private/Serialization/BulkData.cpp
@@ -783,7 +783,9 @@ void FUntypedBulkData::Serialize( FArchive& Ar, UObject* Owner, int32 Idx )
 		// Offset where the bulkdata flags are stored
 		int64 SavedBulkDataFlagsPos = Ar.Tell();
 		{
+#if WITH_EDITOR
 			FArchive::FScopeSetDebugSerializationFlags S(Ar, DSF_IgnoreDiff);
+#endif
 			Ar << BulkDataFlags;
 		}
 
