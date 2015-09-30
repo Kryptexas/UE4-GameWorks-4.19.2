701e214419235e409f4f3803979718f6ec5c2885
Jurre deBaare <jurre.debaare@epicgames.com>
2015-09-30 09:13:50 -0400

    #jira UE-19060 4.10 integration 2632046
    [CL 2710973 by Jurre deBaare in 4.10 branch]

--- Engine/Source/Editor/PhAT/Private/SPhATPreviewToolbar.h
+++ Engine/Source/Editor/PhAT/Private/SPhATPreviewToolbar.h
@@ -26,6 +26,27 @@ private:
 	/** Generates the toolbar modes menu content */
 	TSharedRef<SWidget> GenerateModesMenu() const;
 
+	/**
+	* Generates the toolbar options menu content
+	*
+	* @return The widget containing the options menu content
+	*/
+	TSharedRef<SWidget> GenerateOptionsMenu() const;
+
+
+	/**
+	* Generates the toolbar FOV menu content
+	*
+	* @return The widget containing the FOV menu content
+	*/
+	TSharedRef<SWidget> GenerateFOVMenu() const;
+
+	/** Called by the FOV slider in the perspective viewport to get the FOV value */
+	float OnGetFOVValue() const;
+
+	/** Called when the FOV slider is adjusted in the perspective viewport */
+	void OnFOVValueChanged(float NewValue);
+
 	FText GetCameraMenuLabel() const;
 	const FSlateBrush* GetCameraMenuLabelIcon() const;
 
