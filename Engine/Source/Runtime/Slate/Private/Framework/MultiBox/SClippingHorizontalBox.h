912fcf14648937f3707c672aceaf65b272658b1c
Matt Kuhlenschmidt <matt.kuhlenschmidt@epicgames.com>
2015-10-01 15:20:26 -0400

    Back out changelist 2712839.  The pre-requsite changes were not merged to 4.10
    #jira UE-20058
    
    [CL 2713396 by Matt Kuhlenschmidt in 4.10 branch]

--- Engine/Source/Runtime/Slate/Private/Framework/MultiBox/SClippingHorizontalBox.h
+++ Engine/Source/Runtime/Slate/Private/Framework/MultiBox/SClippingHorizontalBox.h
@@ -18,10 +18,7 @@ public:
 		SLATE_ARGUMENT(FName, StyleName)
 	SLATE_END_ARGS()
 
-	FORCENOINLINE SClippingHorizontalBox() 
-	{
-		bCanTick = true;
-	}
+	FORCENOINLINE SClippingHorizontalBox() { }
 
 	/** SWidget interface */
 	virtual void Tick( const FGeometry& AllottedGeometry, const double InCurrentTime, const float InDeltaTime ) override;
