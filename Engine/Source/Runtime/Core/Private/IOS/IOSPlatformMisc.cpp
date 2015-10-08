7b5c02d3695dc6f587719b711e4366dbacf46b9b
Josh Adams <josh.adams@epicgames.com>
2015-10-08 12:26:14 -0400

    - Fixed typo in device selection #jira UE-21920
    [CL 2721539 by Josh Adams in 4.10 branch]

--- Engine/Source/Runtime/Core/Private/IOS/IOSPlatformMisc.cpp
+++ Engine/Source/Runtime/Core/Private/IOS/IOSPlatformMisc.cpp
@@ -488,11 +488,11 @@ FIOSPlatformMisc::EIOSDevice FIOSPlatformMisc::GetIOSDeviceType()
 			// note that Apple switched the minor order around between 6 and 6S (gotta keep us on our toes!)
 			if (Minor == 1)
 			{
-				DeviceType == IOS_IPhone6;
+				DeviceType == IOS_IPhone6S;
 			}
 			else if (Minor == 2)
 			{
-				DeviceType == IOS_IPhone6Plus;
+				DeviceType == IOS_IPhone6SPlus;
 			}
 		}
 		else if (Major >= 9)
