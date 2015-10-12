e5290e0992e77c7cafa138bf6f9489e55ce72540
Daniel Lamb <Daniel.Lamb@epicgames.com>
2015-10-12 19:47:12 -0400

    Disabled warning about resaving material packages as it doesn't seem to be working correctly.
    [CL 2725729 by Daniel Lamb in Main branch]

--- Engine/Source/Runtime/Engine/Private/Materials/Material.cpp
+++ Engine/Source/Runtime/Engine/Private/Materials/Material.cpp
@@ -3451,10 +3451,6 @@ bool UMaterial::UpdateLightmassTextureTracking()
 
 	if ( bTexturesHaveChanged )
 	{
-		if (IsRunningCommandlet())
-		{
-			UE_LOG(LogMaterial, Warning, TEXT("UMaterial's dependent textures have changed, resave package %s to avoid deterministic cooking issues."), *GetPathName());
-		}
 		// This will invalidate any cached Lightmass material exports
 		SetLightingGuid();
 	}
