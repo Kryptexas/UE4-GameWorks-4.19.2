fc8eba21e94c922e4be4e0b180630b899665d04d
Mike Beach <Mike.Beach@epicgames.com>
2015-10-16 17:20:18 -0400

    [UE-22195] Fixing up UAssetClassProperty so it works with Blueprint class references (handling placeholder classes that result from deferred loading).
    #jira UE-22195
    #lockdown Nick.Whiting
    
    [CL 2732508 by Mike Beach in 4.10 branch]

--- Engine/Source/Runtime/CoreUObject/Public/UObject/UnrealType.h
+++ Engine/Source/Runtime/CoreUObject/Public/UObject/UnrealType.h
@@ -2143,11 +2143,27 @@ public:
 	// UObject interface
 	virtual void Serialize( FArchive& Ar ) override;
 	static void AddReferencedObjects(UObject* InThis, FReferenceCollector& Collector);
+	virtual void BeginDestroy() override;
 	// End of UObject interface
 
 	// UProperty interface
 	virtual bool SameType(const UProperty* Other) const override;
 	// End of UProperty interface
+
+	/**
+	 * Setter function for this property's MetaClass member. Favor this function 
+	 * whilst loading (since, to handle circular dependencies, we defer some 
+	 * class loads and use a placeholder class instead). It properly handles 
+	 * deferred loading placeholder classes (so they can properly be replaced 
+	 * later).
+	 * 
+	 * @param  NewMetaClass    The MetaClass you want this property set with.
+	 */
+#if USE_CIRCULAR_DEPENDENCY_LOAD_DEFERRING
+	void SetMetaClass(UClass* NewMetaClass);
+#else
+	FORCEINLINE void SetMetaClass(UClass* NewMetaClass) { MetaClass = NewMetaClass; }
+#endif // USE_CIRCULAR_DEPENDENCY_LOAD_DEFERRING
 };
 
 /*-----------------------------------------------------------------------------
