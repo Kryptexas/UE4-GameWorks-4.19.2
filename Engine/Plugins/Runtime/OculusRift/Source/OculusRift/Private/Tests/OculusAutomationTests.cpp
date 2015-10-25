7dfd6588d665732e7970b9d52333145c32880e27
Nick Whiting <Nick.Whiting@epicgames.com>
2015-10-25 17:56:29 -0400

    Fix for yet-another-build break with header balancing.
    #jira UE-22585
    
    [CL 2741259 by Nick Whiting in 4.10 branch]

--- Engine/Plugins/Runtime/OculusRift/Source/OculusRift/Private/Tests/OculusAutomationTests.cpp
+++ Engine/Plugins/Runtime/OculusRift/Source/OculusRift/Private/Tests/OculusAutomationTests.cpp
@@ -1,10 +1,11 @@
 // Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
 
+#include "HMDPrivatePCH.h"
 #include "IOculusRiftPlugin.h"
 
 #if OCULUS_RIFT_SUPPORTED_PLATFORMS
 
-#include "HMDPrivatePCH.h"
+
 #include "OculusRiftHMD.h"
 #include "EngineGlobals.h"
 #include "EngineVersion.h"
