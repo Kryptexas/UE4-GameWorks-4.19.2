23375a2e7275a21e831684f0ed753b38a4471347
Nick Whiting <Nick.Whiting@epicgames.com>
2015-10-29 21:56:53 -0400

    Fix for crash in OculusInput plugin when Oculus plugin is disabled.
    #jira UE-22744
    
    [CL 2747224 by Nick Whiting in 4.10 branch]

--- Engine/Plugins/Runtime/OculusInput/Source/Private/OculusInputModule.cpp
+++ Engine/Plugins/Runtime/OculusInput/Source/Private/OculusInputModule.cpp
@@ -5,12 +5,23 @@
 
 #if OCULUS_TOUCH_SUPPORTED_PLATFORMS
 
+#define LOCTEXT_NAMESPACE "OculusTouch"
+
 class FOculusInputModule : public IOculusInputPlugin
 {
 	// IInputDeviceModule overrides
 	virtual TSharedPtr< class IInputDevice > CreateInputDevice( const TSharedRef< FGenericApplicationMessageHandler >& InMessageHandler ) override
 	{
-		return TSharedPtr< class IInputDevice >( new FOculusInput( InMessageHandler ) );
+		if (IOculusRiftPlugin::IsAvailable())
+		{
+			return TSharedPtr< class IInputDevice >(new FOculusInput(InMessageHandler));
+		}
+		else
+		{
+			UE_LOG(LogOcInput, Warning, TEXT("OculusInput plugin enabled, but OculusRift plugin is not available."));
+			return nullptr;
+		}
+		
 	}
 };
 
@@ -20,6 +31,7 @@ class FOculusInputModule : public FDefaultModuleImpl
 {
 };
 
+#undef LOCTEXT_NAMESPACE
 #endif	// OCULUS_TOUCH_SUPPORTED_PLATFORMS
 
 IMPLEMENT_MODULE( FOculusInputModule, OculusInput )
