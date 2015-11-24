9b49a27703d9ea4906817d03c05cec70e86c092a
Benn Gallagher <Benn.Gallagher@epicgames.com>
2015-11-23 19:53:38 -0500

    Back out changelist 2777696 - Build failure as dependant API not available #jira UE-22963
    [CL 2778695 by Benn Gallagher in 4.10 branch]

--- Engine/Source/Editor/Persona/Private/SAnimNotifyPanel.cpp
+++ Engine/Source/Editor/Persona/Private/SAnimNotifyPanel.cpp
@@ -4284,7 +4284,6 @@ void SAnimNotifyPanel::OnPropertyChanged(UObject* ChangedObject, FPropertyChange
 			if(Event.Notify == ChangedObject || Event.NotifyStateClass == ChangedObject)
 			{
 				// If we've changed a notify present in the sequence, refresh our tracks.
-				Sequence->RefreshCacheData();
 				RefreshNotifyTracks();
 			}
 		}
