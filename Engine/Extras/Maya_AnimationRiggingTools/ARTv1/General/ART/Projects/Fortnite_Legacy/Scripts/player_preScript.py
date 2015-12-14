cb58beb916a98ad9d62ab9f2bfcfe4e9ae44f2e6
Jeremy Ernst <Jeremy.Ernst@epicgames.com>
2015-12-14 18:02:45 -0500

    removing uneccessary files
    [CL 2802557 by Jeremy Ernst in Main branch]

--- Engine/Extras/Maya_AnimationRiggingTools/ARTv1/General/ART/Projects/Fortnite_Legacy/Scripts/player_preScript.py
+++
@@ -1,5 +0,0 @@
-if cmds.objExists("ik_foot_root"):
-	cmds.delete("ik_foot_root")
-	
-if cmds.objExists("ik_hand_root"):
-	cmds.delete("ik_hand_root")
--- Engine/Extras/Maya_AnimationRiggingTools/ARTv1/General/ART/Projects/Orion/Scripts/Coil_Pre_Script.py
+++
@@ -1,7 +0,0 @@
-import maya.cmds as cmds
-
-if cmds.objExists("ik_foot_root"):
-	cmds.delete("ik_foot_root")
-	
-if cmds.objExists("ik_hand_root"):
-	cmds.delete("ik_hand_root")
