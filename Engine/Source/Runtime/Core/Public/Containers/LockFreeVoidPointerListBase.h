73564819569b37a7f7b9f272ba573839cb254f75
Gil Gribb <gil.gribb@epicgames.com>
2016-01-06 09:59:42 -0500

    UE4 - Added memory barrier to fix very rare hang on consoles. #jira UE-24988
    [CL 2817167 by Gil Gribb in 4.11 branch]

--- Engine/Source/Runtime/Core/Public/Containers/LockFreeVoidPointerListBase.h
+++ Engine/Source/Runtime/Core/Public/Containers/LockFreeVoidPointerListBase.h
@@ -1529,6 +1529,7 @@ public:
 				break;
 			}
 			uint32 Slot = LocalNumPopped % DequeueCacheSize;
+			FPlatformMisc::MemoryBarrier();
 			void *ReturnPtr = Available[Slot];
 			TCounter Result = (TCounter)FPlatformAtomics::InterlockedCompareExchange((volatile TSignedCounter*)&NumPopped, LocalNumPopped + 1, LocalNumPopped);
 			if (Result == LocalNumPopped)
