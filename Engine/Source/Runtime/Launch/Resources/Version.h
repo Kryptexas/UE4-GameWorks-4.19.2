0796c057fc194f64b75c1fc636131062a888d16d
Ben Marsh <Ben.Marsh@epicgames.com>
2016-01-08 16:57:19 -0500

    Update copyright notices in Version.h and Metadata.cs.
    #lockdown Nick.Penwarden
    
    [CL 2821565 by Ben Marsh in Main branch]

--- Engine/Source/Runtime/Launch/Resources/Version.h
+++ Engine/Source/Runtime/Launch/Resources/Version.h
@@ -1,4 +1,4 @@
-// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
 
 #pragma once
 
@@ -14,7 +14,7 @@
 #define ENGINE_IS_PROMOTED_BUILD (BUILT_FROM_CHANGELIST > 0)
 
 #define EPIC_COMPANY_NAME  "Epic Games, Inc."
-#define EPIC_COPYRIGHT_STRING "Copyright 1998-2015 Epic Games, Inc. All Rights Reserved."
+#define EPIC_COPYRIGHT_STRING "Copyright 1998-2016 Epic Games, Inc. All Rights Reserved."
 #define EPIC_PRODUCT_NAME "Unreal Engine"
 #define EPIC_PRODUCT_IDENTIFIER "UnrealEngine"
 
