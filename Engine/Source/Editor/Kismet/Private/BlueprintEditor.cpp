cf1bf0c594e572c9fdf453fe651bbfe095d5631d
Ben Cosh <Ben.Cosh@epicgames.com>
2016-01-12 08:32:56 -0500

    Fix for the blueprint profiler "Cannot spawn tab because no spawner is registered" warning. #jira UE-24965 #Proj Kismet
    [CL 2824600 by Ben Cosh in 4.11 branch]

--- Engine/Source/Editor/Kismet/Private/BlueprintEditor.cpp
+++ Engine/Source/Editor/Kismet/Private/BlueprintEditor.cpp
@@ -508,7 +508,10 @@ bool FBlueprintEditor::IsInAScriptingMode() const
 
 bool FBlueprintEditor::OnRequestClose()
 {
-	TabManager->InvokeTab(FBlueprintEditorTabs::BlueprintProfilerID)->RequestCloseTab();
+	if (GetDefault<UEditorExperimentalSettings>()->bBlueprintPerformanceAnalysisTools)
+	{
+		TabManager->InvokeTab(FBlueprintEditorTabs::BlueprintProfilerID)->RequestCloseTab();
+	}
 	bEditorMarkedAsClosed = true;
 	return FWorkflowCentricApplication::OnRequestClose();
 }
@@ -1674,7 +1677,12 @@ void FBlueprintEditor::InitBlueprintEditor(
 	PostLayoutBlueprintEditorInitialization();
 
 	// Ensure the profiler UI respects the current state if it had previously been enabled.
-	const bool bShowBlueprintProfilerTab = IsProfilerActive() && GetDefault<UEditorExperimentalSettings>()->bUnifiedBlueprintEditor;
+	bool bShowBlueprintProfilerTab = false;
+	if (GetDefault<UEditorExperimentalSettings>()->bBlueprintPerformanceAnalysisTools)
+	{
+		IBlueprintProfilerInterface& ProfilerModule = FModuleManager::LoadModuleChecked<IBlueprintProfilerInterface>("BlueprintProfiler");
+		bShowBlueprintProfilerTab = ProfilerModule.IsProfilerEnabled();
+	}
 	if (bShowBlueprintProfilerTab)
 	{
 		TabManager->InvokeTab(FBlueprintEditorTabs::BlueprintProfilerID);
@@ -2193,12 +2201,10 @@ void FBlueprintEditor::CreateDefaultTabContents(const TArray<UBlueprint*>& InBlu
 			. BlueprintToWatch(InBlueprint)
 			. IsEnabled(!bIsInterface && !bIsMacro);
 
-		if (GetDefault<UEditorExperimentalSettings>()->bBlueprintPerformanceAnalysisTools)
-		{
-			this->BlueprintProfiler =
-				SNew(SBlueprintProfilerView)
-				.ProfileViewType(PVT_LiveView);
-		}
+		this->BlueprintProfiler =
+			SNew(SBlueprintProfilerView)
+			.ProfileViewType(PVT_LiveView);
+
 		this->Palette = 
 			SNew(SBlueprintPalette, SharedThis(this))
 				.IsEnabled(this, &FBlueprintEditor::IsFocusedGraphEditable);
