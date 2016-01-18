1d2d418aeb685bb471d1d2f20fc473ba1f0d878e
Michael Schoell <michael.schoell@gmail.com>
2016-01-18 12:52:37 -0500

    Harvesting components into a new Blueprint will correctly mark the components as movable so they can properly attach to the root.
    #jira UE-19929 - Creating a Blueprint out of existing Blueprints removes all assigned Static Meshes from components
    
    [CL 2832299 by Michael Schoell in 4.11 branch]

--- Engine/Source/Editor/UnrealEd/Private/Kismet2/Kismet2.cpp
+++ Engine/Source/Editor/UnrealEd/Private/Kismet2/Kismet2.cpp
@@ -1217,6 +1217,7 @@ void FKismetEditorUtilities::AddComponentsToBlueprint(UBlueprint* Blueprint, con
 			if (AsSceneComponent != nullptr)
 			{
 				NewSceneComponents.Add(AsSceneComponent, NewSCSNode);
+				Cast<USceneComponent>(NewSCSNode->ComponentTemplate)->SetMobility(EComponentMobility::Movable);
 			}
 			return NewSCSNode;
 		}
