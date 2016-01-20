82bb94a028bac9063f4fe3bf89307eef8ca05870
Jamie Dale <Jamie.Dale@epicgames.com>
2016-01-20 11:16:09 -0500

    The output log now hard-wraps lines to prevent long lines causing performance issues
    #jira UE-24187
    
    [CL 2835995 by Jamie Dale in 4.11 branch]

--- Engine/Source/Developer/OutputLog/Private/SOutputLog.cpp
+++ Engine/Source/Developer/OutputLog/Private/SOutputLog.cpp
@@ -729,9 +729,31 @@ bool SOutputLog::CreateLogMessages( const TCHAR* V, ELogVerbosity::Type Verbosit
 				FString Line = CurrentLogDump.Mid(LineRange.BeginIndex, LineRange.Len());
 				Line = Line.ConvertTabsToSpaces(4);
 
-				OutMessages.Add(MakeShareable(new FLogMessage(MakeShareable(new FString((bIsFirstLineInMessage) ? FOutputDevice::FormatLogLine(Verbosity, Category, *Line, LogTimestampMode) : Line)), Style)));
+				// Hard-wrap lines to avoid them being too long
+				static const int32 HardWrapLen = 360;
+				for (int32 CurrentStartIndex = 0; CurrentStartIndex < Line.Len();)
+				{
+					int32 HardWrapLineLen = 0;
+					if (bIsFirstLineInMessage)
+					{
+						FString MessagePrefix = FOutputDevice::FormatLogLine(Verbosity, Category, nullptr, LogTimestampMode);
+						
+						HardWrapLineLen = FMath::Min(HardWrapLen - MessagePrefix.Len(), Line.Len() - CurrentStartIndex);
+						FString HardWrapLine = Line.Mid(CurrentStartIndex, HardWrapLineLen);
 
-				bIsFirstLineInMessage = false;
+						OutMessages.Add(MakeShareable(new FLogMessage(MakeShareable(new FString(MessagePrefix + HardWrapLine)), Style)));
+					}
+					else
+					{
+						HardWrapLineLen = FMath::Min(HardWrapLen, Line.Len() - CurrentStartIndex);
+						FString HardWrapLine = Line.Mid(CurrentStartIndex, HardWrapLineLen);
+
+						OutMessages.Add(MakeShareable(new FLogMessage(MakeShareable(new FString(MoveTemp(HardWrapLine))), Style)));
+					}
+
+					bIsFirstLineInMessage = false;
+					CurrentStartIndex += HardWrapLineLen;
+				}
 			}
 		}
 
