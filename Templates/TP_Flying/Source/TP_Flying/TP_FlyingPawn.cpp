6e3b9616f490740e5dd0016e25d154e48191617e
Richard Hinckley <Richard.Hinckley@epicgames.com>
2016-01-25 17:12:49 -0500

    #jira UE-12938 Adding deflection to flying template pawn.
    [CL 2842626 by Richard Hinckley in 4.11 branch]

--- Templates/TP_Flying/Source/TP_Flying/TP_FlyingPawn.cpp
+++ Templates/TP_Flying/Source/TP_Flying/TP_FlyingPawn.cpp
@@ -66,8 +66,9 @@ void ATP_FlyingPawn::NotifyHit(class UPrimitiveComponent* MyComp, class AActor* 
 {
 	Super::NotifyHit(MyComp, Other, OtherComp, bSelfMoved, HitLocation, HitNormal, NormalImpulse, Hit);
 
-	// Set velocity to zero upon collision
-	CurrentForwardSpeed = 0.f;
+	// Deflect along the surface when we collide.
+	FRotator CurrentRotation = GetActorRotation(RootComponent);
+	SetActorRotation(FQuat::Slerp(CurrentRotation.Quaternion(), HitNormal.ToOrientationQuat(), 0.025f));
 }
 
 
