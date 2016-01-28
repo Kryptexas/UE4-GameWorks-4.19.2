bfe2690cb9d6be0617b149fef842a1f8bc51efc4
Ori Cohen <Ori.Cohen@epicgames.com>
2016-01-28 16:50:07 -0500

    Fix blocking crash for FN (from Olaf)
    #codereview Olaf.Piesche
    #lockdown Nick.Penwarden
    #JIRA UE-26111
    
    [CL 2847833 by Ori Cohen in Main branch]

--- Engine/Source/Runtime/Engine/Private/Particles/ParticleComponents.cpp
+++ Engine/Source/Runtime/Engine/Private/Particles/ParticleComponents.cpp
@@ -1692,11 +1692,14 @@ void UParticleEmitter::CacheEmitterModuleInfo()
 float UParticleEmitter::GetQualityLevelSpawnRateMult()
 {
 	static IConsoleVariable* EffectsQuality = IConsoleManager::Get().FindConsoleVariable(TEXT("sg.EffectsQuality"));
-	float Level = (1 - EffectsQuality->GetInt());
 	float Q = 1;
-	for (int i = 0; i < Level + 1; i++)
+	if (EffectsQuality)
 	{
-		Q = Q*QualityLevelSpawnRateScale;
+		float Level = (1 - EffectsQuality->GetInt());
+		for (int i = 0; i < Level + 1; i++)
+		{
+			Q = Q*QualityLevelSpawnRateScale;
+		}
 	}
 	return Q;
 }
