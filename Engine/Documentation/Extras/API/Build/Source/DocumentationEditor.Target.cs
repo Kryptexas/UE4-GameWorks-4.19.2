9d288e4107d3a17876644763e6ccb8fbc00dc596
unrealbot <unrealbot@epicgames.com>
2016-02-05 11:55:47 -0500

    Branch snapshot for CL 2857177
    [CL 2857177 in Main branch]

---
+++ Engine/Documentation/Extras/API/Build/Source/DocumentationEditor.Target.cs
@@ -0,0 +1,29 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class DocumentationEditorTarget : TargetRules
+{
+	public DocumentationEditorTarget(TargetInfo Target)
+	{
+		Type = TargetType.Editor;
+	}
+
+	//
+	// TargetRules interface.
+	//
+
+	public override void SetupBinaries(
+		TargetInfo Target,
+		ref List<UEBuildBinaryConfiguration> OutBuildBinaryConfigurations,
+		ref List<string> OutExtraModuleNames
+		)
+	{
+		OutExtraModuleNames.Add("UE4Game");
+		OutExtraModuleNames.Add("OnlineSubsystemAmazon");
+		OutExtraModuleNames.Add("OnlineSubsystemFacebook");
+		OutExtraModuleNames.Add("OnlineSubsystemNull");
+		OutExtraModuleNames.Add("OnlineSubsystemSteam");
+	}
+}
