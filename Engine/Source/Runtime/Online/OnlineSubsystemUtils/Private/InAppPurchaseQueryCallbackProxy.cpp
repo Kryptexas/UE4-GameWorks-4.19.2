03547525a4afd327ea19e6c8cf6976106f3db61d
Jeremiah Waldron <jwaldron@phosphorgames.com>
2016-02-12 13:37:05 -0500

    Making sure IAP related callbacks (purchase, query, restore) are still triggered when the game is paused Pulled from Dev-Platform CLs 2865146 and 2865216 #jira UE-26858
    [CL 2865222 by Jeremiah Waldron in 4.11 branch]

--- Engine/Source/Runtime/Online/OnlineSubsystemUtils/Private/InAppPurchaseQueryCallbackProxy.cpp
+++ Engine/Source/Runtime/Online/OnlineSubsystemUtils/Private/InAppPurchaseQueryCallbackProxy.cpp
@@ -71,9 +71,18 @@ void UInAppPurchaseQueryCallbackProxy::OnInAppPurchaseRead(bool bWasSuccessful)
 
 	if (UWorld* World = WorldPtr.Get())
 	{
-		// Use a local timer handle as we don't need to store it for later but we don't need to look for something to clear
-		FTimerHandle TimerHandle;
-		World->GetTimerManager().SetTimer(OnInAppPurchaseRead_DelayedTimerHandle, this, &UInAppPurchaseQueryCallbackProxy::OnInAppPurchaseRead_Delayed, 0.001f, false);
+		DECLARE_CYCLE_STAT(TEXT("FSimpleDelegateGraphTask.DelayInAppPurchaseRead"), STAT_FSimpleDelegateGraphTask_DelayInAppPurchaseRead, STATGROUP_TaskGraphTasks);
+
+		FSimpleDelegateGraphTask::CreateAndDispatchWhenReady(
+			FSimpleDelegateGraphTask::FDelegate::CreateLambda([=](){
+
+				OnInAppPurchaseRead_Delayed();
+
+			}),
+			GET_STATID(STAT_FSimpleDelegateGraphTask_DelayInAppPurchaseRead), 
+			nullptr, 
+			ENamedThreads::GameThread
+		);
 	}
 
 	ReadObject = NULL;
--- Engine/Source/Runtime/Online/OnlineSubsystemUtils/Private/InAppPurchaseRestoreCallbackProxy.cpp
+++ Engine/Source/Runtime/Online/OnlineSubsystemUtils/Private/InAppPurchaseRestoreCallbackProxy.cpp
@@ -69,9 +69,18 @@ void UInAppPurchaseRestoreCallbackProxy::OnInAppPurchaseRestoreComplete(EInAppPu
     
 	if (UWorld* World = WorldPtr.Get())
 	{
-		// Use a local timer handle as we don't need to store it for later but we don't need to look for something to clear
-		FTimerHandle TimerHandle;
-		World->GetTimerManager().SetTimer(OnInAppPurchaseRestoreComplete_DelayedTimerHandle, this, &UInAppPurchaseRestoreCallbackProxy::OnInAppPurchaseRestoreComplete_Delayed, 0.001f, false);
+		DECLARE_CYCLE_STAT(TEXT("FSimpleDelegateGraphTask.DelayInAppPurchaseRestoreComplete"), STAT_FSimpleDelegateGraphTask_DelayInAppPurchaseRestoreComplete, STATGROUP_TaskGraphTasks);
+
+		FSimpleDelegateGraphTask::CreateAndDispatchWhenReady(
+			FSimpleDelegateGraphTask::FDelegate::CreateLambda([=](){
+
+				OnInAppPurchaseRestoreComplete_Delayed();
+
+			}),
+			GET_STATID(STAT_FSimpleDelegateGraphTask_DelayInAppPurchaseRestoreComplete), 
+			nullptr, 
+			ENamedThreads::GameThread
+		);
     }
 
 	ReadObject = NULL;
