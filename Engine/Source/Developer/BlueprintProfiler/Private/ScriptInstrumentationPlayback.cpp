2e01b0396f0c1a6a3f1c073a71d5b550730a5693
Mike Beach <Mike.Beach@epicgames.com>
2016-02-29 14:06:41 -0500

    CIS fix - missing include for non-unity builds.
    #jira UE-27587
    #lockdown Nick.Penwarden
    
    [CL 2886132 by Mike Beach in Main branch]

--- Engine/Source/Developer/BlueprintProfiler/Private/ScriptInstrumentationPlayback.cpp
+++ Engine/Source/Developer/BlueprintProfiler/Private/ScriptInstrumentationPlayback.cpp
@@ -1,6 +1,7 @@
 // Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
 
 #include "BlueprintProfilerPCH.h"
+#include "EditorStyleSet.h"
 
 #define LOCTEXT_NAMESPACE "ScriptInstrumentationPlayback"
 
