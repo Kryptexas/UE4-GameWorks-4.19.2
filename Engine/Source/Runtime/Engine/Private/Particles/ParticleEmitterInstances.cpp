ba43bfd5ed59b778caeb69696726cd4f4746e7a5
Olaf Piesche <Olaf.Piesche@epicgames.com>
2016-03-21 17:05:48 -0400

    Fix for compile error.
    #jira UE-28607
    
    [CL 2917397 by Olaf Piesche in 4.11 branch]

--- Engine/Source/Runtime/Engine/Private/Particles/ParticleEmitterInstances.cpp
+++ Engine/Source/Runtime/Engine/Private/Particles/ParticleEmitterInstances.cpp
@@ -784,7 +784,7 @@ void FParticleEmitterInstance::CheckEmitterFinished()
 			check(RequiredModule);
 
 			if (HasCompleted() ||
-				(SpawnModule->GetMaximumSpawnRate() == 0
+				SpawnModule->GetMaximumSpawnRate() == 0
 				&& RequiredModule->EmitterDuration == 0
 				&& RequiredModule->EmitterLoops == 0)
 			{
