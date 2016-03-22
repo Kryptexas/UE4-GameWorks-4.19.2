e646b3bf2c5f6633fe70a8f8e72d5c51c5edf2e8
Andrew Grant <Andrew.Grant@epicgames.com>
2016-03-21 22:05:14 -0400

    Fix for missing ENGINE_NET_VERSION define due to Version.h being unsubmittable #lockdown Nick.Penwarden
    [CL 2917730 by Andrew Grant in Main branch]

--- Engine/Source/Runtime/Core/Private/Misc/EngineVersion.cpp
+++ Engine/Source/Runtime/Core/Private/Misc/EngineVersion.cpp
@@ -8,7 +8,7 @@
 #include "UObject/ReleaseObjectVersion.h"
 
 /** Version numbers for networking */
-int32 GEngineNetVersion = ENGINE_NET_VERSION;
+int32 GEngineNetVersion = BUILT_FROM_CHANGELIST;
 const int32 GEngineMinNetVersion		= 7038;
 const int32 GEngineNegotiationVersion	= 3077;
 
