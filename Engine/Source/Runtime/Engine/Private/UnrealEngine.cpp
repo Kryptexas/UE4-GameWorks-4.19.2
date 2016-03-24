06d7f72e03ba28496b204c635df3393c6ceff0ce
Chris Wood <chris.wood@epicgames.com>
2016-03-24 10:06:34 -0400

    Blocked hardware survey in non-editor builds/runs [UE-23812] - Packaged games should not send hardware survey telemetry event #jira UE-23812
    Reimplemented from CL 2781326 and 2782817 //depot/UE4-Releases/4.10.
    
    [CL 2921078 by Chris Wood in 4.11 branch]

--- Engine/Source/Runtime/Engine/Private/UnrealEngine.cpp
+++ Engine/Source/Runtime/Engine/Private/UnrealEngine.cpp
@@ -6191,24 +6191,10 @@ void UEngine::OnLostFocusPause(bool EnablePause)
 
 void UEngine::InitHardwareSurvey()
 {
-	bool bEnabled = true;
-#if !WITH_EDITORONLY_DATA
-	bEnabled = AreGameAnalyticsEnabled();
-#endif
-
 	// The hardware survey costs time and we don't want to slow down debug builds.
 	// This is mostly because of the CPU benchmark running in the survey and the results in debug are not being valid.
-#if UE_BUILD_DEBUG
-	bEnabled = false;
-#endif
-
-	if (bEnabled)
-	{
-		if (IsHardwareSurveyRequired())
-		{
-			bPendingHardwareSurveyResults = true;
-		}
-	}	
+	// Never run the survey in games, only in the editor.
+	bPendingHardwareSurveyResults = WITH_EDITOR && GIsEditor && !IsRunningCommandlet() && FEngineAnalytics::IsAvailable() && IsHardwareSurveyRequired();
 }
 
 void UEngine::TickHardwareSurvey()
