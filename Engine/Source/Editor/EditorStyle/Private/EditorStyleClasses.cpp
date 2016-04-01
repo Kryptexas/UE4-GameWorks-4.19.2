2375d8749cb0aef498264090a1a82ea5270514e6
Josh Adams <josh.adams@epicgames.com>
2016-04-01 17:40:28 -0400

    - Backed out the first bandaid fix for missing menu items (this will make it easier to verify that any changes to my second, better, band-aid are working) #lockdown nick.penwarden
    [CL 2931091 by Josh Adams in Main branch]

--- Engine/Source/Editor/EditorStyle/Private/EditorStyleClasses.cpp
+++ Engine/Source/Editor/EditorStyle/Private/EditorStyleClasses.cpp
@@ -16,9 +16,6 @@ UEditorStyleSettings::UEditorStyleSettings( const FObjectInitializer& ObjectInit
 
 	bShowFriendlyNames = true;
 	LogTimestampMode = ELogTimes::None;
-	
-	bShowProjectMenus = true;
-	bShowLaunchMenus = true;
 }
 
 FLinearColor UEditorStyleSettings::GetSubduedSelectionColor() const
