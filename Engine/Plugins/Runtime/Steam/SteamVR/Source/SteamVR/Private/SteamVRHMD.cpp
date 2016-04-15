1b850ba56676124dd52dcd43393d7fc754670a8e
Nick Whiting <Nick.Whiting@epicgames.com>
2016-04-15 13:57:48 -0400

    Fixing cook failure because of "error" string in a log from SteamVR
    #lockdown nick.penwarden
    #jira UE-29474
    
    [CL 2945470 by Nick Whiting in 4.11 branch]

--- Engine/Plugins/Runtime/Steam/SteamVR/Source/SteamVR/Private/SteamVRHMD.cpp
+++ Engine/Plugins/Runtime/Steam/SteamVR/Source/SteamVR/Private/SteamVRHMD.cpp
@@ -132,7 +132,7 @@ public:
 		VRSystem = (*FSteamVRHMD::VRInitFn)(&VRInitErr, vr::VRApplication_Scene);
         if (!VRSystem || (VRInitErr != vr::VRInitError_None))
         {
-            UE_LOG(LogHMD, Warning, TEXT("Failed to initialize OpenVR with error %d"), (int32)VRInitErr);
+            UE_LOG(LogHMD, Warning, TEXT("Failed to initialize OpenVR with code %d"), (int32)VRInitErr);
 			UnloadOpenVRModule();
             
             return false;
@@ -142,7 +142,7 @@ public:
 		VRSystem = (vr::IVRSystem*)(*FSteamVRHMD::VRGetGenericInterfaceFn)(vr::IVRSystem_Version, &VRInitErr);
         if (!VRSystem || (VRInitErr != vr::VRInitError_None))
         {
-            UE_LOG(LogHMD, Warning, TEXT("Failed to initialize OpenVR (version mismatch) with error %d"), (int32)VRInitErr);
+            UE_LOG(LogHMD, Warning, TEXT("Failed to initialize OpenVR (version mismatch) with code %d"), (int32)VRInitErr);
 			UnloadOpenVRModule();
             
             return false;
