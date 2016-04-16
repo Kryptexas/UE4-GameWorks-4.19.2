c19e33a51ec7bfed57e47822a12f5bdad8df4676
Marc Audy <Marc.Audy@epicgames.com>
2016-04-16 09:24:09 -0400

    Snap to not including scale should KeepWorld for scale not KeepRelative #codereview Thomas.Sarkanen, Zak.Middleton #lockdown Nick.Penwarden
    [CL 2946218 by Marc Audy in Main branch]

--- Engine/Source/Runtime/Engine/Private/EngineTypes.cpp
+++ Engine/Source/Runtime/Engine/Private/EngineTypes.cpp
@@ -5,7 +5,7 @@
 
 FAttachmentTransformRules FAttachmentTransformRules::KeepRelativeTransform(EAttachmentRule::KeepRelative, false);
 FAttachmentTransformRules FAttachmentTransformRules::KeepWorldTransform(EAttachmentRule::KeepWorld, false);
-FAttachmentTransformRules FAttachmentTransformRules::SnapToTargetNotIncludingScale(EAttachmentRule::SnapToTarget, EAttachmentRule::SnapToTarget, EAttachmentRule::KeepRelative, false);
+FAttachmentTransformRules FAttachmentTransformRules::SnapToTargetNotIncludingScale(EAttachmentRule::SnapToTarget, EAttachmentRule::SnapToTarget, EAttachmentRule::KeepWorld, false);
 FAttachmentTransformRules FAttachmentTransformRules::SnapToTargetIncludingScale(EAttachmentRule::SnapToTarget, false);
 
 FDetachmentTransformRules FDetachmentTransformRules::KeepRelativeTransform(EDetachmentRule::KeepRelative, true);
