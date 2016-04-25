fd78cf01ea98b605670a6274853ceb917625f8ed
Zak Middleton <Zak.Middleton@epicgames.com>
2016-04-25 16:48:37 -0400

    #ue4 - (4.12) Don't reject low distance MTD, it could cause us to not process some valid overlaps. (copy of 2955001 in Main)
    #jira UE-29531
    #codereview Ori.Cohen
    #lockdown Nick.Penwarden
    
    [CL 2955130 by Zak Middleton in 4.12 branch]

--- Engine/Source/Runtime/Engine/Private/PhysicsEngine/BodyInstance.cpp
+++ Engine/Source/Runtime/Engine/Private/PhysicsEngine/BodyInstance.cpp
@@ -4486,8 +4486,8 @@ bool FBodyInstance::OverlapPhysX_AssumesLocked(const PxGeometry& PGeom, const Px
 		{
 			PxVec3 POutDirection;
 			float OutDistance;
-			//TODO: this 0 check is a temp fix because physx is reporting contacts with distance 0 which gives us nan
-			if (PxGeometryQuery::computePenetration(POutDirection, OutDistance, PGeom, ShapePose, PShape->getGeometry().any(), PxShapeExt::getGlobalPose(*PShape, *RigidBody)) && !FMath::IsNearlyZero(OutDistance))
+
+			if (PxGeometryQuery::computePenetration(POutDirection, OutDistance, PGeom, ShapePose, PShape->getGeometry().any(), PxShapeExt::getGlobalPose(*PShape, *RigidBody)))
 			{				
 				//TODO: there are some edge cases that give us nan results. In these cases we skip
 				if (!POutDirection.isFinite())
