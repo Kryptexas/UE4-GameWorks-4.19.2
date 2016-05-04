2faee2bfc176102756213c57d97b89f078caa124
Taizyd Korambayil <Taizyd.Korambayil@epicgames.com>
2016-05-04 10:25:48 -0400

    #jira UE-28544 Adjusted Crosshair position
    [CL 2965837 by Taizyd Korambayil in 4.12 branch]

--- Templates/TP_FirstPerson/Source/TP_FirstPerson/TP_FirstPersonHUD.cpp
+++ Templates/TP_FirstPerson/Source/TP_FirstPerson/TP_FirstPersonHUD.cpp
@@ -24,8 +24,8 @@ void ATP_FirstPersonHUD::DrawHUD()
 	const FVector2D Center(Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f);
 
 	// offset by half the texture's dimensions so that the center of the texture aligns with the center of the Canvas
-	const FVector2D CrosshairDrawPosition( (Center.X - (CrosshairTex->GetSurfaceWidth() * 0.5)),
-										   (Center.Y - (CrosshairTex->GetSurfaceHeight() * 0.5f)) );
+	const FVector2D CrosshairDrawPosition( (Center.X),
+										   (Center.Y));
 
 	// draw the crosshair
 	FCanvasTileItem TileItem( CrosshairDrawPosition, CrosshairTex->Resource, FLinearColor::White);
