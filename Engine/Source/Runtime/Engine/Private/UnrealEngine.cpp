b158f76bc2cf95ab2d7c3a77cfef8cb53358a64f
Cody Albert <cody.albert@epicgames.com>
2016-05-09 15:33:08 -0400

    Copied hardware survey gating logic from DevEditor
    #jira UE-28666
    
    [CL 2971176 by Cody Albert in 4.12 branch]

--- Engine/Source/Runtime/Engine/Private/UnrealEngine.cpp
+++ Engine/Source/Runtime/Engine/Private/UnrealEngine.cpp
@@ -6335,22 +6335,14 @@ void UEngine::OnLostFocusPause(bool EnablePause)
 
 void UEngine::StartHardwareSurvey()
 {
-	bool bEnabled = true;
-#if !WITH_EDITORONLY_DATA
-	bEnabled = AreGameAnalyticsEnabled();
-#endif
-
 	// The hardware survey costs time and we don't want to slow down debug builds.
 	// This is mostly because of the CPU benchmark running in the survey and the results in debug are not being valid.
-#if UE_BUILD_DEBUG
-	bEnabled = false;
-#endif
-
-	if (bEnabled && FEngineAnalytics::IsAvailable())
+	// Never run the survey in games, only in the editor.
+	if (WITH_EDITOR && GIsEditor && !IsRunningCommandlet() && FEngineAnalytics::IsAvailable())
 	{
 		IHardwareSurveyModule::Get().StartHardwareSurvey(FEngineAnalytics::GetProvider());
-		}
-	}	
+	}
+}	
 
 void UEngine::InitHardwareSurvey()
 {
