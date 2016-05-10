119680677d779971ba38871a350356fada45b246
Niklas Smedberg <niklas.smedberg@epicgames.com>
2016-05-10 19:50:27 -0400

    Fixed disabling crash handler on Android with VulkanRHI, so we see full crash callstacks. (We tried to do this before, but the code didn't work) #jira UEMOB-138
    [CL 2973178 by Niklas Smedberg in 4.12 branch]

--- Engine/Source/Runtime/Core/Private/Android/AndroidMisc.cpp
+++ Engine/Source/Runtime/Core/Private/Android/AndroidMisc.cpp
@@ -648,7 +648,7 @@ void FAndroidMisc::SetCrashHandler(void (* CrashHandler)(const FGenericCrashCont
 	FMemory::Memzero(&PrevActions, sizeof(PrevActions));
 
 	// Passing -1 will leave these restored and won't trap them
-	if ((int64)CrashHandler == -1)
+	if ((PTRINT)CrashHandler == -1)
 	{
 		return;
 	}
