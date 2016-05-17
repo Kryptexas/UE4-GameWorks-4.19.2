68e7d79497d380af01ceff6e6853f57fa174d262
Gareth Martin <gareth.martin@epicgames.com>
2016-05-17 10:50:15 -0400

    Fixed crash when calling EditorApplySpline with a null spline component Also stopped it doing anything in PIE (it's for blutilities, not runtime) #jira UE-30830
    [CL 2980318 by Gareth Martin in 4.12 branch]

--- Engine/Source/Runtime/Landscape/Private/LandscapeBlueprintSupport.cpp
+++ Engine/Source/Runtime/Landscape/Private/LandscapeBlueprintSupport.cpp
@@ -13,13 +13,14 @@
 
 void ALandscapeProxy::EditorApplySpline(USplineComponent* InSplineComponent, float StartWidth, float EndWidth, float StartSideFalloff, float EndSideFalloff, float StartRoll, float EndRoll, int32 NumSubdivisions, bool bRaiseHeights, bool bLowerHeights, ULandscapeLayerInfoObject* PaintLayer)
 {
-	check(GIsEditor);
-
 #if WITH_EDITOR
-	TArray<FLandscapeSplineInterpPoint> Points;
-	LandscapeSplineRaster::Pointify(InSplineComponent->SplineInfo, Points, NumSubdivisions, 0.0f, 0.0f, StartWidth, EndWidth, StartSideFalloff, EndSideFalloff, StartRoll, EndRoll);
+	if (InSplineComponent && !GetWorld()->IsGameWorld())
+	{
+		TArray<FLandscapeSplineInterpPoint> Points;
+		LandscapeSplineRaster::Pointify(InSplineComponent->SplineInfo, Points, NumSubdivisions, 0.0f, 0.0f, StartWidth, EndWidth, StartSideFalloff, EndSideFalloff, StartRoll, EndRoll);
 
-	FTransform SplineToWorld = InSplineComponent->GetComponentTransform();
-	LandscapeSplineRaster::RasterizeSegmentPoints(GetLandscapeInfo(), MoveTemp(Points), SplineToWorld, bRaiseHeights, bLowerHeights, PaintLayer);
+		FTransform SplineToWorld = InSplineComponent->GetComponentTransform();
+		LandscapeSplineRaster::RasterizeSegmentPoints(GetLandscapeInfo(), MoveTemp(Points), SplineToWorld, bRaiseHeights, bLowerHeights, PaintLayer);
+	}
 #endif
 }
