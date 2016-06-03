460da56b0d31c0560f75a10bc5bf570a38f51f30
Alex Delesky <adelesky@irongalaxystudios.com>
2016-06-03 16:32:43 -0400

    #jira UE-31177 - Reverting CL 3000230 #lockdown cristina.riveron
    [CL 3000278 by Alex Delesky in 4.12 branch]

--- Engine/Source/Runtime/Landscape/Classes/LandscapeProxy.h
+++ Engine/Source/Runtime/Landscape/Classes/LandscapeProxy.h
@@ -32,7 +32,6 @@ class ULandscapeComponent;
 class USplineComponent;
 struct FLandscapeInfoLayerSettings;
 struct FAsyncGrassBuilder;
-struct FRawMesh;
 
 /** Structure storing channel usage for weightmap textures */
 USTRUCT()
