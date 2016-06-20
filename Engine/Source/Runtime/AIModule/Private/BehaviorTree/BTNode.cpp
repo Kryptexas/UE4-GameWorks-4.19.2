e365a66f67b42f92d66c4b7d720d2071d5e146e4
Lukasz Furman <Lukasz.Furman@epicgames.com>
2016-06-20 10:34:43 -0400

    changed behavior tree node instancing to use object duplication #jira UE-32179 #rb Mieszko.Zielinski #lockdown nick.penwarden
    [CL 3019523 by Lukasz Furman in 4.12 branch]

--- Engine/Source/Runtime/AIModule/Private/BehaviorTree/BTNode.cpp
+++ Engine/Source/Runtime/AIModule/Private/BehaviorTree/BTNode.cpp
@@ -74,7 +74,7 @@ void UBTNode::InitializeInSubtree(UBehaviorTreeComponent& OwnerComp, uint8* Node
 		UBTNode* NodeInstance = OwnerComp.NodeInstances.IsValidIndex(NextInstancedIndex) ? OwnerComp.NodeInstances[NextInstancedIndex] : NULL;
 		if (NodeInstance == NULL)
 		{
-			NodeInstance = NewObject<UBTNode>(&OwnerComp, GetClass(), NAME_None, RF_NoFlags, (UObject*)(this));
+			NodeInstance = (UBTNode*)StaticDuplicateObject(this, &OwnerComp);
 			NodeInstance->InitializeNode(GetParentNode(), GetExecutionIndex(), GetMemoryOffset(), GetTreeDepth());
 			NodeInstance->bIsInstanced = true;
 
