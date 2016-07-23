810f327d7d684acfb8845c7266b9602d16347f52
Ben Marsh <Ben.Marsh@epicgames.com>
2016-07-22 20:35:37 -0400

    More fixes for non-unity compile.
    #codereview Max.Preussner, Max.Chen
    #rb none
    #lockdown Nick.Penwarden
    
    [CL 3062462 by Ben Marsh in Main branch]

--- Engine/Plugins/Media/AvfMedia/Source/AvfMediaEditor/Private/AvfMediaEditorModule.cpp
+++ Engine/Plugins/Media/AvfMedia/Source/AvfMediaEditor/Private/AvfMediaEditorModule.cpp
@@ -1,6 +1,8 @@
 // Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
 
 #include "AvfMediaEditorPCH.h"
+#include "CoreUObject.h"
+#include "ModuleManager.h"
 #include "ModuleInterface.h"
 
 
