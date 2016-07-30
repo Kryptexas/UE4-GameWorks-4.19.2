78b1c70241c7b589f7f667ba45547812a1b97d84
Max Chen <Max.Chen@epicgames.com>
2016-07-29 23:38:49 -0400

    Sequencer: Set relative location, rotation, scale explicitly to identity instead of calling ResetRelativeTransform because we don't want overlaps to fire until after the update pass.
    #jira UE-33432
    #rb Frank.Fella
    
    [CL 3071077 by Max Chen in 4.13 branch]

--- Engine/Source/Runtime/MovieSceneTracks/Private/TrackInstances/MovieScene3DTransformTrackInstance.cpp
+++ Engine/Source/Runtime/MovieSceneTracks/Private/TrackInstances/MovieScene3DTransformTrackInstance.cpp
@@ -106,7 +106,11 @@ void FMovieScene3DTransformTrackInstance::Update(EMovieSceneUpdateData& UpdateDa
 			{
 				if (UpdateData.UpdatePass == MSUP_PreUpdate)
 				{		
-					SceneComponent->ResetRelativeTransform();
+					// Set the transforms to identity explicitly instead of ResetRelativeTransform so that overlaps aren't evaluated at the origin
+					SceneComponent->RelativeLocation = FVector(FVector::ZeroVector);
+					SceneComponent->RelativeRotation = FRotator(FRotator::ZeroRotator);
+					SceneComponent->RelativeScale3D = FVector(1.f);
+					SceneComponent->UpdateComponentToWorld();
 				}
 				else if (UpdateData.UpdatePass == MSUP_Update)
 				{
