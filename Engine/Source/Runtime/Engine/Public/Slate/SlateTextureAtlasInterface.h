4e46ce733036824e106d5326600a360cfb4edded
Nick Darnell <Nick.Darnell@epicgames.com>
2016-08-02 11:11:55 -0400

    Slate/UMG - When you select a paper sprite, or any other implementor of the SlateTextureAtlasInterface the editor will now correctly pick a default size for the brush matching the size of the sprite like it does for textures.
    #rb none
    #jira UE-34075
    
    [CL 3073585 by Nick Darnell in 4.13 branch]

--- Engine/Source/Runtime/Engine/Public/Slate/SlateTextureAtlasInterface.h
+++ Engine/Source/Runtime/Engine/Public/Slate/SlateTextureAtlasInterface.h
@@ -1,10 +1,15 @@
 // Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
 
 #pragma once
+
 #include "Interface.h"
+#include "Engine/Texture.h"
+#include "Math/Vector2D.h"
 #include "SlateTextureAtlasInterface.generated.h"
 
-/**  */
+/**
+ * The data representing a region of a UTexture atlas.
+ */
 struct FSlateAtlasData
 {
 public:
@@ -24,19 +29,41 @@ public:
 
 	/** The region size in UVs. */
 	FVector2D SizeUV;
+
+	/**
+	 * Gets the dimensions of the atlas region in pixel coordinates.
+	 */
+	FVector2D GetSourceDimensions() const
+	{
+		if ( AtlasTexture )
+		{
+			return FVector2D(AtlasTexture->GetSurfaceWidth() * SizeUV.X, AtlasTexture->GetSurfaceHeight() * SizeUV.Y);
+		}
+		else
+		{
+			return FVector2D(0, 0);
+		}
+	}
 };
 
-/**  */
+
+/**
+ * 
+ */
 UINTERFACE(meta=( CannotImplementInterfaceInBlueprint ))
 class ENGINE_API USlateTextureAtlasInterface : public UInterface
 {
 	GENERATED_UINTERFACE_BODY()
 };
 
-/**  */
+/**
+ * 
+ */
 class ENGINE_API ISlateTextureAtlasInterface
 {
 	GENERATED_IINTERFACE_BODY()
+
+public:
 
 	/** Gets the atlas data to use when rendering with slate. */
 	virtual FSlateAtlasData GetSlateAtlasData() const = 0;
