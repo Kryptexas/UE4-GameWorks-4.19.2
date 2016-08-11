95274bdb36c7810cc18a4f54e58925758489e417
Ben Marsh <Ben.Marsh@epicgames.com>
2016-08-11 12:43:04 -0400

    Pass the -ignorejunk option to UBT when generating code documentation. Since the UE_SDKS_ROOT environment variable is removed (because we don't want documentation for confidential platforms), UBT was deleting Linux target platform DLLs and causing an error when generating blueprint documentation when the files were missing.
    #codereview Matthew.Griffin
    #rb Matthew.Griffin
    
    [CL 3085799 by Ben Marsh in 4.13 branch]

--- Engine/Source/Programs/UnrealDocTool/APIDocTool/APIDocTool/Program.cs
+++ Engine/Source/Programs/UnrealDocTool/APIDocTool/APIDocTool/Program.cs
@@ -630,7 +630,7 @@ namespace APIDocTool
 				Console.WriteLine("Building target info...");
 				Utility.SafeCreateDirectory(Path.GetDirectoryName(TargetInfoPath));
 
-				string Arguments = String.Format("DocumentationEditor Win64 Debug -noxge -project=\"{0}\"", Path.Combine(EngineDir, "Documentation\\Extras\\API\\Build\\Documentation.uproject"));
+				string Arguments = String.Format("DocumentationEditor Win64 Debug -ignorejunk -noxge -project=\"{0}\"", Path.Combine(EngineDir, "Documentation\\Extras\\API\\Build\\Documentation.uproject"));
 				if (!RunUnrealBuildTool(EngineDir, Arguments + " -clean"))
 				{
 					return false;
