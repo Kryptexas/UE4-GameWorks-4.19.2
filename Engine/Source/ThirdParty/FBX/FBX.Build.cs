d51be69db681d9f736c3f4fc7b5cac8f0ad9b1ed
Alexis Matte <heelys@gmail.com>
2016-09-22 14:09:22 -0400

    #jira UE-36286 Use the fbx sdk 2016 instead of 2017 because of a bug when importing embedded medias. #rb matt.kuhlenschmidt #lockdown nick.penwarden
    [CL 3136379 by Alexis Matte in Main branch]

--- Engine/Source/ThirdParty/FBX/FBX.Build.cs
+++ Engine/Source/ThirdParty/FBX/FBX.Build.cs
@@ -9,7 +9,7 @@ public class FBX : ModuleRules
 	{
 		Type = ModuleType.External;
 
-		string FBXSDKDir = UEBuildConfiguration.UEThirdPartySourceDirectory + "FBX/2017.0.1/";
+		string FBXSDKDir = UEBuildConfiguration.UEThirdPartySourceDirectory + "FBX/2016.1.1/";
 		PublicSystemIncludePaths.AddRange(
 			new string[] {
 					FBXSDKDir + "include",
