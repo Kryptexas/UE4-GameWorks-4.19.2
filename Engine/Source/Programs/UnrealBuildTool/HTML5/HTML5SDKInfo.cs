0d1c5647122b5f0d0d472d70ae4d8e629c5a1077
Nick Shin <nshin@phosphorgames.com>
2016-10-04 18:02:22 -0400

    upgrade emsdk toolchain to 1.36.11
    this is needed to fix up physx compiler errors
    
    #rb none
    #code.review ori.cohen
    #lockdown josh.adams
    #jira UE-36562
    
    [CL 3151140 by Nick Shin in Main branch]

--- Engine/Source/Programs/UnrealBuildTool/HTML5/HTML5SDKInfo.cs
+++ Engine/Source/Programs/UnrealBuildTool/HTML5/HTML5SDKInfo.cs
@@ -11,11 +11,9 @@ namespace UnrealBuildTool
 {
 	public class HTML5SDKInfo
 	{
-//		static string SDKVersion = "tag-1.36.11";
-		static string SDKVersion = "1.35.0";
+		static string SDKVersion = "tag-1.36.11";
 		static string NODE_VER = "4.1.1_64bit";
-//		static string LLVM_VER = "tag-1.36.11";
-		static string LLVM_VER = "e1.35.0_64bit";
+		static string LLVM_VER = "tag-1.36.11";
 		static string PYTHON_VER = "2.7.5.3_64bit";
 
 		// --------------------------------------------------
