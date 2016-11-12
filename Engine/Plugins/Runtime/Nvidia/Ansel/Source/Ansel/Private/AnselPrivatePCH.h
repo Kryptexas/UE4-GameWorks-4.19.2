92a3597aa1eb0d89d661e51c4613d8c2238c8542
Leslie Nivison <Leslie.Nivison@epicgames.com>
2016-11-12 01:23:00 -0500

    Rollback //UE4/Release-4.14/Engine/Plugins/Runtime/Nvidia to changelist 3193712 New GameWorks license from NVIDIA
    #lockdown Cristina.Riveron
    #jira UEPROD-900
    #rb none
    #codereview Marcus.Wassmer
    
    [CL 3195953 by Leslie Nivison in 4.14 branch]

---
+++ Engine/Plugins/Runtime/Nvidia/Ansel/Source/Ansel/Private/AnselPrivatePCH.h
@@ -0,0 +1,11 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "Engine.h"
+#include "IAnselPlugin.h"
+
+#if WITH_ANSEL
+
+#include "CameraPhotographyModule.h"
+
+#endif // WITH_ANSEL
+
