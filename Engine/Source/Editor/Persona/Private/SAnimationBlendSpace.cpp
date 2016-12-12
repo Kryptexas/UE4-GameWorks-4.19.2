6d26347aa58debb9b80c441c5240f3ec8b46c731
Jurre deBaare <jurre.debaare@epicgames.com>
2016-12-12 07:11:40 -0500

    [CrashReport] UE4Editor_Persona!FDelaunayTriangleGenerator::FlipTriangles() [sanimationblendspace.cpp:375] #fix moved over from Dev-Framework CL 3179539 #jira UE-39557 #rb none #lockdown cristina.riveron
    [CL 3230536 by Jurre deBaare in 4.14 branch]

--- Engine/Source/Editor/Persona/Private/SAnimationBlendSpace.cpp
+++ Engine/Source/Editor/Persona/Private/SAnimationBlendSpace.cpp
@@ -259,16 +259,23 @@ FDelaunayTriangleGenerator::ECircumCircleState FDelaunayTriangleGenerator::GetCi
 
 	float Det = M00*M11*M22+M01*M12*M20+M02*M10*M21 - (M02*M11*M20+M01*M10*M22+M00*M12*M21);
 	
-	if( FMath::IsNearlyZero(FMath::Abs(Det)) )
+	// When the vertices are sorted in a counterclockwise order, the determinant is positive if and only if Testpoint lies inside the circumcircle of T.
+	if (FMath::IsNegativeFloat(Det))
 	{
-		return ECCS_On;
+		return ECCS_Outside;
 	}
-	else if( Det > 0.f )
+	else
 	{
-		return ECCS_Inside;
+		// On top of the triangle edge
+		if (FMath::IsNearlyZero(Det))
+		{
+			return ECCS_On;
+		}
+		else
+		{
+			return ECCS_Inside;
+		}
 	}
-
-	return ECCS_Outside;
 }
 
 /** 
@@ -313,66 +320,52 @@ bool FDelaunayTriangleGenerator::AllCoincident(const TArray<FPoint>& InPoints)
 	return false;
 }
 
-bool FDelaunayTriangleGenerator::FlipTriangles(int32 I, int32 J)
+bool FDelaunayTriangleGenerator::FlipTriangles(int32 TriangleIndexOne, int32 TriangleIndexTwo)
 {
-	bool Flipped = false;
-	FTriangle *A = TriangleList[I];
-	FTriangle *B = TriangleList[J];
+	FTriangle* A = TriangleList[TriangleIndexOne];
+	FTriangle* B = TriangleList[TriangleIndexTwo];
 
 	// if already optimized, don't have to do any
-	FPoint * TestPt = A->FindNonSharingPoint(B);
+	FPoint* TestPt = A->FindNonSharingPoint(B);
 
 	// If it's not inside, we don't have to do any
-	if (GetCircumcircleState(A, *TestPt)!=ECCS_Inside)
+	if (GetCircumcircleState(A, *TestPt) != ECCS_Inside)
 	{
 		return false;
 	}
 
-	int32 TrianglesMade=0;
+	FTriangle NewTriangles[2];
+	int32 TrianglesMade = 0;
 
-	// otherwise, try make new triangle, and flip it
-	if (IsEligibleForTriangulation(A->Vertices[0],A->Vertices[1], TestPt))
+	for (int32 VertexIndexOne = 0; VertexIndexOne < 2; ++VertexIndexOne)
 	{
-		FTriangle NewTriangle(A->Vertices[0],A->Vertices[1], TestPt);
-		// only flip if outside
-		if (GetCircumcircleState(&NewTriangle, *A->Vertices[2])==ECCS_Outside)
+		for (int32 VertexIndexTwo = VertexIndexOne + 1; VertexIndexTwo < 3; ++VertexIndexTwo)
 		{
-			// good triangle
-			AddTriangle(NewTriangle, false);
-			Flipped=true;
-			++TrianglesMade;
-		}
-
-	}
-
-	if (IsEligibleForTriangulation(A->Vertices[0],A->Vertices[2], TestPt))
-	{
-		FTriangle NewTriangle(A->Vertices[0],A->Vertices[2], TestPt);
-		if (GetCircumcircleState(&NewTriangle, *A->Vertices[1])==ECCS_Outside)
-		{
-			// good triangle
-			AddTriangle(NewTriangle, false);
-			Flipped=true;
-			++TrianglesMade;
+			// Check if these vertices form a valid triangle (should be non-colinear)
+			if (IsEligibleForTriangulation(A->Vertices[VertexIndexOne], A->Vertices[VertexIndexTwo], TestPt))
+			{
+				// Create the new triangle and check if the final (original) vertex falls inside or outside of it's circumcircle
+				const FTriangle NewTriangle(A->Vertices[VertexIndexOne], A->Vertices[VertexIndexTwo], TestPt);
+				const int32 VertexIndexThree = 3 - (VertexIndexTwo + VertexIndexOne);
+				if (GetCircumcircleState(&NewTriangle, *A->Vertices[VertexIndexThree]) == ECCS_Outside)
+				{
+					// If so store the triangle and increment the number of triangles
+					checkf(TrianglesMade < 2, TEXT("Incorrect number of triangles created"));
+					NewTriangles[TrianglesMade] = NewTriangle;
+					++TrianglesMade;
+				}
+			}
 		}
 	}
 
-	if (IsEligibleForTriangulation(A->Vertices[1],A->Vertices[2], TestPt))
+	// In case two triangles were generated the flip was successful so we can add them to the list
+	if (TrianglesMade == 2)
 	{
-		FTriangle NewTriangle(A->Vertices[1],A->Vertices[2], TestPt);
-		if (GetCircumcircleState(&NewTriangle, *A->Vertices[0])==ECCS_Outside)
-		{
-			// good triangle
-			AddTriangle(NewTriangle, false);
-			Flipped=true;
-			++TrianglesMade;
-		}
+		AddTriangle(NewTriangles[0], false);
+		AddTriangle(NewTriangles[1], false);
 	}
 
-	// should be 2, if not we miss triangles
-	check (	TrianglesMade==2 );
-
-	return Flipped;
+	return TrianglesMade == 2;
 }
 
 void FDelaunayTriangleGenerator::AddTriangle(FTriangle& newTriangle, bool bCheckHalfEdge/*=true*/)
