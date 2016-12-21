ee731349e03ba8df8e7988ed0a09bb1bbd61052f
Josh Adams <josh.adams@epicgames.com>
2016-12-21 11:38:28 -0500

    - Moved NVNRHI stuff out of RHI.Build.cs #rb ben.marsh #jira UE-39966
    [CL 3241902 by Josh Adams in 4.15 branch]

--- Engine/Source/Runtime/RHI/RHI.Build.cs
+++ Engine/Source/Runtime/RHI/RHI.Build.cs
@@ -28,24 +28,6 @@ public class RHI : ModuleRules
 				DynamicallyLoadedModuleNames.Add("VulkanRHI");
 			}
 
-			// need to dynamically load an assembly to check if the SDK exists (not everyone will have access to this)
-			// don't include in monolithic builds, however, because we don't want to put it into the UE4Game in the binary builds
-			if (Target.Platform == UnrealTargetPlatform.Win64 && !Target.IsMonolithic)
-			{
-				System.Type SwitchSDKType = System.Type.GetType("UnrealBuildTool.SwitchPlatformSDK,UnrealBuildTool");
-				if (SwitchSDKType != null)
-				{
-					// check for the location of the SDK
-					string RHIModuleName = SwitchSDKType.GetMethod("GetRHIModuleNameIfAvailable").Invoke(null, null) as string;
-					// compile the Switch RHI if possible
-					if (!string.IsNullOrEmpty(RHIModuleName))
-					{
-						DynamicallyLoadedModuleNames.Add(RHIModuleName);
-						Definitions.Add("SWITCHRHI=" + RHIModuleName);
-					}
-				}
-			}
-
 			if ((Target.Platform == UnrealTargetPlatform.Win32) ||
 				(Target.Platform == UnrealTargetPlatform.Win64) ||
 				(Target.Platform == UnrealTargetPlatform.Mac)   ||
