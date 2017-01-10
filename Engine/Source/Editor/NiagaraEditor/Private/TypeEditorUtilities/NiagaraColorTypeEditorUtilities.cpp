a85d77544b001872ec7fc4322c755af4bd3967b5
Marcus Wassmer <Marcus.Wassmer@epicgames.com>
2017-01-10 12:45:53 -0500

    Include less intrusive engine header for GEngine access. #rb none #lockdown Nick.Penwarden
    [CL 3252426 by Marcus Wassmer in Main branch]

--- Engine/Source/Editor/NiagaraEditor/Private/TypeEditorUtilities/NiagaraColorTypeEditorUtilities.cpp
+++ Engine/Source/Editor/NiagaraEditor/Private/TypeEditorUtilities/NiagaraColorTypeEditorUtilities.cpp
@@ -4,7 +4,7 @@
 #include "SNiagaraParameterEditor.h"
 #include "NiagaraTypes.h"
 #include "NiagaraEditorStyle.h"
-#include "Engine.h"
+#include "Classes/Engine/Engine.h"
 
 #include "SColorPicker.h"
 #include "SNumericEntryBox.h"
