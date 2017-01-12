53c4aa4776d7efac3c657a380f36ac35a6e1b94e
Jack Porter <jack.porter@epicgames.com>
2017-01-12 02:01:43 -0500

    Missing file from CL 3254907 #jira UE-39111 #rb Allan.Bentham
    [CL 3254991 by Jack Porter in 4.15 branch]

--- Engine/Source/Runtime/OpenGLDrv/Private/IOS/IOSOpenGL.cpp
+++ Engine/Source/Runtime/OpenGLDrv/Private/IOS/IOSOpenGL.cpp
@@ -239,7 +239,7 @@ FRHITexture* PlatformCreateBuiltinBackBuffer(FOpenGLDynamicRHI* OpenGLRHI, uint3
 	[GLView UpdateRenderWidth:SizeX andHeight:SizeY];
 
 	uint32 Flags = TexCreate_RenderTargetable | TexCreate_Presentable;
-	FOpenGLTexture2D* Texture2D = new FOpenGLTexture2D(OpenGLRHI, GLView.OnScreenColorRenderBuffer, GL_RENDERBUFFER, GL_COLOR_ATTACHMENT0, SizeX, SizeY, 0, 1, 1, 1, PF_B8G8R8A8, false, false, Flags, nullptr, FClearValueBinding::Transparent);
+	FOpenGLTexture2D* Texture2D = new FOpenGLTexture2D(OpenGLRHI, GLView.OnScreenColorRenderBuffer, GL_RENDERBUFFER, GL_COLOR_ATTACHMENT0, SizeX, SizeY, 0, 1, 1, 1, 1, PF_B8G8R8A8, false, false, Flags, nullptr, FClearValueBinding::Transparent);
 	OpenGLTextureAllocated(Texture2D, Flags);
 
 	return Texture2D;
