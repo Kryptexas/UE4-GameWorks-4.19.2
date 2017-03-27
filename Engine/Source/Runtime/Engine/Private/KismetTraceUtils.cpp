b24c5f3ba9dca6bef4e0593c1943827c0bc35a16
Andrew Grant <Andrew.Grant@epicgames.com>
2017-03-27 17:53:37 -0400

    Pulling following fixes from //UE4/Orion-Staging #lockdown Nick.Penwarden
    - Fixed content warnings in Orion
    - Non-unity compile fix
    - Reenabled DeathCam
    - Disable tests from clearing old builds (somewhere a devkit has a non-windows accessible path..)
    
    [CL 3366734 by Andrew Grant in Main branch]

--- Engine/Source/Runtime/Engine/Private/KismetTraceUtils.cpp
+++ Engine/Source/Runtime/Engine/Private/KismetTraceUtils.cpp
@@ -1,6 +1,7 @@
 // Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
 
 #include "KismetTraceUtils.h"
+#include "GameFramework/Actor.h"
 #include "PhysicsEngine/PhysicsSettings.h"
 #include "GameFramework/Actor.h"
 
