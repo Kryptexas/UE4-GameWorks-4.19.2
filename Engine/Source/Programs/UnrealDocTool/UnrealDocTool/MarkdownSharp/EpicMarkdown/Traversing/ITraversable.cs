30fd7b43f2ec288da754dfb56047b66e157fc11e
Ben Marsh <Ben.Marsh@epicgames.com>
2017-04-13 07:51:42 -0400

    [INTEGRATE] Change 3391225 by Ben.Marsh@Ben.Marsh_T3245_UE4 on 2017/04/12 16:47:02
    	Change documentation build steps to sync tools from //depot/UnrealDocTool rather than using outdated versions in the engine branch. Remove Doxygen and HTML Help Workshop from //UE4/Main.
    
    	As part of this, added a new UAT command ("SyncDepotPath") to create a temporary workspace, sync a folder, and delete the workspace.
    
    	#rb none
    	#codereview Richard.Hinckley, Jeff.Wilson
    	#lockdown Nick.Penwarden
    
    [CL 3392159 by Ben Marsh in 4.16 branch]

--- Engine/Source/Programs/UnrealDocTool/UnrealDocTool/MarkdownSharp/EpicMarkdown/Traversing/ITraversable.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
-
-namespace MarkdownSharp.EpicMarkdown.Traversing
-{
-    public interface ITraversable
-    {
-        void ForEachWithContext<TContext>(TContext context) where TContext : ITraversingContext;
-    }
-}
