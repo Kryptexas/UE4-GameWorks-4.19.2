b26774852c60888cda6b5959543bec0098734390
Arne Schober <Arne.Schober@epicgames.com>
2017-04-19 15:19:34 -0400

    DR - Updated NVAPI #RB Marcus.Wassmer #lockdown Nick.Penwarden #jira UE-42280
    [CL 3400490 by Arne Schober in 4.15 branch]

--- Engine/Source/ThirdParty/NVIDIA/nvapi/nvapi_lite_sli.h
+++ Engine/Source/ThirdParty/NVIDIA/nvapi/nvapi_lite_sli.h
@@ -1,3 +1,37 @@
+ /************************************************************************************************************************************\
+|*                                                                                                                                    *|
+|*     Copyright © 2012 NVIDIA Corporation.  All rights reserved.                                                                     *|
+|*                                                                                                                                    *|
+|*  NOTICE TO USER:                                                                                                                   *|
+|*                                                                                                                                    *|
+|*  This software is subject to NVIDIA ownership rights under U.S. and international Copyright laws.                                  *|
+|*                                                                                                                                    *|
+|*  This software and the information contained herein are PROPRIETARY and CONFIDENTIAL to NVIDIA                                     *|
+|*  and are being provided solely under the terms and conditions of an NVIDIA software license agreement.                             *|
+|*  Otherwise, you have no rights to use or access this software in any manner.                                                       *|
+|*                                                                                                                                    *|
+|*  If not covered by the applicable NVIDIA software license agreement:                                                               *|
+|*  NVIDIA MAKES NO REPRESENTATION ABOUT THE SUITABILITY OF THIS SOFTWARE FOR ANY PURPOSE.                                            *|
+|*  IT IS PROVIDED "AS IS" WITHOUT EXPRESS OR IMPLIED WARRANTY OF ANY KIND.                                                           *|
+|*  NVIDIA DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,                                                                     *|
+|*  INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE.                       *|
+|*  IN NO EVENT SHALL NVIDIA BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL, OR CONSEQUENTIAL DAMAGES,                               *|
+|*  OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,  WHETHER IN AN ACTION OF CONTRACT,                         *|
+|*  NEGLIGENCE OR OTHER TORTIOUS ACTION,  ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOURCE CODE.            *|
+|*                                                                                                                                    *|
+|*  U.S. Government End Users.                                                                                                        *|
+|*  This software is a "commercial item" as that term is defined at 48 C.F.R. 2.101 (OCT 1995),                                       *|
+|*  consisting  of "commercial computer  software"  and "commercial computer software documentation"                                  *|
+|*  as such terms are  used in 48 C.F.R. 12.212 (SEPT 1995) and is provided to the U.S. Government only as a commercial end item.     *|
+|*  Consistent with 48 C.F.R.12.212 and 48 C.F.R. 227.7202-1 through 227.7202-4 (JUNE 1995),                                          *|
+|*  all U.S. Government End Users acquire the software with only those rights set forth herein.                                       *|
+|*                                                                                                                                    *|
+|*  Any use of this software in individual and commercial software must include,                                                      *|
+|*  in the user documentation and internal comments to the code,                                                                      *|
+|*  the above Disclaimer (as applicable) and U.S. Government End Users Notice.                                                        *|
+|*                                                                                                                                    *|
+ \************************************************************************************************************************************/
+
 #pragma once
 #include"nvapi_lite_salstart.h"
 #include"nvapi_lite_common.h"
@@ -66,6 +100,7 @@ typedef struct
 NVAPI_INTERFACE NvAPI_D3D_GetCurrentSLIState(IUnknown *pDevice, NV_GET_CURRENT_SLI_STATE *pSliState);
 #endif //if defined(_D3D9_H_) || defined(__d3d10_h__) || defined(__d3d11_h__)
 #if defined(_D3D9_H_) || defined(__d3d10_h__) || defined(__d3d11_h__)
+
 ///////////////////////////////////////////////////////////////////////////////
 //
 // FUNCTION NAME: NvAPI_D3D_SetResourceHint
@@ -77,18 +112,21 @@ NVAPI_INTERFACE NvAPI_D3D_GetCurrentSLIState(IUnknown *pDevice, NV_GET_CURRENT_S
 //!
 //!   DESCRIPTION: This is a general purpose function for passing down various resource
 //!                related hints to the driver. Hints are divided into categories
-//!                and types within each category.
+//!                and types within each category. For DX11 devices this function is free-threaded.
+//!                An application is responsible to complete this call before making use of the resource 
+//!                in a rendering context (therefore applying inter-thread synchronization as appropriate).
+//!                As a debug help to an application the driver enforces that a resource in this call was never bound.
 //!
 //! SUPPORTED OS:  Windows XP and higher
 //!
 //!
 //! \since Release: 185
 //!
-//! \param [in] pDev            The ID3D10Device or IDirect3DDevice9 that is a using the resource
-//! \param [in] obj             Previously obtained HV resource handle
-//! \param [in] dwHintCategory  Category of the hints
-//! \param [in] dwHintName      A hint within this category
-//! \param [in] *pdwHintValue   Pointer to location containing hint value
+//! \param [in]      pDev            The ID3D10Device or IDirect3DDevice9 that is a using the resource
+//! \param [in]      obj             Previously obtained HV resource handle
+//! \param [in]      dwHintCategory  Category of the hints
+//! \param [in]      dwHintName      A hint within this category
+//! \param [in/out]  *pdwHintValue   Pointer to location containing hint value, function returns previous hint value in this slot
 //!
 //! \return an int which could be an NvAPI status or DX HRESULT code
 //!
@@ -135,6 +173,7 @@ NVAPI_INTERFACE NvAPI_D3D_SetResourceHint(IUnknown *pDev, NVDX_ObjectHandle obj,
                                           NvU32 dwHintName, 
                                           NvU32 *pdwHintValue);
 #endif //defined(_D3D9_H_) || defined(__d3d10_h__) || defined(__d3d11_h__)
+
 #if defined(_D3D9_H_) || defined(__d3d10_h__) || defined(__d3d11_h__)
 ///////////////////////////////////////////////////////////////////////////////
 //
@@ -173,6 +212,7 @@ typedef enum  _NVAPI_D3D_RESOURCERENDERING_FLAG
 NVAPI_INTERFACE NvAPI_D3D_BeginResourceRendering(IUnknown *pDev, NVDX_ObjectHandle obj, NvU32 Flags);
 
 #endif //defined(_D3D9_H_) || defined(__d3d10_h__) || defined(__d3d11_h__)
+
 #if defined(_D3D9_H_) || defined(__d3d10_h__) || defined(__d3d11_h__)
 ///////////////////////////////////////////////////////////////////////////////
 //
