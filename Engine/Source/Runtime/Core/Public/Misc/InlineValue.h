ba02ab3428d509f3fae1a055d5046834b0354e60
Andrew Rodham <Andrew.Rodham@epicgames.com>
2017-04-26 05:21:23 -0400

    Core: Changed TInlineValue::Reset() to not use Swap   - This works around a PS4 compiler optimization that seems to result in reuse of a garbage register as part of FPS4Memory::Memcpy()
    #jira FORT-41952
    #jira UE-44381
    #rb James.Hopkin
    
    [CL 3409095 by Andrew Rodham in 4.16 branch]

--- Engine/Source/Runtime/Core/Public/Misc/InlineValue.h
+++ Engine/Source/Runtime/Core/Public/Misc/InlineValue.h
@@ -83,9 +83,16 @@ public:
 	 */
 	void Reset(TInlineValue&& In)
 	{
-		if (In.bIsValid || bIsValid)
+		Reset();
+
+		if (In.bIsValid)
 		{
-			Swap(*this, In);
+			// Steal the object contained within 'In'
+			bIsValid = true;
+			In.bIsValid = false;
+
+			bInline = In.bInline;
+			Data = In.Data;
 		}
 	}
 
