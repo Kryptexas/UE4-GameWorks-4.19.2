8c89f930c37716002eacc0435f829fcaefce21e1
Ben Marsh <Ben.Marsh@epicgames.com>
2017-06-29 10:37:42 -0400

    Update version in //UE4/Main to 4.18.
    #rb none
    #lockdown Nick.Penwarden
    
    [CL 3515882 by Ben Marsh in Main branch]

--- Engine/Source/Runtime/Launch/Resources/Version.h
+++ Engine/Source/Runtime/Launch/Resources/Version.h
@@ -55,7 +55,7 @@
 // These numbers define the banner UE4 version, and are the most significant numbers when ordering two engine versions (that is, a 4.12.* version is always 
 // newer than a 4.11.* version, regardless of the changelist that it was built with)
 #define ENGINE_MAJOR_VERSION	4
-#define ENGINE_MINOR_VERSION	17
+#define ENGINE_MINOR_VERSION	18
 #define ENGINE_PATCH_VERSION	0
 
 // If set to 1, indicates that this is a licensee build of the engine. For the same major/minor/patch release of the engine, licensee changelists are always 
