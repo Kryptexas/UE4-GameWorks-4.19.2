42208f0d228284a8cc26231fe900ec6df05c9862
Jeff Fisher <Jeff.Fisher@epicgames.com>
2017-06-29 14:22:22 -0400

    UE-46651 Failure to build new C++ Project: Error C1083: Cannot open include file: 'Kismet/HeadMountedDisplayFunctionLibrary.h': No such file or directory -Followup change -Added#pragma once... the 'warn on ever include' idea doesn't really work as you get multiples for modules and it doesn't tell you where the include is anyway. #review-3515917 #jira UE-46651 #rb Ben.Marsh #lockdown nick.penwarden
    [CL 3516318 by Jeff Fisher in Main branch]

--- Engine/Source/Runtime/Engine/Classes/Kismet/HeadMountedDisplayFunctionLibrary.h
+++ Engine/Source/Runtime/Engine/Classes/Kismet/HeadMountedDisplayFunctionLibrary.h
@@ -1,5 +1,7 @@
 // Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
 
+#pragma once
+
 #include "Platform.h"
 
 COMPILE_WARNING("This file has been moved to HeadMountedDisplay/Public/HeadMountedDisplayFunctionLibrary.h. Some types were moved to HeadMountedDisplay/Public/HeadMountedDisplayTypes.h.  Replace all #include \"Kismet\\HeadMountedDisplayFunctionLibrary.h\" with: #include \"HeadMountedDisplayFunctionLibrary.h\" or #include \"HeadMountedDisplayTypes.h\".")
