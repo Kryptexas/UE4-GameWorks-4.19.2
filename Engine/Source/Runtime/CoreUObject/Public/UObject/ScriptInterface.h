905436ba4833f468321917ba4d4928bbcc8830ca
Phillip Kavan <pkavan@squareksoftware.com>
2017-07-11 11:47:36 -0400

    #jira UE-46952 - Fix a packaging code build failure that will occur with a nativized Blueprint class that contains a UInterfaceProperty.
    - Mirrored from //UE4/Dev-Framework (CL# 3531094).
    
    #rb None
    
    [CL 3531116 by Phillip Kavan in 4.17 branch]

--- Engine/Source/Runtime/CoreUObject/Public/UObject/ScriptInterface.h
+++ Engine/Source/Runtime/CoreUObject/Public/UObject/ScriptInterface.h
@@ -158,7 +158,7 @@ public:
 	{
 		SetObject(SourceObject);
 		
-		InterfaceType* SourceInterface = SourceObject;
+		InterfaceType* SourceInterface = Cast<InterfaceType>(SourceObject);
 		SetInterface( SourceInterface );
 
 		return *((InterfaceType*)GetInterface());
