31ef40810e08659be7fa4ba2d90618dacb08513a
Ben Marsh <Ben.Marsh@epicgames.com>
2017-07-12 10:09:45 -0400

    Fixing up case of "iOS" folders (pt 2)
    #rb none
    #jira UE-123
    
    [CL 3533003 by Ben Marsh in Staging-4.17 branch]

---
+++ Engine/Source/Runtime/Core/Public/IOS/IOSPlatformAffinity.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "GenericPlatform/GenericPlatformAffinity.h"
+
+class CORE_API FIOSPlatformAffinity : public FGenericPlatformAffinity
+{
+public:
+	static const uint64 GetMainGameMask();
+	static const uint64 GetRenderingThreadMask();
+	static const uint64 GetRTHeartBeatMask();
+	static const uint64 GetPoolThreadMask();
+	static const uint64 GetTaskGraphThreadMask();
+	static const uint64 GetStatsThreadMask();
+	static const uint64 GetNoAffinityMask();
+};
+
+typedef FIOSPlatformAffinity FPlatformAffinity;
---
+++ Engine/Source/Runtime/Core/Public/IOS/IOSPlatformMemory.h
@@ -0,0 +1,37 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+
+/*=============================================================================================
+	IOSPlatformMemory.h: IOS platform memory functions
+==============================================================================================*/
+
+#pragma once
+#include "GenericPlatform/GenericPlatformMemory.h"
+#include "Apple/ApplePlatformMemory.h"
+
+/**
+ *	IOS implementation of the FGenericPlatformMemoryStats.
+ */
+struct FPlatformMemoryStats : public FGenericPlatformMemoryStats
+{};
+
+/**
+* IOS implementation of the memory OS functions
+**/
+struct CORE_API FIOSPlatformMemory : public FGenericPlatformMemory
+{
+	//~ Begin FGenericPlatformMemory Interface
+	static void Init();
+	static FPlatformMemoryStats GetStats();
+	static const FPlatformMemoryConstants& GetConstants();
+	static FMalloc* BaseAllocator();
+	static bool PageProtect(void* const Ptr, const SIZE_T Size, const bool bCanRead, const bool bCanWrite);
+	static void* BinnedAllocFromOS( SIZE_T Size );
+	static void BinnedFreeToOS( void* Ptr, SIZE_T Size );
+	//~ End FGenericPlatformMemory Interface
+};
+
+typedef FIOSPlatformMemory FPlatformMemory;
+
+
+
