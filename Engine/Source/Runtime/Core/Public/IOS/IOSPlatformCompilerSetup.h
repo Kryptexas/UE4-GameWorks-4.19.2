6e27258e19e4f7628f6c43c1bb6c791ae7cdff09
Ben Marsh <Ben.Marsh@epicgames.com>
2017-07-17 15:04:29 -0400

    Fixing case of files in iOS directory (pt. 1)
    #rb none
    #jira UE-47332
    #lockdown Cristina.Riveron
    
    [CL 3540521 by Ben Marsh in 4.16 branch]

--- Engine/Source/Runtime/Core/Public/IOS/IOSPlatformCompilerSetup.h
+++
@@ -1,7 +0,0 @@
-// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
-
-/*=============================================================================================
-	IOSPlatformCompilerSetup.h: pragmas, version checks and other things for the iOS compiler
-==============================================================================================*/
-
-#pragma once
--- Engine/Source/Runtime/Core/Public/iOS/IOSCriticalSection.h
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
-
-#pragma once
-
-#include "GenericPlatform/GenericPlatformCriticalSection.h"
-#include "PThreadCriticalSection.h"
-
-typedef FPThreadsCriticalSection FCriticalSection;
-typedef FSystemWideCriticalSectionNotImplemented FSystemWideCriticalSection;
