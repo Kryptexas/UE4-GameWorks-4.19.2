c3e1feed3ef7a09472c5e8fbbf05d83700f2989e
Ben Marsh <Ben.Marsh@epicgames.com>
2017-07-27 10:47:12 -0400

    Disable non-portable path warning on TVOS SDK 10.2.
    #rb none
    #lockdown Cristina.Riveron
    #jira UE-47733
    
    [CL 3558291 by Ben Marsh in 4.16 branch]

--- Engine/Source/Programs/UnrealBuildTool/IOS/IOSToolChain.cs
+++ Engine/Source/Programs/UnrealBuildTool/IOS/IOSToolChain.cs
@@ -218,7 +218,7 @@ namespace UnrealBuildTool
 			
 
 			// fix for Xcode 8.3 enabling nonportable include checks, but p4 has some invalid cases in it
-			if (Settings.Value.IOSSDKVersionFloat >= 10.3)
+			if (Settings.Value.IOSSDKVersionFloat >= 10.3 || (CppPlatform == CppPlatform.TVOS && Settings.Value.IOSSDKVersionFloat >= 10.2))
 			{
 				Result += " -Wno-nonportable-include-path";
 			}
