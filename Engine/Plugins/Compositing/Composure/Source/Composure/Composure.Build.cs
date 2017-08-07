bbdd470cb9516705877b730566b31e99c8e71bcb
Ben Marsh <Ben.Marsh@epicgames.com>
2017-08-07 16:21:35 -0400

    Another CIS fix.
    #rb none
    #jira
    
    [CL 3575495 by Ben Marsh in Staging-4.17 branch]

--- Engine/Plugins/Compositing/Composure/Source/Composure/Composure.Build.cs
+++ Engine/Plugins/Compositing/Composure/Source/Composure/Composure.Build.cs
@@ -24,7 +24,7 @@ namespace UnrealBuildTool.Rules
 				}
 				);
             
-            if (UEBuildConfiguration.bBuildEditor == true)
+            if (Target.bBuildEditor == true)
             {
                 PrivateDependencyModuleNames.Add("UnrealEd");
             }
