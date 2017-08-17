1eb465077f3e6820b9775a2633b4f98f4323d061
Ben Marsh <Ben.Marsh@epicgames.com>
2017-08-17 15:17:14 -0400

    UBT: Prevent exception if a plugin lists a module twice.
    #rb none
    #jira UE-48232
    #lockdown Cristina.Riveron
    
    [CL 3594279 by Ben Marsh in 4.17 branch]

--- Engine/Source/Programs/UnrealBuildTool/Configuration/UEBuildTarget.cs
+++ Engine/Source/Programs/UnrealBuildTool/Configuration/UEBuildTarget.cs
@@ -3956,12 +3956,15 @@ namespace UnrealBuildTool
 						if (ModuleInfo.IsCompiledInConfiguration(Platform, TargetType, Rules.bBuildDeveloperTools, Rules.bBuildEditor, Rules.bBuildRequiresCookedData))
 						{
 							UEBuildModule Module = FindOrCreateModuleByName(ModuleInfo.Name);
-							if (!Module.RulesFile.IsUnderDirectory(Info.Directory))
+							if (!Instance.Modules.Contains(Module))
 							{
-								throw new BuildException("Plugin '{0}' does not contain the '{1}' module, but lists it in '{2}'.", Info.Name, ModuleInfo.Name, Info.File);
+								if (!Module.RulesFile.IsUnderDirectory(Info.Directory))
+								{
+									throw new BuildException("Plugin '{0}' does not contain the '{1}' module, but lists it in '{2}'.", Info.Name, ModuleInfo.Name, Info.File);
+								}
+								Instance.bDescriptorNeededAtRuntime = true;
+								Instance.Modules.Add(Module);
 							}
-							Instance.bDescriptorNeededAtRuntime = true;
-							Instance.Modules.Add(Module);
 						}
 					}
 				}
