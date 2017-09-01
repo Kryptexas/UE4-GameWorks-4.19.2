a2c3c4ac4352befc1d4f2924cf47dc52b9ba2385
Arciel Rekman <arciel.rekman@epicgames.com>
2017-09-01 17:09:55 -0400

    Rebuilt glslang libs against bundled libc++ (UE-49046).
    #rb none
    #jira UE-49046
    #lockdown Nick.Penwarden
    #fyi Timothee.Bessett
    
    [CL 3623364 by Arciel Rekman in Main branch]

--- Engine/Source/ThirdParty/glslang/Build/Linux/build.bat
+++ Engine/Source/ThirdParty/glslang/Build/Linux/build.bat
@@ -50,6 +50,38 @@ rem set ARCHITECTURE_TRIPLE="aarch64-unknown-linux-gnueabi"
 
 
 
+rem LibCxx
+
+set LIBCXX_INCLUDE1=
+
+pushd ..\..\..\..\..\Source\ThirdParty\Linux\LibCxx\include\c++\v1\
+
+set LIBCXX_INCLUDE1=%CD%
+
+popd
+
+
+
+set LIBCXX_INCLUDE2=
+
+pushd ..\..\..\..\..\Source\ThirdParty\Linux\LibCxx\include\
+
+set LIBCXX_INCLUDE2=%CD%
+
+popd
+
+
+
+set LIBCXX_LIB=
+
+pushd ..\..\..\..\..\Source\ThirdParty\Linux\LibCxx\lib\Linux\%ARCHITECTURE_TRIPLE%
+
+set LIBCXX_LIB=%CD%
+
+popd
+
+
+
 rem ## Compile and Build.
 
 rmdir /S /Q cmake-temp
@@ -58,7 +90,7 @@ mkdir cmake-temp
 
 cd cmake-temp
 
-%CMAKE_PATH%\cmake -DCMAKE_MAKE_PROGRAM=%MAKE_PATH%\make.exe -DCMAKE_TOOLCHAIN_FILE="../LinuxCrossToolchain.multiarch.cmake" -DARCHITECTURE_TRIPLE=%ARCHITECTURE_TRIPLE% -G"Unix Makefiles" ../../../../glslang/glslang/src/glslang_lib
+%CMAKE_PATH%\cmake -DCMAKE_MAKE_PROGRAM=%MAKE_PATH%\make.exe -DCMAKE_TOOLCHAIN_FILE="../LinuxCrossToolchain.multiarch.cmake" -DARCHITECTURE_TRIPLE=%ARCHITECTURE_TRIPLE% -DUSE_CUSTOM_LIBCXX=ON -DLIBCXX_INCLUDE=%LIBCXX_INCLUDE1% -DLIBCXX_ABI_INCLUDE=%LIBCXX_INCLUDE2% -DLIBCXX_LIB_DIR=%LIBCXX_LIB% -G"Unix Makefiles" ../../../../glslang/glslang/src/glslang_lib
 
 %MAKE_PATH%\make.exe
 
