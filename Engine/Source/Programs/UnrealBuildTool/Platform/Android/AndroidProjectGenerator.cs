ea22028ecdf447b451fa5a9b7f15d46503530d57
Chris Babcock <Chris.Babcock@epicgames.com>
2017-09-21 14:06:44 -0400

    Fix compile error in Android project generator on Linux #jira UE-49862 #ue4 #android #rb Peter.Sauerbrei #lockdown Cristina.Riveron
    [CL 3656732 by Chris Babcock in 4.18 branch]

--- Engine/Source/Programs/UnrealBuildTool/Platform/Android/AndroidProjectGenerator.cs
+++ Engine/Source/Programs/UnrealBuildTool/Platform/Android/AndroidProjectGenerator.cs
@@ -49,8 +49,8 @@ namespace UnrealBuildTool
 			else
 			{
 				// If the sandboxed SDK is not present (pre Visual Studio 15.4) then the non-Sandboxed SDK tools should have the correct build tools for building
-				string Path = Microsoft.Win32.Registry.GetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\Android SDK Tools", "Path", null) as string;
-				if (!String.IsNullOrEmpty(Path) && VSDebugCommandLineOptionPresent)
+				string SDKToolsPath = Microsoft.Win32.Registry.GetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\Android SDK Tools", "Path", null) as string;
+				if (!String.IsNullOrEmpty(SDKToolsPath) && VSDebugCommandLineOptionPresent)
 				{
 					VSDebuggingEnabled = true;
 				}
