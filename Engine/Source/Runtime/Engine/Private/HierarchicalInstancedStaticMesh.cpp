ff17d073e81cb637625136f8af3dfb3e7f2a1993
Michael Dupuis <michel@ifdeftech.com>
2017-09-25 17:40:43 -0400

    #jira UE-48955: Also check the pending request list as we might be in a case where we request an update but it was'nt processed yet #rb none #codereview jack.porter, lukasz.furman #lockdown nick.penwarden
    [CL 3663111 by Michael Dupuis in 4.18 branch]

--- Engine/Source/Runtime/Engine/Private/HierarchicalInstancedStaticMesh.cpp
+++ Engine/Source/Runtime/Engine/Private/HierarchicalInstancedStaticMesh.cpp
@@ -2711,7 +2711,7 @@ void UHierarchicalInstancedStaticMeshComponent::PartialNavigationUpdate(int32 In
 		// Accumulate dirty areas and send them to navigation system once cluster tree is rebuilt
 		UNavigationSystem* NavSys = UNavigationSystem::GetCurrent(GetWorld());
 		// Check if this component is registered in navigation system
-		if (NavSys && NavSys->GetObjectsNavOctreeId(this))
+		if (NavSys && (NavSys->GetObjectsNavOctreeId(this) || NavSys->HasPendingObjectNavOctreeId(this)))
 		{
 			FTransform InstanceTransform(PerInstanceSMData[InstanceIdx].Transform);
 			FBox InstanceBox = GetStaticMesh()->GetBounds().TransformBy(InstanceTransform*GetComponentTransform()).GetBox(); // in world space
