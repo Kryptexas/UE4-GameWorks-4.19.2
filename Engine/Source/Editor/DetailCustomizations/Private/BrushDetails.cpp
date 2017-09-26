77efa1078ce48e82c879e151395c1000dc791eaa
Matt Kuhlenschmidt <matt.kuhlenschmidt@epicgames.com>
2017-09-26 11:28:07 -0400

    Fix unsafe delegate access in FBrushDetails
    #jira UE-48998
    #rb none
    
    [CL 3664393 by Matt Kuhlenschmidt in 4.18 branch]

--- Engine/Source/Editor/DetailCustomizations/Private/BrushDetails.cpp
+++ Engine/Source/Editor/DetailCustomizations/Private/BrushDetails.cpp
@@ -44,8 +44,10 @@ FBrushDetails::~FBrushDetails()
 }
 
 BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
-void FBrushDetails::CustomizeDetails( IDetailLayoutBuilder& DetailLayout )
+void FBrushDetails::CustomizeDetails( IDetailLayoutBuilder& InDetailLayout )
 {
+	DetailLayout = &InDetailLayout;
+
 	// Get level editor commands for our menus
 	FLevelEditorModule& LevelEditor = FModuleManager::GetModuleChecked<FLevelEditorModule>( TEXT("LevelEditor") );
 	TSharedRef<const FUICommandList> CommandBindings = LevelEditor.GetGlobalLevelEditorActions();
@@ -53,7 +55,7 @@ void FBrushDetails::CustomizeDetails( IDetailLayoutBuilder& DetailLayout )
 
 	// See if we have a volume. If we do - we hide the BSP stuff (solidity, order)
 	bool bHaveAVolume = false;
-	const TArray< TWeakObjectPtr<UObject> >& SelectedObjects = DetailLayout.GetSelectedObjects();
+	const TArray< TWeakObjectPtr<UObject> >& SelectedObjects = InDetailLayout.GetSelectedObjects();
 	for (int32 ObjIdx = 0; ObjIdx < SelectedObjects.Num(); ObjIdx++)
 	{
 		if (ABrush* Brush = Cast<ABrush>(SelectedObjects[ObjIdx].Get()))
@@ -95,119 +97,20 @@ void FBrushDetails::CustomizeDetails( IDetailLayoutBuilder& DetailLayout )
 		OrderMenuBuilder.AddMenuEntry( Commands.OrderLast );
 	}
 
-	struct Local
-	{
-		static FReply ExecuteExecCommand(FString InCommand)
-		{
-			GUnrealEd->Exec( GWorld, *InCommand );
-			return FReply::Handled();
-		}
-
-		static TSharedRef<SWidget> GenerateBuildMenuContent(TSharedRef<IPropertyHandle> BrushBuilderHandle, IDetailLayoutBuilder* InDetailLayout)
-		{
-			class FBrushFilter : public IClassViewerFilter
-			{
-			public:
-				virtual bool IsClassAllowed(const FClassViewerInitializationOptions& InInitOptions, const UClass* InClass, TSharedRef< class FClassViewerFilterFuncs > InFilterFuncs )
-				{
-					return !InClass->HasAnyClassFlags(CLASS_NotPlaceable) && !InClass->HasAnyClassFlags(CLASS_Abstract) && InClass->IsChildOf(UBrushBuilder::StaticClass());
-				}
-
-				virtual bool IsUnloadedClassAllowed(const FClassViewerInitializationOptions& InInitOptions, const TSharedRef< const class IUnloadedBlueprintData > InUnloadedClassData, TSharedRef< class FClassViewerFilterFuncs > InFilterFuncs)
-				{
-					return false;
-				}
-			};
-
-			FClassViewerInitializationOptions Options;
-			Options.ClassFilter = MakeShareable(new FBrushFilter);
-			Options.Mode = EClassViewerMode::ClassPicker;
-			Options.DisplayMode = EClassViewerDisplayMode::ListView;
-			return FModuleManager::LoadModuleChecked<FClassViewerModule>("ClassViewer").CreateClassViewer(Options, FOnClassPicked::CreateStatic(&Local::OnClassPicked, BrushBuilderHandle, InDetailLayout));
-		}
-
-		static void OnClassPicked(UClass* InChosenClass, TSharedRef<IPropertyHandle> BrushBuilderHandle, IDetailLayoutBuilder* InDetailLayout)
-		{
-			FSlateApplication::Get().DismissAllMenus();
-
-			TArray<UObject*> OuterObjects;
-			BrushBuilderHandle->GetOuterObjects(OuterObjects);
-
-			struct FNewBrushBuilder
-			{
-				UBrushBuilder* Builder;
-				ABrush* Brush;
-			};
-
-			TArray<FNewBrushBuilder> NewBuilders;
-			TArray<FString> NewObjectPaths;
-
-			{
-				const FScopedTransaction Transaction(NSLOCTEXT("UnrealEd", "BrushSet", "Brush Set"));
-				for (UObject* OuterObject : OuterObjects)
-				{
-					UBrushBuilder* NewUObject = NewObject<UBrushBuilder>(OuterObject, InChosenClass, NAME_None, RF_Transactional);
-
-					FNewBrushBuilder NewBuilder;
-					NewBuilder.Builder = NewUObject;
-					NewBuilder.Brush = CastChecked<ABrush>(OuterObject);
-
-					NewBuilders.Add(NewBuilder);
-					NewObjectPaths.Add(NewUObject->GetPathName());
-				}
-
-				BrushBuilderHandle->SetPerObjectValues(NewObjectPaths);
-
-				// make sure the brushes are rebuilt
-				for (FNewBrushBuilder& NewObject : NewBuilders)
-				{
-					NewObject.Builder->Build(NewObject.Brush->GetWorld(), NewObject.Brush);
-				}
-
-				GEditor->RebuildAlteredBSP();
-			}
-
-
-
-			InDetailLayout->ForceRefreshDetails();
-		}
-
-		static FText GetBuilderText(TSharedRef<IPropertyHandle> BrushBuilderHandle)
-		{
-			UObject* Object = nullptr;
-			BrushBuilderHandle->GetValue(Object);
-			if(Object != nullptr)
-			{
-				UBrushBuilder* BrushBuilder = CastChecked<UBrushBuilder>(Object);
-				const FText NameText = BrushBuilder->GetClass()->GetDisplayNameText();
-				if(!NameText.IsEmpty())
-				{
-					return NameText;
-				}
-				else
-				{
-					return FText::FromString(FName::NameToDisplayString(BrushBuilder->GetClass()->GetName(), false));
-				}
-			}
-
-			return LOCTEXT("None", "None");
-		}
-	};
-
 	// Hide the brush builder if it is NULL
-	TSharedRef<IPropertyHandle> BrushBuilderPropertyHandle = DetailLayout.GetProperty(GET_MEMBER_NAME_CHECKED(ABrush, BrushBuilder));
+	BrushBuilderHandle = InDetailLayout.GetProperty(GET_MEMBER_NAME_CHECKED(ABrush, BrushBuilder));
 	UObject* BrushBuilderObject = nullptr;
-	BrushBuilderPropertyHandle->GetValue(BrushBuilderObject);
+	BrushBuilderHandle->GetValue(BrushBuilderObject);
 	if(BrushBuilderObject == nullptr)
 	{
-		DetailLayout.HideProperty("BrushBuilder");
+		InDetailLayout.HideProperty("BrushBuilder");
 	}
 	else
 	{
 		BrushBuilderObject->SetFlags( RF_Transactional );
 	}
 
-	IDetailCategoryBuilder& BrushBuilderCategory = DetailLayout.EditCategory( "BrushSettings", FText::GetEmpty() );
+	IDetailCategoryBuilder& BrushBuilderCategory = InDetailLayout.EditCategory( "BrushSettings", FText::GetEmpty() );
 
 	BrushBuilderCategory.AddProperty( GET_MEMBER_NAME_CHECKED(ABrush, BrushType) );
 	BrushBuilderCategory.AddCustomRow( LOCTEXT("BrushShape", "Brush Shape") )
@@ -218,17 +121,17 @@ void FBrushDetails::CustomizeDetails( IDetailLayoutBuilder& DetailLayout )
 		.Font( IDetailLayoutBuilder::GetDetailFont() )
 	]
 	.ValueContent()
-	.MinDesiredWidth(113)
-	.MaxDesiredWidth(113)
+	.MinDesiredWidth(105)
+	.MaxDesiredWidth(105)
 	[
 		SNew(SComboButton)
 		.ToolTipText(LOCTEXT("BspModeBuildTooltip", "Rebuild this brush from a parametric builder."))
-		.OnGetMenuContent_Static(&Local::GenerateBuildMenuContent, BrushBuilderPropertyHandle, &DetailLayout)
+		.OnGetMenuContent(this, &FBrushDetails::GenerateBuildMenuContent)
 		.ContentPadding(2)
 		.ButtonContent()
 		[
 			SNew(STextBlock)
-			.Text_Static(&Local::GetBuilderText, BrushBuilderPropertyHandle)
+			.Text(this, &FBrushDetails::GetBuilderText)
 			.Font( IDetailLayoutBuilder::GetDetailFont() )
 		]
 	];
@@ -303,7 +206,7 @@ void FBrushDetails::CustomizeDetails( IDetailLayoutBuilder& DetailLayout )
 		[
 			SNew( SButton )
 			.ToolTipText( LOCTEXT("AlignBrushVerts_Tooltip", "Aligns each vertex of the brush to the grid.") )
-			.OnClicked( FOnClicked::CreateStatic( &Local::ExecuteExecCommand, FString( TEXT("ACTOR ALIGN VERTS") ) ) )
+			.OnClicked( FOnClicked::CreateSP(this, &FBrushDetails::ExecuteExecCommand, FString( TEXT("ACTOR ALIGN VERTS") ) ) )
 			.HAlign( HAlign_Center )
 			[
 				SNew( STextBlock )
@@ -329,6 +232,102 @@ void FBrushDetails::CustomizeDetails( IDetailLayoutBuilder& DetailLayout )
 		];
 	}
 }
+
+FReply FBrushDetails::ExecuteExecCommand(FString InCommand)
+{
+	GUnrealEd->Exec(GWorld, *InCommand);
+	return FReply::Handled();
+}
+
+TSharedRef<SWidget> FBrushDetails::GenerateBuildMenuContent()
+{
+	class FBrushFilter : public IClassViewerFilter
+	{
+	public:
+		virtual bool IsClassAllowed(const FClassViewerInitializationOptions& InInitOptions, const UClass* InClass, TSharedRef< class FClassViewerFilterFuncs > InFilterFuncs)
+		{
+			return !InClass->HasAnyClassFlags(CLASS_NotPlaceable) && !InClass->HasAnyClassFlags(CLASS_Abstract) && InClass->IsChildOf(UBrushBuilder::StaticClass());
+		}
+
+		virtual bool IsUnloadedClassAllowed(const FClassViewerInitializationOptions& InInitOptions, const TSharedRef< const class IUnloadedBlueprintData > InUnloadedClassData, TSharedRef< class FClassViewerFilterFuncs > InFilterFuncs)
+		{
+			return false;
+		}
+	};
+
+	FClassViewerInitializationOptions Options;
+	Options.ClassFilter = MakeShareable(new FBrushFilter);
+	Options.Mode = EClassViewerMode::ClassPicker;
+	Options.DisplayMode = EClassViewerDisplayMode::ListView;
+	return FModuleManager::LoadModuleChecked<FClassViewerModule>("ClassViewer").CreateClassViewer(Options, FOnClassPicked::CreateSP(this, &FBrushDetails::OnClassPicked));
+}
+
+void FBrushDetails::OnClassPicked(UClass* InChosenClass)
+{
+	FSlateApplication::Get().DismissAllMenus();
+
+	TArray<UObject*> OuterObjects;
+	BrushBuilderHandle->GetOuterObjects(OuterObjects);
+
+	struct FNewBrushBuilder
+	{
+		UBrushBuilder* Builder;
+		ABrush* Brush;
+	};
+
+	TArray<FNewBrushBuilder> NewBuilders;
+	TArray<FString> NewObjectPaths;
+
+	{
+		const FScopedTransaction Transaction(NSLOCTEXT("UnrealEd", "BrushSet", "Brush Set"));
+		for (UObject* OuterObject : OuterObjects)
+		{
+			UBrushBuilder* NewUObject = NewObject<UBrushBuilder>(OuterObject, InChosenClass, NAME_None, RF_Transactional);
+
+			FNewBrushBuilder NewBuilder;
+			NewBuilder.Builder = NewUObject;
+			NewBuilder.Brush = CastChecked<ABrush>(OuterObject);
+
+			NewBuilders.Add(NewBuilder);
+			NewObjectPaths.Add(NewUObject->GetPathName());
+		}
+
+		BrushBuilderHandle->SetPerObjectValues(NewObjectPaths);
+
+		// make sure the brushes are rebuilt
+		for (FNewBrushBuilder& NewObject : NewBuilders)
+		{
+			NewObject.Builder->Build(NewObject.Brush->GetWorld(), NewObject.Brush);
+		}
+
+		GEditor->RebuildAlteredBSP();
+	}
+
+	DetailLayout->ForceRefreshDetails();
+}
+
+FText FBrushDetails::GetBuilderText() const
+{
+	UObject* Object = nullptr;
+	BrushBuilderHandle->GetValue(Object);
+	if (Object != nullptr)
+	{
+		UBrushBuilder* BrushBuilder = CastChecked<UBrushBuilder>(Object);
+		const FText NameText = BrushBuilder->GetClass()->GetDisplayNameText();
+		if (!NameText.IsEmpty())
+		{
+			return NameText;
+		}
+		else
+		{
+			return FText::FromString(FName::NameToDisplayString(BrushBuilder->GetClass()->GetName(), false));
+		}
+	}
+
+	return LOCTEXT("None", "None");
+}
+
+
 END_SLATE_FUNCTION_BUILD_OPTIMIZATION
 
 FReply FBrushDetails::OnCreateStaticMesh()
