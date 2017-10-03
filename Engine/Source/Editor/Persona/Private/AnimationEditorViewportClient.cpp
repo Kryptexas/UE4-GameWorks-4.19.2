e5942d2c648e6bc996a034c00e805783f7128dfa
James Golding <james.golding@epicgames.com>
2017-10-03 13:30:21 -0400

    Fix highlighting material no longer working in skel mesh tools #jira UE-50690 #rb lina.halper #lockdown nick.penwarden
    [CL 3677966 by James Golding in 4.18 branch]

--- Engine/Source/Editor/Persona/Private/AnimationEditorViewportClient.cpp
+++ Engine/Source/Editor/Persona/Private/AnimationEditorViewportClient.cpp
@@ -1527,7 +1527,7 @@ bool FAnimationViewportClient::PreviewComponentSelectionOverride(const UPrimitiv
 		USkeletalMesh* Mesh = Component->SkeletalMesh;
 		if (Mesh)
 		{
-			return (Mesh->SelectedEditorSection != INDEX_NONE);
+			return (Mesh->SelectedEditorSection != INDEX_NONE || Mesh->SelectedEditorMaterial != INDEX_NONE);
 		}
 	}
 
