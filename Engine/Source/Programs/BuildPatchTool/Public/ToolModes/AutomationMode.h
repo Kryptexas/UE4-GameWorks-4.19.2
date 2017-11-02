136a0da70bc60b5f31d66f4df3b9b41a2a3bd4de
Steve Robb <Steve.Robb@epicgames.com>
2017-11-02 14:55:28 -0400

    Fixes for build errors.
    #rb none
    #jira UE-51925
    #lockdown nick.penwarden
    
    [CL 3733381 by Steve Robb in 4.18 branch]

--- Engine/Source/Programs/BuildPatchTool/Public/ToolModes/AutomationMode.h
+++ Engine/Source/Programs/BuildPatchTool/Public/ToolModes/AutomationMode.h
@@ -1,9 +1,10 @@
 // Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
 #pragma once
 
+#include "CoreMinimal.h"
+
 #if !UE_BUILD_SHIPPING
 
-#include "CoreMinimal.h"
 #include "ToolMode.h"
 
 namespace BuildPatchTool
