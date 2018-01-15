d623b0e868946d6c526be4fd359997dd82e8bf46
Mike Erwin <Mike.Erwin@epicgames.com>
2018-01-15 16:29:21 -0500

    glTF: binary format's BIN chunk is not necessarily right after the JSON chunk.
    Discussed this with glTF spec authors. See https://github.com/KhronosGroup/glTF/issues/1177
    
    Rearranged how we read "chunks" from the file since they are not fixed in number or order, besides JSON always being first.
    
    #jira UE-50695
    #rb none
    
    [CL 3838919 by Mike Erwin in 4.19 branch]

--- Engine/Plugins/Editor/GLTFImporter/Source/GLTFImporter/Private/GLTFImportFactory.cpp
+++ Engine/Plugins/Editor/GLTFImporter/Source/GLTFImporter/Private/GLTFImportFactory.cpp
@@ -29,6 +29,39 @@ UGLTFImportFactory::UGLTFImportFactory(const FObjectInitializer& ObjectInitializ
 	Formats.Add(TEXT("glb;GL Transmission Format (Binary)"));
 }
 
+static bool SignatureMatches(uint32 Signature, const char* ExpectedSignature)
+{
+	return Signature == *(const uint32*)ExpectedSignature;
+}
+
+static bool IsHeaderValid(FArchive* Archive)
+{
+	// Binary glTF files begin with a 12-byte header:
+	// - magic bytes "glTF"
+	// - format version
+	// - size of this file
+
+	const int64 FileSize = Archive->TotalSize();
+	if (FileSize < 12)
+	{
+		return false;
+	}
+
+	uint32 Magic;
+	Archive->SerializeInt(Magic, MAX_uint32);
+	bool MagicOk = SignatureMatches(Magic, "glTF");
+
+	uint32 Version;
+	Archive->SerializeInt(Version, MAX_uint32);
+	bool VersionOk = Version == 2;
+
+	uint32 Size;
+	Archive->SerializeInt(Size, MAX_uint32);
+	bool SizeOk = Size == FileSize;
+
+	return MagicOk && VersionOk && SizeOk;
+}
+
 bool UGLTFImportFactory::FactoryCanImport(const FString& Filename)
 {
 	bool bCanImport = false;
@@ -41,34 +74,10 @@ bool UGLTFImportFactory::FactoryCanImport(const FString& Filename)
 	}
 	else if (Extension == TEXT("glb"))
 	{
-		// Binary glTF files begin with a 12-byte header:
-		// - magic bytes "glTF"
-		// - format version
-		// - size of this file
-
 		FArchive* Reader = IFileManager::Get().CreateFileReader(*Filename);
 		if (Reader)
 		{
-			const int64 FileSize = Reader->TotalSize();
-			if (FileSize >= 12)
-			{
-				uint8 Magic[4];
-				Reader->Serialize(Magic, 4);
-				bool MagicOk = (Magic[0] == 'g' &&
-				                Magic[1] == 'l' &&
-				                Magic[2] == 'T' &&
-				                Magic[3] == 'F');
-
-				uint32 Version;
-				Reader->SerializeInt(Version, MAX_uint32);
-				bool VersionOk = Version == 2;
-
-				uint32 Size;
-				Reader->SerializeInt(Size, MAX_uint32);
-				bool SizeOk = Size == FileSize;
-
-				bCanImport = MagicOk && VersionOk && SizeOk;
-			}
+			bCanImport = IsHeaderValid(Reader);
 
 			Reader->Close();
 			delete Reader;
@@ -113,6 +122,50 @@ static UStaticMesh* ImportMeshesAndMaterialsFromJSON(FArchive* Archive, const FS
 	return StaticMesh;
 }
 
+// Returns whether the current chunk is of the expected type
+// OutData is filled only if these match
+static bool ReadChunk(FArchive* Archive, bool& OutHasMoreData, TArray<uint8>& OutData, const char* ExpectedChunkType)
+{
+	// Align to next 4-byte boundary before reading anything
+	uint32 Offset = Archive->Tell();
+	uint32 AlignedOffset = Pad4(Offset);
+	if (Offset != AlignedOffset)
+	{
+		Archive->Seek(AlignedOffset);
+	}
+
+	// Each chunk has the form [Size][Type][...Data...]
+	uint32 ChunkType, ChunkDataSize;
+	Archive->SerializeInt(ChunkDataSize, MAX_uint32);
+	Archive->SerializeInt(ChunkType, MAX_uint32);
+
+	constexpr uint32 ChunkHeaderSize = 8;
+	const uint32 AvailableData = Archive->TotalSize() - (AlignedOffset + ChunkHeaderSize);
+
+	// Is there room for another chunk after this one?
+	OutHasMoreData = AvailableData - Pad4(ChunkDataSize) >= ChunkHeaderSize;
+
+	// Is there room for this chunk's data? (should always be true)
+	if (ChunkDataSize > AvailableData)
+	{
+		return false;
+	}
+
+	if (SignatureMatches(ChunkType, ExpectedChunkType))
+	{
+		// Read this chunk's data
+		OutData.SetNumUninitialized(ChunkDataSize, true);
+		Archive->Serialize(OutData.GetData(), ChunkDataSize);
+		return true;
+	}
+	else
+	{
+		// Skip past this chunk's data
+		Archive->Seek(AlignedOffset + ChunkHeaderSize + ChunkDataSize);
+		return false;
+	}
+}
+
 UObject* UGLTFImportFactory::FactoryCreateFile(UClass* InClass, UObject* InParent, FName InName, EObjectFlags Flags, const FString& Filename, const TCHAR* Parms, FFeedbackContext* Warn, bool& bOutOperationCanceled)
 {
 	UStaticMesh* StaticMesh = nullptr;
@@ -136,96 +189,35 @@ UObject* UGLTFImportFactory::FactoryCreateFile(UClass* InClass, UObject* InParen
 	}
 	else if (Extension == TEXT("glb"))
 	{
-		// Binary glTF files begin with a 12-byte header:
-		// - magic bytes "glTF"
-		// - format version
-		// - size of this file
+		// Binary glTF files begin with a 12-byte header
 		// followed by 1 chunk of JSON and (optionally) 1 chunk of binary data.
 
 		FArchive* FileReader = IFileManager::Get().CreateFileReader(*Filename);
 		if (FileReader)
 		{
-			const int64 FileSize = FileReader->TotalSize();
-
 			// Can we count on glTF header being valid, since FactoryCanImport returned true?
 			// If so, skip Magic and Version checks.
 			// Update: We get here even if FactoryCanImport returns false :/
 
-			if (FileSize >= 12)
+			if (IsHeaderValid(FileReader))
 			{
-				uint8 Magic[4];
-				FileReader->Serialize(Magic, 4);
-				bool MagicOk = (Magic[0] == 'g' &&
-				                Magic[1] == 'l' &&
-				                Magic[2] == 'T' &&
-				                Magic[3] == 'F');
+				// Get JSON chunk for later parsing (always first in the file)
+				// JSON reader fails when it hits the first non-ASCII byte (BIN chunk size),
+				// so we must isolate the JSON chunk.
 
-				uint32 Version;
-				FileReader->SerializeInt(Version, MAX_uint32);
-				bool VersionOk = Version == 2;
+				FArrayReader JSON;
+				bool HasMoreData;
 
-				uint32 ReportedSize;
-				FileReader->SerializeInt(ReportedSize, MAX_uint32);
-				bool SizeOk = ReportedSize == FileSize;
-
-				if (MagicOk && VersionOk && SizeOk)
+				if (ReadChunk(FileReader, HasMoreData, JSON, "JSON"))
 				{
-					uint32 ChunkSize;
-					FileReader->SerializeInt(ChunkSize, MAX_uint32);
-
-					uint8 ChunkType[4];
-					FileReader->Serialize(ChunkType, 4);
-					bool ChunkTypeOk = (ChunkType[0] == 'J' &&
-					                    ChunkType[1] == 'S' &&
-					                    ChunkType[2] == 'O' &&
-					                    ChunkType[3] == 'N');
-
-					bool ChunkSizeOk = ChunkSize <= (FileSize - FileReader->Tell());
-
-					// Get JSON chunk 0 for later parsing
-					// JSON reader fails when it hits the first non-ASCII byte (BIN chunk size),
-					// so we must isolate the JSON chunk.
-					FArrayReader JSON;
-					JSON.AddUninitialized(ChunkSize);
-					FileReader->Serialize(JSON.GetData(), ChunkSize);
-
-					// Get non-JSON chunk 1 if present
+					// Get BIN chunk if present
 					TArray<uint8> PackedBinData;
 
-					//                  _ GLB header
-					//                 /    _ JSON chunk type + size
-					//                /    /           _ JSON data
-					//               /    /           /         _ BIN chunk type + size 
-					//              /    /           /         /
-					if (FileSize > 12 + 8 + Pad4(ChunkSize) + 8) // anything after this is BIN data
+					while (HasMoreData)
 					{
-						// Plenty of room for post-JSON data...
-
-						// Skip past inter-chunk padding.
-						if (ChunkSize < Pad4(ChunkSize))
+						if (ReadChunk(FileReader, HasMoreData, PackedBinData, "BIN"))
 						{
-							FileReader->Seek(12 + 8 + Pad4(ChunkSize));
-						}
-
-						// get BIN ChunkSize
-						FileReader->SerializeInt(ChunkSize, MAX_uint32);
-
-						// verify ChunkType == "BIN"
-						FileReader->Serialize(ChunkType, 4);
-						ChunkTypeOk = (ChunkType[0] == 'B' &&
-						               ChunkType[1] == 'I' &&
-						               ChunkType[2] == 'N' &&
-						               ChunkType[3] == '\0');
-
-						ChunkSizeOk = ChunkSize <= (FileSize - FileReader->Tell());
-
-						if (ChunkTypeOk && ChunkSizeOk)
-						{
-							PackedBinData.AddUninitialized(ChunkSize);
-							FileReader->Serialize(PackedBinData.GetData(), ChunkSize);
-							// Could get this data asynchronously while parsing JSON...
-							// Buffer.ByteLength needs to be in place while building Asset;
-							// contents of buffer can wait until just before *using* Asset.
+							break;
 						}
 					}
 
