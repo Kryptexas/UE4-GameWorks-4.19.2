df4509ab1de079dfc9529e3b8225d9fa6f6790da
Chrispykins <fermion@email.com>
2018-01-26 18:13:58 -0500

    #jira UE-54265
    PR #4428: Fixing Envelope Bug in AudioMixer (Contributed by Chrispykins)
    
    #rb Ethan.Geller
    
    [CL 3858945 by Aaron McLeran in 4.19 branch]

--- Engine/Source/Runtime/AudioMixer/Private/DSP/Envelope.cpp
+++ Engine/Source/Runtime/AudioMixer/Private/DSP/Envelope.cpp
@@ -257,8 +257,8 @@ namespace Audio
 		CurrentBiasedOutput -= SustainGain;
 		CurrentBiasedOutput *= BiasDepth;
 
-		const float OutputEnvValue = bInvert ? 1.0f - CurrentEnvelopeValue : CurrentEnvelopeValue;
-		CurrentEnvelopeValue *= Depth;
+		float OutputEnvValue = bInvert ? 1.0f - CurrentEnvelopeValue : CurrentEnvelopeValue;
+		OutputEnvValue *= Depth;
 
 		if (BiasedOutput)
 		{
@@ -267,11 +267,11 @@ namespace Audio
 
 		if (ModMatrix)
 		{
-			ModMatrix->SetSourceValue(VoiceId, EnvSource, CurrentEnvelopeValue);
+			ModMatrix->SetSourceValue(VoiceId, EnvSource, OutputEnvValue);
 			ModMatrix->SetSourceValue(VoiceId, BiasedEnvSource, CurrentBiasedOutput);
 		}
 
-		return CurrentEnvelopeValue;
+		return OutputEnvValue;
 	}
 
 	void FEnvelope::SetAttackTime(const float InAttackTimeMsec)
