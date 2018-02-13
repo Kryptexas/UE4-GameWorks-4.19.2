d751aefb56a551556e3ffd1fe19294c9feba709f
Benn Gallagher <Benn.Gallagher@epicgames.com>
2018-02-13 08:58:13 -0500

    Fixed crash closing clothing tab if workflow centric application puts the tab spawners in a bad state due to incorrect handling of tab context menus. #rb Thomas.Sarkanen #JIRA UE-55067
    [CL 3886621 by Benn Gallagher in 4.19 branch]

--- Engine/Source/Editor/ClothPainter/Private/ClothPaintingModule.cpp
+++ Engine/Source/Editor/ClothPainter/Private/ClothPaintingModule.cpp
@@ -173,13 +173,18 @@ TSharedPtr<SClothPaintTab> FClothPaintingModule::GetActiveClothTab(TWeakPtr<ISke
 		TabManager->InvokeTab(FTabId(FClothPaintTabSummoner::TabName));
 	}
 
-	TSharedPtr<SDockTab> Tab = TabManager->FindExistingLiveTab(FTabId(FClothPaintTabSummoner::TabName));
-
-	if(Tab.IsValid())
+	// If we can't summon this tab we will have spawned a placeholder which we can not cast as done below,
+	// there is no valid active clothing tab so return nullptr
+	if(TabManager->CanSpawnTab(FClothPaintTabSummoner::TabName))
 	{
-		TSharedPtr<SWidget> TabContent = Tab->GetContent();
-		TSharedPtr<SClothPaintTab> ClothingTab = StaticCastSharedPtr<SClothPaintTab>(TabContent);
-		return ClothingTab;
+		TSharedPtr<SDockTab> Tab = TabManager->FindExistingLiveTab(FTabId(FClothPaintTabSummoner::TabName));
+
+		if(Tab.IsValid())
+		{
+			TSharedPtr<SWidget> TabContent = Tab->GetContent();
+			TSharedPtr<SClothPaintTab> ClothingTab = StaticCastSharedPtr<SClothPaintTab>(TabContent);
+			return ClothingTab;
+		}
 	}
 
 	return nullptr;
