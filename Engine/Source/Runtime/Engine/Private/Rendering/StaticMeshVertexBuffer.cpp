3b29f9b9e9c0de0a3ccbf9b601129109a0a6ec73
Krzysztof Narkowicz <Krzysztof.Narkowicz@epicgames.com>
2018-03-26 12:41:26 -0400

    Fixed assert inside BindLightMapVertexBuffer. FSplineMeshSceneProxy was calling BindLightMapVertexBuffer for invalid (still not generated) lightmap UV channel after mesh reimport. Simplified assert, as at the moment almost all of the high callsites already clamp lightmap uv channel. #jira UE-56321 #rb Arne.Schober #lockdown Nick.Penwarden
    [CL 3964893 by Krzysztof Narkowicz in 4.19 branch]

--- Engine/Source/Runtime/Engine/Private/Rendering/StaticMeshVertexBuffer.cpp
+++ Engine/Source/Runtime/Engine/Private/Rendering/StaticMeshVertexBuffer.cpp
@@ -491,7 +491,9 @@ void FStaticMeshVertexBuffer::BindTexCoordVertexBuffer(const FVertexFactory* Ver
 
 void FStaticMeshVertexBuffer::BindLightMapVertexBuffer(const FVertexFactory* VertexFactory, FStaticMeshDataType& Data, int LightMapCoordinateIndex) const
 {
-	check(LightMapCoordinateIndex < (int)GetNumTexCoords() && LightMapCoordinateIndex >= 0);
+	LightMapCoordinateIndex = LightMapCoordinateIndex < (int32)GetNumTexCoords() ? LightMapCoordinateIndex : (int32)GetNumTexCoords() - 1;
+	check(LightMapCoordinateIndex >= 0);
+
 	Data.LightMapCoordinateIndex = LightMapCoordinateIndex;
 	Data.NumTexCoords = GetNumTexCoords();
 
