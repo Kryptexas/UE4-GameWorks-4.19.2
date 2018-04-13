d7cabeaaf3f1a91ce3ed2cb7190529a3f364e761
Michael Dupuis <michel@ifdeftech.com>
2018-04-13 16:05:54 -0400

    #jira UE-57276: #jira UE-56239: #jira UE-54547: Make sure we can't go above MaxLOD even for texture streaming #rb none #codereview jack.porter #lockdown nick.penwarden
    [CL 4003358 by Michael Dupuis in 4.19 branch]

--- Engine/Source/Runtime/Landscape/Private/LandscapeRender.cpp
+++ Engine/Source/Runtime/Landscape/Private/LandscapeRender.cpp
@@ -1515,7 +1515,9 @@ void FLandscapeComponentSceneProxy::CalculateLODFromScreenSize(const FSceneView&
 		PreferedLOD = (float)ForcedLOD;
 	}
 
-	const int32 MinStreamedLOD = HeightmapTexture ? FMath::Min<int32>(((FTexture2DResource*)HeightmapTexture->Resource)->GetCurrentFirstMip(), FMath::CeilLogTwo(SubsectionSizeVerts) - 1) : 0;
+	int8 MinStreamedLOD = HeightmapTexture ? FMath::Min<int8>(((FTexture2DResource*)HeightmapTexture->Resource)->GetCurrentFirstMip(), FMath::CeilLogTwo(SubsectionSizeVerts) - 1) : 0;
+	MinStreamedLOD = FMath::Min(MinStreamedLOD, MaxLOD); // We can't go above MaxLOD even for texture streaming
+
 	int8 LocalLODBias = LODBias + (int8)GLandscapeMeshLODBias;
 	FViewCustomDataSubSectionLOD& SubSectionLODData = InOutLODData.SubSections[InSubSectionIndex];
 
@@ -1528,7 +1530,7 @@ void FLandscapeComponentSceneProxy::CalculateLODFromScreenSize(const FSceneView&
 		PreferedLOD = FMath::Clamp<float>(ComputeBatchElementCurrentLOD(GetLODFromScreenSize(InMeshScreenSizeSquared, InViewLODScale), InMeshScreenSizeSquared) + LocalLODBias, FMath::Max((float)MinStreamedLOD, MinValidLOD), FMath::Min((float)LastLOD, MaxValidLOD));
 	}
 
-	check(PreferedLOD != -1.0f);
+	check(PreferedLOD != -1.0f && PreferedLOD <= MaxLOD);
 	SubSectionLODData.fBatchElementCurrentLOD = PreferedLOD;
 	SubSectionLODData.BatchElementCurrentLOD = FMath::FloorToInt(PreferedLOD);
 }
